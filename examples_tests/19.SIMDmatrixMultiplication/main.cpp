690ddb1a5f3a239f29fe69e64e470bdc80d6069a
Criss <crisserpl2@gmail.com>
2018-03-14 00:04:15 +0100

    Ex19 fixes

--- examples_tests/19.SIMDmatrixMultiplication/main.cpp
+++ examples_tests/19.SIMDmatrixMultiplication/main.cpp
@@ -10,10 +10,11 @@
 // _mm256_extractf128_ps for extracting hi part
 // _mm256_castps256_ps128 for extracting lo part (no cycles!)
 
-#define EXEC_CNT (1e7)
+#define EXEC_CNT (1e6)
 #define BROADCAST32(fpx) _MM_SHUFFLE(fpx, fpx, fpx, fpx)
 
 #define AVX 1 // set to 0 or 1 (sse3/avx), set appropriate compiler flags and run
+#define VERIFY 0
 
 #if AVX
 #define ALIGN 32
@@ -128,10 +129,10 @@ namespace sse3
 		{
 			matrix4x3_row out;
 
-			out.row[0] = doJob(row[0], _other);
-			out.row[1] = doJob(row[1], _other);
-			out.row[2] = doJob(row[2], _other);
-			out.row[3] = row[3]; // 0 0 0 1
+			_mm_store_ps(out.m[0], doJob(row[0], _other));
+			_mm_store_ps(out.m[1], doJob(row[1], _other));
+			_mm_store_ps(out.m[2], doJob(row[2], _other));
+			_mm_store_ps(out.m[3], row[3]); // 0 0 0 1
 
 			return out;
 		}
@@ -143,10 +144,18 @@ namespace sse3
 		static inline __m128 doJob(const __m128& a, const matrix4x3_row& _mtx)
 		{
 			__m128 res;
-			res = _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(0)), _mtx.row[0]);
-			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(1)), _mtx.row[1]));
-			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(2)), _mtx.row[2]));
-			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(3)), _mtx.row[3]));
+
+			__m128 r0 = _mm_load_ps(_mtx.m[0]); 
+			__m128 r1 = _mm_load_ps(_mtx.m[1]);
+			__m128 r2 = _mm_load_ps(_mtx.m[2]);
+			__m128 r3 = _mm_load_ps(_mtx.m[3]);
+
+			__m128 mask = _mm_castsi128_ps(_mm_setr_epi32(0, 0, 0, 0xffffffff));
+
+			res = _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(0)), r0);
+			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(1)), r1));
+			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(2)), r2));
+			res = _mm_add_ps(res, _mm_and_ps(_mm_shuffle_ps(a, a, BROADCAST32(3)), mask)); // always 0 0 0 a3
 			return res;
 		}
 	}
@@ -170,10 +179,10 @@ namespace sse3
 		{
 			matrix4x3_col out;
 
-			out.col[0] = doJob(_other.col[0], 0, *this);
-			out.col[1] = doJob(_other.col[1], 1, *this);
-			out.col[2] = doJob(_other.col[2], 2, *this);
-			out.col[3] = doJob(_other.col[3], 3, *this);
+			_mm_store_ps(out.m[0], doJob(_other.col[0], 0, *this));
+			_mm_store_ps(out.m[1], doJob(_other.col[1], 1, *this));
+			_mm_store_ps(out.m[2], doJob(_other.col[2], 2, *this));
+			_mm_store_ps(out.m[3], doJob(_other.col[3], 3, *this));
 
 			return out;
 		}
@@ -185,11 +194,17 @@ namespace sse3
 		static inline __m128 doJob(__m128 a, size_t j, const matrix4x3_col& _mtx)
 		{
 			__m128 res;
-			res = _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(0)), _mtx.col[0]);
-			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(1)), _mtx.col[1]));
-			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(2)), _mtx.col[2]));
+
+			__m128 c0 = _mm_load_ps(_mtx.m[0]);
+			__m128 c1 = _mm_load_ps(_mtx.m[1]);
+			__m128 c2 = _mm_load_ps(_mtx.m[2]);
+			__m128 c3 = _mm_load_ps(_mtx.m[3]);
+
+			res = _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(0)), c0);
+			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(1)), c1));
+			res = _mm_add_ps(res, _mm_mul_ps(_mm_shuffle_ps(a, a, BROADCAST32(2)), c2));
 			if (j == 3) // w of every column except last one is 0; in the last one it's 1
-				res = _mm_add_ps(res, _mtx.col[3]);
+				res = _mm_add_ps(res, c3);
 			return res;
 		}
 	}
@@ -200,6 +215,32 @@ namespace sse3
 } // sse3
 #endif
 
+struct matrix4x3_col_nosimd
+{
+	float m[4][4];
+
+	inline matrix4x3_col_nosimd concatenate(const matrix4x3_col_nosimd& _other)
+	{
+		matrix4x3_col_nosimd ret;
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				float sum = 0;
+				for (int e = 0; e < 3; e++)
+					sum += (*this)(i, e) * _other(e, j);
+				if (j == 3)
+					sum += (*this)(i, 3);
+				ret(i, j) = sum;
+			}
+		}
+		return ret;
+	}
+
+	float& operator()(size_t _i, size_t _j) { return m[_j][_i]; }
+	const float& operator()(size_t _i, size_t _j) const { return m[_j][_i]; }
+};
+
 namespace measure
 {
 	using Clock = std::chrono::high_resolution_clock;
@@ -208,7 +249,9 @@ namespace measure
 }
 
 template<typename T>
-static double run(void*);
+static bool compare(T* _m1, T* _m2);
+template<typename T>
+static double run(void*, void*, void*);
 
 int main()
 {
@@ -218,18 +261,31 @@ int main()
 	size_t offset = reinterpret_cast<const size_t&>(alignedData)%ALIGN;
 	alignedData += ALIGN-offset;
 
+	for (uint32_t* p = (uint32_t*)alignedData; (void*)p != (void*)(alignedData + (size_t)EXEC_CNT*16*4); ++p)
+		*p = rand();
+
+	void* dataOut = malloc(16*4*(size_t)EXEC_CNT);
+	void* nosimdOut = malloc(16*4*(size_t)EXEC_CNT);
+
+	double nosimdtime = 0.0;
 	double rowtime = 0.0;
 	double coltime = 0.0;
+
+	for (size_t i = 0; i < 10; ++i)
+		nosimdtime += run<matrix4x3_col_nosimd>(alignedData, nosimdOut, 0);
+
 	for (size_t i = 0; i < 10; ++i)
 	{
 #if AVX
-		rowtime += run<avx::matrix4x3_row>(alignedData);
-		coltime += run<avx::matrix4x3_col>(alignedData);
+		rowtime += run<avx::matrix4x3_row>(alignedData, dataOut, nosimdOut);
+		coltime += run<avx::matrix4x3_col>(alignedData, dataOut, nosimdOut);
 #else
-		rowtime += run<sse3::matrix4x3_row>(alignedData);
-		coltime += run<sse3::matrix4x3_col>(alignedData);
+		rowtime += run<sse3::matrix4x3_row>(alignedData, dataOut, nosimdOut);
+		coltime += run<sse3::matrix4x3_col>(alignedData, dataOut, nosimdOut);
 #endif
 	}
+
+	printf("nosimd  : %f\n", nosimdtime);
 #if AVX
 	printf("avx  row: %f\n", rowtime);
     printf("avx  col: %f\n", coltime);
@@ -243,17 +299,29 @@ int main()
 }
 
 template<typename T>
-static double run(void* _data)
+static bool compare(T* _m1, T* _m2) // naive cmp function for now
 {
-	T* const mtx = (T*)_data;
-	for (uint32_t* p = (uint32_t*)mtx; (void*)p != (void*)(mtx+(size_t)EXEC_CNT); ++p)
-		*p = rand();
+	for (float* p1 = (float*)_m1, *p2 = (float*)_m2; (uint8_t*)p1 != (uint8_t*)(p1)+16*4; ++p1, ++p2)
+		if (p1 != p2)
+			return false;
+	return true;
+}
 
+template<typename T>
+static double run(void* _data, void* _out, void* _cmp)
+{
 	const measure::TimePoint start = measure::Clock::now();
 
-	T m;
+	T* const mtx = (T*)_data;
+	T* out = (T*)_out;
 	for (size_t i = 0; i < (size_t)EXEC_CNT-1; ++i)
-		m = mtx[i].concatenate(mtx[i+1]);
+	{
+		out[i] = mtx[i].concatenate(mtx[i+1]);
+#if VERIFY
+		if (!compare<T>(out+i, ((T*)_cmp)+i))
+			printf("???\n");
+#endif
+	}
 
 	const measure::Duration dt = measure::Clock::now() - start;
 	return dt.count();
