b6058631ede203a9cce32b37be0ec95432a5c730
Matt Kielan <devsh.graphicsprogramming@gmail.com>
2018-06-10 23:23:03 +0100

    Binary Insertion Sorted Vector will work better for VAO cache, usually less than 32 formats used in all BaW scenes.

--- source/Irrlicht/COpenGLDriver.h
+++ source/Irrlicht/COpenGLDriver.h
@@ -311,10 +311,13 @@ namespace video
         struct SAuxContext
         {
         //public:
+            constexpr static size_t maxVAOCacheSize = 0x1u<<14; //make this cache configurable
+
             SAuxContext() : threadId(std::thread::id()), ctx(NULL), XFormFeedbackRunning(false), CurrentXFormFeedback(NULL),
                             CurrentFBO(0), CurrentRendertargetSize(0,0)
             {
-                CurrentVAO = std::pair<COpenGLVAOSpec::HashAttribs,COpenGLVAO*>(COpenGLVAOSpec::HashAttribs(),NULL);
+                VAOMap.reserve(maxVAOCacheSize);
+                CurrentVAO = HashVAOPair(COpenGLVAOSpec::HashAttribs(),NULL);
 
                 for (size_t i=0; i<MATERIAL_MAX_TEXTURES; i++)
                 {
@@ -504,8 +507,9 @@ namespace video
             };
 
             //!
-            std::pair<COpenGLVAOSpec::HashAttribs,COpenGLVAO*>          CurrentVAO;
-            std::unordered_map<COpenGLVAOSpec::HashAttribs,COpenGLVAO*> VAOMap;
+            typedef std::pair<COpenGLVAOSpec::HashAttribs,COpenGLVAO*> HashVAOPair;
+            HashVAOPair                 CurrentVAO;
+            std::vector<HashVAOPair>    VAOMap;
 
             inline size_t getVAOCacheSize() const
             {
@@ -514,10 +518,9 @@ namespace video
 
             inline void freeUpVAOCache(bool exitOnFirstDelete)
             {
-                constexpr size_t maxCacheSize = 0x1u<<14; //make this cache configurable
-                for(std::unordered_map<COpenGLVAOSpec::HashAttribs,COpenGLVAO*>::iterator it = VAOMap.begin(); VAOMap.size()>maxCacheSize&&it!=VAOMap.end(); it++)
+                for(auto it = VAOMap.begin(); VAOMap.size()>maxVAOCacheSize&&it!=VAOMap.end(); it++)
                 {
-                    if (it->first==CurrentVAO.first&&it->second==CurrentVAO.second)
+                    if (it->first==CurrentVAO.first)
                         continue;
 
                     if (CNullDriver::ReallocationCounter-it->second->getLastBoundStamp()>1000) //maybe make this configurable
