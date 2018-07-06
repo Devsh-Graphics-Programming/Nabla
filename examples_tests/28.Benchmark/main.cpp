45245768c8a0c524ee2f498142316c383a7ad493
Criss <crisserpl2@gmail.com>
2018-07-06 15:22:02 +0200

    Example 28

--- examples_tests/28.Benchmark/main.cpp
+++ examples_tests/28.Benchmark/main.cpp
@@ -11,8 +11,7 @@
 #define TEST_CASE 3 // [1..4]
 #define INCPUMEM false
 #define RASTERIZER_DISCARD false
-#define LARGE_ATTR_DIVISOR 1 // 0 or 1
-#define DONT_UPDATE_BUFFER 1 // 0 or 1
+#define DONT_UPDATE_BUFFER 0 // 0 or 1
 
 using namespace irr;
 using namespace core;
@@ -54,18 +53,8 @@ int main()
 
     scene::IGPUMeshBuffer* meshes[100];
     scene::IGPUMeshDataFormatDesc* desc = driver->createGPUMeshDataFormatDesc();
-    auto attrBuf = driver->createGPUBuffer(4
-#if !LARGE_ATTR_DIVISOR
-        * 30000
-#endif
-        , nullptr);
-    desc->mapVertexAttrBuffer(attrBuf, scene::EVAI_ATTR0, scene::ECPA_ONE, scene::ECT_FLOAT, 0, 0,
-#if LARGE_ATTR_DIVISOR
-        1000
-#else
-        0
-#endif
-        ); // map whatever buffer just to activate whatever vertex attribute (look below)
+    auto attrBuf = driver->createGPUBuffer(4 * 30000, nullptr);
+    desc->mapVertexAttrBuffer(attrBuf, scene::EVAI_ATTR0, scene::ECPA_ONE, scene::ECT_FLOAT); // map whatever buffer just to activate whatever vertex attribute (look below)
     {
         size_t triBudget = 1600000u; //1.6M
         for (size_t i = 0u; i < 100u; ++i)
@@ -126,22 +115,34 @@ int main()
         }
     }
     const size_t bufSize = offsets[99] + meshes[99]->getInstanceCount() * uboStructSizes[15];
+#if (TEST_CASE==3 || TEST_CASE==4)
+#if DONT_UPDATE_BUFFER
+    const size_t persistentlyMappedBufSize = bufSize;
+#else
+    const size_t persistentlyMappedBufSize = 4*bufSize;
+#endif
+#endif
 
     core::ICPUBuffer* cpubuffer = new core::ICPUBuffer(bufSize);
     for (size_t i = 0u; i < bufSize / 2; ++i)
         ((uint16_t*)(cpubuffer->getPointer()))[i] = rand();
 
-    video::COpenGLBuffer* buffer;
-#if TEST_CASE==2
+    video::COpenGLBuffer* buffer = nullptr;
+#if ((TEST_CASE==1 || TEST_CASE==2) && DONT_UPDATE_BUFFER)
+    buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createGPUBuffer(bufSize, cpubuffer->getPointer(), false, INCPUMEM));
+#elif TEST_CASE==2
     buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createGPUBuffer(bufSize, cpubuffer->getPointer(), true, INCPUMEM));
 #elif TEST_CASE==3
-    buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createPersistentlyMappedBuffer(bufSize * 4, nullptr, video::EGBA_WRITE, false, INCPUMEM));
+    buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createPersistentlyMappedBuffer(persistentlyMappedBufSize, nullptr, video::EGBA_WRITE, false, INCPUMEM));
 #elif TEST_CASE==4
-    buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createPersistentlyMappedBuffer(bufSize * 4, nullptr, video::EGBA_WRITE, true, INCPUMEM));
+    buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createPersistentlyMappedBuffer(persistentlyMappedBufSize, nullptr, video::EGBA_WRITE, true, INCPUMEM));
 #endif
+
 #if ((TEST_CASE==3 || TEST_CASE==4) && DONT_UPDATE_BUFFER)
-    for (size_t i = 0u; i < 4u; ++i)
-        memcpy(((uint8_t*)(dynamic_cast<video::COpenGLPersistentlyMappedBuffer*>(buffer)->getPointer())) + bufSize*i, cpubuffer->getPointer(), bufSize);
+    memcpy(dynamic_cast<video::COpenGLPersistentlyMappedBuffer*>(buffer)->getPointer(), cpubuffer->getPointer(), persistentlyMappedBufSize);
+#if TEST_CASE==3
+    video::COpenGLExtensionHandler::extGlFlushMappedNamedBufferRange(buffer->getOpenGLName(), 0, bufSize);
+#endif
 #endif
 
     //GLint meshVao[100];
@@ -169,8 +170,7 @@ int main()
         queries[frameNum] = driver->createElapsedTimeQuery();
         driver->beginQuery(queries[frameNum]);
 
-        memset(cpubuffer->getPointer(), 0, bufSize);
-#if TEST_CASE==1
+#if (TEST_CASE==1 && !DONT_UPDATE_BUFFER)
         buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createGPUBuffer(bufSize, cpubuffer->getPointer(), false, INCPUMEM));
 #elif TEST_CASE==2
 #if !DONT_UPDATE_BUFFER
@@ -178,13 +178,28 @@ int main()
 #endif //!DONT_UPDATE_BUFFER
 #elif (TEST_CASE==3 || TEST_CASE==4)
 #if !DONT_UPDATE_BUFFER
-        memcpy(dynamic_cast<video::COpenGLPersistentlyMappedBuffer*>(buffer)->getPointer(), cpubuffer->getPointer(), bufSize);
+        if (fences[frameNum % 4])
+        {
+            auto res = fences[frameNum % 4]->waitCPU(10000000000ull);
+            if (res == video::EDFR_CONDITION_SATISFIED || res == video::EDFR_ALREADY_SIGNALED)
+            {
+                fences[frameNum % 4]->drop();
+                fences[frameNum % 4] = nullptr;
+            }
+        }
+        memcpy(((uint8_t*)(dynamic_cast<video::COpenGLPersistentlyMappedBuffer*>(buffer)->getPointer()))+(frameNum%4)*bufSize, cpubuffer->getPointer(), bufSize);
+#if TEST_CASE==3
         video::COpenGLExtensionHandler::extGlFlushMappedNamedBufferRange(buffer->getOpenGLName(), (frameNum%4)*bufSize, bufSize);
+#endif //TEST_CASE==3
 #endif //!DONT_UPDATE_BUFFER
         {
         auto glbuf = static_cast<const video::COpenGLBuffer*>(buffer);
         const ptrdiff_t sz = bufSize;
+#if DONT_UPDATE_BUFFER
+        const ptrdiff_t off = 0;
+#else
         const ptrdiff_t off = (frameNum%4)*bufSize;
+#endif //DONT_UPDATE_BUFFER
         auxCtx->setActiveUBO(0u, 1u, &glbuf, &off, &sz);
         }
 #endif
@@ -199,7 +214,13 @@ int main()
             for (; i < tmp + batchSizes[j]; ++i)
             {
                 const ptrdiff_t sz = (i == 99u ? (ptrdiff_t)bufSize : offsets[i+1]) - offsets[i];
-                auxCtx->setActiveUBO(0u, 1u, &glbuf, offsets+i, &sz);
+                const ptrdiff_t off = offsets[i]
+#if (TEST_CASE==3 || TEST_CASE==4)
+                    + (frameNum % 4) * bufSize;
+#else
+                    ;
+#endif
+                auxCtx->setActiveUBO(0u, 1u, &glbuf, &off, &sz);
                 driver->drawMeshBuffer(meshes[i]);
                 //if (!frameNum)
                 //    glGetIntegerv(GL_VERTEX_ARRAY_BINDING, meshVao+i);
@@ -210,12 +231,11 @@ int main()
 #if ((TEST_CASE==3 || TEST_CASE==4) && !DONT_UPDATE_BUFFER)
         if (!fences[frameNum%4])
             fences[frameNum%4] = driver->placeFence();
-        fences[frameNum%4]->waitGPU(); // ???
 #endif
 
         driver->endQuery(queries[frameNum]);
 
-#if TEST_CASE==1
+#if (TEST_CASE==1 && !DONT_UPDATE_BUFFER)
         buffer->drop();
         buffer = nullptr;
 #endif
