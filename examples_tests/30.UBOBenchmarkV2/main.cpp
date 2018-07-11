e4730cf1555d09d53cb50e5b342455a1f9579a20
Matt Kielan <devsh.graphicsprogramming@gmail.com>
2018-07-11 14:24:27 +0100

    Renamed example 28 to something more reasonable.
    Introduced example 30 which closes #121

---
+++ examples_tests/30.UBOBenchmarkV2/main.cpp
@@ -0,0 +1,123 @@
+#include <chrono>
+#include <iostream>
+#include <numeric>
+#define _IRR_STATIC_LIB_
+#include <irrlicht.h>
+#include "../source/Irrlicht/COpenGLExtensionHandler.h"
+
+#include "../source/Irrlicht/COpenGLBuffer.h"
+#include "../source/Irrlicht/COpenGLDriver.h"
+#include "../source/Irrlicht/COpenGLPersistentlyMappedBuffer.h"
+
+using namespace irr;
+using namespace core;
+
+
+#define WITH_COMPUTE_SHADER
+
+
+// benchmark controls
+#define INCPUMEM true
+#define FLUSH_EXPLICIT false
+
+
+//#define OPENGL_DEBUG
+
+#ifdef OPENGL_DEBUG
+void APIENTRY openGLCBFunc(GLenum source, GLenum type, GLuint id, GLenum severity,
+                           GLsizei length, const GLchar* message, const void* userParam)
+{
+    core::stringc outStr;
+    switch (severity)
+    {
+        //case GL_DEBUG_SEVERITY_HIGH:
+        case GL_DEBUG_SEVERITY_HIGH_ARB:
+            outStr = "[H.I.G.H]";
+            break;
+        //case GL_DEBUG_SEVERITY_MEDIUM:
+        case GL_DEBUG_SEVERITY_MEDIUM_ARB:
+            outStr = "[MEDIUM]";
+            break;
+        //case GL_DEBUG_SEVERITY_LOW:
+        case GL_DEBUG_SEVERITY_LOW_ARB:
+            outStr = "[  LOW  ]";
+            break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            outStr = "[  LOW  ]";
+            break;
+        default:
+            outStr = "[UNKNOWN]";
+            break;
+    }
+    switch (source)
+    {
+        //case GL_DEBUG_SOURCE_API:
+        case GL_DEBUG_SOURCE_API_ARB:
+            switch (type)
+            {
+                //case GL_DEBUG_TYPE_ERROR:
+                case GL_DEBUG_TYPE_ERROR_ARB:
+                    outStr += "[OPENGL  API ERROR]\t\t";
+                    break;
+                //case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+                case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB:
+                    outStr += "[OPENGL  DEPRECATED]\t\t";
+                    break;
+                //case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+                case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB:
+                    outStr += "[OPENGL   UNDEFINED]\t\t";
+                    break;
+                //case GL_DEBUG_TYPE_PORTABILITY:
+                case GL_DEBUG_TYPE_PORTABILITY_ARB:
+                    outStr += "[OPENGL PORTABILITY]\t\t";
+                    break;
+                //case GL_DEBUG_TYPE_PERFORMANCE:
+                case GL_DEBUG_TYPE_PERFORMANCE_ARB:
+                    outStr += "[OPENGL PERFORMANCE]\t\t";
+                    break;
+                default:
+                    outStr += "[OPENGL       OTHER]\t\t";
+                    //! special sauce
+                    //return;
+                    break;
+            }
+            outStr += message;
+            break;
+        //case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        case GL_DEBUG_SOURCE_SHADER_COMPILER_ARB:
+            outStr += "[SHADER]\t\t";
+            outStr += message;
+            break;
+        //case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB:
+            outStr += "[WINDOW SYS]\t\t";
+            outStr += message;
+            break;
+        //case GL_DEBUG_SOURCE_THIRD_PARTY:
+        case GL_DEBUG_SOURCE_THIRD_PARTY_ARB:
+            outStr += "[3RDPARTY]\t\t";
+            outStr += message;
+            break;
+        //case GL_DEBUG_SOURCE_APPLICATION:
+        case GL_DEBUG_SOURCE_APPLICATION_ARB:
+            outStr += "[APP]\t\t";
+            outStr += message;
+            break;
+        //case GL_DEBUG_SOURCE_OTHER:
+        case GL_DEBUG_SOURCE_OTHER_ARB:
+            outStr += "[OTHER]\t\t";
+            outStr += message;
+            break;
+        default:
+            break;
+    }
+    outStr += "\n";
+    printf("%s",outStr.c_str());
+}
+#endif // OPENGL_DEBUG
+
+#if defined(WITH_COMPUTE_SHADER)
+#include "main_cs.h"
+#else
+#include "main_vs.h"
+#endif
---
+++ examples_tests/30.UBOBenchmarkV2/main_cs.h
@@ -0,0 +1,208 @@
+#include "createComputeShader.h"
+
+
+int main()
+{
+    srand(time(nullptr));
+
+    irr::SIrrlichtCreationParameters params;
+    params.Bits = 24; //may have to set to 32bit for some platforms
+    params.ZBufferBits = 24; //we'd like 32bit here
+    params.DriverType = video::EDT_OPENGL; //! Only Well functioning driver, software renderer left for sake of 2D image drawing
+    params.WindowSize = dimension2d<uint32_t>(64, 64);
+    params.Fullscreen = false;
+    params.Vsync = true; //! If supported by target platform
+    params.Doublebuffer = true;
+    params.Stencilbuffer = false; //! This will not even be a choice soon
+    IrrlichtDevice* device = createDeviceEx(params);
+
+#ifdef OPENGL_DEBUG
+    if (video::COpenGLExtensionHandler::FeatureAvailable[video::COpenGLExtensionHandler::IRR_KHR_debug])
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        //glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        video::COpenGLExtensionHandler::pGlDebugMessageControl(GL_DONT_CARE,GL_DONT_CARE,GL_DONT_CARE,0,NULL,true);
+
+        video::COpenGLExtensionHandler::pGlDebugMessageCallback(openGLCBFunc,NULL);
+    }
+    else
+    {
+        //glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS_ARB);
+        video::COpenGLExtensionHandler::pGlDebugMessageControlARB(GL_DONT_CARE,GL_DONT_CARE,GL_DONT_CARE,0,NULL,true);
+
+        video::COpenGLExtensionHandler::pGlDebugMessageCallbackARB(openGLCBFunc,NULL);
+    }
+#endif // OPENGL_DEBUG
+
+    if (!device)
+        return 1;
+
+    video::IVideoDriver* driver = device->getVideoDriver();
+
+    GLuint shaders[16];
+    for (size_t i = 0u; i < 16u; ++i)
+        shaders[i] = createComputeShaderFromFile(("../cs" + std::to_string(i)).c_str());
+
+#define CS_WG_SIZE 1024
+    size_t wgBudget = 3000000 / CS_WG_SIZE;
+    size_t wgCnts[100];
+    for (size_t i = 0u; i < 100u; ++i)
+    {
+        const size_t r = rand() % 15 + 15;
+        if (r > wgBudget)
+            wgCnts[i] = wgBudget;
+        else
+            wgCnts[i] = r;
+        wgBudget -= wgCnts[i];
+    }
+    if (wgBudget > 99u)
+    {
+        for (size_t i = 0u; i < 99u; ++i)
+            wgCnts[i] += wgBudget / 100u;
+    }
+    for (size_t i = 0u; i < 100u; ++i)
+        wgCnts[i] = std::ceil(std::sqrt((float)wgCnts[i]));
+
+    size_t batchSizes[16];
+    memset(batchSizes, 0, sizeof(batchSizes));
+    {
+        const size_t invocCntMax = 180000u;
+        size_t b = 0u;
+
+        size_t invocCnt = 0u;
+        for (size_t i = 0u; i < 100u; ++i)
+        {
+            if (invocCnt + wgCnts[i]*CS_WG_SIZE <= invocCntMax || b == 15u)
+            {
+                ++batchSizes[b];
+                invocCnt += wgCnts[i] * CS_WG_SIZE;
+            }
+            else
+            {
+                invocCnt = 0u;
+                ++b;
+            }
+        }
+        const size_t sum = std::accumulate(batchSizes, batchSizes + 16, 0ull);
+        for (size_t i = 0; i < 100u - sum; ++i)
+            batchSizes[15 - (i % 16)]++;
+    }
+
+    const size_t uboStructSizes[16]{ 144u, 144u, 144u, 144u, 128u, 128u, 144u, 80u, 80u, 128u, 112u, 96u, 144u, 96u, 144u, 96u };
+    ptrdiff_t offsets[100]{ 0 };
+    {
+        size_t batchesPsum[16]{ batchSizes[0] };
+        for (size_t i = 1u; i < 16u; ++i)
+            batchesPsum[i] = batchesPsum[i - 1] + batchSizes[i];
+
+        size_t b = 0u;
+        for (size_t i = 1u; i < 100u; ++i)
+        {
+            if (i >= batchesPsum[b])
+                ++b;
+            offsets[i] = offsets[i - 1] + wgCnts[i - 1] * uboStructSizes[b];
+        }
+    }
+
+    const size_t bufSize = offsets[99] + wgCnts[99] * uboStructSizes[15];
+
+    core::ICPUBuffer* cpubuffer = new core::ICPUBuffer(bufSize);
+    for (size_t i = 0u; i < bufSize / 2; ++i)
+        ((uint16_t*)(cpubuffer->getPointer()))[i] = rand();
+
+    //! Set up immutable device local buffer to use as UBO
+    video::COpenGLBuffer* buffer = dynamic_cast<video::COpenGLBuffer*>(driver->createGPUBuffer(bufSize, cpubuffer->getPointer(), false, INCPUMEM));
+
+
+    //! Set up staging buffer
+    const size_t persistentlyMappedBufSize = 4*bufSize;
+    video::COpenGLBuffer* stagingBuffer = dynamic_cast<video::COpenGLBuffer*>(driver->createPersistentlyMappedBuffer(persistentlyMappedBufSize, nullptr, video::EGBA_WRITE, !FLUSH_EXPLICIT, INCPUMEM));
+
+
+    auto auxCtx = const_cast<video::COpenGLDriver::SAuxContext*>(static_cast<video::COpenGLDriver*>(driver)->getThreadContext());
+    size_t frameNum = 0u;
+
+
+#define ITER_CNT 50000
+    video::IQueryObject* queries[ITER_CNT];
+
+    video::IDriverFence* fences[4]{ nullptr, nullptr, nullptr, nullptr };
+
+
+    auto cpustart = std::chrono::steady_clock::now();
+    while (device->run() && frameNum < ITER_CNT)
+    {
+        queries[frameNum] = driver->createElapsedTimeQuery();
+        driver->beginQuery(queries[frameNum]);
+
+        if (fences[frameNum % 4])
+        {
+            auto waitf = [&frameNum, &fences] {
+                auto res = fences[frameNum % 4]->waitCPU(10000000000ull);
+                return (res == video::EDFR_CONDITION_SATISFIED || res == video::EDFR_ALREADY_SIGNALED);
+            };
+            while (!waitf())
+            {
+                fences[frameNum % 4]->drop();
+                fences[frameNum % 4] = nullptr;
+            }
+        }
+        memcpy(((uint8_t*)(dynamic_cast<video::COpenGLPersistentlyMappedBuffer*>(stagingBuffer)->getPointer()))+(frameNum%4)*bufSize, cpubuffer->getPointer(), bufSize);
+#if FLUSH_EXPLICIT==true
+        video::COpenGLExtensionHandler::extGlFlushMappedNamedBufferRange(stagingBuffer->getOpenGLName(), (frameNum%4)*bufSize, bufSize);
+#endif
+
+        //! copy from staging to local
+        driver->bufferCopy(stagingBuffer,buffer,(frameNum % 4) * bufSize,0,bufSize);
+
+        if (!fences[frameNum%4])
+            fences[frameNum%4] = driver->placeFence();
+
+
+        video::COpenGLExtensionHandler::extGlMemoryBarrier(GL_UNIFORM_BARRIER_BIT);
+
+        size_t i = 0u;
+        for (size_t j = 0u; j < 16u; ++j)
+        {
+            auto glbuf = static_cast<const video::COpenGLBuffer*>(buffer);
+            video::COpenGLExtensionHandler::extGlUseProgram(shaders[j]);
+            size_t tmp = i;
+            for (; i < tmp + batchSizes[j]; ++i)
+            {
+                const ptrdiff_t sz = (i == 99u ? (ptrdiff_t)bufSize : offsets[i + 1]) - offsets[i];
+                const ptrdiff_t off = offsets[i];
+
+                auxCtx->setActiveUBO(0u, 1u, &glbuf, &off, &sz);
+                video::COpenGLExtensionHandler::extGlDispatchCompute(wgCnts[i], wgCnts[i], 1);
+            }
+        }
+
+        driver->endQuery(queries[frameNum]);
+
+
+        glFlush();
+        ++frameNum;
+    }
+    auto diff = std::chrono::steady_clock::now() - cpustart;
+    std::cout << "Elapsed CPU time " << std::chrono::duration <double, std::milli> (diff).count() << " ms\n";
+
+    size_t elapsed = 0u;
+    for (size_t i = 0u; i < ITER_CNT; ++i)
+    {
+        uint32_t res{};
+        queries[i]->getQueryResult(&res);
+        elapsed += res;
+    }
+    os::Printer::log("Elapsed time", std::to_string(elapsed).c_str());
+
+    for (size_t i = 0u; i < 16u; ++i)
+        video::COpenGLExtensionHandler::extGlDeleteProgram(shaders[i]);
+#if TEST_CASE!=1
+    buffer->drop();
+#endif
+    cpubuffer->drop();
+
+    device->drop();
+
+    return 0;
+}
