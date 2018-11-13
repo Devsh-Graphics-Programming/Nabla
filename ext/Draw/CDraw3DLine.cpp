c9687d668d113f0ddadf8383f0db8b1ce8007153
ManhNT <manhnt.9@outlook.com>
2018-11-13 10:13:54 +0700

    Set up in draw function

--- ext/Draw/CDraw3DLine.cpp
+++ ext/Draw/CDraw3DLine.cpp
@@ -27,12 +27,6 @@ CDraw3DLine::CDraw3DLine(IVideoDriver* _driver)
         callBack,
         0);
     callBack->drop();
-
-    m_meshBuffer->setPrimitiveType(EPT_LINES);
-    m_meshBuffer->setMeshDataAndFormat(m_desc);
-    m_meshBuffer->setIndexType(EIT_16BIT);
-    m_meshBuffer->setIndexCount(2);
-    m_lineData[1] = (void*) m_indices;
 }
 
 void CDraw3DLine::draw(
@@ -46,7 +40,8 @@ void CDraw3DLine::draw(
     };
 
     auto upStreamBuff = m_driver->getDefaultUpStreamingBuffer();
-    m_lineData[0] = (void*) vertices;
+    m_lineData[0] = vertices;
+    m_lineData[1] = (void*) m_indices;
 
     upStreamBuff->multi_place(2u, (const void* const*)m_lineData, (uint32_t*)&m_offsets,(uint32_t*)&sizes,(uint32_t*)&alignments);
     if (upStreamBuff->needsManualFlushOrInvalidate())
@@ -55,12 +50,16 @@ void CDraw3DLine::draw(
         m_driver->flushMappedMemoryRanges({{ upStreamMem,m_offsets[0],sizes[0] }, { upStreamMem,m_offsets[1],sizes[1] }});
     }
 
-    m_meshBuffer->setIndexBufferOffset(m_offsets[1]);
-
     auto buff = upStreamBuff->getBuffer();
     m_desc->mapVertexAttrBuffer(buff,EVAI_ATTR0,ECPA_THREE,ECT_FLOAT,sizeof(S3DLineVertex), offsetof(S3DLineVertex, Position[0]) + m_offsets[0]);
-    m_desc->mapVertexAttrBuffer(buff,EVAI_ATTR1,ECPA_FOUR,ECT_FLOAT,sizeof(S3DLineVertex), offsetof(S3DLineVertex, Color[0]) + m_offsets[0]);
+    m_desc->mapVertexAttrBuffer(buff,EVAI_ATTR1,ECPA_FOUR,ECT_UNSIGNED_INT,sizeof(S3DLineVertex), offsetof(S3DLineVertex, Color[0]) + m_offsets[0]);
     m_desc->mapIndexBuffer(buff);
+
+    m_meshBuffer->setPrimitiveType(EPT_LINES);
+    m_meshBuffer->setMeshDataAndFormat(m_desc);
+    m_meshBuffer->setIndexBufferOffset(m_offsets[1]);
+    // m_meshBuffer->setIndexType(EIT_16BIT);
+    m_meshBuffer->setIndexCount(2);
 
     m_driver->setTransform(E4X3TS_WORLD, core::matrix4x3());
     m_driver->setMaterial(m_material);
