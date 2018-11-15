b5aaaa4820422c2905c65579ca56647fe4649041
Matt Kielan <devsh.graphicsprogramming@gmail.com>
2018-11-15 15:06:51 +0000

    Update example 34 source and binary

--- examples_tests/34.AddressAllocatorTraitsTest/main.cpp
+++ examples_tests/34.AddressAllocatorTraitsTest/main.cpp
@@ -2,6 +2,7 @@
 
 #include <irrlicht.h>
 
+#include "irr/core/alloc/address_allocator_traits.h"
 #include "irr/core/alloc/LinearAddressAllocator.h"
 #include "irr/core/alloc/StackAddressAllocator.h"
 
@@ -10,6 +11,7 @@ using namespace irr;
 
 int main()
 {
+    printf("SINGLE THREADED======================================================\n");
 	printf("Linear \n");
 	irr::core::address_allocator_traits<core::LinearAddressAllocatorST<uint32_t> >::printDebugInfo();
 	printf("Stack \n");
@@ -17,9 +19,19 @@ int main()
 	printf("Pool \n");
 	irr::core::address_allocator_traits<core::PoolAddressAllocatorST<uint32_t> >::printDebugInfo();
 	printf("Cont \n");
-	irr::core::address_allocator_traits<core::ContiguousPoolAddressAllocator<uint32_t> >::printDebugInfo();
+	irr::core::address_allocator_traits<core::ContiguousPoolAddressAllocatorST<uint32_t> >::printDebugInfo();
 	printf("General \n");
-	irr::core::address_allocator_traits<core::GeneralpurposeAddressAllocator<uint32_t> >::printDebugInfo();
+	irr::core::address_allocator_traits<core::GeneralpurposeAddressAllocatorST<uint32_t> >::printDebugInfo();
+
+    printf("MULTI THREADED=======================================================\n");
+	printf("Linear \n");
+	irr::core::address_allocator_traits<core::LinearAddressAllocatorMT<uint32_t,std::recursive_mutex> >::printDebugInfo();
+	printf("Pool \n");
+	irr::core::address_allocator_traits<core::PoolAddressAllocatorMT<uint32_t,std::recursive_mutex> >::printDebugInfo();
+	printf("Cont \n");
+	irr::core::address_allocator_traits<core::ContiguousPoolAddressAllocatorMT<uint32_t,std::recursive_mutex> >::printDebugInfo();
+	printf("General \n");
+	irr::core::address_allocator_traits<core::GeneralpurposeAddressAllocatorMT<uint32_t,std::recursive_mutex> >::printDebugInfo();
 
 	return 0;
 }
