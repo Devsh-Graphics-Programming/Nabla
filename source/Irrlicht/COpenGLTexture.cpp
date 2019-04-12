353e23c55bb216016d888fda94aca4a27c2de54e
Criss <crisserpl2@gmail.com>
2019-04-12 23:05:30 +0200

    Added BC1-3 SRGB mapping to GLenum

--- source/Irrlicht/COpenGLTexture.cpp
+++ source/Irrlicht/COpenGLTexture.cpp
@@ -621,6 +621,30 @@ void COpenGLTexture::getOpenGLFormatAndParametersFromColorFormat(const asset::E_
 			type = GL_UNSIGNED_BYTE;
 		}
 			break;
+        case asset::EF_BC1_RGB_SRGB_BLOCK:
+        {
+            colorformat = GL_RGBA;
+            type = GL_UNSIGNED_BYTE;
+        }
+        break;
+        case asset::EF_BC1_RGBA_SRGB_BLOCK:
+        {
+            colorformat = GL_RGBA;
+            type = GL_UNSIGNED_BYTE;
+        }
+        break;
+        case asset::EF_BC2_SRGB_BLOCK:
+        {
+            colorformat = GL_RGBA;
+            type = GL_UNSIGNED_BYTE;
+        }
+        break;
+        case asset::EF_BC3_SRGB_BLOCK:
+        {
+            colorformat = GL_RGBA;
+            type = GL_UNSIGNED_BYTE;
+        }
+        break;
         case asset::EF_BC7_UNORM_BLOCK:
         {
             colorformat = GL_RGBA;
@@ -1120,6 +1144,18 @@ GLint COpenGLTexture::getOpenGLFormatAndParametersFromColorFormat(const asset::E
 		case asset::EF_BC3_UNORM_BLOCK:
 		    return GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
 			break;
+        case asset::EF_BC1_RGB_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_S3TC_DXT1_EXT;
+            break;
+        case asset::EF_BC1_RGBA_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT;
+            break;
+        case asset::EF_BC2_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT;
+            break;
+        case asset::EF_BC3_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT;
+            break;
         case asset::EF_BC7_UNORM_BLOCK:
             return GL_COMPRESSED_RGBA_BPTC_UNORM;
         case asset::EF_BC7_SRGB_BLOCK:
@@ -1307,6 +1343,26 @@ asset::E_FORMAT COpenGLTexture::getColorFormatFromSizedOpenGLFormat(const GLenum
         case GL_COMPRESSED_SRGB8_ALPHA8_ASTC_12x12_KHR:
             return EF_ASTC_12x12_SRGB_BLOCK;
 
+        /*case asset::EF_BC1_RGB_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_S3TC_DXT1_EXT;
+            break;
+        case asset::EF_BC1_RGBA_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT;
+            break;
+        case asset::EF_BC2_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT;
+            break;
+        case asset::EF_BC3_SRGB_BLOCK:
+            return GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT;
+            break;*/
+        case GL_COMPRESSED_SRGB_S3TC_DXT1_EXT:
+            return asset::EF_BC1_RGB_SRGB_BLOCK;
+        case GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT:
+            return asset::EF_BC1_RGBA_SRGB_BLOCK;
+        case GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT:
+            return asset::EF_BC2_SRGB_BLOCK;
+        case GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT:
+            return asset::EF_BC3_SRGB_BLOCK;
         case GL_COMPRESSED_RGB_S3TC_DXT1_EXT:
             return asset::EF_BC1_RGB_UNORM_BLOCK;
             break;
