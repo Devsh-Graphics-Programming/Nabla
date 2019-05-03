95429e33349b3312dd1851b92028396a67a832f2
Florastamine <florastamine@gmail.com>
2019-05-03 14:15:55 +0700

    Revert "Don't hardcode testlist.txt"
    This reverts commit a8ccadbf87a4ef0d3c2686582ee64a84a53cf54d.

--- examples_tests/09.ColorSpaceTest/main.cpp
+++ examples_tests/09.ColorSpaceTest/main.cpp
@@ -125,7 +125,7 @@ void testImage(const std::string& path, IrrlichtDevice* device, video::IGPUMeshB
 	}
 }
 
-int main(int argc, const char *argv[])
+int main()
 {
 	irr::SIrrlichtCreationParameters params;
 	params.Bits = 24; //may have to set to 32bit for some platforms
@@ -154,7 +154,7 @@ int main(int argc, const char *argv[])
 	
 	if (fs)
 	{
-		io::IReadFile* file = fs->createAndOpenFile(argv[1] ? argv[1] : "testlist.txt");
+		io::IReadFile* file = fs->createAndOpenFile("testlist.txt");
 		if (file)
 		{
 			std::string line;
