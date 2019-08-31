46ee6b81a95e835aa1a21448a5056a4d590f3b52
devsh <devsh.graphicsprogramming@gmail.com>
2019-09-01 01:29:06 +0200

    Fix examples 17 and 20

--- examples_tests/20.Megatexture/main.cpp
+++ examples_tests/20.Megatexture/main.cpp
@@ -47,7 +47,7 @@ int main()
 	params.DriverType = video::EDT_OPENGL; //! Only Well functioning driver, software renderer left for sake of 2D image drawing
 	params.WindowSize = dimension2d<uint32_t>(1280, 720);
 	params.Fullscreen = false;
-	params.Vsync = true; //! If supported by target platform
+	params.Vsync = false;
 	params.Doublebuffer = true;
 	params.Stencilbuffer = false; //! This will not even be a choice soon
 	IrrlichtDevice* device = createDeviceEx(params);
