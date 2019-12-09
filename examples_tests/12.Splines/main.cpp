77b1eddc5ab9493410b81b8ab5789f4dde0e5a48
devsh <devsh.graphicsprogramming@gmail.com>
2019-12-09 21:47:23 +0100

    ex 12 rework, but not done yet

--- examples_tests/12.Splines/main.cpp
+++ examples_tests/12.Splines/main.cpp
@@ -8,6 +8,8 @@
 
 using namespace irr;
 using namespace core;
+using namespace asset;
+using namespace video;
 
 vector3df camPos;
 vector<vectorSIMDf> controlPts;
@@ -43,7 +45,7 @@ vector<vectorSIMDf> preprocessBSplineControlPoints(const IteratorType& _begin, c
 	{
 		auto deltaPrev = original - prev;
 		auto deltaNext = next - original;
-		float currentRelativeLen = core::min_(core::length(deltaPrev).x, core::length(deltaNext).x) * relativeLen;
+		float currentRelativeLen = core::min(core::length(deltaPrev).x, core::length(deltaNext).x) * relativeLen;
 		auto tangent = core::normalize(next - prev) * currentRelativeLen;
 		*(out++) = original - tangent;
 		*(out++) = original + tangent;
@@ -225,27 +227,83 @@ int main()
 	camera->setFarValue(100.0f);
     smgr->setActiveCamera(camera);
 
+	constexpr uint32_t PER_MATERIAL_DESC_SET = 2u; //don't touch this, will go into scene namespace
+
+	auto layout = assMgr->findGPUObject("irr/builtin/materials/lambertian/singletexture/pipelinelayout",IAsset::ET_PIPELINE_LAYOUT);
+	auto shaders = assMgr->findGPUObject("irr/builtin/materials/lambertian/singletexture/specializedshader",IAsset::ET_SPECIALIZED_SHADER);
+	if (!layout->size() || !shaders->size())
+		return 2;
+
+	auto pLayout = core::smart_refctd_ptr_static_cast<IGPUPipelineLayout>(layout->front());
 	//! Test Creation Of Builtin
-	auto cubeMesh = assMgr->getGeometryCreator()->createCubeMesh(core::vector3df(size));
-	auto res = driver->getGPUObjectsFromAssets(&cubeMesh.get(),(&cubeMesh.get())+1);
+	auto cubeParams = assMgr->getGeometryCreator()->createCubeMesh(core::vector3df(1.f));
 
-	auto* cube = smgr->addCubeSceneNode(1.f, 0, -1);
-	cube->setRotation(core::vector3df(45, 20, 15));
+	IGPUSpecializedShader** pShaders = reinterpret_cast<IGPUSpecializedShader**>(shaders->data()); // hack but works, shorter code
 
-	auto assetMgr = device->getAssetManager();
+	SBlendParams blendParams; // gets sane defaults
+
+	SRasterizationParams rasterParams; // default is depth testing
+	auto pipeline = driver->createGPURenderpassIndependentPipeline(	nullptr,core::smart_refctd_ptr(pLayout),pShaders,pShaders+shaders->size(),
+																	cubeParams.inputParams,blendParams,cubeParams.assemblyParams,rasterParams);
+
+	auto sampler = assMgr->findGPUObject("irr/builtin/samplers/default",IAsset::ET_SAMPLER);
+
+	struct alignas(64) CustomObject
+	{
+		core::smart_refctd_ptr<IGPUMeshBuffer> mb = nullptr;
+		core::matrix3x4SIMD transform = core::matrix3x4SIMD();
+	};
+	
+	auto createCube = [=](	const char* jpegPath, const core::quaternion& rotation,
+							const core::vector3df_SIMD& scale=core::vector3df_SIMD(1.f,1.f,1.f)) -> CustomObject
 	{
 		asset::IAssetLoader::SAssetLoadParams lparams;
-		asset::ICPUTexture* cputextures[]{
-			static_cast<asset::ICPUTexture*>(assetMgr->getAsset("../../media/irrlicht2_dn.jpg", lparams).getContents().first->get()),
-			static_cast<asset::ICPUTexture*>(assetMgr->getAsset("../../media/wall.jpg", lparams).getContents().first->get())
+		auto contents = assMgr->getAsset(jpegPath, lparams).getContents();
+		if (contents.first == contents.second)
+			return {};
+
+		auto cpuImg = static_cast<asset::ICPUImage*>(contents.first->get());
+		auto gpuImages = driver->getGPUObjectsFromAssets(&cpuImg,&cpuImg+1u);
+		if (!gpuImages->size())
+			return {};
+
+		auto gpuImage = core::smart_refctd_ptr_static_cast<IGPUImage>(gpuImages->front());
+		const auto& imgCreationParams = gpuImage->getCreationParameters();
+		auto imgView = driver->createGPUImageView({{},std::move(gpuImage),IGPUImageView::ET_2D,imgCreationParams.format,{},{{},0u,imgCreationParams.mipLevels,0u,1u}});
+
+		core::smart_refctd_ptr<IGPUDescriptorSetLayout> dsLayout;
+		{
+			std::string path("irr/builtin/materials/lambertian/singletexture/descriptorsetlayout/");
+			path += std::to_string(PER_MATERIAL_DESC_SET);
+			auto layout = assMgr->findGPUObject(path.c_str(),IAsset::ET_PIPELINE_LAYOUT);
+			assert(layout->front().get()==pLayout->getDescriptorSetLayout(PER_MATERIAL_DESC_SET));
+			dsLayout = core::smart_refctd_ptr_dynamic_cast<IGPUDescriptorSetLayout>(layout->front());
+		}
+		auto ds = driver->createGPUDescriptorSet(std::move(dsLayout));
+
+		IGPUDescriptorSet::SDescriptorInfo pInfo[1];
+		pInfo[0].desc = std::move(imgView);
+		pInfo[0].image.imageLayout = EIL_SHADER_READ_ONLY_OPTIMAL;
+		pInfo[0].image.sampler = core::smart_refctd_ptr_dynamic_cast<IGPUSampler>(sampler->front());
+		IGPUDescriptorSet::SWriteDescriptorSet pWrites[1] = {{ds.get(),0u,0u,1u,EDT_COMBINED_IMAGE_SAMPLER,pInfo}};
+		driver->updateDescriptorSets(1u,pWrites,0u,nullptr);
+
+		auto getGPUBufferBinding = [&](const SBufferBinding<ICPUBuffer>& other)
+		{
+			return SBufferBinding<IGPUBuffer>{other.offset,core::smart_refctd_ptr_dynamic_cast<IGPUBuffer>(assMgr->findGPUObject(other.buffer.get()))};
 		};
-		auto gputextures = driver->getGPUObjectsFromAssets(cputextures, cputextures + 2);
+		SBufferBinding<IGPUBuffer> bindings[IGPUMeshBuffer::MAX_ATTR_BUF_BINDING_COUNT];
+		for (size_t i=0ull; i<IGPUMeshBuffer::MAX_ATTR_BUF_BINDING_COUNT; i++)
+			bindings[i] = getGPUBufferBinding(cubeParams.bindings[i]);
+		CustomObject retval;
+		retval.mb = core::make_smart_refctd_ptr<IGPUMeshBuffer>(core::smart_refctd_ptr(pipeline),std::move(ds),bindings,getGPUBufferBinding(cubeParams.indexBuffer));
+		retval.transform.setRotation(rotation);
+		retval.transform.concatenateAfter(core::matrix3x4SIMD().setScale(scale));
+		return retval;
+	};
 
-		cube->getMesh()->getMeshBuffer(0)->getMaterial().setTexture(0, std::move(gputextures->operator[](0u)));
-
-		auto* billboard = smgr->addCubeSceneNode(2.f, 0, -1, core::vector3df(0, 0, 0));
-		billboard->getMesh()->getMeshBuffer(0)->getMaterial().setTexture(0, std::move(gputextures->operator[](1u)));
-	}
+	auto animatedCube = createCube("../../media/bumpmap.jpg",core::quaternion::fromEuler(core::radians(core::vector3df_SIMD(45.f,20.f,15.f))));
+	auto centerCube = createCube("../../media/wall.jpg",core::quaternion(),core::vector3df_SIMD(2.f));
 
     float cubeDistance = 0.f;
     float cubeParameterHint = 0.f;
@@ -253,29 +311,45 @@ int main()
     #define kCircleControlPts 4
     for (size_t i=0; i<kCircleControlPts; i++)
     {
-        float x = float(i)*core::PI*2.f/float(kCircleControlPts);
+        float x = float(i)*core::PI<float>()*2.f/float(kCircleControlPts);
         vectorSIMDf pos(sin(x),0,-cos(x)); pos *= 4.f;
         controlPts.push_back(pos);
-		smgr->addCubeSceneNode(0.5f, 0, -1, pos.getAsVector3df());
     }
 
 	uint64_t lastFPSTime = 0;
 
-	uint64_t lastTime = device->getTimer()->getRealTime();
-    uint64_t timeDelta = 0;
+	auto timer = device->getTimer();
+	auto lastTime = timer->getTime();
 
 	while(device->run() && receiver.keepOpen())
 	{
 		driver->beginScene(true, true, video::SColor(255,0,0,255) );
 
-        uint64_t nowTime = device->getTimer()->getRealTime();
-        timeDelta = nowTime-lastTime;
-        lastTime = nowTime;
+		auto nowtime = timer->getTime();
+		auto timeDelta = nowtime - lastTime;
+		lastTime = nowtime;
+
+		camera->OnAnimate(std::chrono::duration_cast<std::chrono::milliseconds>(nowtime).count());
+		camPos = camera->getAbsolutePosition();
+
+		camera->render();
+		core::matrix4SIMD vp = camera->getConcatenatedMatrix();
+
+		driver->bindGraphicsPipeline(pipeline.get());
+		auto drawCube = [&](const CustomObject& cube) -> void
+		{
+			auto ds = cube.mb->getAttachedDescriptorSet();
+			driver->bindDescriptorSets(EPBP_GRAPHICS,pLayout.get(),PER_MATERIAL_DESC_SET,1u,&ds,nullptr);
+			core::matrix4SIMD mvp = core::concatenateBFollowedByA(vp, cube.transform);
+			driver->pushConstants(pipeline->getLayout(), ISpecializedShader::ESS_VERTEX, 0u, sizeof(core::matrix4SIMD), mvp.pointer());
+			driver->drawMeshBuffer(cube.mb.get());
+		};
+		drawCube(centerCube);
 
 		if (spline)
         {
             vectorSIMDf newPos;
-            cubeDistance += float(timeDelta)*0.001f; //1 unit per second
+            cubeDistance += float(std::chrono::duration_cast<std::chrono::milliseconds>(timeDelta).count())*0.001f; //1 unit per second
             cubeSegment = spline->getPos(newPos,cubeDistance,cubeSegment,&cubeParameterHint);
             if (cubeSegment>=0xdeadbeefu) //reached end of non-loop, or spline changed
             {
@@ -299,15 +373,11 @@ int main()
             mat.getColumn(1) = reinterpret_cast<vector3df&>(pseudoUp);
             mat.getColumn(2) = reinterpret_cast<vector3df&>(sideDir);
             mat.setTranslation(reinterpret_cast<const vector3df&>(newPos));
-            cube->setRelativeTransformationMatrix(mat);
+			animatedCube.transform.set(mat);
         }
+		drawCube(animatedCube);
 
-        //! This animates (moves) the camera and sets the transforms
-        //! Also draws the meshbuffer
-        smgr->drawAll();
-        camPos = camera->getAbsolutePosition();
-
-		draw3DLine->draw(lines);
+		draw3DLine->draw(vp,lines);
 
 		driver->endScene();
 
