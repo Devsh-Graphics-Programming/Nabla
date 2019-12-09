2e4a5542e5d968731850d7e939cbadd7d3f603f4
devsh <devsh.graphicsprogramming@gmail.com>
2019-12-09 21:17:58 +0100

    fix sampler creation and CDerivativeMapCreator formatting

--- include/irr/video/CDerivativeMapCreator.h
+++ include/irr/video/CDerivativeMapCreator.h
@@ -3,7 +3,9 @@
 
 #include "irr/core/IReferenceCounted.h"
 
-namespace irr { namespace video
+namespace irr
+{
+namespace video
 {
 
 class IVideoDriver;
@@ -11,21 +13,22 @@ class IRenderableVirtualTexture;
 
 class CDerivativeMapCreator final : public core::IReferenceCounted
 {
-protected:
-    ~CDerivativeMapCreator();
+	protected:
+		~CDerivativeMapCreator();
 
-public:
-    CDerivativeMapCreator(video::IVideoDriver* _driver);
-	core::smart_refctd_ptr<video::IGPUImageView> createDerivMapFromBumpMap(video::IGPUImageView* _bumpMap, float _heightFactor, bool _texWrapRepeat = false) const;
+	public:
+		CDerivativeMapCreator(video::IVideoDriver* _driver);
+		core::smart_refctd_ptr<video::IGPUImageView> createDerivMapFromBumpMap(video::IGPUImageView* _bumpMap, float _heightFactor, bool _texWrapRepeat = false) const;
 
-private:
-    uint32_t createComputeShader(const char*) const;
+	private:
+		uint32_t createComputeShader(const char*) const;
 
-    uint32_t m_deriv_map_gen_cs;
-    uint32_t m_bumpMapSampler;
-    video::IVideoDriver* m_driver;
+		uint32_t m_deriv_map_gen_cs;
+		uint32_t m_bumpMapSampler;
+		video::IVideoDriver* m_driver;
 };
 
-}}
+}
+}
 
 #endif//__IRR_C_DERIVATIVE_MAP_CREATOR_H_INCLUDED__
