dbbef0a954636f835dd6ce71d079eda8f1220060
AnastaZIuk <areklachowicz@gmail.com>
2020-01-04 19:23:20 +0100

    Prepare myself to merge Criss's stuff

--- src/irr/asset/CGLIWriter.cpp
+++ src/irr/asset/CGLIWriter.cpp
@@ -32,6 +32,8 @@ namespace irr
 {
 	namespace asset
 	{
+		inline std::pair<gli::texture::format_type, std::array<gli::gl::swizzle, 4>> getTranslatedIRRFormat(const IImageView<ICPUImage>::SCreationParams& params);
+
 		template<typename aType>
 		aType getSingleChannel(const void* data)
 		{
@@ -107,13 +109,13 @@ namespace irr
 				return std::make_pair(layers, faces);
 			};
 
+			auto gliFormatAndSwizzles = getTranslatedIRRFormat(imageViewInfo);
 			gli::target gliTarget = getTarget();
-			gli::format gliFormat;						 // TODO ! ! ! format + swizzle
 			gli::extent3d gliExtent3d = {imageInfo.extent.width, imageInfo.extent.height, imageInfo.extent.depth};
 			size_t gliLevels = imageInfo.mipLevels;
 			std::pair<size_t, size_t> layersAndFacesAmount = getFacesAndLayersAmount();
 
-			gli::texture texture(gliTarget, gliFormat, gliExtent3d, layersAndFacesAmount.first, layersAndFacesAmount.second, gliLevels);
+			gli::texture texture(gliTarget, gliFormatAndSwizzles.first, gliExtent3d, layersAndFacesAmount.first, layersAndFacesAmount.second, gliLevels);
 
 			const auto begginingOfRegion = image->getRegions().begin();
 			for (uint16_t layer = 0; layer < imageInfo.arrayLayers; ++layer)
@@ -172,6 +174,126 @@ namespace irr
 				case ICPUImageView::ET_2D_ARRAY: return true;
 				case ICPUImageView::ET_CUBE_MAP_ARRAY: return true;
 				default: return false;
+			}
+		}
+
+		inline std::pair<gli::texture::format_type, std::array<gli::gl::swizzle, 4>> getTranslatedIRRFormat(const IImageView<ICPUImage>::SCreationParams& params)
+		{
+			using namespace gli;
+
+			auto getTranslatedFinalFormat = [&](const gli::texture::format_type& format = FORMAT_UNDEFINED)
+			{
+				return std::make_pair(format, std::array<gli::gl::swizzle, 4>{gl::SWIZZLE_RED, gl::SWIZZLE_GREEN, gl::SWIZZLE_BLUE, gl::SWIZZLE_ALPHA}); // TODO swizzles
+			};
+
+			// TODO - fill formats
+			switch (params.format)
+			{
+				/*
+			case EF_R8G8B8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGB_UNORM);			//GL_RGB
+			case EF_B8G8R8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_BGR_UNORM);			//GL_BGR
+			case EF_R8G8B8A8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_UNORM);		//GL_RGBA
+			case EF_B8G8R8A8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_BGRA_UNORM);		//GL_BGRA
+
+			// unorm formats
+			case EF_R8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_R8_UNORM);			//GL_R8
+			case EF_R8G8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RG8_UNORM);		//GL_RG8
+			//case EF_R8G8B8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGB8_UNORM);		//GL_RGB8
+			//case EF_R8G8B8A8_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGBA8_UNORM);		//GL_RGBA8
+
+			case EF_R16_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_R16_UNORM);		//GL_R16
+			case EF_R16G16_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RG16_UNORM);		//GL_RG16
+			case EF_R16G16B16_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGB16_UNORM);		//GL_RGB16
+			case EF_R16G16B16A16_UNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGBA16_UNORM);		//GL_RGBA16
+
+			case EF_A2R10G10B10_UNORM_PACK32: return getTranslatedFinalFormat(gl::INTERNAL_RGB10A2_UNORM);	//GL_RGB10_A2
+			//case EF_A2R10G10B10_UNORM_PACK32: return getTranslatedFinalFormat(gl::INTERNAL_RGB10A2_SNORM_EXT);
+
+				// snorm formats
+			case EF_R8_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_R8_SNORM);			//GL_R8_SNORM
+			case EF_R8G8_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RG8_SNORM);		//GL_RG8_SNORM
+			case EF_R8G8B8_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGB8_SNORM);		//GL_RGB8_SNORM
+			case EF_R8G8B8A8_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGBA8_SNORM);		//GL_RGBA8_SNORM
+
+			case EF_R16_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_R16_SNORM);		//GL_R16_SNORM
+			case EF_R16G16_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RG16_SNORM);		//GL_RG16_SNORM
+			case EF_R16G16B16_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGB16_SNORM);		//GL_RGB16_SNORM
+			case EF_R16G16B16A16_SNORM: return getTranslatedFinalFormat(gl::INTERNAL_RGBA16_SNORM);		//GL_RGBA16_SNORM
+
+			// unsigned integer formats
+			case EF_R8_UINT: return getTranslatedFinalFormat(gl::INTERNAL_R8U);				//GL_R8UI
+			case EF_R8G8_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RG8U);				//GL_RG8UI
+			case EF_R8G8B8_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB8U);			//GL_RGB8UI
+			case EF_R8G8B8A8_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA8U);			//GL_RGBA8UI
+
+			case EF_R16_UINT: return getTranslatedFinalFormat(gl::INTERNAL_R16U);				//GL_R16UI
+			case EF_R16G16_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RG16U);			//GL_RG16UI
+			case EF_R16G16B16_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB16U);			//GL_RGB16UI
+			case EF_R16G16B16A16_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA16U);			//GL_RGBA16UI
+
+			case EF_R32_UINT: return getTranslatedFinalFormat(gl::INTERNAL_R32U);				//GL_R32UI
+			case EF_R32G32_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RG32U);			//GL_RG32UI
+			case EF_R32G32B32_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB32U);			//GL_RGB32UI
+			case EF_R32G32B32A32_UINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA32U);			//GL_RGBA32UI
+
+			case EF_A2R10G10B10_UINT_PACK32: return getTranslatedFinalFormat(gl::INTERNAL_RGB10A2U);			//GL_RGB10_A2UI
+			case EF_A2R10G10B10_SINT_PACK32: return getTranslatedFinalFormat(gl::INTERNAL_RGB10A2I_EXT);	//GL_RGB10_A2I
+
+			// signed integer formats
+			case EF_R8_SINT: return getTranslatedFinalFormat(gl::INTERNAL_R8I);				//GL_R8I
+			case EF_R8G8_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RG8I);				//GL_RG8I
+			case EF_R8G8B8_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB8I);			//GL_RGB8I
+			case EF_R8G8B8A8_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA8I);			//GL_RGBA8I
+
+			case EF_R16_SINT: return getTranslatedFinalFormat(gl::INTERNAL_R16I);				//GL_R16I
+			case EF_R16G16_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RG16I);			//GL_RG16I
+			case EF_R16G16B16_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB16I);			//GL_RGB16I
+			case EF_R16G16B16A16_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA16I);			//GL_RGBA16I
+
+			case EF_R32_SINT: return getTranslatedFinalFormat(gl::INTERNAL_R32I);				//GL_R32I
+			case EF_R32G32_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RG32I);			//GL_RG32I
+			case EF_R32G32B32_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGB32I);			//GL_RGB32I
+			case EF_R32G32B32A32_SINT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA32I);			//GL_RGBA32I
+
+			// Floating formats
+			case EF_R16_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_R16F);				//GL_R16F
+			case EF_R16G16_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RG16F);			//GL_RG16F
+			case EF_R16G16B16_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGB16F);			//GL_RGB16F
+			case EF_R16G16B16A16_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA16F);			//GL_RGBA16F
+
+			case EF_R32_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_R32F);				//GL_R32F
+			case EF_R32G32_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RG32F);			//GL_RG32F
+			case EF_R32G32B32_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGB32F);			//GL_RGB32F
+			case EF_R32G32B32A32_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA32F);			//GL_RGBA32F
+
+			case EF_R64_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_R64F_EXT);			//GL_R64F
+			case EF_R64G64_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RG64F_EXT);		//GL_RG64F
+			case EF_R64G64B64_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGB64F_EXT);		//GL_RGB64F
+			case EF_R64G64B64A64_SFLOAT: return getTranslatedFinalFormat(gl::INTERNAL_RGBA64F_EXT);		//GL_RGBA64F
+
+			// sRGB formats
+			case EF_R8_SRGB: return getTranslatedFinalFormat(gl::INTERNAL_SR8);				//GL_SR8_EXT
+			case EF_R8G8_SRGB: return getTranslatedFinalFormat(gl::INTERNAL_SRG8);				//GL_SRG8_EXT
+			case EF_R8G8B8_SRGB: return getTranslatedFinalFormat(gl::INTERNAL_SRGB8);			//GL_SRGB8
+			case EF_R8G8B8A8_SRGB: return getTranslatedFinalFormat(gl::INTERNAL_SRGB8_ALPHA8);		//GL_SRGB8_ALPHA8
+
+			// Compressed formats
+			case EF_ASTC_4x4_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_4x4);				//GL_COMPRESSED_RGBA_ASTC_4x4_KHR
+			case EF_ASTC_5x4_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_5x4);				//GL_COMPRESSED_RGBA_ASTC_5x4_KHR
+			case EF_ASTC_5x5_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_5x5);				//GL_COMPRESSED_RGBA_ASTC_5x5_KHR
+			case EF_ASTC_6x5_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_6x5);				//GL_COMPRESSED_RGBA_ASTC_6x5_KHR
+			case EF_ASTC_6x6_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_6x6);				//GL_COMPRESSED_RGBA_ASTC_6x6_KHR
+			case EF_ASTC_8x5_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_8x5);				//GL_COMPRESSED_RGBA_ASTC_8x5_KHR
+			case EF_ASTC_8x6_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_8x6);				//GL_COMPRESSED_RGBA_ASTC_8x6_KHR
+			case EF_ASTC_8x8_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_8x8);				//GL_COMPRESSED_RGBA_ASTC_8x8_KHR
+			case EF_ASTC_10x5_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_10x5); 				//GL_COMPRESSED_RGBA_ASTC_10x5_KHR
+			case EF_ASTC_10x6_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_10x6);				//GL_COMPRESSED_RGBA_ASTC_10x6_KHR
+			case EF_ASTC_10x8_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_10x8);				//GL_COMPRESSED_RGBA_ASTC_10x8_KHR
+			case EF_ASTC_10x10_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_10x10);				//GL_COMPRESSED_RGBA_ASTC_10x10_KHR
+			case EF_ASTC_12x10_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_12x10);				//GL_COMPRESSED_RGBA_ASTC_12x10_KHR
+			case EF_ASTC_12x12_UNORM_BLOCK: return getTranslatedFinalFormat(gl::INTERNAL_RGBA_ASTC_12x12);				//GL_COMPRESSED_RGBA_ASTC_12x12_KHR
+			*/
+			default: return getTranslatedFinalFormat();
 			}
 		}
 	}
