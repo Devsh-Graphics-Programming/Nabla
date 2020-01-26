c1cbeb55df5a979b8a57be9966135483b193ad13
Criss <crisserpl2@gmail.com>
2020-01-26 01:02:29 +0100

    this is not needed for a long time now

--- src/irr/video/COpenGLRenderpassIndependentPipeline.h
+++ src/irr/video/COpenGLRenderpassIndependentPipeline.h
@@ -174,31 +174,6 @@ public:
     const SVAOHash& getVAOHash() const { return m_vaoHashval; }
 
 private:
-    // TODO move GL object creation to driver
-    GLuint createGLobject(uint32_t _ctxID)
-    {
-        static_assert(SHADER_STAGE_COUNT == 5u, "SHADER_STAGE_COUNT is expected to be 5");
-        const GLenum stages[SHADER_STAGE_COUNT]{ GL_VERTEX_SHADER, GL_TESS_CONTROL_SHADER, GL_TESS_EVALUATION_SHADER, GL_GEOMETRY_SHADER, GL_FRAGMENT_SHADER };
-        const GLenum stageFlags[SHADER_STAGE_COUNT]{ GL_VERTEX_SHADER_BIT, GL_TESS_CONTROL_SHADER_BIT, GL_TESS_EVALUATION_SHADER_BIT, GL_GEOMETRY_SHADER_BIT, GL_FRAGMENT_SHADER_BIT };
-
-        GLuint pipeline = 0u;
-        COpenGLExtensionHandler::extGlCreateProgramPipelines(1u, &pipeline);
-        
-        for (uint32_t ix = 0u; ix < SHADER_STAGE_COUNT; ++ix) {
-            COpenGLSpecializedShader* glshdr = static_cast<COpenGLSpecializedShader*>(m_shaders[ix].get());
-            GLuint progName = 0u;
-
-            if (!glshdr || glshdr->getStage() != stages[ix])
-                continue;
-            progName = glshdr->getGLnameForCtx(_ctxID);
-
-            if (progName)
-                COpenGLExtensionHandler::extGlUseProgramStages(pipeline, stageFlags[ix], progName);
-        }
-        
-        return pipeline;
-    }
-
     SVAOHash m_vaoHashval;
     uint32_t m_stagePresenceMask;
 };
