b9525fd8016700958fca516f783e3e161d3754f4
criss <crisserpl2@gmail.com>
2020-02-13 18:19:40 +0100

    forgot to adjust this

--- include/irr/asset/IAsset.h
+++ include/irr/asset/IAsset.h
@@ -96,7 +96,7 @@ class IAsset : virtual public core::IReferenceCounted
 			ET_IMPLEMENTATION_SPECIFIC_METADATA = 1ull<<31u     //!< lights, etc.
 			//! Reserved special value used for things like terminating lists of this enum
 		};
-		constexpr static size_t ET_STANDARD_TYPES_COUNT = 16u;
+		constexpr static size_t ET_STANDARD_TYPES_COUNT = 17u;
 
 		//! Returns a representaion of an Asset type in decimal system
 		/**
