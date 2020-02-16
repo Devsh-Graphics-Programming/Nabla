bc1e570b551b63c7ac46d80a9f6b4a6bfa12d4f4
devsh <devsh.graphicsprogramming@gmail.com>
2020-02-16 23:47:52 +0100

    Move the latest conversion function to another section

--- src/irr/asset/bawformat/legacy/CBAWVersionUpFunctions.h
+++ src/irr/asset/bawformat/legacy/CBAWVersionUpFunctions.h
@@ -2,6 +2,9 @@ namespace irr
 {
     namespace asset
     {
+		template<> // V2 to V3 conversion
+		io::IReadFile* CBAWMeshFileLoader::createConvertIntoVer_spec<3>(SContext& _ctx, io::IReadFile* _baw2file, asset::IAssetLoader::IAssetLoaderOverride* _override, const CommonDataTuple<2>& _common);
+
         template<> // V1 to V2 conversion
         io::IReadFile* CBAWMeshFileLoader::createConvertIntoVer_spec<2>(SContext& _ctx, io::IReadFile* _baw1file, asset::IAssetLoader::IAssetLoaderOverride* _override, const CommonDataTuple<1>& _common);
 
