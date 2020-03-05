64a38519a9001f611e11c10c353a31b792da8edf
devsh <devsh.graphicsprogramming@gmail.com>
2020-03-05 22:36:55 +0100

    fixed Optix 7.0 SDK's bug XD

--- examples_tests/36.OptiXTriangle/optixTriangle.cu
+++ examples_tests/36.OptiXTriangle/optixTriangle.cu
@@ -1,5 +1,6 @@
 #include "optix.h"
 
+#include "stdint.h"
 #include "stdio.h"
 
 #include "common.h"
@@ -8,12 +9,19 @@ extern "C" {
     __constant__ Params params;
 }
 
+static __forceinline__ __device__ uint64_t irrGetSbtDataPointer()
+{
+    uint64_t ptr;
+    asm("call (%0), _optix_get_sbt_data_ptr_64, ();" : "=l"(ptr) : );
+    return ptr;
+}
+
 extern "C"
 __global__ void __raygen__draw_solid_color()
 {
     uint3 launch_index = optixGetLaunchIndex();
-    RayGenData* rtData = (RayGenData*)optixGetSbtDataPointer();
+    RayGenData* rtData = (RayGenData*)irrGetSbtDataPointer();
     if (launch_index.x == 0 && launch_index.y == 0)
         printf("GPU %p\n",rtData);
-    params.image[launch_index.y * params.image_width + launch_index.x] = make_uchar4(127,0,0, 255);
+    params.image[launch_index.y * params.image_width + launch_index.x] = make_uchar4(rtData->r * 255, rtData->g * 255, rtData->b * 255, 255);
 }
