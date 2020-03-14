08a31b22167f54a276a72fb79f9362fa380e453a
criss <crisserpl2@gmail.com>
2020-03-14 17:07:50 +0100

    VT: alloc/pack separated, page table, etc

--- include/irr/asset/CTexturePacker.h
+++ include/irr/asset/CTexturePacker.h
@@ -17,20 +17,22 @@ class CSquareAddressAllocator
 protected:
     using lin_addr_alctr_t = core::GeneralpurposeAddressAllocator<addr_type>;
 
-    const uint32_t ADDR_LAYER_SHIFT;
-    const uint32_t ADDR_MORTON_MASK;
+    uint32_t ADDR_LAYER_SHIFT;
+    uint32_t ADDR_MORTON_MASK;
 
     core::smart_refctd_dynamic_array<uint8_t> m_alctrReservedSpace;
     lin_addr_alctr_t m_addrAlctr;
 
 public:
-    CSquareAllocator(uint32_t _addrBitCntPerDim, uint32_t _squareSz) : 
+    _IRR_STATIC_INLINE_CONSTEXPR addr_type invalid_address = lin_addr_alctr_t::invalid_address;
+
+    CSquareAddressAllocator(uint32_t _addrBitCntPerDim, uint32_t _squareSz, uint32_t _layers) :
         ADDR_LAYER_SHIFT(_addrBitCntPerDim*2u),
         ADDR_MORTON_MASK((1u<<ADDR_LAYER_SHIFT)-1u),
         m_alctrReservedSpace(core::make_refctd_dynamic_array<decltype(m_alctrReservedSpace)>(
             lin_addr_alctr_t::reserved_size(_squareSz*_squareSz, _squareSz*_squareSz, 1ull))
         ),
-        m_addrAlctr(reinterpret_cast<void*>(m_alctrReservedSpace->data()), 0u, 0u, _squareSz*_squareSz, _squareSz*_squareSz, 1u)
+        m_addrAlctr(reinterpret_cast<void*>(m_alctrReservedSpace->data()), 0u, 0u, _squareSz*_squareSz, _squareSz*_squareSz*_layers, 1u)
     {}
 
     inline addr_type alloc_addr(size_t _subsquareSz)
@@ -53,20 +55,119 @@ public:
     }
 };
 
-class CTexturePacker
+class ITexturePacker
 {
-    _IRR_STATIC_INLINE_CONSTEXPR uint32_t TILE_PADDING = 8u;
-
+protected:
     _IRR_STATIC_INLINE_CONSTEXPR uint32_t ADDR_LAYER_SHIFT = 28u;//14bit 2d page address and 4bit for 3rd address dimension (layer)
     _IRR_STATIC_INLINE_CONSTEXPR uint32_t ADDR_MORTON_MASK = (1u<<ADDR_LAYER_SHIFT)-1u;
 
-    using addr_alctr_t = core::GeneralpurposeAddressAllocator<uint32_t>;
+    const uint32_t m_pgSzxy;
+    const uint32_t m_tilesPerDim;
+    CSquareAddressAllocator<uint32_t> m_addrAlctr;
 
-    static uint32_t megaimgLayerSize(uint32_t _pgSz, uint32_t _tilesPerDim)
+    core::smart_refctd_ptr<ICPUImage> m_pageTable;
+public:
+    struct STexOffset
+    {
+        _IRR_STATIC_INLINE_CONSTEXPR uint32_t X_MASK = ((1u<<(ADDR_LAYER_SHIFT>>1))-1u);
+        _IRR_STATIC_INLINE_CONSTEXPR uint32_t invalid_addr = (~0u);
+
+        STexOffset(uint32_t _addr) : addr(_addr) {}
+        STexOffset(uint32_t _x, uint32_t _y, uint32_t _layer)
+        {
+            addr = (_x&X_MASK);
+            addr |= (_y&X_MASK)<<(ADDR_LAYER_SHIFT>>1);
+            addr |= (_layer<<ADDR_LAYER_SHIFT);
+        }
+        uint32_t x() const { return addr&X_MASK; }
+        uint32_t y() const { return (addr>>(ADDR_LAYER_SHIFT>>1))&X_MASK; }
+        uint32_t layer() const { return addr>>ADDR_LAYER_SHIFT; }
+
+        uint32_t addr;
+    };
+
+    ITexturePacker(uint32_t _pgSzxy = 256u, uint32_t _tilesPerDim = 32u, uint32_t _numLayers = 4u) :
+        m_pgSzxy(_pgSzxy),
+        m_tilesPerDim(_tilesPerDim),
+        m_addrAlctr(14u, _tilesPerDim, _numLayers)
+    {
+        ICPUImage::SCreationParams params;
+        params.arrayLayers = _numLayers;
+        params.extent = {_tilesPerDim,_tilesPerDim,1u};
+        params.flags = static_cast<ICPUImage::E_CREATE_FLAGS>(0);
+        params.format = EF_R32_UINT;
+        params.mipLevels = core::findMSB(_tilesPerDim)+1;
+        params.samples = ICPUImage::ESCF_1_BIT;
+        params.type = IImage::ET_2D;
+        m_pageTable = ICPUImage::create(std::move(params));
+        
+        auto regions = core::make_refctd_dynamic_array<core::smart_refctd_dynamic_array<ICPUImage::SBufferCopy>>(m_pageTable->getCreationParameters().mipLevels);
+
+        uint32_t bufOffset = 0u;
+        for (uint32_t i = 0u; i < m_pageTable->getCreationParameters().mipLevels; ++i)
+        {
+            const uint32_t tilesPerLodDim = _tilesPerDim>>i;
+            const uint32_t regionSz = tilesPerLodDim*tilesPerLodDim*_numLayers*4u;
+            auto& region = (*regions)[i];
+            region.bufferOffset = bufOffset;
+            region.bufferImageHeight = 0u;
+            region.bufferRowLength = tilesPerLodDim*4u;
+            region.imageExtent = {tilesPerLodDim,tilesPerLodDim,1u};
+            region.imageOffset = {0,0,0};
+            region.imageSubresource.baseArrayLayer = 0u;
+            region.imageSubresource.layerCount = _numLayers;
+            region.imageSubresource.mipLevel = i;
+            region.imageSubresource.aspectMask = static_cast<IImage::E_ASPECT_FLAGS>(0);
+
+            bufOffset += regionSz;
+        }
+        auto buf = core::make_smart_refctd_ptr<ICPUBuffer>(bufOffset);
+
+        m_pageTable->setBufferAndRegions(std::move(buf), regions);
+    }
+
+    core::smart_refctd_dynamic_array<STexOffset> alloc(const ICPUImage* _img, const ICPUImage::SSubresourceRange& _subres)
+    {
+        auto extent = _img->getCreationParameters().extent;
+        uint32_t levelCount = 1u;
+        for (uint32_t i = 0u; (extent.width>>(_subres.baseMipLevel+i) > m_pgSzxy) || (extent.height>>(_subres.baseMipLevel+i) > m_pgSzxy); ++i)
+            levelCount = i;
+        levelCount = std::min(_subres.levelCount, levelCount);
+
+        auto offsets = core::make_refctd_dynamic_array<core::smart_refctd_dynamic_array<STexOffset>>(levelCount);
+        for (uint32_t i = 0u; i < levelCount; ++i)
+        {
+            const uint32_t w = std::max(extent.width>>(_subres.baseMipLevel+i),1u);
+            const uint32_t h = std::max(extent.height>>(_subres.baseMipLevel+i),1u);
+            const uint32_t pgSqrSz = core::roundUpToPoT((std::max(w,h)+m_pgSzxy-1u)/m_pgSzxy);
+
+            const uint32_t addr = m_addrAlctr.alloc_addr(pgSqrSz);
+            (*offsets)[i] = (addr==CSquareAddressAllocator<uint32_t>::invalid_address) ? 
+                STexOffset::invalid_addr :
+                STexOffset(m_addrAlctr.unpackAddress_x(addr), m_addrAlctr.unpackAddress_y(addr), m_addrAlctr.unpackAddress_layer(addr));
+
+            //i didnt know where to put page table page table and filling code so it is here for now
+            uint32_t* const pgTab = reinterpret_cast<uint32_t*>(
+                reinterpret_cast<uint8_t*>(m_pageTable->getBuffer()->getPointer()) + m_pageTable->getRegions().begin()[i].bufferOffset
+                );
+            const uint32_t pitch = m_pageTable->getRegions().begin()[i].bufferRowLength;
+            const uint32_t offset = (pitch*(m_tilesPerDim>>i))*(*offsets)[i].layer() + (*offsets)[i].y()*pitch + (*offsets)[i].x();
+            for (uint32_t x = 0u; x < pgSqrSz; ++x)
+                for (uint32_t y = 0u; y < pgSqrSz; ++y)
+                    pgTab[offset + y*pitch + x] = (*offsets)[i].addr;
+        }
+        
+        return offsets;
+    }
+};
+
+class ICPUTexturePacker : protected ITexturePacker
+{
+    uint32_t megaimgLayerSize(uint32_t _pgSz, uint32_t _tilesPerDim) const
     {
         return _tilesPerDim*(TILE_PADDING + _pgSz) + TILE_PADDING;
     }
-    static core::vector2du32_SIMD pageCoords(uint32_t _x, uint32_t _y, uint32_t _pgSz)
+    core::vector2du32_SIMD pageCoords(uint32_t _x, uint32_t _y, uint32_t _pgSz) const
     {
         core::vector2du32_SIMD coords(_x,_y);
         coords *= (_pgSz + TILE_PADDING);
@@ -75,18 +176,10 @@ class CTexturePacker
     }
 
 public:
-    struct STexOffset
-    {
-        uint16_t x, y;
-    };
-
-    CTexturePacker(E_FORMAT _format, uint32_t _pgSzxy = 256u, uint32_t _tilesPerDim = 32u, uint32_t _numLayers = 4u) :
-        m_pgSzxy(_pgSzxy),
-        m_tilesPerDim(_tilesPerDim),
-        m_addrAlctr(14u, _tilesPerDim)
+    ICPUTexturePacker(E_FORMAT _format, uint32_t _pgSzxy = 256u, uint32_t _tilesPerDim = 32u, uint32_t _numLayers = 4u) :
+        ITexturePacker(_pgSzxy, _tilesPerDim, _numLayers)
     {
         assert(core::isPoT(_tilesPerDim));
-        assert(core::isPoT(_pgSzxy));
 
         const uint32_t megaimgSz = megaimgLayerSize(_pgSzxy, _tilesPerDim);
 
@@ -114,25 +207,21 @@ public:
         m_megaimg->setBufferAndRegions(std::move(buffer), regions);
     }
 
-    STexOffset pack(const ICPUImage* _img)
+    STexOffset pack(const ICPUImage* _img, const ICPUImage::SSubresourceRange& _subres)
     {
-        assert(m_megaimg->getCreationParameters().format==_img->getCreationParameters().format);
+        //assert(m_megaimg->getCreationParameters().format==_img->getCreationParameters().format);
 
-        auto extent = _img->getCreationParameters().extent;
-        uint32_t rnd = core::roundUpToPoT(std::max(extent.width,extent.height))/m_pgSzxy;
-        uint32_t pgCnt = rnd*rnd;
-        uint32_t addr = m_addrAlctr.alloc_addr(pgCnt, pgCnt);
-        uint32_t addr_layer = m_addrAlctr.unpackAddress_layer(addr);
-        assert(addr!=addr_alctr_t::invalid_address);
-
-        const core::vector2du32_SIMD texOffset = pageCoords(m_addrAlctr.unpackAddress_x(addr), m_addrAlctr.unpackAddress_y(addr), m_pgSzxy);
         const uint32_t texelSz = getTexelOrBlockBytesize(m_megaimg->getCreationParameters().format);
-        const uint64_t bufOffset = static_cast<uint64_t>(m_megaimg->getCreationParameters().extent.width*m_megaimg->getCreationParameters().extent.height*addr_layer + texOffset.y*m_megaimg->getCreationParameters().extent.width + texOffset.x) * texelSz;
-
+        auto offsets = alloc(_img, _subres);
         for (const auto& reg : _img->getRegions())
         {
-            if (reg.imageSubresource.mipLevel!=0u)
+            if (reg.imageSubresource.mipLevel>=_subres.baseMipLevel && reg.imageSubresource.mipLevel<(_subres.baseMipLevel+offsets->size()))
                 continue;
+
+            const uint32_t j = reg.imageSubresource.mipLevel-_subres.baseMipLevel;
+
+            const core::vector2du32_SIMD texOffset = pageCoords((*offsets)[j].x(), (*offsets)[j].y(), m_pgSzxy);
+            const uint64_t bufOffset = static_cast<uint64_t>(m_megaimg->getCreationParameters().extent.width * m_megaimg->getCreationParameters().extent.height * (*offsets)[j].layer() + texOffset.y * m_megaimg->getCreationParameters().extent.width + texOffset.x)* texelSz;
 
             const uint8_t* src = reinterpret_cast<const uint8_t*>(_img->getBuffer()->getPointer())+reg.bufferOffset;
             uint8_t* dst = reinterpret_cast<uint8_t*>(m_megaimg->getBuffer()->getPointer()) + bufOffset;
@@ -143,16 +232,14 @@ public:
             }
         }
 
-        return {texOffset.x,texOffset.y};
+        return offsets->front();
     }
 
     ICPUImage* getImage() { return m_megaimg.get(); }
 
 private:
-    const uint32_t m_pgSzxy;
-    const uint32_t m_tilesPerDim;
-    CSquareAddressAllocator<uint32_t> m_addrAlctr;//TODO this allocator will be used by GPU packer later to pack mip levels of stored later (when they actually exist)
     core::smart_refctd_ptr<ICPUImage> m_megaimg;
+    const uint32_t TILE_PADDING = 8u;
 };
 
 }}
