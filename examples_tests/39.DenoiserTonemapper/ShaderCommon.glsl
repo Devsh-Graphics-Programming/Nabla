fd6396b9354d0587b03415ef79f15c9e3c31725d
devsh <devsh.graphicsprogramming@gmail.com>
2020-03-25 17:44:42 +0100

    add common shader code and C++

---
+++ examples_tests/39.DenoiserTonemapper/ShaderCommon.glsl
@@ -0,0 +1,14 @@
+layout(constant_id = 0) const uint kComputeWGSize = 1024u;
+layout(local_size_x=1024) in; // bug : https://github.com/KhronosGroup/SPIRV-Cross/issues/671#issuecomment-603943800
+
+layout(constant_id = 1) const uint EII_COLOR = 0u;
+layout(constant_id = 2) const uint EII_NORMAL = 2u;
+
+#include "./CommonPushConstants.h"
+
+layout(push_constant, row_major) uniform PushConstants{
+	CommonPushConstants pc;
+};
+
+#define SHARED_CHANNELS 3u
+shared float repackBuffer[kComputeWGSize * SHARED_CHANNELS];
