9f7e54dd0b8335bf7b9eb57668c50b5336f7999e
devsh <devsh.graphicsprogramming@gmail.com>
2020-04-06 11:30:33 +0200

    `convertR8SRGBdataIntoRGB8SRGBAAndGetIt` should be achieved via a `CSwizzleAndConvertImageFilter<EF_R8_SRGB,EF_R8G8B8_SRGB,>`

--- include/irr/asset/IImageManipulator.h
+++ include/irr/asset/IImageManipulator.h
@@ -14,16 +14,6 @@ namespace irr
 namespace asset
 {
 
-// remember about sampler wrap modes
-class CPaddedCopyImageFilter : public CCopyImageFilter
-{
-	public:
-};
-
-// respecifies the image in terms of the least amount of region entries
-class CFlattenRegionsImageFilter; // note: make an option that tries to reuse a buffer
-using CBufferToImageCopyFilter = CFlattenRegionsImageFilter;
-
 // scaled copies with filters
 class CBlitImageFilter;
 
@@ -36,15 +26,6 @@ class CMipMapGenerationImageFilter;
 #endif
 
 /*
-
-uint8_t* convertR8SRGBdataIntoRGB8SRGBAAndGetIt(const void* redChannelDataLayer, const core::smart_refctd_ptr<ICPUImage>& image, const irr::asset::IImage::SBufferCopy& region)	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\include\irr\asset\IImageLoader.h	36	113
-
-
-auto view = core::make_smart_refctd_ptr<ICPUImageView>(std::move(viewParams));	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\ext\MitsubaLoader\CMitsubaLoader.cpp	1286	42
-auto pgtabView = ICPUImageView::create(std::move(params));	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\ext\MitsubaLoader\CMitsubaLoader.cpp	1614	20
-auto physPgTexView = ICPUImageView::create(std::move(params));	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\ext\MitsubaLoader\CMitsubaLoader.cpp	1634	24
-
-SRC:
 
 CGraphicsPipelineLoaderMTL.cpp	964	         const size_t alignment = 1u<<core::findLSB(images[CMTLPipelineMetadata::EMP_REFL_POSX]->getRegions().begin()->bufferRowLength); 	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\src\irr\asset
 CGraphicsPipelineLoaderMTL.cpp	975	                 assert(images[i]->getRegions().size()==1ull); 	C:\work\IrrlichtBaw\IrrlichtBAW parallel work\src\irr\asset
