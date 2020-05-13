701c8608e096379f2bf8c60b8c239728f716b5bd
devsh <devsh.graphicsprogramming@gmail.com>
2020-05-13 02:15:52 +0200

    try and hammer out the tonemapping operators

--- ext/AutoExposure/CToneMapper.cpp
+++ ext/AutoExposure/CToneMapper.cpp
@@ -4,10 +4,8 @@
 using namespace irr;
 using namespace irr::asset;
 using namespace irr::video;
-using namespace ext::AutoExposure;
+using namespace ext::ToneMapper;
 
-
-constexpr uint32_t DISPATCH_SIZE = 16u;
 
 
 core::smart_refctd_ptr<CToneMapper> CToneMapper::create(IVideoDriver* _driver, asset::E_FORMAT inputFormat, const asset::IGLSLCompiler* compiler)
@@ -16,26 +14,6 @@ core::smart_refctd_ptr<CToneMapper> CToneMapper::create(IVideoDriver* _driver, a
 		return nullptr;
 	if (inputFormat!=asset::EF_R16G16B16A16_SFLOAT)
 		return nullptr;
-
-	IGPUDescriptorSetLayout::SBinding bnd[3];
-	bnd[0].binding = 0u;
-	bnd[0].type = asset::EDT_UNIFORM_BUFFER_DYNAMIC;
-	bnd[0].count = 1u;
-	bnd[0].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
-	bnd[0].samplers = nullptr;
-	bnd[1].binding = 1u;
-	bnd[1].type = asset::EDT_COMBINED_IMAGE_SAMPLER;
-	bnd[1].count = 1u;
-	bnd[1].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
-	bnd[1].samplers = nullptr;
-	bnd[2].binding = 2u;
-	bnd[2].type = asset::EDT_STORAGE_IMAGE;
-	bnd[2].count = 1u;
-	bnd[2].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
-	bnd[2].samplers = nullptr;
-	auto dsLayout = _driver->createGPUDescriptorSetLayout(bnd, bnd+sizeof(bnd)/sizeof(IGPUDescriptorSetLayout::SBinding));
-
-	auto pipelineLayout = _driver->createGPUPipelineLayout(nullptr, nullptr, nullptr, nullptr, nullptr, core::smart_refctd_ptr(dsLayout));
 
 	std::ostringstream glsl;
 	glsl << "#version 430 core\n";
@@ -68,10 +46,7 @@ void main()
 	
 	asset::ISpecializedShader::SInfo specInfo(nullptr, nullptr, "main", asset::ISpecializedShader::ESS_COMPUTE);
 
-	auto computePipeline = _driver->createGPUComputePipeline(nullptr, core::smart_refctd_ptr(pipelineLayout), _driver->createGPUSpecializedShader(shader.get(),std::move(specInfo)));
-
-	auto tmp = new CToneMapper(_driver,inputFormat,std::move(dsLayout),std::move(pipelineLayout),std::move(computePipeline));
-	return core::smart_refctd_ptr<CToneMapper>(tmp,core::dont_grab);
+	_driver->createGPUSpecializedShader(shader.get(),std::move(specInfo));
 }
 
 CToneMapper::CToneMapper(	IVideoDriver* _driver, asset::E_FORMAT inputFormat,
@@ -104,68 +79,3 @@ bool CToneMapper::tonemap(video::IGPUImageView* inputThatsInTheSet, video::IGPUD
 	m_driver->dispatch(imgViewSize.x,imgViewSize.y,1u);
 	return true;
 }
-
-#if 0
-//#define PROFILE_TONEMAPPER
-
-bool CToneMapper::CalculateFrameExposureFactors(video::IGPUBuffer* outBuffer, video::IGPUBuffer* uniformBuffer, core::smart_refctd_ptr<video::ITexture>&& inputTexture)
-{
-    bool highRes = false;
-    if (!inputTexture)
-        return false;
-
-    video::COpenGLTexture* asGlTex = dynamic_cast<video::COpenGLTexture*>(inputTexture.get());
-    if (asGlTex->getOpenGLTextureType()!=GL_TEXTURE_2D)
-        return false;
-
-    GLint prevProgram;
-    glGetIntegerv(GL_CURRENT_PROGRAM,&prevProgram);
-
-
-#ifdef PROFILE_TONEMAPPER
-    video::IQueryObject* timeQuery = m_driver->createElapsedTimeQuery();
-    m_driver->beginQuery(timeQuery);
-#endif // PROFILE_TONEMAPPER
-
-    video::STextureSamplingParams params;
-    params.MaxFilter = video::ETFT_LINEAR_NO_MIP;
-    params.MinFilter = video::ETFT_LINEAR_NO_MIP;
-    params.UseMipmaps = 0;
-
-    const video::COpenGLDriver::SAuxContext* foundConst = static_cast<video::COpenGLDriver*>(m_driver)->getThreadContext();
-    video::COpenGLDriver::SAuxContext* found = const_cast<video::COpenGLDriver::SAuxContext*>(foundConst);
-    found->setActiveTexture(0,std::move(inputTexture),params);
-
-
-    video::COpenGLExtensionHandler::extGlUseProgram(m_histogramProgram);
-
-    const video::COpenGLBuffer* buffers[2] = {static_cast<const video::COpenGLBuffer*>(m_histogramBuffer),static_cast<const video::COpenGLBuffer*>(outBuffer)};
-    ptrdiff_t offsets[2] = {0,0};
-    ptrdiff_t sizes[2] = {m_histogramBuffer->getSize(),outBuffer->getSize()};
-    found->setActiveSSBO(0,2,buffers,offsets,sizes);
-
-    buffers[0] = static_cast<const video::COpenGLBuffer*>(uniformBuffer);
-    sizes[0] = uniformBuffer->getSize();
-    found->setActiveUBO(0,1,buffers,offsets,sizes);
-
-    video::COpenGLExtensionHandler::pGlDispatchCompute(m_workGroupCount[0],m_workGroupCount[1],1);
-    video::COpenGLExtensionHandler::pGlMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-
-
-    video::COpenGLExtensionHandler::extGlUseProgram(m_autoExpParamProgram);
-    video::COpenGLExtensionHandler::pGlDispatchCompute(1,1, 1);
-
-
-    video::COpenGLExtensionHandler::extGlUseProgram(prevProgram);
-    video::COpenGLExtensionHandler::pGlMemoryBarrier(GL_UNIFORM_BARRIER_BIT|GL_SHADER_STORAGE_BARRIER_BIT);
-
-#ifdef PROFILE_TONEMAPPER
-    m_driver->endQuery(timeQuery);
-    uint32_t timeTaken=0;
-    timeQuery->getQueryResult(&timeTaken);
-    os::Printer::log("irr::ext::AutoExposure CS Time Taken:", std::to_string(timeTaken).c_str(),ELL_ERROR);
-#endif // PROFILE_TONEMAPPER
-
-    return true;
-}
-#endif
--- ext/AutoExposure/CToneMapper.h
+++ ext/AutoExposure/CToneMapper.h
@@ -1,5 +1,5 @@
-#ifndef _IRR_EXT_AUTO_EXPOSURE_C_TONE_MAPPER_INCLUDED_
-#define _IRR_EXT_AUTO_EXPOSURE_C_TONE_MAPPER_INCLUDED_
+#ifndef _IRR_EXT_TONE_MAPPER_C_TONE_MAPPER_INCLUDED_
+#define _IRR_EXT_TONE_MAPPER_C_TONE_MAPPER_INCLUDED_
 
 #include "irrlicht.h"
 
@@ -7,7 +7,7 @@ namespace irr
 {
 namespace ext
 {
-namespace AutoExposure
+namespace ToneMapper
 {
 
 // TODO: move this to common header along with tonemapping technique enums
@@ -49,9 +49,8 @@ class CToneMapper : public core::IReferenceCounted, public core::InterfaceUnmova
     public:
         static core::smart_refctd_ptr<CToneMapper> create(video::IVideoDriver* _driver, asset::E_FORMAT inputFormat, const asset::IGLSLCompiler* compiler);
 
-		auto* getDescriptorSetLayout() {return dsLayout.get();}
-
 		bool tonemap(video::IGPUImageView* inputThatsInTheSet, video::IGPUDescriptorSet* set, uint32_t parameterUBOOffset);
+
     private:
         CToneMapper(video::IVideoDriver* _driver, asset::E_FORMAT inputFormat,
 					core::smart_refctd_ptr<video::IGPUDescriptorSetLayout>&& _dsLayout,
@@ -62,79 +61,81 @@ class CToneMapper : public core::IReferenceCounted, public core::InterfaceUnmova
 		static inline const char* getInclude()
 		{
 			return R"===(
-vec3 irr_ext_Autoexposure_linear2SRGB(in vec3 color)
-{
-	return mix(color*12.92,1.055*pow(color,vec3(1.0/2.4))-0.055,greaterThan(color,vec3(0.0031308)));
-}
+#include "irr/builtin/glsl/colorspace/EOTF.glsl"
+#include "irr/builtin/glsl/colorspace/encodeCIEXYZ.glsl"
+#include "irr/builtin/glsl/colorspace/decodeCIEXYZ.glsl"
+#include "irr/builtin/glsl/colorspace/OETF.glsl"
 
-float irr_ext_Autoexposure_luminance_R709(in vec3 color)
-{
-	return dot(vec3(0.2126,0.7152,0.0722),color);
-}
 
-struct irr_ext_Autoexposure_ReinhardParams
+struct irr_glsl_ext_ToneMapper_ReinhardParams_t
 {
-	float keyAndLinearExposure; // 0.18*exp2(exposure)
+	float keyAndLinearExposure; // 0.18*exp2(manualExposure)
 	float rcpWhite2; // 1.0/(maxWhite*maxWhite)
-
-	// for std140 compatibility
-	uint uselessPadding0;
-	uint uselessPadding1;
 };
 
-vec3 irr_ext_Autoexposure_ToneMapReinhard(in irr_ext_Autoexposure_ReinhardParams params, vec3 color)
-{
-	color *= params.keyAndLinearExposure;
-	float luma = irr_ext_Autoexposure_luminance_R709(color);
-	return color*(1.0+luma*params.rcpWhite2)/(1.0+luma);
-}
-
-// https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
-struct irr_ext_Autoexposure_ACESParams
+struct irr_glsl_ext_ToneMapper_ACESParams_t
 {
 	float exposure; // actualExposure-midGrayLog2*(gamma-1)
 	float preGammaMinus1; // 0.0
-
-	// for std140 compatibility
-	uint uselessPadding0;
-	uint uselessPadding1;
 };
 
-//! This function has to be wrong, sRGB is apparently included but its a non-linear function!
-// its operating on each ACES color space channel separately, then doing a linear inversion to gamma sRGB !?
-vec3 irr_ext_Autoexposure_ACES(in irr_ext_Autoexposure_ACESParams params, in vec3 color)
+
+// TODO #if
+	#define irr_glsl_ext_ToneMapper_Params_t irr_glsl_ext_ToneMapper_ReinhardParams_t
+	float irr_glsl_ext_ToneMapper_operator(in irr_glsl_ext_ToneMapper_Params_t params, inout float autoexposedLuma)
+	{
+		autoexposedLuma *= params.keyAndLinearExposure;
+		return (1.0+autoexposedLuma*params.rcpWhite2)/(1.0+autoexposedLuma);
+	}
+//#else
+	#define irr_glsl_ext_ToneMapper_Params_t irr_glsl_ext_ToneMapper_ACESParams_t
+	vec3 irr_ext_Autoexposure_ACES(in irr_ext_Autoexposure_ACESParams params, inout float autoexposedLuma)
+	{
+		float luma = irr_ext_Autoexposure_luminance_R709(color);
+		float logLuma = log2(luma);
+		vec3 exposed = color*exp2(logLuma*params.preGammaMinus1+params.exposure);
+
+
+		// XYZ => sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+		const mat3 ACES_R709_Input = irr_glsl_XYZtosRGB*mat3(
+			0.59719, 0.35458, 0.04823,
+			0.07600, 0.90834, 0.01566,
+			0.02840, 0.13383, 0.83777
+		);
+		vec3 v = ACES_R709_Input*exposed;
+
+		vec3 a = v*(v+vec3(0.0245786))-vec3(0.000090537);
+		vec3 b = v*(0.983729*v+vec3(0.4329510))+vec3(0.238081);
+
+		// ODT_SAT => XYZ => D60_2_D65 => sRGB => XYZ
+		const mat3 ACES_R709_Output = mat3(
+			 1.60475, -0.53108, -0.07367,
+			-0.10208,  1.10813, -0.00605,
+			-0.00327, -0.07276,  1.07602
+		)*irr_glsl_sRGBtoXYZ;
+		return clamp(ACES_R709_Output*(a/b), 0.0, 1.0);
+	}
+//#endif
+
+// ideas for more operators https://web.archive.org/web/20191226154550/http://cs.columbia.edu/CAVE/software/softlib/dorf.php
+
+
+vec3 irr_glsl_ext_ToneMapper_tonemap(vec3 cieXYZColor, in irr_glsl_ext_ToneMapper_Params_t params, in float extraNegEV)
 {
-	float luma = irr_ext_Autoexposure_luminance_R709(color);
-	float logLuma = log2(luma);
-	vec3 exposed = color*exp2(logLuma*params.preGammaMinus1+params.exposure);
-
-	mat3 ACES_R709_Input = mat3(
-		0.59719, 0.35458, 0.04823,
-		0.07600, 0.90834, 0.01566,
-		0.02840, 0.13383, 0.83777
-	);
-	vec3 v = ACES_R709_Input*exposed;
-
-	vec3 a = v*(v+vec3(0.0245786))-vec3(0.000090537);
-    vec3 b = v*(0.983729*v+vec3(0.4329510))+vec3(0.238081);
-
-	mat3 ACES_R709_Output = mat3(
-		 1.60475, -0.53108, -0.07367,
-		-0.10208,  1.10813, -0.00605,
-		-0.00327, -0.07276,  1.07602
-	);
-	return ACES_R709_Output*(a/b);
+	cieXYZColor.y *= exp2(-extraNegEV);
+	cieXYZColor *= irr_glsl_ext_ToneMapper_operator(params,cieXYZColor.y);
+	return cieXYZColor;
 }
+
+#endif
 			)===";
 		}
+
+		_IRR_STATIC_INLINE_CONSTEXPR uint32_t DISPATCH_SIZE = 16u;
 
         video::IVideoDriver* m_driver;
 		asset::E_FORMAT format;
 		asset::E_FORMAT viewFormat;
-		core::smart_refctd_ptr<video::IGPUDescriptorSetLayout> dsLayout;
-
-		core::smart_refctd_ptr<video::IGPUPipelineLayout> pipelineLayout;
-		core::smart_refctd_ptr<video::IGPUComputePipeline> computePipeline;
 };
 
 }
