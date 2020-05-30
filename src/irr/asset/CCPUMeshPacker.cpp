0b3ab7ae0d21c6164f5e23c47d9f0912d86bfc23
Przemog1 <minikers21@gmail.com>
2020-05-30 19:50:50 +0200

    Added attribute input rate validation

--- src/irr/asset/CCPUMeshPacker.cpp
+++ src/irr/asset/CCPUMeshPacker.cpp
@@ -32,58 +32,32 @@ std::optional<std::pair<ICPUMeshBuffer*, DrawElementsIndirectCommand_t>> CCPUMes
 
 	//define SVertexInputParams of packed mesh buffer
 	SVertexInputParams outVtxInputParams = meshBuffers[0]->getPipeline()->getVertexInputParams();
-	outVtxInputParams.enabledBindingFlags = 0x0001; // < binding for unified VBO
+	outVtxInputParams.enabledBindingFlags = 0x0003; // < 0 == binding for unified VBO with per vertex data, 1 == binding for unified VBO with per instance data
 	memset(outVtxInputParams.bindings, 0, sizeof(SVertexInputParams::bindings));
 
-	auto findAttribIndexBoundToN = [](const uint32_t N, const SVertexInputParams& attrParams) -> int
-	{
-		if (N > 15)
-			return -1;
+	//git?
+	outVtxInputParams.bindings[0].inputRate = asset::EVIR_PER_VERTEX;
+	outVtxInputParams.bindings[1].inputRate = asset::EVIR_PER_INSTANCE;
 
-		for (uint32_t i = 0; i < SVertexInputParams::MAX_VERTEX_ATTRIB_COUNT; i++)
-		{
-			const SVertexInputAttribParams& vtxInputParam = attrParams.attributes[i];
-			if (vtxInputParam.binding == N)
-				return i;
-		}
-
-		return -1;
-	};
-
-	uint16_t sharedVtxAttribsMask = outVtxInputParams.enabledAttribFlags; //< attributes that are enabled for EVERY input mesh buffers ("alive" attributes)
-	//establish which attrib bindings are shared among all mesh buffers ("alive" attributes)
+	//establish which attrib bindings are enabled (in all of the mesh buffers)
 	for (size_t i = 1; i < meshBuffers.size(); i++)
 	{
 		const auto& currMBVtxInputParams = meshBuffers[i]->getPipeline()->getVertexInputParams();
 
 		if (currMBVtxInputParams.enabledAttribFlags != outVtxInputParams.enabledAttribFlags)
 		{
-			//if currently processed mesh buffer has given attrib enabled but packed mesh buffer don't, then eneble this attrib in packed mesh buffer
-			outVtxInputParams.enabledAttribFlags |= currMBVtxInputParams.enabledAttribFlags;
-
-			for (uint16_t attrBit = 0x0001, binding = 0; binding < 16; attrBit <<= 1, binding++)
+			//if output mesh buffer doesn't have given attrib enabled but currently processed mesh buffer has, enable this attribute for output mesh buffer and set its format and binding
+			for (uint16_t attrBit = 0x0001, location = 0; location < 16; attrBit <<= 1, location++)
 			{
-				//if currently processed mesh buffer doesn't have given attrib enabled but all of the previous mesh buffers had, then remove this attrib from `sharedVtxAttribsMask`
-				if (!(attrBit & currMBVtxInputParams.enabledAttribFlags) && (attrBit & sharedVtxAttribsMask))
-					sharedVtxAttribsMask ^= attrBit;
+				uint16_t oldFlags = outVtxInputParams.enabledAttribFlags;
+				outVtxInputParams.enabledAttribFlags |= currMBVtxInputParams.enabledAttribFlags;
 
-				//set format of new attribute
-				const int attrBindingIndex = findAttribIndexBoundToN(binding, currMBVtxInputParams);
-				assert(attrBindingIndex != -1); //imposibru 
-
-				const asset::E_FORMAT formatOfNewAttr = static_cast<asset::E_FORMAT>(currMBVtxInputParams.attributes[attrBindingIndex].format);
-
-				//assign to first matching attr param
-				SVertexInputAttribParams* attrToAssign = outVtxInputParams.attributes;
-				for (uint32_t i = 0; i < SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT; i++)
+				uint16_t flagDiff = outVtxInputParams.enabledAttribFlags ^ oldFlags;
+				
+				if (flagDiff & attrBit)
 				{
-					if (attrToAssign->format == asset::EF_UNKNOWN)
-					{
-						attrToAssign->format = formatOfNewAttr;
-						break;
-					}
-
-					attrToAssign++;
+					outVtxInputParams.attributes[location].format = currMBVtxInputParams.attributes[location].format;
+					outVtxInputParams.attributes[location].binding =  (currMBVtxInputParams.bindings[currMBVtxInputParams.attributes[location].binding].inputRate == asset::EVIR_PER_VERTEX) ? 0 : 1;
 				}
 			}
 		}
@@ -93,21 +67,17 @@ std::optional<std::pair<ICPUMeshBuffer*, DrawElementsIndirectCommand_t>> CCPUMes
 	//check if formats of attributes with the same bindings are identical
 	for (size_t i = 1; i < meshBuffers.size(); i++)
 	{
-		const auto& vtxInputParams = meshBuffers[i]->getPipeline()->getVertexInputParams();
+		const auto& currMBVtxInputParams = meshBuffers[i]->getPipeline()->getVertexInputParams();
 
-		for (uint16_t attrBit = 0x0001, binding = 0; binding < 16; attrBit <<= 1, binding++)
+		for (uint16_t attrBit = 0x0001, location = 0; location < 16; attrBit <<= 1, location++)
 		{
-			if (!(attrBit & vtxInputParams.enabledAttribFlags))
+			if (!(attrBit & currMBVtxInputParams.enabledAttribFlags))
 				continue;
 
-			_IRR_DEBUG_BREAK_IF(!(attrBit & outVtxInputParams.enabledAttribFlags));
+			assert(!(attrBit & outVtxInputParams.enabledAttribFlags)); //imposibru
 
-			const int attrBindingOutMeshBuffer = findAttribIndexBoundToN(binding, outVtxInputParams);
-			const int attrBindingCurrMeshBuffer = findAttribIndexBoundToN(binding, vtxInputParams);
-
-			assert((attrBindingOutMeshBuffer != -1) && (attrBindingCurrMeshBuffer != -1)); //imposibru
-
-			if (vtxInputParams.attributes[attrBindingCurrMeshBuffer].format != outVtxInputParams.attributes[attrBindingOutMeshBuffer].format)
+			if (currMBVtxInputParams.attributes[location].format != outVtxInputParams.attributes[location].format ||
+				currMBVtxInputParams.bindings[currMBVtxInputParams.attributes[location].binding].inputRate != outVtxInputParams.bindings[outVtxInputParams.attributes[location].binding].inputRate)
 			{
 				_IRR_DEBUG_BREAK_IF(true);
 				return {};
@@ -131,17 +101,13 @@ std::optional<std::pair<ICPUMeshBuffer*, DrawElementsIndirectCommand_t>> CCPUMes
 			return sum + (*mBuff)->getIndexCount();
 		});
 
-	const size_t vtxSize = meshBuffers[0]->calcVertexSize();
-
 	if (idxCnt > std::numeric_limits<uint16_t>::max())
 	{
 		//TODO: create multiple draw calls in this case
-		//TODO: if any mesh buffer has E_INDEX_TYPE::EIT_32BIT, then make sure that none of its indices is > std::numeric_limits<uint16_t>::max()
 		_IRR_DEBUG_BREAK_IF(true);
 		return {};
 	}
 
-	//how is bad alloc handled? is there custom new_handler implemented or something?
 	//TODO: alignment
 	//SBufferBinding<ICPUBuffer> packedVtxBuff{ 0u, core::make_smart_refctd_ptr<ICPUBuffer*>(vtxSize * vtxCnt) };
 	SBufferBinding<ICPUBuffer> packedIdxBuff{ 0u, core::make_smart_refctd_ptr<ICPUBuffer*>(sizeof(uint16_t) * idxCnt) };
