4e80d59177381927ae44e3689a25a46a71db2c63
Przemog1 <minikers21@gmail.com>
2020-06-20 20:04:37 +0200

    Custom MDI data

--- include/irr/asset/IMeshPacker.h
+++ include/irr/asset/IMeshPacker.h
@@ -5,28 +5,19 @@ namespace irr
 {
 namespace asset
 {
-struct DrawElementsIndirectCommand_t
-{
-    uint32_t count;
-    uint32_t instanceCount;
-    uint32_t firstIndex;
-    uint32_t baseVertex;
-    uint32_t baseInstance;
-};
 
-template <typename MeshBufferType>
-class IMeshPacker
+namespace meshPackerUtil
 {
-public:
     struct AllocationParams
     {
-        size_t indexBuffSupportedSizeInBytes                    = 2147483648ull; /*  2GB*/
-        size_t vertexBuffSupportedSizeInBytes                   = 4294967296ull; /*  4GB*/
-        size_t perInstanceVertexBuffSupportedSizeInBytes        = 33554432ull;   /* 32MB*/
-        size_t MDIDataSizeInBytes                               = 2147483648ull; /*  2GB*/
-        size_t vertexBufferMinAllocSize                         = 256ull;        /* 256B*/
-        size_t indexBufferMinAllocSize                          = 512ull;        /* 512B*/
-        size_t perInstanceVertexBufferMinAllocSize              = 32ull;         /*  32B*/
+        size_t indexBuffSupportedSizeInBytes                 = 2147483648ull;      /*  2GB*/
+        size_t vertexBuffSupportedSizeInBytes                = 2147483648ull;      /*  2GB*/
+        size_t perInstanceVertexBuffSupportedSizeInBytes     = 33554432ull;        /* 32MB*/
+        size_t MDIDataBuffSupportedSizeInBytes               = 2147483648ull;      /*  2GB*/
+        size_t vertexBufferMinAllocSize                      = 512ull;             /* 512B*/
+        size_t indexBufferMinAllocSize                       = 256ull;             /* 256B*/
+        size_t perInstanceVertexBufferMinAllocSize           = 32ull;              /*  32B*/
+        size_t MDIDataBuffMinAllocSize                       = 32ull;              /*  32B*/
     };
 
     template <typename MeshBufferType>
@@ -37,37 +28,58 @@ public:
 
     struct PackedMeshBufferData
     {
-        const size_t mdiParameterByteOffset; // add to `CCPUMeshPacker::getMultiDrawIndirectBuffer()->getPointer() to get `DrawElementsIndirectCommand_t` address
-        const uint32_t mdiParameterCount;
+        uint32_t mdiParameterByteOffset; // add to `CCPUMeshPacker::getMultiDrawIndirectBuffer()->getPointer() to get `DrawElementsIndirectCommand_t` address
+        uint32_t mdiParameterCount;
+
+        inline bool isValid() 
+        { 
+            return this->mdiParameterByteOffset != ~uint32_t(0) &&
+                   this->mdiParameterCount != ~uint32_t(0);
+        }
     };
+
+    
+}
+
+struct DrawElementsIndirectCommand_t
+{
+    uint32_t count;
+    uint32_t instanceCount;
+    uint32_t firstIndex;
+    uint32_t baseVertex;
+    uint32_t baseInstance;
+};
+
+using namespace meshPackerUtil;
+
+template <typename MeshBufferType, typename MDIStructType>
+class IMeshPacker
+{
+    static_assert(std::is_base_of<DrawElementsIndirectCommand_t, MDIStructType>::value);
 
 public:
 
-    IMeshPacker(const SVertexInputParams& preDefinedLayout, const AllocationParams& allocParams,uint16_t maxIndexCountPerMDIData = std::numeric_limits<uint16_t>::max())
-        :m_maxIndexCountPerMDIData(maxIndexCountPerMDIData), m_drawCount(0)
+    IMeshPacker(const SVertexInputParams& preDefinedLayout, const AllocationParams& allocParams, uint16_t maxIndexCountPerMDIData = std::numeric_limits<uint16_t>::max())
+        :m_maxIndexCountPerMDIData(maxIndexCountPerMDIData), /*TODO: delete when all alocators are done: */m_alctrReservedSpace{nullptr, nullptr, nullptr, nullptr}
     {
         m_outVtxInputParams.enabledAttribFlags  = preDefinedLayout.enabledAttribFlags;
         m_outVtxInputParams.enabledBindingFlags = preDefinedLayout.enabledBindingFlags;
 
         memcpy(m_outVtxInputParams.attributes, preDefinedLayout.attributes, sizeof(m_outVtxInputParams.attributes));
 
-        m_outMDIData = core::make_smart_refctd_ptr<ICPUBuffer>(allocParams.MDIDataSizeInBytes);
-
         //TODO: init allocators 
         //m_alctrReservedSpace[0] = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator::reserved_size(/**/), _IRR_SIMD_ALIGNMENT);
         //m_vtxBuffAlctr = core::GeneralpurposeAddressAllocator(m_alctrReservedSpace[0], 0u, 0u, 0u, allocParams.vertexBuffSupportedSizeInBytes / allocParams.vertexbufferMinAllocSize, allocParams.vertexbufferMinAllocSize);
-
-        //m_alctrReservedSpace[1] = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator::reserved_size(/**/), _IRR_SIMD_ALIGNMENT);
-        //m_idxBuffAlctr = core::GeneralpurposeAddressAllocator(m_alctrReservedSpace[1], 0u, 0u, 0u, allocParams.indexBuffSupportedSizeInBytes / allocParams.indexBufferMinAllocSize, allocParams.indexBufferMinAllocSize);
+        
+        m_alctrReservedSpace[1] = static_cast<uint8_t*>(_IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<uint32_t>::reserved_size(alignof(uint16_t), allocParams.indexBuffSupportedSizeInBytes / sizeof(uint16_t), allocParams.indexBufferMinAllocSize), _IRR_SIMD_ALIGNMENT));
+        m_idxBuffAlctr = core::GeneralpurposeAddressAllocator<uint32_t>(m_alctrReservedSpace[1], 0u, 0u, alignof(uint16_t), allocParams.indexBuffSupportedSizeInBytes / sizeof(uint16_t), allocParams.indexBufferMinAllocSize);
 
         //m_alctrReservedSpace[2] = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator::reserved_size(/**/), _IRR_SIMD_ALIGNMENT);
         //m_perInsVtxBuffAlctr = core::GeneralpurposeAddressAllocator(m_alctrReservedSpace[2], 0u, 0u, 0u, allocParams.perInstanceVertexBuffSupportedSizeInBytes / allocParams.perInstanceVertexbufferMinAllocSize, allocParams.perInstanceVertexbufferMinAllocSize);
-        
-
 
         //init MDI data allocator
-        m_alctrReservedSpace[3] = static_cast<uint8_t*>(_IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<size_t>::reserved_size(alignof(uint32_t), allocParams.MDIDataSizeInBytes / sizeof(DrawElementsIndirectCommand_t), 1u), _IRR_SIMD_ALIGNMENT));
-        m_MDIDataAlctr = core::GeneralpurposeAddressAllocator<size_t>(m_alctrReservedSpace[3], 0u, 0u, alignof(uint32_t), allocParams.MDIDataSizeInBytes / sizeof(DrawElementsIndirectCommand_t), /*i git*/1u);
+        m_alctrReservedSpace[3] = static_cast<uint8_t*>(_IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<uint32_t>::reserved_size(alignof(uint32_t), allocParams.MDIDataBuffSupportedSizeInBytes / sizeof(MDIStructType), allocParams.MDIDataBuffMinAllocSize), _IRR_SIMD_ALIGNMENT));
+        m_MDIDataAlctr = core::GeneralpurposeAddressAllocator<uint32_t>(m_alctrReservedSpace[3], 0u, 0u, alignof(uint32_t), allocParams.MDIDataBuffSupportedSizeInBytes / sizeof(MDIStructType), allocParams.MDIDataBuffMinAllocSize);
 
         //1 attrib enabled == 1 binding
         for (uint16_t attrBit = 0x0001, location = 0; location < 16; attrBit <<= 1, location++)
@@ -80,10 +92,8 @@ public:
         }
     }
 
-    virtual std::optional<PackedMeshBufferData> alloc(const MeshBufferType* meshBuffer) = 0;
-    virtual PackedMeshBuffer<MeshBufferType> commit(const core::vector<std::pair<const ICPUMeshBuffer*, PackedMeshBufferData>>& meshBuffers) = 0;
-
-    asset::ICPUBuffer* getMultiDrawIndirectBuffer() { return m_outMDIData.get(); }
+    virtual PackedMeshBufferData alloc(const MeshBufferType const* meshBuffer) = 0;
+    virtual void commit(/*TODO*/) = 0;
 
 protected:
     virtual ~IMeshPacker() 
@@ -97,17 +107,14 @@ protected:
     SVertexInputParams m_outVtxInputParams;
 
     uint8_t* m_alctrReservedSpace[4];
-    core::GeneralpurposeAddressAllocator<size_t> m_vtxBuffAlctr;
-    core::GeneralpurposeAddressAllocator<size_t> m_idxBuffAlctr;
-    core::GeneralpurposeAddressAllocator<size_t> m_perInsVtxBuffAlctr;
-    core::GeneralpurposeAddressAllocator<size_t> m_MDIDataAlctr;
-
-    core::smart_refctd_ptr<asset::ICPUBuffer> m_outMDIData;
-
-    size_t m_drawCount;
-
+    core::GeneralpurposeAddressAllocator<uint32_t> m_vtxBuffAlctr;
+    core::GeneralpurposeAddressAllocator<uint32_t> m_idxBuffAlctr;
+    core::GeneralpurposeAddressAllocator<uint32_t> m_perInsVtxBuffAlctr;
+    core::GeneralpurposeAddressAllocator<uint32_t> m_MDIDataAlctr;
 
     const uint16_t m_maxIndexCountPerMDIData;
+    
+    _IRR_STATIC_INLINE_CONSTEXPR PackedMeshBufferData invalidPackedMeshBufferData{ ~uint32_t(0), ~uint32_t(0) };
 };
 
 }
--- src/irr/asset/CCPUMeshPacker.h
+++ src/irr/asset/CCPUMeshPacker.h
@@ -9,16 +9,103 @@ namespace irr
 namespace asset
 {
 
-class CCPUMeshPacker final : public IMeshPacker<ICPUMeshBuffer>
+using namespace meshPackerUtil;
+
+template <typename MDIStructType = DrawElementsIndirectCommand_t>
+class CCPUMeshPacker final : public IMeshPacker<ICPUMeshBuffer, MDIStructType>
 {
 public:
 	CCPUMeshPacker(const SVertexInputParams& preDefinedLayout, const AllocationParams& allocParams, uint16_t maxIndexCountPerMDIData = std::numeric_limits<uint16_t>::max())
-		:IMeshPacker<ICPUMeshBuffer>(preDefinedLayout, allocParams, maxIndexCountPerMDIData) {}
+		:IMeshPacker<ICPUMeshBuffer, MDIStructType>(preDefinedLayout, allocParams, maxIndexCountPerMDIData) 
+	{
+		m_outMDIData = core::make_smart_refctd_ptr<ICPUBuffer>(allocParams.MDIDataBuffSupportedSizeInBytes);
+		m_outIdxBuffer = core::make_smart_refctd_ptr<ICPUBuffer>(allocParams.indexBuffSupportedSizeInBytes);
+	}
 
-	virtual std::optional<CCPUMeshPacker::PackedMeshBufferData> alloc(const ICPUMeshBuffer* meshBuffer) override;
-	virtual PackedMeshBuffer<ICPUMeshBuffer> commit(const core::vector<std::pair<const ICPUMeshBuffer*, PackedMeshBufferData>>& meshBuffers) override;
+	virtual PackedMeshBufferData alloc(const ICPUMeshBuffer const* meshBuffer) override;
+	virtual void commit() override;
 
+	ICPUBuffer* getMultiDrawIndirectBuffer() { return m_outMDIData.get(); }
+
+private: 
+	core::smart_refctd_ptr<ICPUBuffer> m_outMDIData;
+	core::smart_refctd_ptr<ICPUBuffer> m_outIdxBuffer;
 };
+
+template <typename MDIStructType>
+PackedMeshBufferData CCPUMeshPacker<MDIStructType>::alloc(const ICPUMeshBuffer const* meshBuffer)
+{
+	if (meshBuffer == nullptr)
+		return invalidPackedMeshBufferData;
+
+	/*
+	Requirements for input mesh buffers:
+		- attributes bound to the same binding must have identical format
+		- all meshbufers have indexed triangle list (temporary)
+	*/
+
+	//validation
+	//TODO: remove this condition
+	{
+		auto* pipeline = meshBuffer->getPipeline();
+
+		auto a = meshBuffer->getIndexBufferBinding().buffer;
+
+		if (meshBuffer->getIndexBufferBinding().buffer.get() == nullptr ||
+			pipeline->getPrimitiveAssemblyParams().primitiveType != EPT_TRIANGLE_LIST)
+		{
+			_IRR_DEBUG_BREAK_IF(true);
+			return invalidPackedMeshBufferData;
+		}
+	}
+
+	//validation
+	const auto& mbVtxInputParams = meshBuffer->getPipeline()->getVertexInputParams();
+	for (uint16_t attrBit = 0x0001, location = 0; location < 16; attrBit <<= 1, location++)
+	{
+		if (!(attrBit & mbVtxInputParams.enabledAttribFlags))
+			continue;
+
+		//assert((attrBit & m_outVtxInputParams.enabledAttribFlags));
+
+		if (mbVtxInputParams.attributes[location].format != m_outVtxInputParams.attributes[location].format ||
+			mbVtxInputParams.bindings[mbVtxInputParams.attributes[location].binding].inputRate != m_outVtxInputParams.bindings[location].inputRate)
+		{
+			_IRR_DEBUG_BREAK_IF(true);
+			return invalidPackedMeshBufferData;
+		}
+	}
+
+	auto MDIAllocAddr = m_MDIDataAlctr.alloc_addr(1u, 1u);
+	if (MDIAllocAddr == m_MDIDataAlctr.invalid_address)
+		return invalidPackedMeshBufferData;
+
+	auto idxAllocAddr = m_idxBuffAlctr.alloc_addr(meshBuffer->getIndexCount(), 1u);
+	if (idxAllocAddr == m_idxBuffAlctr.invalid_address)
+		return invalidPackedMeshBufferData;
+
+	//TODO: divide into multiple mdi structs if(idxCnt > m_maxIndexCountPerMDIData)
+	MDIStructType* mdiBuffPtr = static_cast<MDIStructType*>(m_outMDIData.get()->getPointer());
+	*(mdiBuffPtr + MDIAllocAddr) =
+	{
+		static_cast<uint32_t>(meshBuffer->getIndexCount()),
+		static_cast<uint32_t>(meshBuffer->getInstanceCount()),
+		idxAllocAddr * sizeof(uint16_t),
+		0,
+		static_cast<uint32_t>(meshBuffer->getBaseInstance())
+	};
+
+
+
+	PackedMeshBufferData result{ MDIAllocAddr * sizeof(MDIStructType), 1u };
+	return result;
+}
+
+template <typename MDIStructType>
+void CCPUMeshPacker<MDIStructType>::commit()
+{
+	return;
+}
 
 }
 }
