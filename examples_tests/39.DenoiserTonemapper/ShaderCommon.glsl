b23d7c50874b0a57a87fbd0dd57cb2777e077bba
devsh <devsh.graphicsprogramming@gmail.com>
2020-07-15 20:28:36 +0200

    add double exposure measurement, because denoising changes the median luma value

--- examples_tests/39.DenoiserTonemapper/ShaderCommon.glsl
+++ examples_tests/39.DenoiserTonemapper/ShaderCommon.glsl
@@ -41,8 +41,6 @@ shared uint repackBuffer[_IRR_GLSL_SCRATCH_SHARED_SIZE_DEFINED_];
 // won't be using an image as input (we'll provide the colors ourselves)
 #define _IRR_GLSL_EXT_LUMA_METER_INPUT_IMAGE_DESCRIPTOR_DEFINED_
 #define _IRR_GLSL_EXT_LUMA_METER_OUTPUT_BINDING_DEFINED_ 2
-#define _IRR_GLSL_EXT_LUMA_METER_CLEAR_FIRST_PASS_OUTPUT_FUNC_DEFINED_
-#define _IRR_GLSL_EXT_LUMA_METER_GET_NEXT_LUMA_OUTPUT_OFFSET_FUNC_DECLARED_
 #define _IRR_GLSL_EXT_LUMA_METER_GET_NEXT_LUMA_OUTPUT_OFFSET_FUNC_DEFINED_
 #define _IRR_GLSL_EXT_LUMA_METER_GET_CURRENT_LUMA_OUTPUT_OFFSET_FUNC_DEFINED_
 
@@ -52,15 +50,15 @@ shared uint repackBuffer[_IRR_GLSL_SCRATCH_SHARED_SIZE_DEFINED_];
 #ifdef _IRR_GLSL_EXT_LUMA_METER_FIRST_PASS_DEFINED_
 	#include "irr/builtin/glsl/ext/LumaMeter/impl.glsl"
 
-	// we will clear buffer to 0 with command buffer
-	void irr_glsl_ext_LumaMeter_clearFirstPassOutput()
+	// need to override the offset and color provision functions
+	int irr_glsl_ext_LumaMeter_getNextLumaOutputOffset()
 	{
+		return pc.data.beforeDenoise!=0u ? 1:0;
 	}
 
-	// need to override the offset and color provision functions
 	int irr_glsl_ext_LumaMeter_getCurrentLumaOutputOffset()
 	{
-		return 0;
+		return pc.data.beforeDenoise!=0u ? 0:1;
 	}
 
 	vec3 globalPixelData;
