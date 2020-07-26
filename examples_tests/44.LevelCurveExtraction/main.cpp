2b8acc74b453b049878b35d06ce37a67dcce4b6f
Hazard (Cyprian Skrzypczak) <42750285+Hazardu@users.noreply.github.com>
2020-07-26 15:07:14 +0200

    DescriptorSetLayout is null when compiling the shader

--- examples_tests/44.LevelCurveExtraction/main.cpp
+++ examples_tests/44.LevelCurveExtraction/main.cpp
@@ -53,10 +53,10 @@ int main()
     qnc->loadNormalQuantCacheFromFile<asset::CQuantNormalCache::E_CACHE_TYPE::ECT_2_10_10_10>(fs,"../../tmp/normalCache101010.sse", true);
 
     // register the zip
-    device->getFileSystem()->addFileArchive("../../media/sphere.zip");
+    device->getFileSystem()->addFileArchive("../../media/sponza.zip");
 
     asset::IAssetLoader::SAssetLoadParams lp;
-    auto meshes_bundle = am->getAsset("sphere.obj", lp);
+    auto meshes_bundle = am->getAsset("sponza.obj", lp);
     assert(!meshes_bundle.isEmpty());
     auto mesh = meshes_bundle.getContents().begin()[0];
     auto mesh_raw = static_cast<asset::ICPUMesh*>(mesh.get());
@@ -66,29 +66,80 @@ int main()
     
     //copy the pipeline
     auto pipeline_cp = core::smart_refctd_ptr_static_cast<asset::ICPURenderpassIndependentPipeline>(mesh_raw->getMeshBuffer(0u)->getPipeline()->clone());
+    auto geometryShaderCode = R"===(
+#version 440 core
+struct DrawIndirectArrays_t
+{
+	uint  count;
+	uint  instanceCount;
+	uint  first;
+	uint  baseInstance;
+};
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+
+layout(set=0, binding=0) coherent buffer LineCount
+{
+  DrawIndirectArrays_t lineDraw;
+};
+layout(set=0, binding=1) writeonly buffer Lines
+{
+  float linePoints[]; // 6 floats decribe a line, 3d start, 3d end
+};
+
+void main() {    
+    uint i;
+    vec3 end = vec3(0,0,0);
+    vec3 start = vec3(gl_in[0].gl_Position);
+    for(i = 0;i < gl_in.length();i++)
+    {
+        gl_Position = gl_in[i].gl_Position;
+        end += vec3(gl_in[i].gl_Position);
+        EmitVertex();
+    }
+    EndPrimitive();
+
+    end /= gl_in.length();
+    uint outId = atomicAdd(lineDraw.count,1u);
+    outId *= 6u;
+    linePoints[outId+0u] = start.x;
+    linePoints[outId+1u] = start.y;
+    linePoints[outId+2u] = start.z;
+    linePoints[outId+3u] = end.x;
+    linePoints[outId+4u] = end.y;
+    linePoints[outId+5u] = end.z;
+} 
+)===";
 
     //get the simple geometry shader data and turn it into ICPUSpecializedShader
-    auto shaderData = device->getAssetManager()->getFileSystem()->loadBuiltinData("irr/builtin/shaders/testGeomShader.geom");
+    auto len = strlen(geometryShaderCode);
+    auto shaderData = core::make_smart_refctd_ptr<asset::ICPUBuffer>(len);
+    memcpy(shaderData->getPointer(), geometryShaderCode, len);
     auto unspecializedShader = core::make_smart_refctd_ptr<asset::ICPUShader>(std::move(shaderData), asset::ICPUShader::buffer_contains_glsl);
     auto shader = core::make_smart_refctd_ptr<asset::ICPUSpecializedShader>(std::move(unspecializedShader), asset::ISpecializedShader::SInfo({}, nullptr, "main", asset::ISpecializedShader::ESS_GEOMETRY));
     pipeline_cp->setShaderAtIndex(asset::ICPURenderpassIndependentPipeline::ESSI_GEOMETRY_SHADER_IX, shader.get());
 
-    //replace the ICPURenderpassIndependentPipeline with a copy that uses the replaced geometry shader
-    mesh_raw->getMeshBuffer(0u)->setPipeline(std::move(pipeline_cp));
+    mesh_raw->getMeshBuffer(0)->setPipeline(std::move(pipeline_cp));
+
 
     //create buffers for the geometry shader
-    auto lineCountBuffer = driver->createDeviceLocalGPUBufferOnDedMem(sizeof(uint32_t));
+    auto lineCountBuffer = driver->createDeviceLocalGPUBufferOnDedMem(roundUp(sizeof(irr::asset::DrawArraysIndirectCommand_t),16ull));
     uint32_t triangleCount;
     if (!asset::IMeshManipulator::getPolyCount(triangleCount, mesh_raw))
-        throw;
+        assert(false);
 
     auto linesBuffer = driver->createDeviceLocalGPUBufferOnDedMem(triangleCount * 6 * sizeof(float));
+    
+    driver->fillBuffer(lineCountBuffer.get(), offsetof(irr::asset::DrawArraysIndirectCommand_t, instanceCount), sizeof(uint32_t), triangleCount * 6 * sizeof(uint32_t));
+    //not sure what first refers to
+    driver->fillBuffer(lineCountBuffer.get(), offsetof(irr::asset::DrawArraysIndirectCommand_t, first), sizeof(uint32_t), triangleCount * 6 * sizeof(uint32_t));        
+    //not sure what baseInstance does
+    driver->fillBuffer(lineCountBuffer.get(), offsetof(irr::asset::DrawArraysIndirectCommand_t, baseInstance), sizeof(uint32_t), triangleCount * 6 * sizeof(uint32_t)); 
 
     const asset::SBufferBinding<video::IGPUBuffer> bufferBinding = { 0u ,linesBuffer };
-
     //we can safely assume that all meshbuffers within mesh loaded from OBJ has same DS1 layout (used for camera-specific data)
     //so we can create just one DS
-    asset::ICPUDescriptorSetLayout* ds1layout = mesh_raw->getMeshBuffer(0u)->getPipeline()->getLayout()->getDescriptorSetLayout(1u); //set 1u ---> 0u ?
+    asset::ICPUDescriptorSetLayout* ds1layout = mesh_raw->getMeshBuffer(0u)->getPipeline()->getLayout()->getDescriptorSetLayout(0u); //set 1u ---> 0u ?
     uint32_t ds1UboBinding = 0u;
     for (const auto& bnd : ds1layout->getBindings())
         if (bnd.type==asset::EDT_UNIFORM_BUFFER)
@@ -125,7 +176,7 @@ int main()
         write.info = &info;
         driver->updateDescriptorSets(1u, &write, 0u, nullptr);
     }
-
+    //DescriptorSetLayout is null
     auto gpumesh = driver->getGPUObjectsFromAssets(&mesh_raw, &mesh_raw+1)->front();
 
 	//! we want to move around the scene and view it from different angles
@@ -145,6 +196,7 @@ int main()
         
         // zero out buffer LineCount
         driver->fillBuffer(lineCountBuffer.get(),0,sizeof(uint32_t),0);
+        driver->fillBuffer(lineCountBuffer.get(),offsetof(irr::asset::DrawArraysIndirectCommand_t,count), roundUp(sizeof(uint32_t), 16ull),0u);
 
         //! This animates (moves) the camera and sets the transforms
 		camera->OnAnimate(std::chrono::duration_cast<std::chrono::milliseconds>(device->getTimer()->getTime()).count());
@@ -208,7 +260,13 @@ int main()
 
         
         //invoke driver->drawIndirect() and use linesBuffer
-        driver->drawArraysIndirect(gpumesh->getMeshBuffer(0)->getVertexBufferBindings(), asset::EPT_LINE_LIST, linesBuffer.get(), 0u, triangleCount * 6 * sizeof(uint32_t), 6 * sizeof(uint32_t));
+        /*  _vtxBindings[IGPUMeshBuffer::MAX_ATTR_BUF_BINDING_COUNT],           &bufferBinding that has linesBuffer ptr, what do the other 16 do
+            asset::E_PRIMITIVE_TOPOLOGY mode,                                   asset::EPT_LINE_LIST
+            const IGPUBuffer* indirectDrawBuff,                                 not sure about this one, since linesBuffer is already used in bufferBinding
+            size_t offset, size_t maxCount, size_t stride,                      theres no overload that takes DrawArraysIndirectCommand_t
+            const IGPUBuffer* countBuffer = nullptr, size_t countOffset = 0u    dont need to use these
+            */
+        driver->drawArraysIndirect(&bufferBinding, asset::EPT_LINE_LIST, linesBuffer.get(), 0u, triangleCount * 6 * sizeof(uint32_t), 6 * sizeof(uint32_t));
             
         //driver->drawArraysIndirect( asset::SBufferBinding<video::IGPUBuffer>(linesBuffer),);
 		driver->endScene();
