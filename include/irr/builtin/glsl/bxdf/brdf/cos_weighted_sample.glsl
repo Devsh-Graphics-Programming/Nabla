c30acdeabe311a181118e816f9a27db13c0e4662
criss <crisserpl2@gmail.com>
2020-08-13 18:26:17 +0200

    Sampling validation and remainder*pdf test

--- include/irr/builtin/glsl/bxdf/brdf/cos_weighted_sample.glsl
+++ include/irr/builtin/glsl/bxdf/brdf/cos_weighted_sample.glsl
@@ -16,15 +16,15 @@ irr_glsl_BSDFSample irr_glsl_cos_weighted_cos_generate(in irr_glsl_AnisotropicVi
     smpl.L = L;
 	smpl.NdotL = z;
     smpl.TdotL = p.x;
-    smpl.BdtoL = p.y;
+    smpl.BdotL = p.y;
 
     return smpl;
 }
 
-float irr_glsl_cos_weighted_cos_remainder_and_pdf(out float pdf, in irr_glsl_BSDFSample s, in irr_glsl_IsotropicViewSurfaceInteraction interaction)
+vec3 irr_glsl_cos_weighted_cos_remainder_and_pdf(out float pdf, in irr_glsl_BSDFSample s, in irr_glsl_IsotropicViewSurfaceInteraction interaction)
 {
 	pdf = s.NdotL*irr_glsl_RECIPROCAL_PI;
-	return 1.0;
+	return vec3(1.0);
 }
 
 #endif
