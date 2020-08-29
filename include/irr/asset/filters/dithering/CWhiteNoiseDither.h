c3e928892ba3d9c91e96773da917521b260d89bf
AnastaZIuk <areklachowicz@gmail.com>
2020-08-29 16:31:02 +0200

    TODO: perform white noise in CWhiteNoiseDither and change blit filter API to new one

--- include/irr/asset/filters/dithering/CWhiteNoiseDither.h
+++ include/irr/asset/filters/dithering/CWhiteNoiseDither.h
@@ -1,8 +1,6 @@
 // This file is part of the "IrrlichtBAW" engine.
 // For conditions of distribution and use, see copyright notice in irrlicht.h
 
-#if 0 // TODO
-
 #ifndef __IRR_C_WHITE_NOISE_DITHER_H_INCLUDED__
 #define __IRR_C_WHITE_NOISE_DITHER_H_INCLUDED__
 
@@ -21,20 +19,46 @@ namespace irr
 				class CState
 				{
 					public:
-						CState() {}
+						CState()
+							: sampler(std::chrono::high_resolution_clock::now().time_since_epoch().count())
+						{
+						
+						}
 						virtual ~CState() {}
+
+						core::RandomSampler sampler;
 				};
 
 				using state_type = CState;
 
 				static float get(const state_type* state, const core::vectorSIMDu32& pixelCoord, const int32_t& channel)
 				{
-					// TODO: to define in future
+					/*
+
+						TODO - handle it somehow
+
+					auto storeToTexel = [nonPremultBlendSemantic, alphaChannel, &sampler, outFormat](value_type* const sample, void* const dstPix) -> void
+					{
+						if (nonPremultBlendSemantic && sample[alphaChannel] > FLT_MIN * 1024.0 * 512.0)
+						{
+							for (auto i = 0; i < MaxChannels; i++)
+								if (i != alphaChannel)
+									sample[i] /= sample[alphaChannel];
+						}
+						for (auto i = 0; i < MaxChannels; i++)
+						{
+							//sample[i] = core::clamp<value_type,value_type>(sample[i],0.0,1.0);
+							// @Crisspl replace this with epic quantization (actually it would be good if you cached the max and min values for the 4 channels outside the hot loop
+							sample[i] += double(sampler.nextSample()) * (asset::getFormatPrecision<value_type>(outFormat, i, sample[i]) / double(~0u));
+							sample[i] = core::clamp<value_type, value_type>(sample[i], asset::getFormatMinValue<value_type>(outFormat, i), asset::getFormatMaxValue<value_type>(outFormat, i));
+						}
+						asset::encodePixels<value_type>(outFormat, dstPix, sample);
+					};
+
+					*/
 				}
 		};
 	}
 }
 
 #endif // __IRR_C_WHITE_NOISE_DITHER_H_INCLUDED__
-
-#endif // TODO
