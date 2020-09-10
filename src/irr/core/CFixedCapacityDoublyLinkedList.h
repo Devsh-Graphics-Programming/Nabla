9eaf0c3108450eb44425ef1f00dd78729bce14f9
Hazard (Cyprian Skrzypczak) <42750285+Hazardu@users.noreply.github.com>
2020-09-10 21:27:57 +0200

    Update CFixedCapacityDoublyLinkedList.h

--- src/irr/core/CFixedCapacityDoublyLinkedList.h
+++ src/irr/core/CFixedCapacityDoublyLinkedList.h
@@ -5,18 +5,23 @@
 
 namespace irr {
 	namespace core {
-
 		template<typename Value>
 		struct Snode
 		{
+			_IRR_STATIC_INLINE_CONSTEXPR uint32_t invalid_iterator = 0xdeadbeefu;
+
 			Value data;
 			uint32_t prev;
 			uint32_t next;
 
 			Snode(const Value& val) : data(val)
-			{ }
+			{
+				prev = invalid_iterator;
+				next = invalid_iterator;
+			}
 			Snode(Snode<Value>&& other) : data(std::move(other.data)), prev(std::move(other.prev)), next(std::move(other.next))
-			{ }
+			{
+			}
 
 			Snode<Value>& operator=(const Snode<Value>& other)
 			{
@@ -37,18 +42,16 @@ namespace irr {
 
 		template<typename Value>
 		class DoublyLinkedList
-		{
-			typedef uint32_t node_address;
-			
-		private:
+		{		
+			_IRR_STATIC_INLINE_CONSTEXPR uint32_t invalid_iterator = 0xdeadbeefu;
+
 			void* reservedSpace;
 			PoolAddressAllocator<uint32_t> alloc;
-			node_address m_back;
-			node_address m_begin;
+			uint32_t m_back;
+			uint32_t m_begin;
 			Snode<Value>* m_array;
 			uint32_t cap;
 
-			#define invalid_iterator alloc.invalid_address	//0xdeadbeef
 
 		public:
 
@@ -56,30 +59,29 @@ namespace irr {
 			{
 				if(m_back->prev != invalid_iterator)
 					m_back->prev->next = invalid_iterator;
-				uint32_t temp = m_end;
-				m_end = m_back->prev;
+				uint32_t temp = m_back;
+				m_back = m_back->prev;
 				alloc.free_addr(temp, 1u);
 			}
 
-			inline void pushFront(Value &val) 
+			inline void pushFront(Value &&val) 
 			{
 				uint32_t addr = alloc.alloc_addr(1u, 1u);
-				Snode<Value>* n = (reinterpret_cast<Snode<Value>*>(reservedSpace) + addr);
+				Snode<Value>* n = m_array[addr];
 				n->prev = invalid_iterator;
-				n->data = val;
+				n->data = std::move(val);
 				n->next = m_begin;
 
-				m_array[addr] = *n;
 				if (m_begin != invalid_iterator)
-					m_begin->prev = n;
-				m_begin = n;
+					m_begin->prev = addr;
+				m_begin = addr;
 			}
 
-			inline node_address getBegin() { return m_begin; }
+			inline Snode<Value>* getBegin() { return m_array[m_begin]; }
 
-			inline node_address getBack() { return m_back; }
+			inline Snode<Value>* getBack() { return m_array[m_back]; }
 
-			inline void erase(uint32_t& nodeAddr)
+			inline void erase(uint32_t nodeAddr)
 			{
 				if(nodeAddr->prev != invalid_iterator)
 				nodeAddr->prev->next = nodeAddr->next;
@@ -89,33 +91,32 @@ namespace irr {
 				alloc.free_addr(nodeAddr, 1u);
 			}
 
-			inline void moveToFront(uint32_t& node)
+			inline void moveToFront(uint32_t nodeAddr)
 			{
-				if (m_begin == node) return;
-				m_begin->prev = node;
-				if (node->next != invalid_iterator)
-					node->next->prev = node->prev;
-				if (node->prev != invalid_iterator)
-					node->next->prev = node->prev;
-				node->next = m_begin;
-				node->prev = invalid_iterator;
-				m_begin = node;
+				if (m_begin == nodeAddr) return;
+				m_begin->prev = nodeAddr;
+				if (nodeAddr->next != invalid_iterator)
+					nodeAddr->next->prev = nodeAddr->prev;
+				if (nodeAddr->prev != invalid_iterator)
+					nodeAddr->next->prev = nodeAddr->prev;
+				nodeAddr->next = m_begin;
+				nodeAddr->prev = invalid_iterator;
+				m_begin = nodeAddr;
 			}
 
 			DoublyLinkedList(const uint32_t& capacity) :
-				alloc(reservedSpace, 0u, 0u, 1u, capacity, 1u),
-				reservedSpace(_IRR_ALIGNED_MALLOC(PoolAddressAllocator<uint32_t>::reserved_size(1u, capacity, 1u), alignof(void*))),
 				cap(capacity)
+				reservedSpace(_IRR_ALIGNED_MALLOC(PoolAddressAllocator<uint32_t>::reserved_size(1u, capacity, 1u), alignof(void*))),
+				alloc(reservedSpace, 0u, 0u, 1u, capacity, 1u),
 			{
 				m_back = invalid_iterator;
 				m_begin = invalid_iterator;
-				m_array = new (reservedSpace) Snode<Value>;
+				m_array = new Snode<Value>[capacity];
 			}
 			~DoublyLinkedList()
 			{
 				_IRR_ALIGNED_FREE(reservedSpace);
 			}
-			#undef invalid_iterator
 		};
 
 
