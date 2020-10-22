18b8980a51d627b589542e76a5ca8f131db2ffc8
Przemog1 <minikers21@gmail.com>
2020-10-22 08:22:30 +0200

    Deleted old example code

--- examples_tests/10.AllocatorTest/main.cpp
+++ examples_tests/10.AllocatorTest/main.cpp
@@ -106,7 +106,7 @@ private:
 		else
 		{
 			const auto reservedSize = AlctrType::reserved_size(randAllocParams.maxAlign, randAllocParams.addressSpaceSize, randAllocParams.blockSz);
-			reservedSpace = _IRR_ALIGNED_MALLOC(reservedSize, _IRR_SIMD_ALIGNMENT);
+			reservedSpace = _NBL_ALIGNED_MALLOC(reservedSize, _NBL_SIMD_ALIGNMENT);
 			alctr = AlctrType(reservedSpace, randAllocParams.offset, randAllocParams.alignOffset, randAllocParams.maxAlign, randAllocParams.addressSpaceSize, randAllocParams.blockSz);
 		}
 
@@ -171,7 +171,7 @@ private:
 		}
 		 
 		if constexpr (!std::is_same<AlctrType, core::LinearAddressAllocator<uint32_t>>::value)
-			_IRR_ALIGNED_FREE(reservedSpace);
+			_NBL_ALIGNED_FREE(reservedSpace);
 
 	}
 
@@ -276,55 +276,6 @@ int main()
 	}
 }
 
-#endif
-
-#ifdef ALLOCATOR_TEST_OLD
-
-/*
-	Problems with GeneralPurposeallocator:
-		1. GeneralPurposeallocator is not able to allocate as much address space as it is expected to
-		2. Program doesn't compile, when I try to call safe_shrink_size
-		3. "Assertion failed: false, file *\include\irr\core\alloc\GeneralpurposeAddressAllocator.h, line 368"
-*/
-
-int main()
-{
-	//1
-	{
-		void* resSpc = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<uint32_t>::reserved_size(alignof(uint32_t), 16u, 1u), _IRR_SIMD_ALIGNMENT);
-		assert(resSpc != nullptr);
-		auto alctr = core::GeneralpurposeAddressAllocator<uint32_t>(resSpc, 0u, 0u, alignof(uint32_t), 16u, 1u);
-
-		for (int i = 0; i < 16u; i++)
-		{
-			auto addr = alctr.alloc_addr(1u, 1u);
-			if (addr == alctr.invalid_address)
-				os::Printer::print("invalid adress");
-			else
-				os::Printer::print(std::to_string(addr));
-		}
-		_IRR_ALIGNED_FREE(resSpc);
-	}
-
-	//2
-	{
-		void* resSpc = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<uint32_t>::reserved_size(alignof(uint32_t), 16u, 1u), _IRR_SIMD_ALIGNMENT);
-		auto alctr = core::GeneralpurposeAddressAllocator<uint32_t>(resSpc, 0u, 0u, alignof(uint32_t), 16u, 1u);
-		alctr.safe_shrink_size(8u);
-		_IRR_ALIGNED_FREE(resSpc);
-	}
-
-	//3
-	{
-		void* resSpc = _IRR_ALIGNED_MALLOC(core::GeneralpurposeAddressAllocator<uint32_t>::reserved_size(alignof(uint32_t), 1413, 32), _IRR_SIMD_ALIGNMENT);
-		auto alctr = core::GeneralpurposeAddressAllocator<uint32_t>(resSpc, 0u, 0u, alignof(uint32_t), 1413, 32);
-
-		auto a = alctr.alloc_addr(1401, 1);
-		_IRR_ALIGNED_FREE(resSpc);
-	}
-
-	return 0;
-}
 #endif
 
 #ifdef ADDRESS_ALLOCATOR_TRAITS_TEST
