4ea31bcf6db36d244b071c1c984d9392109d6d37
Przemog1 <minikers21@gmail.com>
2020-10-22 07:20:19 +0200

    Few corrections

--- include/irr/ext/MitsubaLoader/CElementIntegrator.h
+++ include/irr/ext/MitsubaLoader/CElementIntegrator.h
@@ -37,7 +37,7 @@ class CElementIntegrator : public IElement
 			ADJ_P_TRACER,
 			ADAPTIVE,
 			VPL,
-			NBL_CACHE,
+			IRR_CACHE,
 			MULTI_CHANNEL,
 			FIELD_EXTRACT
 		};
@@ -252,7 +252,7 @@ class CElementIntegrator : public IElement
 				case CElementIntegrator::Type::VPL:
 					vpl = other.vpl;
 					break;
-				case CElementIntegrator::Type::NBL_CACHE:
+				case CElementIntegrator::Type::IRR_CACHE:
 					irrcache = other.irrcache;
 					break;
 				case CElementIntegrator::Type::MULTI_CHANNEL:
@@ -279,7 +279,7 @@ class CElementIntegrator : public IElement
 
 			switch (type)
 			{
-				case Type::NBL_CACHE:
+				case Type::IRR_CACHE:
 					[[fallthrough]];
 				case Type::MULTI_CHANNEL:
 					if (_child->getType() != IElement::Type::INTEGRATOR)
@@ -290,7 +290,7 @@ class CElementIntegrator : public IElement
 			}
 			switch (type)
 			{
-				case Type::NBL_CACHE:
+				case Type::IRR_CACHE:
 					irrcache.children[0u] = static_cast<CElementIntegrator*>(_child);
 					irrcache.childCount = 1u;
 					return true;
