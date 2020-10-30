3607b28af14c53d5aaab4ffa1575f86514f6f326
devsh <devsh.graphicsprogramming@gmail.com>
2020-10-30 18:21:51 +0100

    fix bugs in bounds

--- include/irr/builtin/glsl/algorithm.glsl
+++ include/irr/builtin/glsl/algorithm.glsl
@@ -16,7 +16,6 @@ IRR_GLSL_CONCATENATE2(uint lower_bound_,ARRAY_NAME)(uint begin, in uint end, in 
 IRR_GLSL_CONCATENATE2(uint upper_bound_,ARRAY_NAME)(uint begin, in uint end, in TYPE value) {return IRR_GLSL_CONCATENATE4(upper_bound_,ARRAY_NAME,_,IRR_GLSL_LESS)(begin,end,value);}
 
 
-// could unroll 3 or more times
 #define IRR_GLSL_DEFINE_BOUND_COMP_IMPL(FUNC_NAME,ARRAY_NAME,TYPE,COMP) IRR_GLSL_CONCATENATE4(uint FUNC_NAME,ARRAY_NAME,_,COMP)(uint begin, in uint end, in TYPE value) \
 { \
 	const uint len = end-begin; \
@@ -28,19 +27,23 @@ IRR_GLSL_CONCATENATE2(uint upper_bound_,ARRAY_NAME)(uint begin, in uint end, in 
 		len = newLen; \
 	} \
 	while (len) \
-	{ \
-		begin += COMP(IRR_GLSL_EVAL(ARRAY_NAME)[len>>=1u],value) ? len:0u; \
-		begin += COMP(IRR_GLSL_EVAL(ARRAY_NAME)[len>>=1u],value) ? len:0u; \
-	}
+	{
 
 
+// could unroll 3 or more times
 #define IRR_GLSL_DEFINE_LOWER_BOUND_COMP(ARRAY_NAME,TYPE,COMP) IRR_GLSL_DEFINE_BOUND_COMP_IMPL(lower_bound_,ARRAY_NAME,TYPE,COMP) \
+		begin += COMP(IRR_GLSL_EVAL(ARRAY_NAME)[begin+(len>>=1u)],value) ? len:0u; \
+		begin += COMP(IRR_GLSL_EVAL(ARRAY_NAME)[begin+(len>>=1u)],value) ? len:0u; \
+	} \
 	return begin+(COMP(IRR_GLSL_EVAL(ARRAY_NAME)[begin],value) ? 1u:0u); \
 }
 
 #define IRR_GLSL_DEFINE_LOWER_BOUND(ARRAY_NAME,TYPE) IRR_GLSL_DEFINE_LOWER_BOUND_COMP(ARRAY_NAME,TYPE,IRR_GLSL_LESS)
 
 #define IRR_GLSL_DEFINE_UPPER_BOUND_COMP(ARRAY_NAME,TYPE,COMP) IRR_GLSL_DEFINE_BOUND_COMP_IMPL(upper_bound_,ARRAY_NAME,TYPE,COMP) \
+		begin += COMP(value,IRR_GLSL_EVAL(ARRAY_NAME)[begin+(len>>=1u)]) ? 0u:len; \
+		begin += COMP(value,IRR_GLSL_EVAL(ARRAY_NAME)[begin+(len>>=1u)]) ? 0u:len; \
+	} \
 	return begin+(COMP(value,IRR_GLSL_EVAL(ARRAY_NAME)[begin]) ? 0u:1u); \
 }
 
@@ -50,6 +53,11 @@ IRR_GLSL_CONCATENATE2(uint upper_bound_,ARRAY_NAME)(uint begin, in uint end, in 
 /**
 
 TODOs:
+Higher Priority:
+- https://moderngpu.github.io/sortedsearch.html
+We need to make a `irr_glsl_workgroupSort()` function as a utility to facilitate it.
+
+Low Priority:
 - https://moderngpu.github.io/bulkinsert.html
 - https://moderngpu.github.io/merge.html 
 
