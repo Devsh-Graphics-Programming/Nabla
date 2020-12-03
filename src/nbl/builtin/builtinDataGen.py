38fd50abd02d9ad9c8d2071238956d4e6ada0026
Przemog1 <minikers21@gmail.com>
2020-12-03 02:39:50 +0100

    Fixes

--- src/nbl/builtin/builtinDataGen.py
+++ src/nbl/builtin/builtinDataGen.py
@@ -24,9 +24,9 @@ else:
     outp = open(outputFilename,"w+")
   
     outp.write("#include \"nbl/builtin/builtinResources.h\"\n\n")
-    outp.write("using namespace irr;\n")
-    outp.write("using namespace irr::builtin;\n\n")
-    outp.write("namespace irr {\n")
+    outp.write("using namespace nbl;\n")
+    outp.write("using namespace nbl::builtin;\n\n")
+    outp.write("namespace nbl {\n")
     outp.write("\tnamespace builtin {\n\n")
   
     # writing binary  data of all files in a loop
--- src/nbl/builtin/builtinHeaderGen.py
+++ src/nbl/builtin/builtinHeaderGen.py
@@ -27,7 +27,7 @@ else:
     outp.write("#include <string>\n")
     outp.write("#include <unordered_map>\n")
     outp.write("#include <utility>\n#include \"nbl\\core\\string\\UniqueStringLiteralType.h\"\n#include \"nbl\\builtin\\common.h\"\n")
-    outp.write("namespace irr { \n\tnamespace builtin { \n")
+    outp.write("namespace nbl { \n\tnamespace builtin { \n")
 
     #Iterating through input list
     for x in resourcePaths:
