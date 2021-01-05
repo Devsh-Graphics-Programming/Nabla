5947dd00f4a362c97840f102e9a2027636bf1687
devsh <devsh.graphicsprogramming@gmail.com>
2021-01-05 21:41:13 +0100

    @Crisspl we still dont survive roundtrip in RGB19E7

--- examples_tests/22.RaytracedAO/raytraceCommon.glsl
+++ examples_tests/22.RaytracedAO/raytraceCommon.glsl
@@ -142,15 +142,17 @@ layout(set = 1, binding = 5, std430, row_major) restrict readonly buffer LightRa
 
 #include <nbl/builtin/glsl/format/decode.glsl>
 #include <nbl/builtin/glsl/format/encode.glsl>
+//! @Crisspl we still dont survive roundtrip in RGB19E7
 vec3 fetchAccumulation(in ivec2 coord)
 {
     const uvec2 data = imageLoad(accumulation,coord).rg;
-	return nbl_glsl_decodeRGB19E7(data);
+	return vec4(unpackHalf2x16(data[0]),unpackHalf2x16(data[1])).rgb;
+	//return nbl_glsl_decodeRGB19E7(data);
 }
-
 void storeAccumulation(in vec3 color, in ivec2 coord)
 {
-	const uvec2 data = nbl_glsl_encodeRGB19E7(color);
+	//const uvec2 data = nbl_glsl_encodeRGB19E7(color);
+	const uvec2 data = uvec2(packHalf2x16(color.rg),packHalf2x16(vec2(color.b,1.0)));
 	imageStore(accumulation,coord,uvec4(data,0u,0u));
 }
 
