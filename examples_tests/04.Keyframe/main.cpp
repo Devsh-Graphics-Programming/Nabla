cde0a48edb9c482107dac5c113764766418a562b
devsh <devsh.graphicsprogramming@gmail.com>
2021-01-22 19:04:28 +0100

    refactor example 04 for friendlier perf

--- examples_tests/04.Keyframe/main.cpp
+++ examples_tests/04.Keyframe/main.cpp
@@ -89,6 +89,82 @@ int main()
 	auto* driver = device->getVideoDriver();
 	auto* smgr = device->getSceneManager();
 
+	//
+	core::smart_refctd_ptr<video::IGPUMeshBuffer> mb;
+    {
+        VertexStruct vertices[8];
+        vertices[0] = VertexStruct{{-1.f,-1.f,-1.f},{  0,  0}};
+        vertices[1] = VertexStruct{{ 1.f,-1.f,-1.f},{127,  0}};
+        vertices[2] = VertexStruct{{-1.f, 1.f,-1.f},{255,  0}};
+        vertices[3] = VertexStruct{{ 1.f, 1.f,-1.f},{  0,127}};
+        vertices[4] = VertexStruct{{-1.f,-1.f, 1.f},{127,127}};
+        vertices[5] = VertexStruct{{ 1.f,-1.f, 1.f},{255,127}};
+        vertices[6] = VertexStruct{{-1.f, 1.f, 1.f},{  0,255}};
+        vertices[7] = VertexStruct{{ 1.f, 1.f, 1.f},{127,255}};
+
+        uint16_t indices_indexed16[] =
+        {
+            0,1,2,1,2,3,
+            4,5,6,5,6,7,
+            0,1,4,1,4,5,
+            2,3,6,3,6,7,
+            0,2,4,2,4,6,
+            1,3,5,3,5,7
+        };
+			
+		asset::SPushConstantRange range[1] = {asset::ISpecializedShader::ESS_VERTEX,0u,sizeof(core::matrix4SIMD)};
+
+		auto createGPUSpecializedShaderFromSource = [=](const char* source, asset::ISpecializedShader::E_SHADER_STAGE stage)
+		{
+			auto spirv = device->getAssetManager()->getGLSLCompiler()->createSPIRVFromGLSL(source, stage, "main", "runtimeID");
+			auto unspec = driver->createGPUShader(std::move(spirv));
+			return driver->createGPUSpecializedShader(unspec.get(), { nullptr,nullptr,"main",stage });
+		};
+		// origFilepath is only relevant when you have filesystem #includes in your shader
+		auto createGPUSpecializedShaderFromSourceWithIncludes = [&](const char* source, asset::ISpecializedShader::E_SHADER_STAGE stage, const char* origFilepath)
+		{
+			auto resolved_includes = device->getAssetManager()->getGLSLCompiler()->resolveIncludeDirectives(source, stage, origFilepath);
+			return createGPUSpecializedShaderFromSource(reinterpret_cast<const char*>(resolved_includes->getSPVorGLSL()->getPointer()), stage);
+		};
+		core::smart_refctd_ptr<video::IGPUSpecializedShader> shaders[2] =
+		{
+			createGPUSpecializedShaderFromSourceWithIncludes(vertexSource,asset::ISpecializedShader::ESS_VERTEX, "shader.vert"),
+			createGPUSpecializedShaderFromSource(fragmentSource,asset::ISpecializedShader::ESS_FRAGMENT)
+		};
+		auto shadersPtr = reinterpret_cast<video::IGPUSpecializedShader**>(shaders);
+			
+		asset::SVertexInputParams inputParams;
+		inputParams.enabledAttribFlags = 0b11u;
+		inputParams.enabledBindingFlags = 0b1u;
+		inputParams.attributes[0].binding = 0u;
+		inputParams.attributes[0].format = asset::EF_R32G32B32_SFLOAT;
+		inputParams.attributes[0].relativeOffset = offsetof(VertexStruct,Pos[0]);
+		inputParams.attributes[1].binding = 0u;
+		inputParams.attributes[1].format = asset::EF_R8G8_UNORM;
+		inputParams.attributes[1].relativeOffset = offsetof(VertexStruct,Col[0]);
+		inputParams.bindings[0].stride = sizeof(VertexStruct);
+		inputParams.bindings[0].inputRate = asset::EVIR_PER_VERTEX;
+
+		asset::SBlendParams blendParams; // defaults are sane
+
+		asset::SPrimitiveAssemblyParams assemblyParams = {asset::EPT_TRIANGLE_LIST,false,1u};
+
+		asset::SStencilOpParams defaultStencil;
+		asset::SRasterizationParams rasterParams;
+		rasterParams.faceCullingMode = asset::EFCM_NONE;
+		auto pipeline = driver->createGPURenderpassIndependentPipeline(	nullptr,driver->createGPUPipelineLayout(range,range+1u,nullptr,nullptr,nullptr,nullptr),
+																		shadersPtr,shadersPtr+sizeof(shaders)/sizeof(core::smart_refctd_ptr<video::IGPUSpecializedShader>),
+																		inputParams,blendParams,assemblyParams,rasterParams);
+			
+		asset::SBufferBinding<video::IGPUBuffer> bindings[video::IGPUMeshBuffer::MAX_ATTR_BUF_BINDING_COUNT];
+		bindings[0u] = {0u,driver->createFilledDeviceLocalGPUBufferOnDedMem(sizeof(vertices),vertices)};
+		mb = core::make_smart_refctd_ptr<video::IGPUMeshBuffer>(std::move(pipeline),nullptr,bindings,asset::SBufferBinding<video::IGPUBuffer>{0u,driver->createFilledDeviceLocalGPUBufferOnDedMem(sizeof(indices_indexed16),indices_indexed16)});
+		{
+			mb->setIndexType(asset::EIT_16BIT);
+			mb->setIndexCount(2*3*6);
+		}
+	}
+
 
 	//! we want to move around the scene and view it from different angles
 	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0,100.0f,0.001f);
@@ -111,97 +187,9 @@ int main()
 
         //! Stress test for memleaks aside from demo how to create meshes that live on the GPU RAM
         {
-            VertexStruct vertices[8];
-            vertices[0] = VertexStruct{{-1.f,-1.f,-1.f},{  0,  0}};
-            vertices[1] = VertexStruct{{ 1.f,-1.f,-1.f},{127,  0}};
-            vertices[2] = VertexStruct{{-1.f, 1.f,-1.f},{255,  0}};
-            vertices[3] = VertexStruct{{ 1.f, 1.f,-1.f},{  0,127}};
-            vertices[4] = VertexStruct{{-1.f,-1.f, 1.f},{127,127}};
-            vertices[5] = VertexStruct{{ 1.f,-1.f, 1.f},{255,127}};
-            vertices[6] = VertexStruct{{-1.f, 1.f, 1.f},{  0,255}};
-            vertices[7] = VertexStruct{{ 1.f, 1.f, 1.f},{127,255}};
-
-            uint16_t indices_indexed16[] =
-            {
-                0,1,2,1,2,3,
-                4,5,6,5,6,7,
-                0,1,4,1,4,5,
-                2,3,6,3,6,7,
-                0,2,4,2,4,6,
-                1,3,5,3,5,7
-            };
-
-            auto upStreamBuff = driver->getDefaultUpStreamingBuffer();
-			core::smart_refctd_ptr<video::IGPUBuffer> upStreamRef(upStreamBuff->getBuffer());
-
-            const void* dataToPlace[2]  = {vertices,indices_indexed16};
-            uint32_t offsets[2]         = {video::StreamingTransientDataBufferMT<>::invalid_address,video::StreamingTransientDataBufferMT<>::invalid_address};
-            uint32_t alignments[2]      = {sizeof(decltype(vertices[0u])),sizeof(decltype(indices_indexed16[0u]))};
-            uint32_t sizes[2]           = {sizeof(vertices),sizeof(indices_indexed16)};
-            upStreamBuff->multi_place(2u,(const void* const*)dataToPlace,(uint32_t*)offsets,(uint32_t*)sizes,(uint32_t*)alignments);
-            if (upStreamBuff->needsManualFlushOrInvalidate())
-            {
-                auto upStreamMem = upStreamBuff->getBuffer()->getBoundMemory();
-                driver->flushMappedMemoryRanges({video::IDriverMemoryAllocation::MappedMemoryRange(upStreamMem,offsets[0],sizes[0]),video::IDriverMemoryAllocation::MappedMemoryRange(upStreamMem,offsets[1],sizes[1])});
-            }
-			
-			asset::SPushConstantRange range[1] = {asset::ISpecializedShader::ESS_VERTEX,0u,sizeof(core::matrix4SIMD)};
-
-			auto createGPUSpecializedShaderFromSource = [=](const char* source, asset::ISpecializedShader::E_SHADER_STAGE stage)
-			{
-				auto spirv = device->getAssetManager()->getGLSLCompiler()->createSPIRVFromGLSL(source, stage, "main", "runtimeID");
-				auto unspec = driver->createGPUShader(std::move(spirv));
-				return driver->createGPUSpecializedShader(unspec.get(), { nullptr,nullptr,"main",stage });
-			};
-			// origFilepath is only relevant when you have filesystem #includes in your shader
-			auto createGPUSpecializedShaderFromSourceWithIncludes = [&](const char* source, asset::ISpecializedShader::E_SHADER_STAGE stage, const char* origFilepath)
-			{
-				auto resolved_includes = device->getAssetManager()->getGLSLCompiler()->resolveIncludeDirectives(source, stage, origFilepath);
-				return createGPUSpecializedShaderFromSource(reinterpret_cast<const char*>(resolved_includes->getSPVorGLSL()->getPointer()), stage);
-			};
-			core::smart_refctd_ptr<video::IGPUSpecializedShader> shaders[2] =
-			{
-				createGPUSpecializedShaderFromSourceWithIncludes(vertexSource,asset::ISpecializedShader::ESS_VERTEX, "shader.vert"),
-				createGPUSpecializedShaderFromSource(fragmentSource,asset::ISpecializedShader::ESS_FRAGMENT)
-			};
-			auto shadersPtr = reinterpret_cast<video::IGPUSpecializedShader**>(shaders);
-			
-			asset::SVertexInputParams inputParams;
-			inputParams.enabledAttribFlags = 0b11u;
-			inputParams.enabledBindingFlags = 0b1u;
-			inputParams.attributes[0].binding = 0u;
-			inputParams.attributes[0].format = asset::EF_R32G32B32_SFLOAT;
-			inputParams.attributes[0].relativeOffset = offsetof(VertexStruct,Pos[0]);
-			inputParams.attributes[1].binding = 0u;
-			inputParams.attributes[1].format = asset::EF_R8G8_UNORM;
-			inputParams.attributes[1].relativeOffset = offsetof(VertexStruct,Col[0]);
-			inputParams.bindings[0].stride = sizeof(VertexStruct);
-			inputParams.bindings[0].inputRate = asset::EVIR_PER_VERTEX;
-
-			asset::SBlendParams blendParams; // defaults are sane
-
-			asset::SPrimitiveAssemblyParams assemblyParams = {asset::EPT_TRIANGLE_LIST,false,1u};
-
-			asset::SStencilOpParams defaultStencil;
-			asset::SRasterizationParams rasterParams;
-			rasterParams.faceCullingMode = asset::EFCM_NONE;
-			auto pipeline = driver->createGPURenderpassIndependentPipeline(	nullptr,driver->createGPUPipelineLayout(range,range+1u,nullptr,nullptr,nullptr,nullptr),
-																			shadersPtr,shadersPtr+sizeof(shaders)/sizeof(core::smart_refctd_ptr<video::IGPUSpecializedShader>),
-																			inputParams,blendParams,assemblyParams,rasterParams);
-			
-			asset::SBufferBinding<video::IGPUBuffer> bindings[video::IGPUMeshBuffer::MAX_ATTR_BUF_BINDING_COUNT];
-			bindings[0u] = {offsets[0],upStreamRef};
-			auto mb = core::make_smart_refctd_ptr<video::IGPUMeshBuffer>(std::move(pipeline),nullptr,bindings,asset::SBufferBinding<video::IGPUBuffer>{offsets[1],upStreamRef});
-			{
-				mb->setIndexType(asset::EIT_16BIT);
-				mb->setIndexCount(2*3*6);
-			}
-
 			driver->bindGraphicsPipeline(mb->getPipeline());
 			driver->pushConstants(mb->getPipeline()->getLayout(), asset::ISpecializedShader::ESS_VERTEX, 0u, sizeof(core::matrix4SIMD), mvp.pointer());
 			driver->drawMeshBuffer(mb.get());
-
-            upStreamBuff->multi_free(2u,(uint32_t*)&offsets,(uint32_t*)&sizes,driver->placeFence());
         }
 		driver->endScene();
 
