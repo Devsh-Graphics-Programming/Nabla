49a950719e73be2901efb4855f478ca9271886b5
Przemog1 <minikers21@gmail.com>
2021-03-08 18:51:11 +0100

    Added new overrides

--- include/nbl/builtin/shader/loader/mtl/fragment_impl.glsl
+++ include/nbl/builtin/shader/loader/mtl/fragment_impl.glsl
@@ -28,15 +28,29 @@ layout (location = 0) out vec4 OutColor;
 
 #include <nbl/builtin/glsl/bxdf/common.glsl> // change to bxdf/common.glsl
 
+#ifndef _NBL_FRAG_MATERIAL_PARAMETERS_STRUCT_DEFINED_
+#define _NBL_FRAG_MATERIAL_PARAMETERS_STRUCT_DEFINED_
+
+#include <nbl/builtin/glsl/loader/mtl/common.glsl>
+#define nbl_glsl_MaterialParametersStruct nbl_glsl_MTLMaterialParameters
+#endif //_NBL_FRAG_MATERIAL_PARAMETERS_STRUCT_DEFINED_
+
 #ifndef _NBL_FRAG_PUSH_CONSTANTS_DEFINED_
 #define _NBL_FRAG_PUSH_CONSTANTS_DEFINED_
 
-#include <nbl/builtin/glsl/loader/mtl/common.glsl>
-
 layout (push_constant) uniform Block {
-    nbl_glsl_MTLMaterialParameters params;
+    nbl_glsl_MaterialParametersStruct params;
 } PC;
 #endif //_NBL_FRAG_PUSH_CONSTANTS_DEFINED_
+
+#ifndef _NBL_FRAG_GET_MATERIAL_PARAMETERS_FUNCTION_DEFINED_
+#define _NBL_FRAG_GET_MATERIAL_PARAMETERS_FUNCTION_DEFINED_
+
+nbl_glsl_MaterialParametersStruct nbl_glsl_getMaterialParameters()
+{
+    return PC.params;
+}
+#endif //_NBL_FRAG_RETRIVE_MATERIAL_PARAMETERS_FUNCTION_DEFINED_
 
 #if !defined(_NBL_FRAG_SET3_BINDINGS_DEFINED_) && !defined(_NO_UV)
 #define _NBL_FRAG_SET3_BINDINGS_DEFINED_
@@ -85,39 +99,41 @@ vec4 nbl_sample_bump(in vec2 uv, in mat2 dUV) { return texture(map_bump, uv); }
 // params can be either BSDFIsotropicParams or BSDFAnisotropicParams 
 Spectrum nbl_bsdf_cos_eval(in nbl_glsl_LightSample _sample, in nbl_glsl_IsotropicViewSurfaceInteraction inter, in mat2 dUV)
 {
+    nbl_glsl_MaterialParametersStruct mtParams = nbl_glsl_getMaterialParameters();
+
     vec3 Kd;
 #ifndef _NO_UV
-    if ((PC.params.extra&(map_Kd_MASK)) == (map_Kd_MASK))
+    if ((mtParams.extra&(map_Kd_MASK)) == (map_Kd_MASK))
         Kd = nbl_sample_Kd(UV, dUV).rgb;
     else
 #endif
-        Kd = PC.params.Kd;
+        Kd = mtParams.Kd;
 
     vec3 color = vec3(0.0);
     vec3 Ks;
     float Ns;
 
 #ifndef _NO_UV
-    if ((PC.params.extra&(map_Ks_MASK)) == (map_Ks_MASK))
+    if ((mtParams.extra&(map_Ks_MASK)) == (map_Ks_MASK))
         Ks = nbl_sample_Ks(UV, dUV).rgb;
     else
 #endif
-        Ks = PC.params.Ks;
+        Ks = mtParams.Ks;
 #ifndef _NO_UV
-    if ((PC.params.extra&(map_Ns_MASK)) == (map_Ns_MASK))
+    if ((mtParams.extra&(map_Ns_MASK)) == (map_Ns_MASK))
         Ns = nbl_sample_Ns(UV, dUV).x;
     else
 #endif
-        Ns = PC.params.Ns;
+        Ns = mtParams.Ns;
 
-    vec3 Ni = vec3(PC.params.Ni);
+    vec3 Ni = vec3(mtParams.Ni);
 
 
     vec3 diff = nbl_glsl_lambertian_cos_eval(_sample) * Kd * (1.0-nbl_glsl_fresnel_dielectric(Ni,_sample.NdotL)) * (1.0-nbl_glsl_fresnel_dielectric(Ni,inter.NdotV));
     diff *= nbl_glsl_diffuseFresnelCorrectionFactor(Ni, Ni*Ni);
 
     nbl_glsl_IsotropicMicrofacetCache _cache = nbl_glsl_calcIsotropicMicrofacetCache(inter,_sample);
-    switch (PC.params.extra&ILLUM_MODEL_MASK)
+    switch (mtParams.extra&ILLUM_MODEL_MASK)
     {
     case 0:
         color = vec3(0.0);
@@ -160,9 +176,10 @@ Spectrum nbl_bsdf_cos_eval(in nbl_glsl_LightSample _sample, in nbl_glsl_Isotropi
 vec3 nbl_computeLighting(out nbl_glsl_IsotropicViewSurfaceInteraction out_interaction, in mat2 dUV)
 {
     nbl_glsl_IsotropicViewSurfaceInteraction interaction = nbl_glsl_calcSurfaceInteraction(vec3(0.0), ViewPos, Normal, mat2x3(dFdx(ViewPos),dFdy(ViewPos)));
+    nbl_glsl_MaterialParametersStruct mtParams = nbl_glsl_getMaterialParameters();
 
 #ifndef _NO_UV
-    if ((PC.params.extra&map_bump_MASK) == map_bump_MASK)
+    if ((mtParams.extra&map_bump_MASK) == map_bump_MASK)
     {
         interaction.N = normalize(interaction.N);
 
@@ -177,27 +194,27 @@ vec3 nbl_computeLighting(out nbl_glsl_IsotropicViewSurfaceInteraction out_intera
     nbl_glsl_LightSample _sample = nbl_glsl_createLightSample(L*invLenL, interaction);
 
     vec3 Ka;
-    switch ((PC.params.extra&ILLUM_MODEL_MASK))
+    switch ((mtParams.extra&ILLUM_MODEL_MASK))
     {
     case 0:
     {
 #ifndef _NO_UV
-    if ((PC.params.extra&(map_Kd_MASK)) == (map_Kd_MASK))
+    if ((mtParams.extra&(map_Kd_MASK)) == (map_Kd_MASK))
         Ka = nbl_sample_bump(UV, dUV).rgb;
     else
 #endif
-        Ka = PC.params.Kd;
+        Ka = mtParams.Kd;
     }
     break;
     default:
 #define Ia 0.1
     {
 #ifndef _NO_UV
-    if ((PC.params.extra&(map_Ka_MASK)) == (map_Ka_MASK))
+    if ((mtParams.extra&(map_Ka_MASK)) == (map_Ka_MASK))
         Ka = nbl_sample_Ka(UV, dUV).rgb;
     else
 #endif
-        Ka = PC.params.Ka;
+        Ka = mtParams.Ka;
     Ka *= Ia;
     }
 #undef Ia
@@ -222,13 +239,14 @@ void main()
 #else
     mat2 dUV = mat2(vec2(0,0),vec2(0,0));    
 #endif
+    nbl_glsl_MaterialParametersStruct mtParams = nbl_glsl_getMaterialParameters();
     nbl_glsl_IsotropicViewSurfaceInteraction interaction;
     vec3 color = nbl_computeLighting(interaction, dUV);
 
-    float d = PC.params.d;
+    float d = mtParams.d;
 
     //another illum model switch, required for illum=4,6,7,9 to compute alpha from fresnel (taken from opacity map or constant otherwise)
-    switch (PC.params.extra&ILLUM_MODEL_MASK)
+    switch (mtParams.extra&ILLUM_MODEL_MASK)
     {
         case 4:
         case 6:
@@ -236,12 +254,12 @@ void main()
         case 9:
         {
             float VdotN = dot(interaction.N, interaction.V.dir);
-            d = nbl_glsl_fresnel_dielectric(vec3(PC.params.Ni), VdotN).x;
+            d = nbl_glsl_fresnel_dielectric(vec3(mtParams.Ni), VdotN).x;
         }
             break;
         default:
     #ifndef _NO_UV
-            if ((PC.params.extra&(map_d_MASK)) == (map_d_MASK))
+            if ((mtParams.extra&(map_d_MASK)) == (map_d_MASK))
             {
                 d = nbl_sample_d(UV, dUV).r;
                 color *= d;
