a0896b686ae01c9c54edfe36498424df0c93d3a4
devsh <devsh.graphicsprogramming@gmail.com>
2021-03-20 11:07:55 +0100

    move the decode functions to builtin/glsl/format/decode.glsl

--- examples_tests/41.VisibilityBuffer/main.cpp
+++ examples_tests/41.VisibilityBuffer/main.cpp
@@ -25,21 +25,7 @@ R"(
 
 layout(location = 4) flat out uint drawID; //TODO: override main
 
-vec4 nbl_glsl_decodeRGB10A2_UNORM(in uint x)
-{
-	const uvec4 shifted = uvec4(x,uvec3(x)>>uvec3(10,20,30));
-	const uvec4 rgbaMask = uvec4(uvec3(0x3ffu),0x3u);
-	return vec4(shifted&rgbaMask)/vec4(rgbaMask);
-}
-
-vec4 nbl_glsl_decodeRGB10A2_SNORM(in uint x)
-{
-    const ivec4 shifted = ivec4(x,uvec3(x)>>uvec3(10u,20u,30u));
-	const ivec4 rgbaBias = ivec4(ivec3(0x200u),0x2u);
-	const ivec4 halfMask = rgbaBias-ivec4(1);
-	const ivec4 signed = (-(shifted&rgbaBias))|(shifted&halfMask);
-    return max(vec4(signed)/vec4(halfMask),vec4(-1.0));
-}
+#include "nbl/builtin/glsl/format/decode.glsl"
 
 //pos
 layout(set = 0, binding = 0) uniform samplerBuffer MeshPackedDataFloat[2];
