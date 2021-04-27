d68e3742b12afdda3ad8dacc1e44aa1b574a1e2f
devsh <devsh.graphicsprogramming@gmail.com>
2021-04-27 02:22:32 +0200

    get rid of useless derivatives in the material_compiler

--- examples_tests/18.MitsubaLoader/main.cpp
+++ examples_tests/18.MitsubaLoader/main.cpp
@@ -58,7 +58,7 @@ vec2 SampleSphericalMap(in vec3 v)
     return uv;
 }
 
-vec3 nbl_computeLighting(inout nbl_glsl_AnisotropicViewSurfaceInteraction out_interaction, in mat2 dUV)
+vec3 nbl_computeLighting(inout nbl_glsl_AnisotropicViewSurfaceInteraction out_interaction)
 {
 	nbl_glsl_xoroshiro64star_state_t scramble_start_state = textureLod(scramblebuf,gl_FragCoord.xy/VIEWPORT_SZ,0).rg;
 
@@ -92,7 +92,7 @@ vec3 nbl_computeLighting(inout nbl_glsl_AnisotropicViewSurfaceInteraction out_in
 		const float intensityScale = LIGHT_INTENSITY_SCALE;//ehh might want to render to hdr fbo and do tonemapping
 		nbl_glsl_LightSample _sample;
 		_sample.L = L*inversesqrt(d2);
-		color += nbl_bsdf_cos_eval(_sample,out_interaction, dUV)*l.intensity*intensityScale/d2;
+		color += nbl_bsdf_cos_eval(_sample,out_interaction)*l.intensity*intensityScale/d2;
 	}
 
 	return color+emissive;
@@ -112,12 +112,12 @@ void main()
 	nbl_glsl_MC_runTexPrefetchStream(nbl_glsl_MC_oriented_material_t_getTexPrefetchStream(material), UV, dUV);
 #endif
 #ifdef NORM_PRECOMP_STREAM
-	nbl_glsl_MC_runNormalPrecompStream(nbl_glsl_MC_oriented_material_t_getNormalPrecompStream(material), dUV, precomp);
+	nbl_glsl_MC_runNormalPrecompStream(nbl_glsl_MC_oriented_material_t_getNormalPrecompStream(material), precomp);
 #endif
 
 
 	nbl_glsl_AnisotropicViewSurfaceInteraction inter;
-	vec3 color = nbl_computeLighting(inter, dUV);
+	vec3 color = nbl_computeLighting(inter);
 
 	OutColor = vec4(color, 1.0);
 }
