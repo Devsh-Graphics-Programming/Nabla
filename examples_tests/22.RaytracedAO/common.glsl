daecb18ead0bb240667082c62e168543141de1eb
devsh <devsh.graphicsprogramming@gmail.com>
2021-05-15 20:37:14 +0200

    optimize normal attribute storage in ext/MitsubaLoader/CSerializedLoader.cpp and deinterleave the vertex attributes
    prep for the new descriptor set for packed geometry

--- examples_tests/22.RaytracedAO/common.glsl
+++ examples_tests/22.RaytracedAO/common.glsl
@@ -22,6 +22,26 @@
 	#define vec4 nbl::core::vectorSIMDf
 	#define mat4 nbl::core::matrix4SIMD
 	#define mat4x3 nbl::core::matrix3x4SIMD
+#else
+
+#extension GL_EXT_shader_16bit_storage : require
+
+#include <nbl/builtin/glsl/virtual_geometry/virtual_attribute.glsl>
+/*
+#define _NBL_VG_USE_SSBO
+#define _NBL_VG_SSBO_DESCRIPTOR_SET 1
+#define _NBL_VG_USE_SSBO_UINT
+#define _NBL_VG_SSBO_UINT_BINDING 0
+#define _NBL_VG_USE_SSBO_UVEC3
+#define _NBL_VG_SSBO_UVEC3_BINDING 1
+#define _NBL_VG_USE_SSBO_INDEX
+#define _NBL_VG_SSBO_INDEX_BINDING 2
+// TODO: remove after all quantization optimizations in CSerializedLoader and the like
+#define _NBL_VG_USE_SSBO_UVEC2
+#define _NBL_VG_SSBO_UVEC2_BINDING 3
+#include <nbl/builtin/glsl/virtual_geometry/virtual_attribute_fetch.glsl>
+*/
+
 #endif
 
 #endif
