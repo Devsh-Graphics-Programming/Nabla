6c2be0272ad441b3e88690aa20ea9085d6bbd3c4
devsh <devsh.graphicsprogramming@gmail.com>
2021-05-15 12:37:08 +0200

    prep the barycentric utilities for NV/AMD builitins to be implemented by @Przemog1

--- include/nbl/builtin/glsl/barycentric/vert.glsl
+++ include/nbl/builtin/glsl/barycentric/vert.glsl
@@ -5,11 +5,15 @@
 #ifndef _NBL_BUILTIN_GLSL_BARYCENTRIC_VERT_INCLUDED_
 #define _NBL_BUILTIN_GLSL_BARYCENTRIC_VERT_INCLUDED_
 
+
+
+// forward declaration is useful for offline compilers to not moan until the end
 void nbl_glsl_barycentric_vert_set(in vec3 pos);
 
-// TODO: Check for Nvidia Pascal Barycentric extension
-// TODO: Check for AMD Barycentric extension
-#if 0||0
+
+
+// TODO: Check for Nvidia Pascal Barycentric extension when SPIRV-Cross supports it and add an NBL_GL_ cap macro
+#ifdef NBL_GL_NV_fragment_shader_barycentric
 
 
 void nbl_glsl_barycentric_vert_set(in vec3 pos) {} // noop
@@ -20,17 +24,20 @@ void nbl_glsl_barycentric_vert_set(in vec3 pos) {} // noop
 
 #ifndef NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT
 #define NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT
-#ifndef NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT_LOC
-#define NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT_LOC 0
-#endif
+    #ifndef NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT_LOC
+    #define NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT_LOC 0
+    #endif
+    
 layout(location = NBL_GLSL_BARYCENTRIC_VERT_POS_OUTPUT_LOC) out vec3 nbl_glsl_barycentric_vert_pos;
 #endif
+
 #ifndef NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT
 #define NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT
-#ifndef NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC
-#define NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC 1
-#endif
-layout(location = NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC) flat out vec3 nbl_glsl_barycentric_vert_provokingPos;
+    #ifndef NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC
+    #define NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC 1
+    #endif
+
+    layout(location = NBL_GLSL_BARYCENTRIC_VERT_PROVOKINGPOS_OUTPUT_LOC) flat out vec3 nbl_glsl_barycentric_vert_provokingPos;
 #endif
 
 void nbl_glsl_barycentric_vert_set(in vec3 pos)
@@ -39,7 +46,9 @@ void nbl_glsl_barycentric_vert_set(in vec3 pos)
     nbl_glsl_barycentric_vert_provokingPos = pos;
 }
 
+
 #endif
+
 
 
 #endif
