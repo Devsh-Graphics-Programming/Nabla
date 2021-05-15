a3a601083e48684310d7e722d1f57365cdf83eb1
devsh <devsh.graphicsprogramming@gmail.com>
2021-05-15 22:42:23 +0200

    reorganize some more

--- examples_tests/22.RaytracedAO/Renderer.h
+++ examples_tests/22.RaytracedAO/Renderer.h
@@ -19,8 +19,8 @@
 class Renderer : public nbl::core::IReferenceCounted, public nbl::core::InterfaceUnmovable
 {
     public:
-		#include "drawCommon.glsl"
-		#include "raytraceCommon.glsl"
+		#include "drawCommon.h"
+		#include "raytraceCommon.h"
 		#ifdef __cplusplus
 			#undef uint
 			#undef mat4
