dd71d9b0b77c513291236862d72c1f7dffe8292d
AnastaZIuk <areklachowicz@gmail.com>
2021-06-02 15:22:32 +0200

    Eliminate namespace gcc error, make Nabla build

--- src/nbl/video/IOpenGL_FunctionTable.cpp
+++ src/nbl/video/IOpenGL_FunctionTable.cpp
@@ -1,12 +1,15 @@
 #include "nbl/video/IOpenGL_FunctionTable.h"
 
-thread_local char nbl::video::impl::g_NBL_GL_CALL_msg_buffer[4096];
-
 namespace nbl {
 namespace video
 {
 
 std::atomic_uint32_t IOpenGL_FunctionTable::s_guidGenerator = 0u;
+
+namespace impl
+{
+    thread_local char g_NBL_GL_CALL_msg_buffer[4096];
+}
 
 }
 }
