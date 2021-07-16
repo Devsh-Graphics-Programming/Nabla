2281dbe65b28a34e12fd1cb91d2cd553c71f04f9
devsh <devsh.graphicsprogramming@gmail.com>
2021-07-16 19:08:36 +0200

    remove deprecated compatibility profile code

--- src/nbl/video/IOpenGL_PhysicalDeviceBase.h
+++ src/nbl/video/IOpenGL_PhysicalDeviceBase.h
@@ -350,27 +350,6 @@ public:
 		GetIntegerv(GL_MAX_DRAW_BUFFERS, &num);
 		m_glfeatures.MaxMultipleRenderTargets = static_cast<uint8_t>(num);
 
-		GetFloatv(GL_ALIASED_LINE_WIDTH_RANGE, m_glfeatures.DimAliasedLine);
-		GetFloatv(GL_ALIASED_POINT_SIZE_RANGE, m_glfeatures.DimAliasedPoint);
-
-		if constexpr (!IsGLES) // TODO: useless to try and support smoothed lines and points
-		{
-			/**
-			As per the spec
-				Enable/Disable targets POINT_SMOOTH and POINT_SPRITE, and all associated state.
-				Point rasterization is always performed as though POINT_SPRITE were enabled.
-			**/
-			GetFloatv(GL_SMOOTH_LINE_WIDTH_RANGE, m_glfeatures.DimSmoothedLine);
-			GetFloatv(GL_SMOOTH_POINT_SIZE_RANGE, m_glfeatures.DimSmoothedPoint);
-		}
-		else
-		{
-			m_glfeatures.DimSmoothedLine[0] = 0;
-			m_glfeatures.DimSmoothedLine[1] = 0;
-			m_glfeatures.DimSmoothedPoint[0] = 0;
-			m_glfeatures.DimSmoothedPoint[1] = 0;
-		}
-
 		bool runningInRenderDoc = false;
 #ifdef _NBL_PLATFORM_WINDOWS_
 		if (GetModuleHandleA("renderdoc.dll"))
