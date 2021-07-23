f6b27988005b70d0cb83714e62cf3a1871b2713a
devsh <devsh.graphicsprogramming@gmail.com>
2021-07-24 00:11:49 +0200

    fixed a huge buffer upload/download alignment OpenGL unpack/pack state bug, @Crisspl make sure this is not present on `criss_android` branch!

--- source/Nabla/COpenGLDriver.cpp
+++ source/Nabla/COpenGLDriver.cpp
@@ -1474,7 +1474,7 @@ void COpenGLDriver::copyBufferToImage(IGPUBuffer* srcBuffer, IGPUImage* dstImage
 		//assert(?);
 
 		uint32_t pitch = ((it->bufferRowLength ? it->bufferRowLength:it->imageExtent.width)*bpp).getIntegerApprox();
-		int32_t alignment = 0x1<<core::min(core::max(core::findLSB(it->bufferOffset),core::findLSB(pitch)),3u);
+		int32_t alignment = 0x1<<core::min(core::min(core::findLSB(it->bufferOffset),core::findLSB(pitch)),3u);
         ctx->nextState.pixelUnpack.alignment = alignment;
         ctx->nextState.pixelUnpack.rowLength = it->bufferRowLength;
         ctx->nextState.pixelUnpack.imgHeight = it->bufferImageHeight;
@@ -1568,7 +1568,7 @@ void COpenGLDriver::copyImageToBuffer(IGPUImage* srcImage, IGPUBuffer* dstBuffer
 		//assert(?);
 
 		uint32_t pitch = ((it->bufferRowLength ? it->bufferRowLength:it->imageExtent.width)*bpp).getIntegerApprox();
-		int32_t alignment = 0x1<<core::min(core::max(core::findLSB(it->bufferOffset),core::findLSB(pitch)),3u);
+		int32_t alignment = 0x1<<core::min(core::min(core::findLSB(it->bufferOffset),core::findLSB(pitch)),3u);
         ctx->nextState.pixelPack.alignment = alignment;
         ctx->nextState.pixelPack.rowLength = it->bufferRowLength;
         ctx->nextState.pixelPack.imgHeight = it->bufferImageHeight;
