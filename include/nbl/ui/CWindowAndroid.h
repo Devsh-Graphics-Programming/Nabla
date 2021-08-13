60074b90c9d5cf454f4514b21f37ce88560dd953
Danylo <danylotv43@gmail.com>
2021-08-13 11:23:45 +0300

    The rest of build errors

--- include/nbl/ui/CWindowAndroid.h
+++ include/nbl/ui/CWindowAndroid.h
@@ -12,20 +12,21 @@ namespace ui
 class CWindowAndroid : public IWindowAndroid
 {
 public:
-    explicit CWindowAndroid(native_handle_t anw) : m_native(anw)
+    explicit CWindowAndroid(native_handle_t anw) : m_native(anw), IWindowAndroid(SCreationParams{})
     {
         m_width = ANativeWindow_getWidth(anw);
         m_height = ANativeWindow_getHeight(anw);
     }
 
     native_handle_t getNativeHandle() const override { return m_native; }
-
+    virtual IClipboardManager* getClipboardManager() { return nullptr; }
+    virtual ICursorControl* getCursorControl() { return nullptr; }
 private:
     native_handle_t m_native;
 };
 
 }
-}
+} 
 
 #endif //_NBL_PLATFORM_ANDROID_
 
