a8a2f420429c1fc81b8c589500afae15af7a20bd
criss <crisserpl2@gmail.com>
2021-09-06 16:05:34 +0200

    Ability to create GL surface from native window handle

--- include/nbl/video/surface/ISurfaceGL.h
+++ include/nbl/video/surface/ISurfaceGL.h
@@ -12,12 +12,12 @@
 namespace nbl::video
 {
 
-template<class Window>
-class CSurfaceGL final : public CSurface<Window>
+template<class Window, template<typename> typename Base>
+class CSurfaceGLImpl final : public Base<Window>
 {
     public:
-        using this_t = CSurfaceGL<Window>;
-        using base_t = CSurface<Window>;
+        using this_t = CSurfaceGLImpl<Window,Base>;
+        using base_t = Base<Window>;
 
         template<video::E_API_TYPE API_TYPE>
         static inline core::smart_refctd_ptr<this_t> create(core::smart_refctd_ptr<video::COpenGL_Connection<API_TYPE>>&& api, core::smart_refctd_ptr<Window>&& window)
@@ -26,6 +26,14 @@ class CSurfaceGL final : public CSurface<Window>
                 return nullptr;
             auto retval = new this_t(std::move(api),std::move(window));
             return core::smart_refctd_ptr<this_t>(retval,core::dont_grab);
+        }
+        template<video::E_API_TYPE API_TYPE>
+        static inline core::smart_refctd_ptr<this_t> create(core::smart_refctd_ptr<video::COpenGL_Connection<API_TYPE>>&& api, typename Window::native_handle_t ntv_window)
+        {
+            if (!api || !window)
+                return nullptr;
+            auto retval = new this_t(std::move(api), ntv_window);
+            return core::smart_refctd_ptr<this_t>(retval, core::dont_grab);
         }
 
         bool isSupportedForPhysicalDevice(const IPhysicalDevice* dev, uint32_t _queueFamIx) const override
@@ -63,21 +71,19 @@ class CSurfaceGL final : public CSurface<Window>
             return false;
         }
 
-        inline const void* getNativeWindowHandle() const override
-        {
-            return &base_t::m_window->getNativeHandle();
-        }
-
     protected:
-        template<video::E_API_TYPE API_TYPE>
-        explicit CSurfaceGL(core::smart_refctd_ptr<video::COpenGL_Connection<API_TYPE>>&& api, core::smart_refctd_ptr<Window>&& window) : base_t(std::move(api),std::move(window))
-        {
-        }
+        using base_t::base_t;
 };
 
+template <typename Window>
+using CSurfaceGL = CSurfaceGLImpl<Window, CSurface>;
+template <typename Window>
+using CSurfaceNativeGL = CSurfaceGLImpl<Window, CSurfaceNative>;
 
 // TODO: conditional defines
 using CSurfaceGLWin32 = CSurfaceGL<ui::IWindowWin32>;
+using CSurfaceNativeGLWin32 = CSurfaceNativeGL<ui::IWindowWin32>;
+
 //using CSurfaceGLAndroid = CSurfaceGL<ui::IWindowAndroid>;
 //using CSurfaceGLX11 = CSurfaceGL<ui::IWindowX11>;
 //using CSurfaceGLWayland = CSurfaceGL<ui::IWindowWayland>;
