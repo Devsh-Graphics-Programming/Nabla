bdd2d84e2d7b8e5d072e6afefd88798cb307e779
Erfan Ahmadi <ahmadierfan99@gmail.com>
2021-09-13 16:48:51 +0430

    ICPUImage Regions Copy Fix

--- include/nbl/asset/ICPUImage.h
+++ include/nbl/asset/ICPUImage.h
@@ -34,7 +34,13 @@ class ICPUImage final : public IImage, public IAsset
             auto cp = core::smart_refctd_ptr<ICPUImage>(new ICPUImage(std::move(par)), core::dont_grab);
             clone_common(cp.get());
 
-            cp->regions = regions;
+            auto regionsCount = regions->size();
+            if(regionsCount > 0)
+            {
+                cp->regions = core::make_refctd_dynamic_array<core::smart_refctd_dynamic_array<IImage::SBufferCopy>>(regionsCount);
+                std::copy_n(regions->begin(), regionsCount, cp->regions->begin());
+            }
+
             cp->buffer = (_depth > 0u && buffer) ? core::smart_refctd_ptr_static_cast<ICPUBuffer>(buffer->clone(_depth-1u)) : buffer;
 
             return cp;
