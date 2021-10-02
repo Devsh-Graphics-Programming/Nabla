033d955a25aae44775df974665f289660a46b2ee
devsh <devsh.graphicsprogramming@gmail.com>
2021-10-02 16:08:23 +0200

    some more prototyping

--- include/nbl/scene/ICullingLoDSelectionSystem.h
+++ include/nbl/scene/ICullingLoDSelectionSystem.h
@@ -5,6 +5,8 @@
 #ifndef __NBL_SCENE_I_CULLING_LOD_SELECTION_SYSTEM_H_INCLUDED__
 #define __NBL_SCENE_I_CULLING_LOD_SELECTION_SYSTEM_H_INCLUDED__
 
+#include "nbl/video/utilities/CScanner.h"
+
 #include "nbl/scene/ITransformTree.h"
 #include "nbl/scene/ILevelOfDetailLibrary.h"
 
@@ -19,6 +21,9 @@ namespace nbl::scene
 class ICullingLoDSelectionSystem : public virtual core::IReferenceCounted
 {
 	public:
+		static inline constexpr uint32_t DefaultWorkGroupSize = 256u;
+
+		//
 		#define nbl_glsl_DispatchIndirectCommand_t asset::DispatchIndirectCommand_t
 		#include "nbl/builtin/glsl/culling_lod_selection/dispatch_indirect_params.glsl"
 		#undef nbl_glsl_DispatchIndirectCommand_t
@@ -34,6 +39,58 @@ class ICullingLoDSelectionSystem : public virtual core::IReferenceCounted
             return logicalDevice->createDeviceLocalGPUBufferOnDedMem(params,sizeof(DispatchIndirectParams));
 		}
 
+		//
+		struct ScratchBufferRanges
+		{
+			asset::SBufferRange<video::IGPUBuffer> lodDrawCallOffsets;
+			asset::SBufferRange<video::IGPUBuffer> lodDrawCallCounts;
+			asset::SBufferRange<video::IGPUBuffer> prefixSumScratch;
+		};
+		static ScratchBufferRanges createScratchBuffer(
+			video::ILogicalDevice* logicalDevice,
+			const uint32_t maxTotalInstances,
+			const uint32_t maxTotalDrawcallInstances,
+			const uint32_t wg_size=DefaultWorkGroupSize
+		)
+		{
+			ScratchBufferRanges retval;
+			{
+				const auto& limits = logicalDevice->getPhysicalDevice()->getLimits();
+				const auto ssboAlignment = limits.SSBOAlignment;
+
+				retval.lodDrawCallOffsets.offset = 0u;
+				retval.lodDrawCallCounts.offset = retval.lodDrawCallOffsets.size = core::alignUp(maxTotalInstances*sizeof(uint32_t),ssboAlignment);
+				retval.lodDrawCallCounts.size = retval.lodDrawCallOffsets.size;
+				retval.prefixSumScratch.offset = retval.lodDrawCallCounts.offset+retval.lodDrawCallCounts.size;
+				{
+					video::CScanner::Parameters params;
+					auto schedulerParams = video::CScanner::SchedulerParameters(params,maxTotalDrawcallInstances,wg_size);
+					retval.prefixSumScratch.size = params.getScratchSize(ssboAlignment);
+				}
+			}
+			{
+				video::IGPUBuffer::SCreationParams params;
+				params.usage = asset::IBuffer::EUF_STORAGE_BUFFER_BIT;
+				retval.lodDrawCallOffsets.buffer = logicalDevice->createDeviceLocalGPUBufferOnDedMem(params,retval.prefixSumScratch.offset+retval.prefixSumScratch.size);
+			}
+			retval.lodDrawCallCounts.buffer = retval.lodDrawCallOffsets.buffer;
+			retval.prefixSumScratch.buffer = retval.lodDrawCallCounts.buffer;
+			return retval;
+		}
+
+		// Per-View Per-Instance buffer holds at least an MVP matrix
+		template<typename PerViewPerInstanceDataType>
+		static core::smart_refctd_ptr<video::IGPUBuffer> createPerViewPerInstanceDataBuffer(video::ILogicalDevice* logicalDevice, const uint32_t maxTotalDrawcallInstances)
+		{
+			return createPerViewPerInstanceDataBuffer(logicalDevice,maxTotalDrawcallInstances,sizeof(PerViewPerInstanceDataType));
+		}
+		static core::smart_refctd_ptr<video::IGPUBuffer> createPerViewPerInstanceDataBuffer(video::ILogicalDevice* logicalDevice, const uint32_t maxTotalDrawcallInstances, const uint32_t perViewPerInstanceDataSize)
+		{
+            video::IGPUBuffer::SCreationParams params;
+			params.usage = asset::IBuffer::EUF_STORAGE_BUFFER_BIT;
+            return logicalDevice->createDeviceLocalGPUBufferOnDedMem(params,perViewPerInstanceDataSize*maxTotalDrawcallInstances);
+		}
+
 		// Instance Redirect buffer holds a `uvec2` of `{instanceGUID,perViewPerInstanceDataID}` for each instace of a drawcall
 		static core::smart_refctd_ptr<video::IGPUBuffer> createInstanceRedirectBuffer(video::ILogicalDevice* logicalDevice, const uint32_t maxTotalDrawcallInstances)
 		{
@@ -42,70 +99,263 @@ class ICullingLoDSelectionSystem : public virtual core::IReferenceCounted
             return logicalDevice->createDeviceLocalGPUBufferOnDedMem(params,sizeof(uint32_t)*2u*maxTotalDrawcallInstances);
 		}
 
-		// lod_table
-		// lod_info
-		// drawcall AABB SSBO
+		//
+		static inline constexpr auto InputDescriptorBindingCount = 5u;
+		static inline core::smart_refctd_ptr<video::IGPUDescriptorSetLayout> createInputDescriptorSetLayout(video::ILogicalDevice* device)
+		{
+			video::IGPUDescriptorSetLayout::SBinding bindings[InputDescriptorBindingCount];
+			for (auto i=0u; i<InputDescriptorBindingCount; i++)
+			{
+				bindings[i].binding = i;
+				bindings[i].type = asset::EDT_STORAGE_BUFFER;
+				bindings[i].count = 1u;
+				bindings[i].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
+				bindings[i].samplers = nullptr;
+			}
+			return device->createGPUDescriptorSetLayout(bindings,bindings+InputDescriptorBindingCount);
+		}
+		//
+		static inline constexpr auto OutputDescriptorBindingCount = 4u;
+		static inline core::smart_refctd_ptr<video::IGPUDescriptorSetLayout> createOutputDescriptorSetLayout(video::ILogicalDevice* device, bool withMDICounts=false)
+		{
+			withMDICounts &= device->getPhysicalDevice()->getFeatures().multiDrawIndirect;
+			//withMDICounts &= device->getPhysicalDevice()->getFeatures().multiDrawIndirectCount; // TODO: @achal
 
-		// dispatch indirect params [input]
-		// instance list [input]
-		// lod drawcall offsets [working mem]
-		// lod drawcall counts [working mem]
-		// prefix sum scratch memory
+			video::IGPUDescriptorSetLayout::SBinding bindings[OutputDescriptorBindingCount];
+			for (auto i=0u; i<OutputDescriptorBindingCount; i++)
+			{
+				bindings[i].binding = i;
+				bindings[i].type = asset::EDT_STORAGE_BUFFER;
+				bindings[i].count = 1u;
+				bindings[i].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
+				bindings[i].samplers = nullptr;
+			}
 
-		// drawcall pool [inout]
-		// transient per view data [output]
-		// transient per instance attribute [output]
-		// draw count pool [inout]
+			uint32_t count = OutputDescriptorBindingCount;
+			if (!withMDICounts)
+				count--;
+			return device->createGPUDescriptorSetLayout(bindings,bindings+count);
+		}
 
-		// custom DS
+		//
+		static inline core::smart_refctd_ptr<video::IGPUDescriptorSet> createInputDescriptorSet(
+			video::ILogicalDevice* device, video::IDescriptorPool* pool,
+			core::smart_refctd_ptr<const video::IGPUDescriptorSetLayout>&& layout,
+			const asset::SBufferRange<video::IGPUBuffer>& dispatchIndirect,
+			const asset::SBufferRange<video::IGPUBuffer>& instanceList,
+			const asset::SBufferRange<video::IGPUBuffer>& lodDrawCallOffsets,
+			const asset::SBufferRange<video::IGPUBuffer>& lodDrawCallCount,
+			const asset::SBufferRange<video::IGPUBuffer>& prefixSumScratch
+		)
+		{
+			auto ds = device->createGPUDescriptorSet(pool,std::move(layout));
+			{
+				video::IGPUDescriptorSet::SWriteDescriptorSet writes[InputDescriptorBindingCount];
+				video::IGPUDescriptorSet::SDescriptorInfo infos[InputDescriptorBindingCount] =
+				{
+					dispatchIndirect,
+					instanceList,
+					lodDrawCallOffsets,
+					lodDrawCallCount,
+					prefixSumScratch
+				};
+				for (auto i=0u; i<InputDescriptorBindingCount; i++)
+				{
+					writes[i].dstSet = ds.get();
+					writes[i].binding = i;
+					writes[i].arrayElement = 0u;
+					writes[i].count = 1u;
+					writes[i].descriptorType = asset::EDT_STORAGE_BUFFER;
+					writes[i].info = infos+i;
+				}
+				device->updateDescriptorSets(InputDescriptorBindingCount,writes,0u,nullptr);
+			}
+			return ds;
+		}
+		//
+		static inline core::smart_refctd_ptr<video::IGPUDescriptorSet> createInputDescriptorSet(
+			video::ILogicalDevice* device, video::IDescriptorPool* pool,
+			core::smart_refctd_ptr<const video::IGPUDescriptorSetLayout>&& layout,
+			const asset::SBufferRange<video::IGPUBuffer>& drawCalls,
+			const asset::SBufferRange<video::IGPUBuffer>& perViewPerInstance,
+			const asset::SBufferRange<video::IGPUBuffer>& perInstanceRedirectAttribs,
+			const asset::SBufferRange<video::IGPUBuffer>& drawCallCounts={}
+		)
+		{
+			auto _layout = layout.get();
+			auto ds = device->createGPUDescriptorSet(pool,std::move(layout));
+			{
+				video::IGPUDescriptorSet::SWriteDescriptorSet writes[OutputDescriptorBindingCount];
+				video::IGPUDescriptorSet::SDescriptorInfo infos[OutputDescriptorBindingCount] =
+				{
+					drawCalls,
+					perViewPerInstance,
+					perInstanceRedirectAttribs,
+					drawCallCounts
+				};
+				for (auto i=0u; i<OutputDescriptorBindingCount; i++)
+				{
+					writes[i].dstSet = ds.get();
+					writes[i].binding = i;
+					writes[i].arrayElement = 0u;
+					writes[i].count = 1u;
+					writes[i].descriptorType = asset::EDT_STORAGE_BUFFER;
+					writes[i].info = infos+i;
+				}
+				uint32_t count = OutputDescriptorBindingCount;
+				if (_layout->getBindings().size()==OutputDescriptorBindingCount)
+				{
+					assert(drawCallCounts.buffer && drawCallCounts.size!=0ull);
+				}
+				else
+					count--;
+				device->updateDescriptorSets(count,writes,0u,nullptr);
+			}
+			return ds;
+		}
+
+		//
 		struct Params
 		{
 			video::IGPUCommandBuffer* cmdbuf; // must already be in recording state
-			const video::IGPUBuffer* indirectDispatchParams;
-			const video::IGPUDescriptorSet* lodTableAndDrawcallDS;
+			asset::SBufferBinding<const video::IGPUBuffer> indirectDispatchParams;
+			const video::IGPUDescriptorSet* lodLibraryDS;
 			const video::IGPUDescriptorSet* transientInputDS;
 			const video::IGPUDescriptorSet* transientOutputDS;
 			const video::IGPUDescriptorSet* customDS;
 			uint32_t directInstanceCount; // set as 0u for indirect dispatch
+			// these are for the pipeline barriers
+			asset::SBufferRange<const video::IGPUBuffer> lodDrawCallOffsets;
+			asset::SBufferRange<const video::IGPUBuffer> lodDrawCallCount;
+			asset::SBufferRange<const video::IGPUBuffer> prefixSumScratch;
+			asset::SBufferRange<const video::IGPUBuffer> perInstanceRedirectAttribs;
 		};
 		void processInstancesAndFillIndirectDraws(const Params& params)
 		{
 			auto cmdbuf = params.cmdbuf;
-			const auto indirectDispatchParams = params.indirectDispatchParams;
+			const auto queueFamilyIndex = cmdbuf->getPool()->getQueueFamilyIndex();
+			const asset::SBufferRange<const video::IGPUBuffer> indirectRange = {
+				params.indirectDispatchParams.offset,sizeof(DispatchIndirectParams),
+				params.indirectDispatchParams.buffer
+			};
+
+			const auto internalStageFlags = core::bitflag(asset::EPSF_DRAW_INDIRECT_BIT)|asset::EPSF_COMPUTE_SHADER_BIT;
+
+			constexpr auto MaxBufferBarriers = 5u;
+			video::IGPUCommandBuffer::SBufferMemoryBarrier barriers[MaxBufferBarriers];
+			for (auto i=0u; i<MaxBufferBarriers; i++)
+			{
+				barriers[i].srcQueueFamilyIndex = queueFamilyIndex;
+				barriers[i].dstQueueFamilyIndex = queueFamilyIndex;
+			}
+			auto setBarrierBuffer = [](
+				video::IGPUCommandBuffer::SBufferMemoryBarrier& barrier, const asset::SBufferRange<const video::IGPUBuffer>& range,
+				core::bitflag<asset::E_ACCESS_FLAGS> srcAccessMask,
+				core::bitflag<asset::E_ACCESS_FLAGS> dstAccessMask//=asset::EAF_SHADER_READ_BIT
+			) -> void
+			{
+				barrier.barrier.srcAccessMask = srcAccessMask;
+				barrier.barrier.dstAccessMask = dstAccessMask;
+				barrier.buffer = range.buffer;
+				barrier.offset = range.offset;
+				barrier.size = range.size;
+			};
+			const auto wAccessMask = core::bitflag(asset::EAF_SHADER_WRITE_BIT);
+			const auto rwAccessMask = core::bitflag(asset::EAF_SHADER_READ_BIT)|wAccessMask;
+			const auto indirectAccessMask = core::bitflag(asset::EAF_INDIRECT_COMMAND_READ_BIT)|rwAccessMask;
+			setBarrierBuffer(barriers[0],indirectRange,indirectAccessMask,indirectAccessMask);
+
 #if 0
+			auto srcStageFlags = core::bitflag(asset::EPSF_COMPUTE_SHADER_BIT);
 			if (params.directInstanceCount)
 			{
-				cmdbuf->bindComputePipeline(directInstanceCullAndLoDSelect);
-				cmdbuf->bindDescriptorSets(EPBP_COMPUTE,directInstanceCullAndLoDSelectLayout.get(),0u,4u,&params.lodTableAndDrawcallDS);
-				//cmdbuf->dispatch(,1u,1u);
+				cmdbuf->bindComputePipeline(directInstanceCullAndLoDSelect.get());
+				cmdbuf->bindDescriptorSets(EPBP_COMPUTE,directInstanceCullAndLoDSelectLayout.get(),0u,4u,&params.lodLibraryDS);
+				//cmdbuf->dispatch(,1u,1u); TODO
 			}
 			else
 			{
-				cmdbuf->bindComputePipeline(indirectInstanceCullAndLoDSelect);
-				cmdbuf->bindDescriptorSets(EPBP_COMPUTE,indirectInstanceCullAndLoDSelectLayout.get(),0u,4u,&params.lodTableAndDrawcallDS);
-				cmdbuf->dispatchIndirect(indirectDispatchParams,offsetof(DispatchIndirectParams,instanceCullAndLoDSelect));
+				srcStageFlags |= asset::EPSF_DRAW_INDIRECT_BIT;
+				cmdbuf->bindComputePipeline(indirectInstanceCullAndLoDSelect.get());
+				cmdbuf->bindDescriptorSets(EPBP_COMPUTE,indirectInstanceCullAndLoDSelectLayout.get(),0u,4u,&params.lodLibraryDS);
+				cmdbuf->dispatchIndirect(indirectRange.buffer.get(),indirectRange.offset+offsetof(DispatchIndirectParams,instanceCullAndLoDSelect));
 			}
-			//barrier(compute->compute);
-			cmdbuf->bindComputePipeline(instanceDrawCull);
-			cmdbuf->bindDescriptorSets(EPBP_COMPUTE,sharedPipelineLayout.get(),0u,4u,&params.lodTableAndDrawcallDS);
-			cmdbuf->dispatchIndirect(indirectDispatchParams,offsetof(DispatchIndirectParams,instanceDrawCull));
-			//barrier(compute->compute);
-			cmdbuf->bindComputePipeline(drawInstanceCountPrefixSum);
-			cmdbuf->dispatchIndirect(indirectDispatchParams,offsetof(DispatchIndirectParams,drawInstanceCountPrefixSum));
-			//barrier(compute -> compute);
-#endif
-			cmdbuf->bindComputePipeline(instanceRefCountingSortScatter.get());
-			cmdbuf->dispatchIndirect(indirectDispatchParams,offsetof(DispatchIndirectParams,instanceRefCountingSortScatter));
-			/*
-			if (features.drawIndirectCount)
 			{
-				cmdbuf->bindComputePipeline(drawCompact);
-				cmdbuf->dispatchIndirect(indirectDispatchParams,offsetof(DispatchIndirectParams,drawCompact));
+				setBarrierBuffer(barriers[1],params.lodDrawCallOffsets,core::bitflag(asset::EAF_SHADER_WRITE_BIT),core::bitflag(asset::EAF_SHADER_READ_BIT));
+				setBarrierBuffer(barriers[2],params.lodDrawCallCount,rwAccessMask,core::bitflag(asset::EAF_SHADER_READ_BIT));
+				setBarrierBuffer(barriers[3],params.prefixSumScratch,rwAccessMask,rwAccessMask);
+				// TODO: perViewData
+				cmdbuf->pipelineBarrier(srcStageFlags,internalStageFlags,asset::EDF_NONE,0u,nullptr,5u,barriers,0u,nullptr);
 			}
-			barrier(compute -> compute|indirect|vertex_input);
-			*/
+
+			cmdbuf->bindComputePipeline(instanceDrawCull.get());
+			cmdbuf->bindDescriptorSets(asset::EPBP_COMPUTE,sharedPipelineLayout.get(),0u,4u,&params.lodLibraryDS);
+			cmdbuf->dispatchIndirect(indirectRange.buffer.get(),indirectRange.offset+offsetof(DispatchIndirectParams,instanceDrawCull));
+			{
+				//setBarrierBuffer(barriers[1],params.drawCalls,rwAccessMask,rwAccessMask);
+				//setBarrierBuffer(barriers[2],params.unorderedDrawCalls,rwAccessMask,core::bitflag(asset::EAF_SHADER_READ_BIT));
+				cmdbuf->pipelineBarrier(internalStageFlags,internalStageFlags,asset::EDF_NONE,0u,nullptr,3u,barriers,0u,nullptr);
+			}
+
+			cmdbuf->bindComputePipeline(drawInstanceCountPrefixSum.get());
+			cmdbuf->dispatchIndirect(indirectRange.buffer.get(),indirectRange.offset+offsetof(DispatchIndirectParams,drawInstanceCountPrefixSum));
+			{
+				//setBarrierBuffer(barriers[1],params.drawCalls,rwAccessMask,rwAccessMask);
+				cmdbuf->pipelineBarrier(internalStageFlags,internalStageFlags,asset::EDF_NONE,0u,nullptr,3u,barriers+1,0u,nullptr);
+			}
+#endif			
+			cmdbuf->bindComputePipeline(instanceRefCountingSortScatter.get());
+			cmdbuf->dispatchIndirect(indirectRange.buffer.get(),indirectRange.offset+offsetof(DispatchIndirectParams,instanceRefCountingSortScatter));
+			{
+				setBarrierBuffer(barriers[1],params.perInstanceRedirectAttribs,wAccessMask,wAccessMask|asset::EAF_VERTEX_ATTRIBUTE_READ_BIT);
+				cmdbuf->pipelineBarrier(internalStageFlags,internalStageFlags|asset::EPSF_VERTEX_INPUT_BIT,asset::EDF_NONE,0u,nullptr,2u,barriers,0u,nullptr);
+			}
+#if 0
+			if (params.transientOutputDS->getLayout()->getBindings().size()==OutputDescriptorBindingCount)
+			{
+				cmdbuf->bindComputePipeline(drawCompact.get());
+				cmdbuf->dispatchIndirect(indirectRange.buffer.get(),indirectRange.offset+offsetof(DispatchIndirectParams,drawCompact));
+			}
+#endif
+		}
+
+	//protected:
+		ICullingLoDSelectionSystem(video::ILogicalDevice* device, core::smart_refctd_ptr<video::IGPUDescriptorSetLayout>&& customDSLayout)
+		{
+			auto lodLibraryDSLayout = ILevelOfDetailLibrary::createDescriptorSetLayout(device);
+			auto transientInputDSLayout = createInputDescriptorSetLayout(device);
+			auto transientOutputDSLayout = createOutputDescriptorSetLayout(device);
+
+			asset::SPushConstantRange range = {asset::ISpecializedShader::ESS_COMPUTE,0u,sizeof(uint32_t)};
+			directInstanceCullAndLoDSelectLayout = device->createGPUPipelineLayout(
+				&range,&range+1u,
+				core::smart_refctd_ptr(lodLibraryDSLayout),
+				core::smart_refctd_ptr(transientInputDSLayout),
+				core::smart_refctd_ptr(transientOutputDSLayout),
+				core::smart_refctd_ptr(customDSLayout)
+			);
+			sharedPipelineLayout = device->createGPUPipelineLayout(
+				nullptr,nullptr,
+				core::smart_refctd_ptr(lodLibraryDSLayout),
+				core::smart_refctd_ptr(transientInputDSLayout),
+				core::smart_refctd_ptr(transientOutputDSLayout),
+				core::smart_refctd_ptr(customDSLayout)
+			);
+			indirectInstanceCullAndLoDSelectLayout = sharedPipelineLayout;
+
+			auto loadShader = []() -> core::smart_refctd_ptr<video::IGPUSpecializedShader>
+			{
+				return nullptr;
+			};
+			//directInstanceCullAndLoDSelect = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(directInstanceCullAndLoDSelectLayout),loadShader());
+			//indirectInstanceCullAndLoDSelect = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(indirectInstanceCullAndLoDSelectLayout),loadShader());
+			//instanceDrawCull = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(sharedPipelineLayout),loadShader());
+			//drawInstanceCountPrefixSum = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(sharedPipelineLayout),loadShader());
+			instanceRefCountingSortScatter = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(sharedPipelineLayout),loadShader());
+			//drawCompact = device->createGPUComputePipeline(nullptr,core::smart_refctd_ptr(sharedPipelineLayout),loadShader());
 		}
 	protected:
+
 		core::smart_refctd_ptr<video::IGPUPipelineLayout> directInstanceCullAndLoDSelectLayout,indirectInstanceCullAndLoDSelectLayout,sharedPipelineLayout;
 		core::smart_refctd_ptr<video::IGPUComputePipeline> directInstanceCullAndLoDSelect,indirectInstanceCullAndLoDSelect,instanceDrawCull,drawInstanceCountPrefixSum,instanceRefCountingSortScatter,drawCompact;
 };
--- include/nbl/scene/ILevelOfDetailLibrary.h
+++ include/nbl/scene/ILevelOfDetailLibrary.h
@@ -5,6 +5,7 @@
 #ifndef __NBL_SCENE_I_LEVEL_OF_DETAIL_LIBRARY_H_INCLUDED__
 #define __NBL_SCENE_I_LEVEL_OF_DETAIL_LIBRARY_H_INCLUDED__
 
+#include "nbl/video/ILogicalDevice.h"
 #include "nbl/video/utilities/IDrawIndirectAllocator.h"
 
 namespace nbl::scene
@@ -20,34 +21,36 @@ class ILevelOfDetailLibrary : public virtual core::IReferenceCounted
 			float aabbMax[3];
 			uint32_t levelInfoOffsets[1];
 		};
-		// LoD will store a contiguous list of draw_call_t inside itself (past its end)
-		using lod_info_t = uint32_t;
-		struct alignas(16) DefaultLevelInfo
+		struct alignas(16) LoDInfo
 		{
-			float aabbMin[3]; 
-			float distanceSqAtReferenceFoV;
+			float aabbMin[3];
+			uint32_t drawCallInfoCount; // only really need 30 bits for this
 			float aabbMax[3];
-			uint32_t drawCallCount;
+			float distanceSqAtReferenceFoV; // TODO: template and assert alignof==alignof(uint32_t)
+			uint32_t drawCallInfoOffsets[1];
+		};
+		struct alignas(8) DrawCallInfo
+		{
+			uint32_t drawCallDWORDOffset; // only really need 27 bits for this
+			uint32_t aabbCount : 8u; // should probably make the bit allocation a function of max bone count
+			uint32_t aabbOffset : 24u; // could want to give 26 bits here
 		};
 
-		// TODO: Drawcall struct?
-		using draw_call_t = uint32_t;
-		struct CullParameters
+		//
+		struct DefaultLoDParameters
 		{
-			float distanceSq;
+			float distanceSqAtReferenceFoV;
 
-			inline bool operator<(const CullParameters& other) const
+			inline bool operator<(const DefaultLoDParameters& other) const
 			{
-				return distanceSq<other.distanceSq;
+				return distanceSqAtReferenceFoV<other.distanceSqAtReferenceFoV;
 			}
 		};
-		// LoDTable will store a contiguous list of lod_t inside itself (first uint is the count)
-		using lod_table_t = uint32_t;
-
+#if 0
         static inline core::smart_refctd_ptr<ILevelOfDetailLibrary> create(core::smart_refctd_ptr<video::ILogicalDevice>&& _device, const uint32_t tableCapacity, const uint32_t lodCapacity, const uint32_t drawCallCapacity)
         {
 			const uint32_t tableBufferSize = tableCapacity*sizeof(LoDTableInfo)+core::roundUp<uint32_t>((lodCapacity-1u)/tableCapacity+1u,alignof(LoDTableInfo))*sizeof(uint32_t);
-			const uint32_t lodBufferSize = lodCapacity*sizeof(DefaultLevelInfo)+core::roundUp<uint32_t>((drawCallCapacity-1u)/lodCapacity+1u,alignof(DefaultLevelInfo))*sizeof(uint32_t);
+			const uint32_t lodBufferSize = lodCapacity*sizeof(LoDInfo)+core::roundUp<uint32_t>((drawCallCapacity-1u)/lodCapacity+1u,alignof(LoDInfo))*sizeof(uint32_t);
 			if (true) // TODO: some checks and validation before creating?
 				return nullptr;
 
@@ -80,17 +83,78 @@ class ILevelOfDetailLibrary : public virtual core::IReferenceCounted
 		{
 			assert(false); // TODO
 		}
+#endif
+		//
+		static inline core::smart_refctd_ptr<video::IGPUDescriptorSetLayout> createDescriptorSetLayout(video::ILogicalDevice* device)
+		{
+			video::IGPUDescriptorSetLayout::SBinding bindings[DescriptorBindingCount];
+			for (auto i=0u; i<DescriptorBindingCount; i++)
+			{
+				bindings[i].binding = i;
+				bindings[i].type = asset::EDT_STORAGE_BUFFER;
+				bindings[i].count = 1u;
+				bindings[i].stageFlags = asset::ISpecializedShader::ESS_COMPUTE;
+				bindings[i].samplers = nullptr;
+			}
+			return device->createGPUDescriptorSetLayout(bindings,bindings+DescriptorBindingCount);
+		}
+
+		inline const auto getDescriptorSet() const
+		{
+			return m_ds.get();
+		}
+
+		inline const auto& getLodTableInfoBinding() const
+		{
+			return m_lodTableInfos;
+		}
+
+		inline const auto& getLoDTInfoBinding() const
+		{
+			return m_lodInfos;
+		}
+
+		inline const auto& getAABBBinding() const
+		{
+			return m_aabbs;
+		}
 
 	protected:
 		ILevelOfDetailLibrary(core::smart_refctd_ptr<video::ILogicalDevice>&& _device) : m_device(std::move(_device))
 		{
+			auto layout = createDescriptorSetLayout(m_device.get());
+			auto pool = m_device->createDescriptorPoolForDSLayouts(video::IDescriptorPool::ECF_NONE,&layout.get(),&layout.get()+1u);
+			m_ds = m_device->createGPUDescriptorSet(pool.get(),std::move(layout));
+			{
+				video::IGPUDescriptorSet::SWriteDescriptorSet writes[DescriptorBindingCount];
+				video::IGPUDescriptorSet::SDescriptorInfo infos[DescriptorBindingCount] =
+				{
+					m_lodTableInfos,
+					m_lodInfos,
+					m_aabbs
+				};
+				for (auto i=0u; i<DescriptorBindingCount; i++)
+				{
+					writes[i].dstSet = m_ds.get();
+					writes[i].binding = i;
+					writes[i].arrayElement = 0u;
+					writes[i].count = 1u;
+					writes[i].descriptorType = asset::EDT_STORAGE_BUFFER;
+					writes[i].info = infos+i;
+				}
+				m_device->updateDescriptorSets(DescriptorBindingCount,writes,0u,nullptr);
+			}
 		}
 		~ILevelOfDetailLibrary()
 		{
 			// everything drops itself automatically
 		}
 
+		static inline constexpr auto DescriptorBindingCount = 3u;
+
 		core::smart_refctd_ptr<video::ILogicalDevice> m_device;
+		core::smart_refctd_ptr<video::IGPUDescriptorSet> m_ds;
+		asset::SBufferRange<video::IGPUBuffer> m_lodTableInfos,m_lodInfos,m_aabbs;
 };
 
 
