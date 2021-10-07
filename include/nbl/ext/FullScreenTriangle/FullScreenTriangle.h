bac68f66f571d88ea504c7317bacdebddaa13f43
achalpandeyy <pandeyachal.ap@gmail.com>
2021-10-07 15:28:18 +0530

    remove unnecessary bindVertexBuffers and bindIndexBuffer calls from FST

--- include/nbl/ext/FullScreenTriangle/FullScreenTriangle.h
+++ include/nbl/ext/FullScreenTriangle/FullScreenTriangle.h
@@ -103,21 +103,6 @@ namespace nbl
 				_NBL_STATIC_INLINE_CONSTEXPR auto VERTEX_COUNT = 3;
 				_NBL_STATIC_INLINE_CONSTEXPR auto INSTANCE_COUNT = 1;
 
-				const nbl::video::IGPUBuffer* gpuBufferBindings[nbl::asset::SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT];
-				{
-					for (size_t i = 0; i < nbl::asset::SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT; ++i)
-						gpuBufferBindings[i] = nullptr;
-				}
-
-				size_t bufferBindingsOffsets[nbl::asset::SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT];
-				{
-					for (size_t i = 0; i < nbl::asset::SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT; ++i)
-						bufferBindingsOffsets[i] = 0;
-				}
-
-				commandBuffer->bindVertexBuffers(0, nbl::asset::SVertexInputParams::MAX_ATTR_BUF_BINDING_COUNT, gpuBufferBindings, bufferBindingsOffsets);
-				commandBuffer->bindIndexBuffer(nullptr, 0, nbl::asset::EIT_UNKNOWN);
-
 				return commandBuffer->draw(VERTEX_COUNT, INSTANCE_COUNT, 0, 0);
 			}
 		}
