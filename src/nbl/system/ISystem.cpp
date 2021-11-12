93c029ccdae06208b983185bbf17fecb1726b69e
Danylo Sadivnychyi <38212505+sadiuk@users.noreply.github.com>
2021-11-12 12:08:14 +0200

    Binary search

--- src/nbl/system/ISystem.cpp
+++ src/nbl/system/ISystem.cpp
@@ -37,14 +37,15 @@ namespace nbl::system
             {
                 auto relative = std::filesystem::relative(_path, path);
                 auto files = archive.second->getArchivedFiles();
-                auto requiredFile = std::find_if(files.begin(), files.end(), [&relative](const IFileArchive::SFileListEntry& entry) { return entry.fullName == relative; });
+                auto itemToFind = IFileArchive::SFileListEntry{ relative, relative, 0 };
+                bool hasFile = std::binary_search(files.begin(), files.end(), itemToFind, [](const IFileArchive::SFileListEntry& l, const IFileArchive::SFileListEntry& r) { return l.fullName == r.fullName; });
                 auto f = archive.second->asFile();
                 if (f)
                 {
                     auto realPath = f->getFileName();
                     auto absolute = (realPath / relative).generic_string();
                     // TODO: file list should be sorted by the path and you should be using a binary search !!!!!!
-                    if (requiredFile != files.end())
+                    if (hasFile)
                     {
                         auto f = archive.second->readFile({ relative, absolute, "" });
                         if (f.get()) return f;
@@ -52,7 +53,7 @@ namespace nbl::system
                 }
                 else
                 {
-                    if (requiredFile != files.end())
+                    if (hasFile)
                     {
                         auto f = archive.second->readFile({ relative, _path, "" });
                         if (f.get()) return f;
