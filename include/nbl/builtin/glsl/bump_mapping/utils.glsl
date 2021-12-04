ac2351fd73043d15f05bc8e8e6845c3ea4ee3db8
devsh <devsh.graphicsprogramming@gmail.com>
2021-12-04 22:50:12 +0100

    fix a mishap with the sqrt

--- include/nbl/builtin/glsl/bump_mapping/utils.glsl
+++ include/nbl/builtin/glsl/bump_mapping/utils.glsl
@@ -6,42 +6,26 @@
 #define _NBL_BUILTIN_GLSL_BUMP_MAPPING_UTILS_INCLUDED_
 
 
-vec3 nbl_glsl_perturbNormal_heightMap(in vec3 vtxN, in mat2x3 dPdQ, in vec2 dHdQ)
+vec3 nbl_glsl_perturbNormal_heightMap(in vec3 vtxN, in vec2 dhdUV, in mat2x3 dPdUV)
 {
-    const vec3 r1 = normalize(cross(vtxN,dPdQ[1]));
-    const vec3 r2 = normalize(cross(dPdQ[0],vtxN));
+    // no idea if this is correct, but seems to work
+    const vec3 r1 = normalize(cross(vtxN,dPdUV[1]));
+    const vec3 r2 = normalize(cross(dPdUV[0],vtxN));
     const float cosInPlane = dot(r1,r2);
-    const vec3 surfGrad = (r1*dHdQ.x+r2*dHdQ.y)/inversesqrt(1.f-cosInPlane*cosInPlane);
-    return normalize(vtxN-surfGrad);
+    const vec3 surfGrad = r1*dhdUV.x+r2*dhdUV.y;
+    return normalize(vtxN*sqrt(1.f-cosInPlane*cosInPlane)+surfGrad);
 }
 
 vec3 nbl_glsl_perturbNormal_derivativeMap(in vec3 vtxN, in vec2 dhdUV, in mat2x3 dPdQ, in mat2 dUVdQ)
 {
-    // apply the chain rule
-    const vec2 dUVdQ1 = normalize(dUVdQ[0]);
-    const vec2 dUVdQ2 = normalize(dUVdQ[1]);
-    const float cs = dot(dUVdQ1,dUVdQ2);
-    const vec2 dHdQ = vec2(dot(dhdUV,dUVdQ1),dot(dhdUV,dUVdQ2))/inversesqrt(1.0-cs*cs);
-    return nbl_glsl_perturbNormal_heightMap(vtxN,dPdQ,dHdQ);
+    // apply the chain rule in reverse
+    const mat2x3 dPdUV = dPdQ*inverse(dUVdQ);
+    return nbl_glsl_perturbNormal_heightMap(vtxN,dhdUV,dPdUV);
 }
 
 #ifdef _NBL_BUILTIN_GLSL_BUMP_MAPPING_DERIVATIVES_DECLARED_
 vec3 nbl_glsl_perturbNormal_derivativeMap(in vec3 vtxN, in vec2 dhdUV)
 {
-    /*
-    mat2x3 dPdQ = nbl_glsl_perturbNormal_dPdSomething();
-    mat2 dUVdQ = nbl_glsl_perturbNormal_dUVdSomething();
-
-	mat2x3 dPdUV = dPdQ*inverse(dUVdQ);
-	
-    const vec3 r1 = cross(vtxN,dPdUV[1]);
-    const float r1len2 = dot(r1,r1);
-    const vec3 r2 = cross(dPdUV[0],vtxN);
-    const float r2len2 = dot(r2,r2);
-
-    vec3 surfGrad = (r1*sqrt(r2len2)*dhdUV.x + r2*sqrt(r1len2)*dhdUV.y)/inversesqrt(r2len2*r1len2-dot(r1,r2)*dot(r1,r2));
-    return normalize(vtxN + surfGrad);
-    */
     return nbl_glsl_perturbNormal_derivativeMap(vtxN,dhdUV,nbl_glsl_perturbNormal_dPdSomething(),nbl_glsl_perturbNormal_dUVdSomething());
 }
 #endif
