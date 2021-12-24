01adab84f994052cf836c6aa34f1eca231d6ffb2
Erfan Ahmadi <ahmadierfan99@gmail.com>
2021-12-24 10:47:35 +0330

    removed denoiser shit from test.bat since It's now in the renderer itself

--- examples_tests/22.RaytracedAO/test.bat
+++ examples_tests/22.RaytracedAO/test.bat
@@ -1,20 +1,12 @@
 @echo off
 
 set pathtracer="raytracedao.exe"
-set denoiser="../../39.DenoiserTonemapper/bin/denoisertonemapper.exe"
 
 pushd bin
 if NOT EXIST %pathtracer% (
     echo BatchScriptError: Pathtracer Executable does not exist. ^(at %pathtracer%^)
     popd
     EXIT /B 0
-)
-
-if NOT EXIST %denoiser% (
-    echo BatchScriptError: Denoiser Executable does not exist. ^(at %denoiser%^)
-    REM Don't Exit when denoiser is not found
-    REM popd
-    REM EXIT /B 0
 )
 popd
 
@@ -33,28 +25,6 @@ pushd bin
 %pathtracer% -SCENE=%1 -TERMINATE
 @echo off
 
-REM throw the first parameter away and iterate through the second to last param to denoise
-shift
-set params=%1
-:loop
-Call :denoise %1
-shift
-if [%1]==[] goto afterloop
-set params=%params% %1
-goto loop
-:afterloop
-
 popd
-
-EXIT /B 0
-
-:denoise
-if NOT EXIST %~dpn1.exr (
-    echo BatchScriptError: Denoiser input file doesn't exist. ^(at %~dpn1.exr^)
-    EXIT /B 0
-)
-@echo on
-%denoiser% -COLOR_FILE=%~dpn1.exr -OUTPUT=%~dpn1_DENOISED.exr -DENOISER_EXPOSURE_BIAS=0.0 -DENOISER_BLEND_FACTOR=0.0 -BLOOM_PSF_FILE=../../media/kernels/physical_flare_512.exr -BLOOM_RELATIVE_SCALE=0.01 -BLOOM_INTENSITY=0.0 -TONEMAPPER=ACES=0.4,0.8 -CAMERA_TRANSFORM=1,0,0,0,1,0,0,0,1
-@echo off
 
 EXIT /B 0
