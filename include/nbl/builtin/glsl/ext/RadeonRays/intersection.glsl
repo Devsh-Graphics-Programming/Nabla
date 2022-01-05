994e388fc1cec8cebac020298d1798336ae07eba
devsh <devsh.graphicsprogramming@gmail.com>
2022-01-05 19:50:03 +0100

    optimize the intersection struct size

--- include/nbl/builtin/glsl/ext/RadeonRays/intersection.glsl
+++ include/nbl/builtin/glsl/ext/RadeonRays/intersection.glsl
@@ -1,19 +1,14 @@
 #ifndef _NBL_EXT_RADEON_RAYS_INTERSECTION_INCLUDED_
 #define _NBL_EXT_RADEON_RAYS_INTERSECTION_INCLUDED_
 
-// for the love of god, lets optimize this into 16 bytes
 struct nbl_glsl_ext_RadeonRays_Intersection
 {
 	// Shape ID
 	int shapeid;
 	// Primitve ID
 	int primid;
-
-	int padding0;
-	int padding1;
-
 	// UV parametrization
-	vec4 uvwt;
+	vec2 uv;
 };
 
 #endif
