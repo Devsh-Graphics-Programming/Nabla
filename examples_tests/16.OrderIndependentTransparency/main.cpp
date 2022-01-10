2311e9c5d272dfef9e60bab0a6f1d728a8fc45df
Erfan <ahmadierfan99@gmail.com>
2022-01-10 16:33:43 +0330

    [EX_16] Change surfaceFormat to work with Android

--- examples_tests/16.OrderIndependentTransparency/main.cpp
+++ examples_tests/16.OrderIndependentTransparency/main.cpp
@@ -14,6 +14,7 @@
 
 using namespace nbl;
 using namespace core;
+using namespace ui;
 /*
     Uncomment for more detailed logging
 */
@@ -173,7 +174,7 @@ public:
         CommonAPI::SFeatureRequest< video::ILogicalDevice::E_FEATURE> optionalDeviceFeatures = {};
 
         const auto swapchainImageUsage = static_cast<asset::IImage::E_USAGE_FLAGS>(asset::IImage::EUF_COLOR_ATTACHMENT_BIT);
-        const video::ISurface::SFormat surfaceFormat(asset::EF_B8G8R8A8_SRGB, asset::ECP_COUNT, asset::EOTF_UNKNOWN);
+        const video::ISurface::SFormat surfaceFormat(asset::EF_R8G8B8A8_SRGB, asset::ECP_COUNT, asset::EOTF_UNKNOWN);
 
         CommonAPI::InitOutput initOutput;
         initOutput.window = core::smart_refctd_ptr(window);
