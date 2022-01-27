4298508cba6b03e4c05eb861d1ec238652cef5c8
devsh <devsh.graphicsprogramming@gmail.com>
2022-01-27 14:00:05 +0100

    tweak the sun pre-filtering trick

--- examples_tests/22.RaytracedAO/raytraceCommon.glsl
+++ examples_tests/22.RaytracedAO/raytraceCommon.glsl
@@ -396,7 +396,7 @@ void Contribution_initMiss(out Contribution contrib)
 {
 	vec2 uv = SampleSphericalMap(-normalizedV);
 	// funny little trick borrowed from things like Progressive Photon Mapping
-	const float bias = 0.25*sqrt(pc.cummon.rcpFramesDispatched);
+	const float bias = 0.0625f*pow(pc.cummon.rcpFramesDispatched,0.08f);
 	contrib.albedo = contrib.color = textureGrad(envMap, uv, vec2(bias*0.5,0.f), vec2(0.f,bias)).rgb;
 	contrib.worldspaceNormal = normalizedV;
 }
