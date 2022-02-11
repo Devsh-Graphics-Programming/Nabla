6dfc5108ba2e9d4d9ab0eddb7fae5225cfa9536d
Danylo Sadivnychyi <38212505+sadiuk@users.noreply.github.com>
2022-02-11 11:35:56 +0200

    Added the assert after 3 months

--- src/nbl/system/IFileArchive.cpp
+++ src/nbl/system/IFileArchive.cpp
@@ -4,6 +4,7 @@ namespace nbl::system
 {
 	void IFileArchive::setFlagsVectorSize(size_t fileCount)
 	{
+		assert(!m_filesBuffer && !m_fileFlags);
 		m_filesBuffer = (std::byte*)_NBL_ALIGNED_MALLOC(fileCount * SIZEOF_INNER_ARCHIVE_FILE, ALIGNOF_INNER_ARCHIVE_FILE);
 		m_fileFlags = (std::atomic_flag*)_NBL_ALIGNED_MALLOC(fileCount * sizeof(std::atomic_flag), alignof(std::atomic_flag));
 		for (int i = 0; i < fileCount; i++)
