4eea3e6b946044c2c176428e423fa12cf5595ad9
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2022-02-22 23:58:09 +0100

    t14

--- examples_tests/43.SumAndCDFFilters/main.cpp
+++ examples_tests/43.SumAndCDFFilters/main.cpp
@@ -11,7 +11,7 @@ using namespace nbl;
 using namespace core;
 using namespace asset;
 using namespace video;
-
+zz
 /*
 	Comment IMAGE_VIEW define to use ordinary cpu image.
 	You can view the results in Renderdoc.
