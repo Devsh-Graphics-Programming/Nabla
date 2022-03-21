b417b72536aa6d1a2fde0a81b8a6f0406dfbd731
Erfan <ahmadierfan99@gmail.com>
2022-03-21 14:26:10 +0330

    added envmap.glsl

---
+++ include/nbl/builtin/glsl/sampling/envmap.glsl
@@ -0,0 +1,15 @@
+#ifndef _NBL_BUILTIN_GLSL_SAMPLING_ENVMAP_INCLUDED_
+#define _NBL_BUILTIN_GLSL_SAMPLING_ENVMAP_INCLUDED_
+
+#include <nbl/builtin/glsl/math/constants.glsl>
+
+vec2 nbl_glsl_sampling_generateUVCoordFromDirection(vec3 v)
+{
+    vec2 uv = vec2(atan(v.z, v.x), acos(v.y));
+    uv.x *= nbl_glsl_RECIPROCAL_PI*0.5;
+    uv.x += 0.5; 
+    uv.y *= nbl_glsl_RECIPROCAL_PI;
+    return uv;
+}
+
+#endif
