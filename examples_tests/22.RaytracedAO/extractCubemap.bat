88a749bb01e0a27834562bd0018e8fc830ad721a
Erfan <ahmadierfan99@gmail.com>
2022-04-25 20:25:25 +0430

    extractCubeMap batch file uses cropOffset and Size to Extract faces

--- examples_tests/22.RaytracedAO/extractCubemap.bat
+++ examples_tests/22.RaytracedAO/extractCubemap.bat
@@ -1,33 +1,40 @@
 @echo off
 
 REM examplary usage: 
-REM mergeCubemap.bat 50 mergedImage.png right.png left.png top.png bottom.png front.png back.png
+REM mergeCubemap.bat 64 64 1024 1024 mergedImage.png right.png left.png top.png bottom.png front.png back.png
 
-set borderSz=%1
+set cropOffsetX=%1
+set cropOffsetY=%2
 
-set img=%2
+set cropWidth=%3
+set cropHeight=%4
 
-set right=%3
-set left=%4
-set top=%5
-set bottom=%6
-set front=%7
-set back=%8
+set img=%5
+
+set right=%6
+set left=%7
+set top=%8
+set bottom=%9
+
+REM you can't do %10 %11 in batch file
+shift
+set front=%9
+shift
+set back=%9
 
 REM set extracted image size
 for /f "tokens=*" %%s in ('magick identify -format "%%w" %img%') do set sz=%%s
 set /a imgSz=sz/3
-set /a extractedImgSz=imgSz-2*borderSz
 
-set /a x0 = borderSz
-set /a x1 = imgSz+borderSz
-set /a x2 = 2*imgSz+borderSz
-set /a y0 = borderSz
-set /a y1 = imgSz+borderSz
+set /a x0 = cropOffsetX
+set /a x1 = imgSz+cropOffsetX
+set /a x2 = 2*imgSz+cropOffsetX
+set /a y0 = cropOffsetY
+set /a y1 = imgSz+cropOffsetY
 
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x0%+%y0% %right%
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x1%+%y0% %left%
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x2%+%y0% %top%
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x0%+%y1% %bottom%
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x1%+%y1% %front%
-magick convert %img% -crop %extractedImgSz%x%extractedImgSz%+%x2%+%y1% %back%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x0%+%y0% %right%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x1%+%y0% %left%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x2%+%y0% %top%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x0%+%y1% %bottom%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x1%+%y1% %front%
+magick convert %img% -crop %cropWidth%x%cropHeight%+%x2%+%y1% %back%
