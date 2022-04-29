2a8afbc2ee2eeaab977e47538834f3c2c411d18c
AnastaZIuk <areklachowicz@gmail.com>
2022-04-30 00:42:16 +0200

    Log all data status to html, change examples_tests submodule pointer

--- ci/ditt/main.py
+++ ci/ditt/main.py
@@ -13,6 +13,106 @@ NBL_CI_REFERENCES_DIR = Path(str(NBL_CI_WORKING_DIR.absolute()) + '/references')
 NBL_CI_LDS_CACHE_FILENAME = 'LowDiscrepancySequenceCache.bin'
 CI_PASS_STATUS = True
 
+HTML_TUPLE_RENDER_INDEX = 0
+HTML_TUPLE_PASS_STATUS_INDEX = 1
+HTML_TUPLE_REFERENCE_INDEX = 2
+HTML_TUPLE_ALBEDO_INDEX = 3
+HTML_TUPLE_NORMAL_INDEX = 4
+HTML_TUPLE_DENOISED_INDEX = 5
+
+HTML_R_A_N_D_D_DIFF = 0
+HTML_R_A_N_D_D_ERROR = 1
+HTML_R_A_N_D_D_PASS = 2
+
+def generateHTMLStatus(_htmlData, _cacheChanged):
+    HTML_BODY = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+      text-align: center;
+    }
+    
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+    
+    table, th, td {
+        border: 1px solid black;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <h2>Ditt Render Scenes job status</h2>
+    
+    '''
+
+    if _cacheChanged:
+        HTML_BODY += '''
+        <h2 style="color: red;">FAILED PASS: Low Discrepancy Sequence Cache has been overwritten by a new one!</h2>
+        
+        '''
+    HTML_BODY += '''
+    <table>
+      <tr>
+        <th>Render</th>
+        <th>Pass status</th>
+        <th colspan="3" scope="colgroup">Reference</th>
+        <th colspan="3" scope="colgroup">Albedo</th>
+        <th colspan="3" scope="colgroup">Normal</th>
+        <th colspan="3" scope="colgroup">Denoised</th>
+      </tr>
+    '''
+
+    for _htmlRowTuple in _htmlData:
+        HTML_ROW_BODY = '''
+        <tr>
+          <td>''' + _htmlRowTuple[HTML_TUPLE_RENDER_INDEX] '</td>'
+
+        if _htmlRowTuple[HTML_TUPLE_PASS_STATUS_INDEX]:
+            HTML_ROW_BODY += '<td style="color: green;">PASSED</td>'
+        else
+            HTML_ROW_BODY += '<td style="color: red;">FAILED</td>'
+
+        for i in range(4):
+            anIndexOfRenderAspect = i + HTML_TUPLE_REFERENCE_INDEX
+
+            aspectRenderData = _htmlRowTuple[anIndexOfRenderAspect]
+
+            HTML_ROW_BODY += '<td scope="col">' + '<a href="https://TODO.com">' + aspectRenderData[HTML_R_A_N_D_D_DIFF] + '</a></td>' +
+            '<td scope="col">An error: ' + aspectRenderData[HTML_R_A_N_D_D_ERROR] + '</td>'
+
+            if aspectRenderData[HTML_R_A_N_D_D_PASS]:
+                HTML_ROW_BODY += '<td scope="col" style="color: green;">PASSED</td>'
+            else
+                HTML_ROW_BODY += '<td scope="col" style="color: red;">FAILED</td>'
+        HTML_ROW_BODY += '</tr>'
+
+        HTML_BODY += HTML_ROW_BODY
+
+    HTML_BODY += '''
+    </table>
+    
+    </body>
+    </html>
+    '''
+
+    htmlFile = open(NBL_CI_WORKING_DIR + '/index.html', "w")
+    htmlFile.write(HTML_BODY)
+    htmlFile.close()
+
+
 if __name__ == '__main__':
     if NBL_PATHTRACER_EXE.is_file() and NBL_SCENES_INPUT_TXT.is_file():
         with open(NBL_SCENES_INPUT_TXT.absolute()) as aFile:
@@ -23,10 +123,16 @@ if __name__ == '__main__':
         if not NBL_CI_REFERENCES_DIR.is_dir():
             os.makedirs(str(NBL_CI_REFERENCES_DIR.absolute()))
 
+        htmlData = []
+        cacheChanged = False
+
         for line in inputLines:
             if list(line)[0] != ';':
+
+                htmlRowTuple = ('', True, ['', '', True], ['', '', True], ['', '', True], ['', '', True])
+
                 renderPath = line.strip().replace('"', '').split()[0]
-                renderName = os.path.splitext(str(Path(renderPath).name))[0]
+                htmlRowTuple[HTML_TUPLE_RENDER_INDEX] = renderName = os.path.splitext(str(Path(renderPath).name))[0]
                 undenoisedTargetName = 'Render_' + renderName + '_scene'
 
                 NBL_DUMMY_CACHE_CASE = not bool(Path(str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME).is_file())
@@ -62,8 +168,10 @@ if __name__ == '__main__':
 
                 # fail CI if the reference cache is different that current generated cache
                 if not filecmp.cmp(destinationReferenceCache, generatedReferenceCache):
+                    cacheChanged = True
                     CI_PASS_STATUS = False
 
+                anIndex = HTML_TUPLE_REFERENCE_INDEX
                 outputDiffTerminators = ['', '_albedo', '_normal', '_denoised']
                 for diffTerminator in outputDiffTerminators:
                     diffImageFilepath = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + renderName + diffTerminator + '_diff.png'
@@ -77,8 +185,18 @@ if __name__ == '__main__':
                     diffValueFile.close()
 
                     # threshold for an error, for now we fail CI on any difference
-                    if float(magicDecodeValue) != 0.0:
+                    DIFF_PASS = float(magicDecodeValue) == 0.0
+                    if not DIFF_PASS:
                         CI_PASS_STATUS = False
+                        htmlRowTuple[HTML_TUPLE_PASS_STATUS_INDEX] = False
+
+                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_DIFF] = renderName + diffTerminator + '_diff.png'
+                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_ERROR] = str(magicDecodeValue)
+                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_PASS] = DIFF_PASS
+                    ++anIndex
+                htmlData.append(htmlRowTuple)
+
+        generateHTMLStatus(htmlData, cacheChanged)
     else:
         print('Path tracer executable or scenes input does not exist!')
         exit(-1)
