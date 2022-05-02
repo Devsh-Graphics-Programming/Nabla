2e91faccef55b63f79a31a2098480f34051a2dd9
AnastaZIuk <areklachowicz@gmail.com>
2022-05-02 17:02:12 +0200

    make it possible to produce Nabla.dll with statically linked 3rdparties to it

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -41,14 +41,12 @@ macro(nbl_create_executable_project _EXTRA_SOURCES _EXTRA_OPTIONS _EXTRA_INCLUDE
 	else()
 		add_executable(${EXECUTABLE_NAME} main.cpp ${_EXTRA_SOURCES})
 		
-		if(NBL_STATIC_BUILD)
-			set_property(TARGET ${EXECUTABLE_NAME} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-		else()
+		if(NBL_DYNAMIC_MSVC_RUNTIME)
 			set_property(TARGET ${EXECUTABLE_NAME} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+		else()
+			set_property(TARGET ${EXECUTABLE_NAME} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
 		endif()
 	endif()
-	
-	
 	
 	# EXTRA_SOURCES is var containing non-common names of sources (if any such sources, then EXTRA_SOURCES must be set before including this cmake code)
 	add_dependencies(${EXECUTABLE_NAME} Nabla)
