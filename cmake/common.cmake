3fab7afe8afef33c1555fe2d385a47c8010672bf
AnastaZIuk <areklachowicz@gmail.com>
2022-05-02 15:36:23 +0200

    save work (I need to switch to master for a while), introduce NBL_STATIC_BUILD and NBL_DYNAMIC_MSVC_RUNTIME CMake options

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -40,10 +40,15 @@ macro(nbl_create_executable_project _EXTRA_SOURCES _EXTRA_OPTIONS _EXTRA_INCLUDE
 		add_library(${EXECUTABLE_NAME} SHARED main.cpp ${_EXTRA_SOURCES})
 	else()
 		add_executable(${EXECUTABLE_NAME} main.cpp ${_EXTRA_SOURCES})
+		
+		if(NBL_STATIC_BUILD)
+			set_property(TARGET ${EXECUTABLE_NAME} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+		else()
+			set_property(TARGET ${EXECUTABLE_NAME} PROPERTY MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+		endif()
 	endif()
 	
-	set_property(TARGET ${EXECUTABLE_NAME} PROPERTY
-             MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+	
 	
 	# EXTRA_SOURCES is var containing non-common names of sources (if any such sources, then EXTRA_SOURCES must be set before including this cmake code)
 	add_dependencies(${EXECUTABLE_NAME} Nabla)
