3af6717ec176c3dfeafbfc1bdd235eeef290a3a5
Erfan <ahmadierfan99@gmail.com>
2022-07-27 12:57:25 +0430

    ICPUBuffer comments and indentation

--- include/nbl/asset/ICPUBuffer.h
+++ include/nbl/asset/ICPUBuffer.h
@@ -19,10 +19,10 @@ namespace nbl::asset
 
 //! One of CPU class-object representing an Asset
 /**
-	One of Assets used for storage of large arrays, so that storage can be decoupled
-	from other objects such as meshbuffers, images, animations and shader source/bytecode.
+    One of Assets used for storage of large arrays, so that storage can be decoupled
+    from other objects such as meshbuffers, images, animations and shader source/bytecode.
 
-	@see IAsset
+    @see IAsset
 */
 class NBL_API ICPUBuffer : public asset::IBuffer, public asset::IAsset
 {
@@ -36,12 +36,12 @@ class NBL_API ICPUBuffer : public asset::IBuffer, public asset::IAsset
         ICPUBuffer(size_t sizeInBytes, void* dat) : size(dat ? sizeInBytes : 0), data(dat), usage(EUF_TRANSFER_DST_BIT)
         {}
     public:
-		//! Constructor.
-		/** @param sizeInBytes Size in bytes. If `dat` argument is present, it denotes size of data pointed by `dat`, otherwise - size of data to be allocated.
-		*/
+        //! Constructor.
+        /** @param sizeInBytes Size in bytes. If `dat` argument is present, it denotes size of data pointed by `dat`, otherwise - size of data to be allocated.
+        */
         ICPUBuffer(size_t sizeInBytes) : size(0), usage(EUF_TRANSFER_DST_BIT)
         {
-			data = _NBL_ALIGNED_MALLOC(sizeInBytes,_NBL_SIMD_ALIGNMENT);
+            data = _NBL_ALIGNED_MALLOC(sizeInBytes,_NBL_SIMD_ALIGNMENT);
             if (!data)
                 return;
 
@@ -78,7 +78,7 @@ class NBL_API ICPUBuffer : public asset::IBuffer, public asset::IAsset
         //! Returns size in bytes.
         virtual uint64_t getSize() const override {return size;}
 
-		//! Returns pointer to data.
+        //! Returns pointer to data.
         virtual const void* getPointer() const {return data;}
         virtual void* getPointer() 
         { 
@@ -95,30 +95,30 @@ class NBL_API ICPUBuffer : public asset::IBuffer, public asset::IAsset
             return true;
         }
         
-		inline core::bitflag<E_USAGE_FLAGS> getUsageFlags() const
-		{
-			return usage;
-		}
-		inline bool setUsageFlags(core::bitflag<E_USAGE_FLAGS> _usage)
-		{
-			assert(!isImmutable_debug());
-			usage = _usage;
-			return true;
-		}
-		inline bool addUsageFlags(core::bitflag<E_USAGE_FLAGS> _usage)
-		{
-			assert(!isImmutable_debug());
-			usage |= _usage;
-			return true;
-		}
+        inline core::bitflag<E_USAGE_FLAGS> getUsageFlags() const
+        {
+            return usage;
+        }
+        inline bool setUsageFlags(core::bitflag<E_USAGE_FLAGS> _usage)
+        {
+            assert(!isImmutable_debug());
+            usage = _usage;
+            return true;
+        }
+        inline bool addUsageFlags(core::bitflag<E_USAGE_FLAGS> _usage)
+        {
+            assert(!isImmutable_debug());
+            usage |= _usage;
+            return true;
+        }
         
-		inline bool getCanUpdateSubRange() const {return canUpdateSubRange;}
-		inline bool setCanUpdateSubRange(const bool _canUpdateSubRange)
-		{
-			assert(!isImmutable_debug());
+        inline bool getCanUpdateSubRange() const {return canUpdateSubRange;}
+        inline bool setCanUpdateSubRange(const bool _canUpdateSubRange)
+        {
+            assert(!isImmutable_debug());
             canUpdateSubRange = _canUpdateSubRange;
-			return true;
-		}
+            return true;
+        }
 
     protected:
         void restoreFromDummy_impl(IAsset* _other, uint32_t _levelsBelow) override
@@ -156,51 +156,53 @@ class NBL_API CCustomAllocatorCPUBuffer;
     but a \inull allocator\i doesn't do anything, even free the memory, so you're all good).
 */
 
+// ! Specialization where Allocator == core::null_allocator
 template<typename Allocator>
 class NBL_API CCustomAllocatorCPUBuffer<Allocator, true> : public ICPUBuffer
 {
-		static_assert(sizeof(typename Allocator::value_type) == 1u, "Allocator::value_type must be of size 1");
-	protected:
-		Allocator m_allocator;
+        static_assert(sizeof(typename Allocator::value_type) == 1u, "Allocator::value_type must be of size 1");
+    protected:
+        Allocator m_allocator;
 
         virtual ~CCustomAllocatorCPUBuffer()
         {
             this->convertToDummyObject();
         }
 
-	public:
-		CCustomAllocatorCPUBuffer(size_t sizeInBytes, void* dat, core::adopt_memory_t, Allocator&& alctr = Allocator()) : ICPUBuffer(sizeInBytes, dat), m_allocator(std::move(alctr))
-		{
-		}
+    public:
+        CCustomAllocatorCPUBuffer(size_t sizeInBytes, void* dat, core::adopt_memory_t, Allocator&& alctr = Allocator()) : ICPUBuffer(sizeInBytes, dat), m_allocator(std::move(alctr))
+        {
+        }
 
-		virtual void convertToDummyObject(uint32_t referenceLevelsBelowToConvert = 0u) override
-		{
+        virtual void convertToDummyObject(uint32_t referenceLevelsBelowToConvert = 0u) override
+        {
             if (isDummyObjectForCacheAliasing)
                 return;
             convertToDummyObject_common(referenceLevelsBelowToConvert);
             if (!canBeConvertedToDummy())
                 return;
 
-			if (ICPUBuffer::data)
-				m_allocator.deallocate(reinterpret_cast<typename Allocator::pointer>(ICPUBuffer::data), ICPUBuffer::size);
-			ICPUBuffer::data = nullptr; // so that ICPUBuffer won't try deallocating
-		}
+            if (ICPUBuffer::data)
+                m_allocator.deallocate(reinterpret_cast<typename Allocator::pointer>(ICPUBuffer::data), ICPUBuffer::size);
+            ICPUBuffer::data = nullptr; // so that ICPUBuffer won't try deallocating
+        }
 };
 
+// ! Specialization where Allocator != core::null_allocator
 template<typename Allocator>
 class NBL_API CCustomAllocatorCPUBuffer<Allocator, false> : public CCustomAllocatorCPUBuffer<Allocator, true>
 {
-		using Base = CCustomAllocatorCPUBuffer<Allocator, true>;
-	protected:
-		virtual ~CCustomAllocatorCPUBuffer() = default;
+        using Base = CCustomAllocatorCPUBuffer<Allocator, true>;
+    protected:
+        virtual ~CCustomAllocatorCPUBuffer() = default;
 
-	public:
-		using Base::Base;
+    public:
+        using Base::Base;
 
-		CCustomAllocatorCPUBuffer(size_t sizeInBytes, const void* dat, Allocator&& alctr = Allocator()) : Base(sizeInBytes, alctr.allocate(sizeInBytes), core::adopt_memory, std::move(alctr))
-		{
-			memcpy(Base::data, dat, sizeInBytes);
-		}
+        CCustomAllocatorCPUBuffer(size_t sizeInBytes, const void* dat, Allocator&& alctr = Allocator()) : Base(sizeInBytes, alctr.allocate(sizeInBytes), core::adopt_memory, std::move(alctr))
+        {
+            memcpy(Base::data, dat, sizeInBytes);
+        }
 };
 
 } // end namespace nbl::asset
