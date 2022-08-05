11a3f043bcaab83dbd86101ca9cd50bdecceb5aa
Erfan <ahmadierfan99@gmail.com>
2022-08-05 08:28:09 +0430

    Small Fix + update examples submodule

--- src/nbl/video/CVulkanSwapchain.cpp
+++ src/nbl/video/CVulkanSwapchain.cpp
@@ -139,11 +139,13 @@ ISwapchain::E_PRESENT_RESULT CVulkanSwapchain::present(IGPUQueue* queue, const S
     auto* vk = static_cast<const CVulkanLogicalDevice*>(logicalDevice)->getFunctionTable();
 
     auto vk_queue = static_cast<const CVulkanQueue*>(queue)->getInternalObject();
+    
+    constexpr uint32_t MaxWaitSemaphores = 100u;
 
-    if (info.waitSemaphoreCount > 100)
+    if (info.waitSemaphoreCount > MaxWaitSemaphores)
         return ISwapchain::EPR_ERROR;
   
-    VkSemaphore vk_waitSemaphores[100];
+    VkSemaphore vk_waitSemaphores[MaxWaitSemaphores];
     for (uint32_t i = 0u; i < info.waitSemaphoreCount; ++i)
     {
         if (info.waitSemaphores[i]->getAPIType() != EAT_VULKAN)
@@ -168,6 +170,8 @@ ISwapchain::E_PRESENT_RESULT CVulkanSwapchain::present(IGPUQueue* queue, const S
         return ISwapchain::EPR_SUCCESS;
     case VK_SUBOPTIMAL_KHR:
         return ISwapchain::EPR_SUBOPTIMAL;
+    case VK_ERROR_OUT_OF_DATE_KHR:
+        std::cout << "VK_ERROR_OUT_OF_DATE_KHR\n***\n***\n***\n" << std::endl;
     default:
         return ISwapchain::EPR_ERROR;
     }
