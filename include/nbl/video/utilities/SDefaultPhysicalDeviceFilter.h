8b616ddd29fbdf5359ae2b3a675c5b53aceb22d2
Erfan <ahmadierfan99@gmail.com>
2022-09-06 16:44:14 +0430

    small edits to PhysicalDeviceFilter

--- include/nbl/video/utilities/SDefaultPhysicalDeviceFilter.h
+++ include/nbl/video/utilities/SDefaultPhysicalDeviceFilter.h
@@ -12,29 +12,30 @@ namespace nbl::video
         core::bitflag<IPhysicalDevice::E_DRIVER_ID>     driverIDMask = core::bitflag<IPhysicalDevice::E_DRIVER_ID>(0xffff'ffffu);
         VkConformanceVersion                            minConformanceVersion = {0u, 0u, 0u, 0u};
         IPhysicalDevice::SLimits                        minimumLimits = {}; // minimum required limits to be satisfied
-        IPhysicalDevice::SFeatures                      requiredFeatures = {};
+        IPhysicalDevice::SFeatures                      requiredFeatures = {}; // required features, will be also used to enable logical device features
         IPhysicalDevice::SFormatBufferUsages            requiredBufferFormatUsages = {};
         IPhysicalDevice::SFormatImageUsages             requiredImageFormatUsagesLinearTiling = {};
         IPhysicalDevice::SFormatImageUsages             requiredImageFormatUsagesOptimalTiling = {};
 
-        // TODO: memory requirements
-        /*
-        using RequiredMemoryType = core::bitflag<IDeviceMemoryAllocation::E_MEMORY_PROPERTY_FLAGS>;
-        core::set<RequiredMemoryType,size_t> requiredMemoryTypesWithMinimumSizes;
-        */
-        
-        // TODO: queue requirements
-        /*
-        struct QueueSiblings
+        struct MemoryRequirement
         {
-            core::bitflag<E_QUEUE_FLAGS> requiredFlags = NONE;
-            core::bitflag<E_QUEUE_FLAGS> disallowedFlags = NONE;
+            core::bitflag<IDeviceMemoryAllocation::E_MEMORY_PROPERTY_FLAGS> memoryFlags = IDeviceMemoryAllocation::E_MEMORY_PROPERTY_FLAGS::EMPF_NONE;
+            size_t size = 0ull;
+        };
+        MemoryRequirement* memoryRequirements = nullptr;
+        uint32_t memoryRequirementsCount = 0u;
+        
+        /*
+        struct QueuesRequirement
+        {
+            core::bitflag<IPhysicalDevice::E_QUEUE_FLAGS> requiredFlags = IPhysicalDevice::E_QUEUE_FLAGS::EQF_NONE;
+            core::bitflag<IPhysicalDevice::E_QUEUE_FLAGS> disallowedFlags = IPhysicalDevice::E_QUEUE_FLAGS::EQF_NONE;
             uint32_t queueCount = 0u;
             // family's transfer granularity needs to be <=
             asset::VkExtent3D maxImageTransferGranularity = {0x80000000u,0x80000000u,0x80000000u};
         };
         */
-        
+
         // To determine whether a queue family of a physical device supports presentation to a given surface
         //  See vkGetPhysicalDeviceSurfaceSupportKHR
         struct SurfaceCompatibility
@@ -43,7 +44,7 @@ namespace nbl::video
             // Setting this to `EQF_NONE` means it sufffices to find any queue family that can present to this surface, regardless of flags it might have
             core::bitflag<IPhysicalDevice::E_QUEUE_FLAGS> presentationQueueFlags = IPhysicalDevice::E_QUEUE_FLAGS::EQF_NONE;
         };
-        SurfaceCompatibility * requiredSurfaceCompatibilities = nullptr;
+        SurfaceCompatibility* requiredSurfaceCompatibilities = nullptr;
         uint32_t requiredSurfaceCompatibilitiesCount = 0u;
 
         bool meetsRequirements(const IPhysicalDevice * const physicalDevice) const
@@ -90,6 +91,7 @@ namespace nbl::video
             if (!requiredImageFormatUsagesOptimalTiling.isSubsetOf(physicalDevice->getImageFormatUsagesOptimalTiling()))
                 return false;
 
+            // Surface Compatibility
             if (requiredSurfaceCompatibilities != nullptr)
             {
                 for (uint32_t i = 0u; i < requiredSurfaceCompatibilitiesCount; ++i)
