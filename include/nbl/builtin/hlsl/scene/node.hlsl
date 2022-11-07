bcb9f7756e365a9290fa9d62d13885442eaea053
nahim-yay <107263862+nahim-yay@users.noreply.github.com>
2022-11-07 08:11:16 +0100

    Update node.hlsl

--- include/nbl/builtin/hlsl/scene/node.hlsl
+++ include/nbl/builtin/hlsl/scene/node.hlsl
@@ -1,4 +1,8 @@
 
+// Copyright (C) 2018-2022 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+
 #ifndef _NBL_HLSL_SCENE_NODE_INCLUDED_
 #define _NBL_HLSL_SCENE_NODE_INCLUDED_
 
