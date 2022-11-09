8096df283b7772508fc712d8c12a5ab21f4db9d4
Erfan <ahmadierfan99@gmail.com>
2022-11-09 13:43:52 +0330

    fix #ifdef

--- include/nbl/asset/utils/IShaderCompiler.h
+++ include/nbl/asset/utils/IShaderCompiler.h
@@ -2,8 +2,8 @@
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
 
-#ifndef NBL_ASSET_I_SHADER_COMPILER_H_INCLUDED
-#define NBL_ASSET_I_SHADER_COMPILER_H_INCLUDED
+#ifndef _NBL_ASSET_I_SHADER_COMPILER_H_INCLUDED_
+#define _NBL_ASSET_I_SHADER_COMPILER_H_INCLUDED_
 
 #include "nbl/core/declarations.h"
 #include "nbl/system/declarations.h"
