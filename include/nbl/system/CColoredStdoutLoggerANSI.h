0ba2cf9b820d05538625b22726044902da7c26c2
Mateusz Kielan <devsh.graphicsprogramming@gmail.com>
2023-02-19 07:49:16 +0100

    Update CColoredStdoutLoggerANSI.h
    do indendation Nabla style and use the correct `NBL_API2` for function export

--- include/nbl/system/CColoredStdoutLoggerANSI.h
+++ include/nbl/system/CColoredStdoutLoggerANSI.h
@@ -1,25 +1,21 @@
-#ifndef _NBL_SYSTEM_C_COLORFUL_STDOUT_LOGGER_ANSI_INCLUDED_
-#define _NBL_SYSTEM_C_COLORFUL_STDOUT_LOGGER_ANSI_INCLUDED_
+#ifndef _NBL_SYSTEM_C_COLORED_STDOUT_LOGGER_ANSI_INCLUDED_
+#define _NBL_SYSTEM_C_COLORED_STDOUT_LOGGER_ANSI_INCLUDED_
 
 #include "nbl/system/IThreadsafeLogger.h"
 
 #include <string_view>
 
-namespace nbl::system {
-
-#include "nbl/system/DefaultFuncPtrLoader.h"
+namespace nbl::system
+{
 
 // logging using ANSI escape codes
-class NBL_API CColoredStdoutLoggerANSI : public IThreadsafeLogger {
+class NBL_API2 CColoredStdoutLoggerANSI : public IThreadsafeLogger
+{
+  public:
+    CColoredStdoutLoggerANSI(core::bitflag<E_LOG_LEVEL> logLevelMask = ILogger::defaultLogMask()) : IThreadsafeLogger(logLevelMask) {}
 
-public:
-  CColoredStdoutLoggerANSI(
-      core::bitflag<E_LOG_LEVEL> logLevelMask = ILogger::defaultLogMask())
-      : IThreadsafeLogger(logLevelMask) {}
-
-private:
-  virtual void threadsafeLog_impl(const std::string_view &fmt,
-                                  E_LOG_LEVEL logLevel, va_list args) override;
+  private:
+    virtual void threadsafeLog_impl(const std::string_view &fmt, E_LOG_LEVEL logLevel, va_list args) override;
 };
 
 } // namespace nbl::system
