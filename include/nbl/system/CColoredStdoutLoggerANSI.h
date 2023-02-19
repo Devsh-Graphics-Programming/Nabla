5be2f38fa7171f5ebaf67f85500ba8668255d0b2
Mateusz Kielan <devsh.graphicsprogramming@gmail.com>
2023-02-19 07:58:00 +0100

    Update CColoredStdoutLoggerANSI.h
    stick everything in the header since its not OS-specific

--- include/nbl/system/CColoredStdoutLoggerANSI.h
+++ include/nbl/system/CColoredStdoutLoggerANSI.h
@@ -15,7 +15,33 @@ class NBL_API2 CColoredStdoutLoggerANSI : public IThreadsafeLogger
     CColoredStdoutLoggerANSI(core::bitflag<E_LOG_LEVEL> logLevelMask = ILogger::defaultLogMask()) : IThreadsafeLogger(logLevelMask) {}
 
   private:
-    virtual void threadsafeLog_impl(const std::string_view &fmt, E_LOG_LEVEL logLevel, va_list args) override;
+    // more info about how this works: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+    virtual void threadsafeLog_impl(const std::string_view &fmt, E_LOG_LEVEL logLevel, va_list args) override
+    {
+      auto str = constructLogString(fmt, logLevel, args);
+      switch (logLevel)
+      {
+        case ELL_DEBUG:
+          printf("\x1b[37m%s", str.data()); // White
+          break;
+        case ELL_INFO:
+          printf("\x1b[37m%s", str.data()); // White
+          break;
+        case ELL_WARNING:
+          printf("\x1b[33m%s", str.data()); // yellow
+          break;
+        case ELL_ERROR:
+          printf("\x1b[31m%s", str.data()); // red
+          break;
+        case ELL_PERFORMANCE:
+          printf("\x1b[34m%s", str.data()); // blue
+          break;
+        case ELL_NONE:
+          assert(false);
+          break;
+      }
+      fflush(stdout);
+    }
 };
 
 } // namespace nbl::system
