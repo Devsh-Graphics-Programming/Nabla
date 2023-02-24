c4b8d7adcd920ad0db7b2e2351d52811e323a38f
Michael Pollind <mpollind@gmail.com>
2023-02-23 19:42:45 -0800

    tweak header generation for clang and gcc
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- src/nbl/builtin/builtinHeaderGen.py
+++ src/nbl/builtin/builtinHeaderGen.py
@@ -37,7 +37,7 @@ else:
     
     #Iterating through input list
     for x in resourcePaths:
-        outp.write('\n\t\textern template const std::pair<const uint8_t*, size_t> get_resource<typename NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("%s")>();' % x)
+        outp.write('\n\t\textern template const std::pair<const uint8_t*, size_t> get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("%s")>();' % x)
 
     outp.write("\n\t}")
     outp.write("\n#endif")
