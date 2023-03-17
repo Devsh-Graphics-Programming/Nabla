a6ff1ae69d84d5027172abf47e20e04b05bee76a
devsh <devsh.graphicsprogramming@gmail.com>
2023-03-17 22:23:50 +0100

    try not to pollute with windows headers

--- include/nbl/system/DynamicLibraryFunctionPointer.h
+++ include/nbl/system/DynamicLibraryFunctionPointer.h
@@ -1,8 +1,8 @@
-// Copyright (C) 2018-2022 - DevSH Graphics Programming Sp. z O.O.
+// Copyright (C) 2018-2023 - DevSH Graphics Programming Sp. z O.O.
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
-#ifndef __NBL_SYSTEM_DYNAMIC_LIBRARY_FUNCTION_POINTER_H_INCLUDED__
-#define __NBL_SYSTEM_DYNAMIC_LIBRARY_FUNCTION_POINTER_H_INCLUDED__
+#ifndef _NBL_SYSTEM_DYNAMIC_LIBRARY_FUNCTION_POINTER_H_INCLUDED_
+#define _NBL_SYSTEM_DYNAMIC_LIBRARY_FUNCTION_POINTER_H_INCLUDED_
 
 
 #include "nbl/core/declarations.h"
