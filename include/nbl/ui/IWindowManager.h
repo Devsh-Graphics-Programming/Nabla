8097e772e1fb5ffbe4dec725a2432003ef33f5b4
devsh <devsh.graphicsprogramming@gmail.com>
2023-03-17 19:54:08 +0100

    refactor a few windowing things

--- include/nbl/ui/IWindowManager.h
+++ include/nbl/ui/IWindowManager.h
@@ -1,32 +1,26 @@
-#ifndef _NBL_UI_I_WINDOWMANAGER_
-#define _NBL_UI_I_WINDOWMANAGER_
+#ifndef _NBL_UI_I_WINDOWMANAGER_INCLUDED_
+#define _NBL_UI_I_WINDOWMANAGER_INCLUDED_
 
-#include <nbl/core/IReferenceCounted.h>
-#include "IWindow.h"
+#include "nbl/ui/IWindow.h"
 
 namespace nbl::ui
 {
-
-struct SDisplayInfo
-{
-	int32_t x;
-	int32_t y;
-	uint32_t resX;
-	uint32_t resY;
-	std::string name; // this one is really more of a placeholder right now
-};
 
 class NBL_API2 IWindowManager : public core::IReferenceCounted
 {
 	public:
 		virtual core::smart_refctd_ptr<IWindow> createWindow(IWindow::SCreationParams&& creationParams) = 0;
-		virtual SDisplayInfo getPrimaryDisplayInfo() const = 0;
 
-		virtual bool setWindowSize_impl(IWindow* window, uint32_t width, uint32_t height) = 0;
-		virtual bool setWindowPosition_impl(IWindow* window, int32_t x, int32_t y) = 0;
-		virtual bool setWindowRotation_impl(IWindow* window, bool landscape) = 0;
-		virtual bool setWindowVisible_impl(IWindow* window, bool visible) = 0;
-		virtual bool setWindowMaximized_impl(IWindow* window, bool maximized) = 0;
+		//!
+		struct SDisplayInfo
+		{
+			int32_t x;
+			int32_t y;
+			uint32_t resX;
+			uint32_t resY;
+			std::string name; // this one is really more of a placeholder right now
+		};
+		virtual SDisplayInfo getPrimaryDisplayInfo() const = 0;
 
 		inline bool setWindowSize(IWindow* window, const uint32_t width, const uint32_t height)
 		{
@@ -99,6 +93,12 @@ class NBL_API2 IWindowManager : public core::IReferenceCounted
 
 	protected:
 		virtual ~IWindowManager() = default;
+
+		virtual bool setWindowSize_impl(IWindow* window, uint32_t width, uint32_t height) = 0;
+		virtual bool setWindowPosition_impl(IWindow* window, int32_t x, int32_t y) = 0;
+		virtual bool setWindowRotation_impl(IWindow* window, bool landscape) = 0;
+		virtual bool setWindowVisible_impl(IWindow* window, bool visible) = 0;
+		virtual bool setWindowMaximized_impl(IWindow* window, bool maximized) = 0;
 };
 
 }
