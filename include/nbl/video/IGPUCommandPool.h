9d43b5302e091aa1bc5a0fcdb8ba6604f312bf57
Michael Pollind <mpollind@gmail.com>
2023-03-22 20:20:19 -0700

    make delete protected
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- include/nbl/video/IGPUCommandPool.h
+++ include/nbl/video/IGPUCommandPool.h
@@ -39,24 +39,26 @@ public:
         friend class CCommandSegment;
 
     public:
+        virtual ~ICommand() = default;
 
         // static void* operator new(std::size_t size) = delete;
         static void* operator new[](std::size_t size) = delete;
         // static void* operator new(std::size_t size, std::align_val_t al) = delete;
         static void* operator new[](std::size_t size, std::align_val_t al) = delete;
 
-        // static void operator delete  (void* ptr) = delete;
-        static void operator delete[](void* ptr) = delete;
-        static void operator delete  (void* ptr, std::align_val_t al) = delete;
-        static void operator delete[](void* ptr, std::align_val_t al) = delete;
-        static void operator delete  (void* ptr, std::size_t sz) = delete;
-        static void operator delete[](void* ptr, std::size_t sz) = delete;
-        static void operator delete  (void* ptr, std::size_t sz, std::align_val_t al) = delete;
-        static void operator delete[](void* ptr, std::size_t sz, std::align_val_t al) = delete;
-
         inline uint32_t getSize() const { return m_size; }
 
     protected:
+        static void operator delete  (void* ptr) { assert(false); }
+        static void operator delete[](void* ptr) { assert(false); }
+        static void operator delete  (void* ptr, std::align_val_t al) { assert(false); }
+        static void operator delete[](void* ptr, std::align_val_t al) { assert(false); }
+        static void operator delete  (void* ptr, std::size_t sz) { assert(false); }
+        static void operator delete[](void* ptr, std::size_t sz) { assert(false); }
+        static void operator delete  (void* ptr, std::size_t sz, std::align_val_t al) { assert(false); }
+        static void operator delete[](void* ptr, std::size_t sz, std::align_val_t al) { assert(false); }
+        
+
         ICommand(uint32_t size) : m_size(size)
         {
             assert(ptrdiff_t(this) % alignof(ICommand) == 0);
