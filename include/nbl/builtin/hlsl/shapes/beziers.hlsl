8efede6352fa002f7e918b0a361970e1ba68c25e
Erfan Ahmadi <ahmadierfan99@gmail.com>
2023-05-17 16:58:52 +0330

    add beziers.hlsl to builtin shape hlsl functions

---
+++ include/nbl/builtin/hlsl/shapes/beziers.hlsl
@@ -0,0 +1,33 @@
+// Copyright (C) 2018-2023 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+namespace nbl
+{
+namespace hlsl
+{
+namespace shapes
+{
+    struct CubicBezier
+    {
+        static CubicBezier construct()
+        {
+        }
+
+        float signedDistance(float2 p)
+        {
+        }
+    };
+
+    struct QuadraticBezier
+    {
+        static QuadraticBezier construct()
+        {
+        }
+
+        float signedDistance(float2 p)
+        {
+        }
+    };
+}
+}
+}
