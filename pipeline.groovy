677c961c1768e2bddf3f95291ba880a06241eaea
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2023-05-26 10:32:47 +0200

    Update and rename Jenkinsfile to pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,16 @@
+def execute(agent)
+{
+	stage('CMake')
+	{
+		  agent.execute("cmake -DNBL_COMPILE_WITH_CUDA:BOOL=ON -DNBL_BUILD_OPTIX:BOOL=ON -DNBL_BUILD_MITSUBA_LOADER:BOOL=ON -DNBL_BUILD_RADEON_RAYS:BOOL=ON -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build -T v143", true)
+		  agent.execute("git -C ./3rdparty/gli reset --hard") // due to gli build system bug
+		  agent.execute("cmake -DNBL_COMPILE_WITH_CUDA:BOOL=ON -DNBL_BUILD_OPTIX:BOOL=ON -DNBL_BUILD_MITSUBA_LOADER:BOOL=ON -DNBL_BUILD_RADEON_RAYS:BOOL=ON -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build -T v143")
+	}
+
+	stage('Compile Nabla')
+	{
+		  agent.execute("cmake --build ./build --target Nabla --config Release -j12 -v")
+	}	
+}
+
+return this
