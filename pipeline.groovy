8ff38dc046cfc7b149d86db5cdd1ad29478a6837
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2023-05-27 13:59:34 +0200

    Update pipeline.groovy with NBL_CI_CONFIG parameter

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,13 +1,13 @@
 def execute(agent)
 {
-	stage('CMake')
+	stage("CMake")
 	{
 		  agent.execute("cmake -DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build -T v143")
 	}
 
-	stage('Compile Nabla')
+	stage("Compile Nabla with ${params.NBL_CI_CONFIG} configuration")
 	{
-		  agent.execute("cmake --build ./build --target Nabla --config Release -j12 -v")
+		agent.execute("cmake --build ./build --target Nabla --config ${params.NBL_CI_CONFIG} -j12 -v")
 	}	
 }
 
