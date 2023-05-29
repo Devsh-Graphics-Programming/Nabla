7e2cba4033fb0dac7dbe9dacedce59b514e3094e
Mateusz Kielan <devsh.graphicsprogramming@gmail.com>
2023-05-29 14:24:17 +0200

    Fix bugs in handling extra scaling in the convolution specializations

--- src/nbl/asset/filters/kernels/CConvolutionWeightFunction.cpp
+++ src/nbl/asset/filters/kernels/CConvolutionWeightFunction.cpp
@@ -4,33 +4,41 @@ namespace nbl::asset
 {
 
 template <>
-double CConvolutionWeightFunction1D<CWeightFunction1D<SBoxFunction>, CWeightFunction1D<SBoxFunction>>::weight_impl(const float x, const uint32_t channel, const uint32_t) const
+double CConvolutionWeightFunction1D<CWeightFunction1D<SBoxFunction>, CWeightFunction1D<SBoxFunction>>::weight_impl(const float x, const uint32_t) const
 {
 	const auto [minIntegrationLimit, maxIntegrationLimit] = getIntegrationDomain(x);
 
 	if (m_isFuncAWider)
-		return (maxIntegrationLimit - minIntegrationLimit) * m_funcA.weight(x, channel) * m_funcB.weight(0.f, channel);
+		return (maxIntegrationLimit - minIntegrationLimit) * m_funcA.weight(x) * m_funcB.weight(0.f);
 	else
-		return (maxIntegrationLimit - minIntegrationLimit) * m_funcB.weight(x, channel) * m_funcA.weight(0.f, channel);
+		return (maxIntegrationLimit - minIntegrationLimit) * m_funcB.weight(x) * m_funcA.weight(0.f);
 }
 
 template <>
-double CConvolutionWeightFunction1D<CWeightFunction1D<SGaussianFunction>, CWeightFunction1D<SGaussianFunction>>::weight_impl(const float x, const uint32_t channel, const uint32_t) const
+double CConvolutionWeightFunction1D<CWeightFunction1D<SGaussianFunction>, CWeightFunction1D<SGaussianFunction>>::weight_impl(const float x, const uint32_t) const
 {
-	const float funcA_stddev = 1.f / (m_funcA.weight(0.f, channel) * core::sqrt(2.f * core::PI<float>()));
-	const float funcB_stddev = 1.f / (m_funcB.weight(0.f, channel) * core::sqrt(2.f * core::PI<float>()));
-
-	const float convolution_stddev = core::sqrt(funcA_stddev * funcA_stddev + funcB_stddev * funcB_stddev);
 	asset::CWeightFunction1D<asset::SGaussianFunction> weightFunction;
+
+	const float funcA_stddev = 1.f/m_funcA.getInvStretch();
+	const float funcB_stddev = 1.f/m_funcB.getInvStretch();
+	const float convolution_stddev = core::sqrt(funcA_stddev * funcA_stddev + funcB_stddev * funcB_stddev);
 	weightFunction.stretchAndScale(convolution_stddev);
+	
+	const double funcA_true_scale = m_funcA.getTotalScale()*funcA_stddev;
+	const double funcB_true_scale = m_funcB.getTotalScale()*funcB_stddev;
+	weightFunction.scale(funcA_true_scale*funcB_true_scale);
 
 	return weightFunction.weight(x, channel);
 }
 
 template <>
-double CConvolutionWeightFunction1D<CWeightFunction1D<SKaiserFunction>, CWeightFunction1D<SKaiserFunction>>::weight_impl(const float x, const uint32_t channel, const uint32_t) const
+double CConvolutionWeightFunction1D<CWeightFunction1D<SKaiserFunction>, CWeightFunction1D<SKaiserFunction>>::weight_impl(const float x, const uint32_t) const
 {
-	return m_isFuncAWider ? m_funcA.weight(x, channel) : m_funcB.weight(x, channel);
+	const double true_scale = m_funcA.getTotalScale()*m_funcB.getTotalScale()/(m_funcA.getInvStretch()*m_funcB.getInvStretch());
+	if (m_isFuncAWider)
+		return true_scale*m_funcA.weight(x);
+	else
+		return true_scale*m_funcB.weight(x);
 }
 
 } // end namespace nbl::asset
