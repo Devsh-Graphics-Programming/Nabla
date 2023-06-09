2aed40f455f153740c46a3b387f69e99c3b1f1e0
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2023-06-09 14:59:52 +0200

    Update pipeline.groovy, split into cmake and build function, add more parameters - build type & platform
    I think we could embed all of that into some groovy class

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,13 +1,30 @@
-def execute(agent, config)
+def cmake(agent, buildType, platform)
 {
-	stage("CMake")
+	// use Multi-Configuration generators only regarding the platform in future
+	
+	stage("Configure Nabla project with CMake as ${buildType} build")
 	{
-		  agent.execute("cmake -DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build -T v143")
+		if(buildType == "Static")
+		{
+			agent.execute("cmake -DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build_static -T v143")
+		}
+		else if(buildType == "Dynamic")
+		{
+			agent.execute("cmake -DNBL_STATIC_BUILD=OFF -DNBL_DYNAMIC_MSVC_RUNTIME=ON -DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON -S ./ -B ./build_dynamic -T v143")
+		}
+		else 
+		{
+			error "Intenral error!"
+		}
 	}
+}
 
-	stage("Compile Nabla with ${config} configuration")
+def build(agent, config, buildType, platform)
+{
+	stage("Build ${buildType} Nabla with ${config} configuration")
 	{
-		agent.execute("cmake --build ./build --target Nabla --config ${config} -j12 -v")
+		def buildDirectory = buildType == "Static" ? "build_static" : (buildType == "Dynamic" ? "build_dynamic" : "")
+		agent.execute("cmake --build ./${buildDirectory} --target Nabla --config ${config} -j12 -v")
 	}	
 }
 
