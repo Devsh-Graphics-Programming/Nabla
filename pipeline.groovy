5854cd4c6ee8b7952c3f414852a7e939fec0c0ad
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2023-06-18 13:41:13 +0200

    Update pipeline.groovy, have CNablaBuilder implementing Builder interface

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,46 +1,42 @@
-class Builder
+@Library('devsh-ci')
+
+import org.DevshGraphicsProgramming.Builder
+
+class CNablaBuilder extends Builder
 {
-	public static enum BUILD_TYPE 
+	@InheritConstructors
+	public CNablaBuilder(Agent _agent)
 	{
-		STATIC, DYNAMIC
-	}
-
-	public static enum CONFIGURATION 
-	{
-		RELEASE, RELWITHDEBINFO, DEBUG
-	}
-
-	public static enum ARCH
-	{
-		X86_64	
-	}
-
-	public static enum PLATFORM
-	{
-		WINDOWS	
-	}
-	
-	public def getBuildTypes() // due to groovy sandbox
-	{
-		return [BUILD_TYPE.STATIC, BUILD_TYPE.DYNAMIC]	
-	}
-	
-	public Builder(_agent, PLATFORM _platform)
-	{
-		agent = _agent
-		platform = _platform
 		
-		matrixAxes = [
-			ARCH: [ARCH.X86_64], // Hardcoded since we only target one arch
-			PLATFORM: [_platform], // Platform is determined by an upstream host
-			BUILD_TYPE: [BUILD_TYPE.STATIC, BUILD_TYPE.DYNAMIC],
-			CONFIGURATION: [CONFIGURATION.RELEASE, CONFIGURATION.RELWITHDEBINFO, CONFIGURATION.DEBUG]
-		    ]
-		
-		axes = getMatrixAxes(matrixAxes)
 	}
 	
-	public def cmake(BUILD_TYPE buildType)
+	@Override
+	public boolean prepare(Map axisMapping)
+	{
+		impl_cmake(axisMapping.get("BUILD_TYPE"))
+		return true
+	}
+	
+	@Override
+  	public boolean build(Map axisMapping)
+	{
+		impl_build(axisMapping.get("CONFIGURATION"), axisMapping.get("BUILD_TYPE"))
+		return true
+	}
+	
+	@Override
+  	public boolean test(Map axisMapping)
+	{
+		return true
+	}
+	
+	@Override
+	public boolean deploy(Map axisMapping)
+	{
+		return true
+	}
+	
+	private def impl_cmake(BUILD_TYPE buildType)
 	{
 		// use Multi-Configuration generators only regarding the platform in future
 		def commonFlags = "-DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON"
@@ -60,69 +56,17 @@ class Builder
 		agent.execute("cmake ${commonFlags} ${extraFlags} -S ./ -B ./${buildDirectory} -T ${toolchain}")
 	}
 	
-	public def build(CONFIGURATION config, BUILD_TYPE buildType)
+	public def impl_build(CONFIGURATION config, BUILD_TYPE buildType)
 	{
 		def buildDirectory = getNameOfBuildDirectory(buildType)
 		def nameOfConfig = getNameOfConfig(config)
 		agent.execute("cmake --build ./${buildDirectory} --target Nabla --config ${nameOfConfig} -j12 -v")
 	}
-	
-	private def getAxes() 
-	{
-	    	return axes
-	}
-	
-	private def getNameOfBuildDirectory(BUILD_TYPE buildType)
-	{
-		switch (buildType)
-		{
-			case BUILD_TYPE.STATIC:
-				return "build_static"
-			case BUILD_TYPE.DYNAMIC:
-				return "build_dynamic"
-		}
-	}
-	
-	private def getNameOfConfig(CONFIGURATION config)
-	{
-		switch (config)
-		{
-			case CONFIGURATION.RELEASE:
-				return "Release"
-			case CONFIGURATION.RELWITHDEBINFO:
-				return "RelWithDebInfo"
-			case CONFIGURATION.DEBUG:
-				return "Debug"
-		}
-	}
-	
-	@NonCPS
-	private def getMatrixAxes(Map matrix_axes) 
-	{
-	    	List axes = []
-	    	matrix_axes.each { axis, values ->
-			List axisList = []
-			values.each { value ->
-		    		axisList << [(axis): value]
-			}
-		axes << axisList
-	    }
-	    // calculate cartesian product
-	    axes.combinations()*.sum()
-	}
-	
-	private def agent
-	private PLATFORM platform
-	private def matrixAxes
-	private def axes
 }
 
-def create(_agent, _platform)
+def create(Agent _agent)
 {
-	if(_platform == "Windows")
-		return new Builder(_agent, Builder.PLATFORM.WINDOWS)
-	else
-		throw new Exception("Could not create Builder due to unknown platform!")
+	return new CNablaBuilder(_agent)
 }
 
 return this
