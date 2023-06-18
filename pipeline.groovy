57201797093b031b4a44f9e6402930a1ecf04508
Arkadiusz Lachowicz <34793522+AnastaZIuk@users.noreply.github.com>
2023-06-18 16:15:26 +0200

    Update pipeline.groovy, remove impl methods and request IBuilder enums

--- pipeline.groovy
+++ pipeline.groovy
@@ -11,14 +11,39 @@ class CNablaBuilder extends IBuilder
 	@Override
 	public boolean prepare(Map axisMapping)
 	{
-		impl_cmake(axisMapping.get("BUILD_TYPE"))
+		IBuilder.BUILD_TYPE buildType = axisMapping.get("BUILD_TYPE")
+		
+		// use Multi-Configuration generators only regarding the platform in future
+		def commonFlags = "-DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON"
+		def extraFlags = ""
+		def toolchain = "v143"
+		def buildDirectory = getNameOfBuildDirectory(buildType)
+		
+		switch (buildType)
+		{
+			case IBuilder.BUILD_TYPE.STATIC:
+				break
+			case IBuilder.BUILD_TYPE.DYNAMIC:
+				extraFlags = "-DNBL_STATIC_BUILD=OFF -DNBL_DYNAMIC_MSVC_RUNTIME=ON"
+				break	
+		}
+		
+		agent.execute("cmake ${commonFlags} ${extraFlags} -S ./ -B ./${buildDirectory} -T ${toolchain}")
+		
 		return true
 	}
 	
 	@Override
   	public boolean build(Map axisMapping)
 	{
-		impl_build(axisMapping.get("CONFIGURATION"), axisMapping.get("BUILD_TYPE"))
+		IBuilder.CONFIGURATION config = axisMapping.get("CONFIGURATION")
+		IBuilder.BUILD_TYPE buildType = axisMapping.get("BUILD_TYPE")
+		
+		def buildDirectory = getNameOfBuildDirectory(buildType)
+		def nameOfConfig = getNameOfConfig(config)
+		
+		agent.execute("cmake --build ./${buildDirectory} --target Nabla --config ${nameOfConfig} -j12 -v")
+		
 		return true
 	}
 	
@@ -32,33 +57,6 @@ class CNablaBuilder extends IBuilder
 	public boolean deploy(Map axisMapping)
 	{
 		return true
-	}
-	
-	private def impl_cmake(BUILD_TYPE buildType)
-	{
-		// use Multi-Configuration generators only regarding the platform in future
-		def commonFlags = "-DNBL_UPDATE_GIT_SUBMODULE=OFF -DNBL_COMPILE_WITH_CUDA:BOOL=OFF -DNBL_BUILD_OPTIX:BOOL=OFF -DNBL_BUILD_MITSUBA_LOADER:BOOL=OFF -DNBL_BUILD_RADEON_RAYS:BOOL=OFF -DNBL_RUN_TESTS:BOOL=ON"
-		def extraFlags = ""
-		def toolchain = "v143"
-		def buildDirectory = getNameOfBuildDirectory(buildType)
-		
-		switch (buildType)
-		{
-			case BUILD_TYPE.STATIC:
-				break
-			case BUILD_TYPE.DYNAMIC:
-				extraFlags = "-DNBL_STATIC_BUILD=OFF -DNBL_DYNAMIC_MSVC_RUNTIME=ON"
-				break	
-		}
-		
-		agent.execute("cmake ${commonFlags} ${extraFlags} -S ./ -B ./${buildDirectory} -T ${toolchain}")
-	}
-	
-	private def impl_build(CONFIGURATION config, BUILD_TYPE buildType)
-	{
-		def buildDirectory = getNameOfBuildDirectory(buildType)
-		def nameOfConfig = getNameOfConfig(config)
-		agent.execute("cmake --build ./${buildDirectory} --target Nabla --config ${nameOfConfig} -j12 -v")
 	}
 }
 
