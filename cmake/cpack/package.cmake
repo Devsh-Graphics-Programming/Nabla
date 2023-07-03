9dea8e7dd9a3f7ab7884b9010c3e16faa57344cf
AnastaZIuk <areklachowicz@gmail.com>
2023-07-03 13:49:01 +0200

    make windows use CPack ZIP generator - have development package ready for deployment, update CPack variables, add more conditions media/examples install, install missing DXC program, clean install code in 3rpdarty, install missing headers, add missing COMPONENT to install rules

--- cmake/cpack/package.cmake
+++ cmake/cpack/package.cmake
@@ -12,51 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if(WIN32) # TODO: to extend in future
-	if(NBL_STATIC_BUILD)
-		set(CPACK_PACKAGE_INSTALL_DIRECTORY "nabla-static")
-		set(CPACK_PACKAGE_NAME "nabla-win64-static")
-	else()
-		set(CPACK_PACKAGE_INSTALL_DIRECTORY "nabla-dynamic")
-		set(CPACK_PACKAGE_NAME "nabla-win64-dynamic")
-	endif()
+if(WIN32)
+	set(CPACK_GENERATOR "ZIP")
+	set(CPACK_ARCHIVE_COMPONENT_INSTALL ON)
+endif() # TODO: Linux, Android, MacOS. Android and MacOS will have non-archive generators but GUI installers. Linux will use 7Z
 
-	set(CPACK_PACKAGE_VENDOR "DevshGraphicsProgramming.org")
-	set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Nabla")
-	set(CPACK_PACKAGE_VERSION_MAJOR "1")
-	set(CPACK_PACKAGE_VERSION_MINOR "0")
-	set(CPACK_PACKAGE_VERSION_PATCH "0")
-	set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
+if(NBL_STATIC_BUILD)
+	set(CPACK_PACKAGE_INSTALL_DIRECTORY "nabla-static")
+	set(CPACK_PACKAGE_NAME "nabla-static")
+else()
+	set(CPACK_PACKAGE_INSTALL_DIRECTORY "nabla-dynamic")
+	set(CPACK_PACKAGE_NAME "nabla-dynamic")
+endif()
 
-	set(CPACK_COMPONENTS_ALL Headers Libraries Runtimes Media) # TODO: add Executables conditionally
+set(CPACK_PACKAGE_VENDOR "DevshGraphicsProgramming.org")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Nabla")
+set(CPACK_PACKAGE_VERSION_MAJOR "1")
+set(CPACK_PACKAGE_VERSION_MINOR "0")
+set(CPACK_PACKAGE_VERSION_PATCH "0")
+set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
 
-	set(CPACK_COMPONENT_HEADERS_DISPLAY_NAME "Headers")
-	set(CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "Libraries")
-	set(CPACK_COMPONENT_RUNTIMES_DISPLAY_NAME "Runtimes")
+set(CPACK_COMPONENTS_ALL Headers Libraries Runtimes)
+
+set(CPACK_COMPONENT_HEADERS_DISPLAY_NAME "Headers")
+set(CPACK_COMPONENT_LIBRARIES_DISPLAY_NAME "Libraries")
+set(CPACK_COMPONENT_RUNTIMES_DISPLAY_NAME "Runtimes")
+
+set(CPACK_COMPONENT_HEADERS_GROUP "development")
+set(CPACK_COMPONENT_LIBRARIES_GROUP "development")
+set(CPACK_COMPONENT_RUNTIMES_GROUP "development")
+
+if(NBL_CPACK_INCLUDE_EXAMPLES)
+	list(APPEND CPACK_COMPONENTS_ALL Media) # TODO: add Executables conditionally only if there is at least 1 which has been built successfully
+	
 	#set(CPACK_COMPONENT_EXECUTABLES_DISPLAY_NAME "Examples")
 	set(CPACK_COMPONENT_MEDIA_DISPLAY_NAME "Media")
-
-	set(CPACK_COMPONENT_HEADERS_DESCRIPTION "C/C++ headers, shaders and embeded builtin resource files for use with Nabla library and extensions")
-	set(CPACK_COMPONENT_LIBRARIES_DESCRIPTION "Static, import and shared libraries used to build programs with Nabla library and extensions")
-
-	if(NBL_STATIC_BUILD)
-		set(CPACK_COMPONENT_RUNTIMES_DESCRIPTION "DLL and PDB files for use with Nabla library and extensions")
-	else()
-		set(CPACK_COMPONENT_RUNTIMES_DESCRIPTION "PDB files for use with Nabla library and extensions")
-	endif()
-
+	
 	#set(CPACK_COMPONENT_EXECUTABLES_DESCRIPTION "Example executables built with Nabla library")
 	set(CPACK_COMPONENT_MEDIA_DESCRIPTION "Media files Nabla example executables load resources from")
-
-	set(CPACK_COMPONENT_HEADERS_DEPENDS Libraries)
+	
 	#set(CPACK_COMPONENT_EXECUTABLES_DEPENDS Media)
-
-	set(CPACK_COMPONENT_LIBRARIES_GROUP "Development")
-	set(CPACK_COMPONENT_HEADERS_GROUP "Development")
-	#set(CPACK_COMPONENT_EXECUTABLES_GROUP "Executables")
-	set(CPACK_COMPONENT_MEDIA_GROUP "Executables")
-
-	set(CPACK_THREADS 0) # try to use all threads for compression 
-
-	include(CPack)
+	
+	#set(CPACK_COMPONENT_EXECUTABLES_GROUP "executables")
+	set(CPACK_COMPONENT_MEDIA_GROUP "executables")	
 endif()
+
+set(CPACK_COMPONENT_HEADERS_DESCRIPTION "C/C++ headers, shaders and embeded builtin resource files for use with Nabla library and extensions")
+set(CPACK_COMPONENT_LIBRARIES_DESCRIPTION "Static, import and shared libraries used to build programs with Nabla library and extensions")
+
+if(NBL_STATIC_BUILD)
+	set(CPACK_COMPONENT_RUNTIMES_DESCRIPTION "DLL and PDB files for use with Nabla library and extensions")
+else()
+	set(CPACK_COMPONENT_RUNTIMES_DESCRIPTION "PDB files for use with Nabla library and extensions")
+endif()
+
+set(CPACK_COMPONENT_HEADERS_DEPENDS Libraries Runtimes)
+
+set(CPACK_THREADS 0) # try to use all threads for compression 
+
+include(CPack)
