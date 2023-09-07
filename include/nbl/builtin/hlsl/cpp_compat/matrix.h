ef0b6c960712a8e3d51a4f2c4828bf2c64358560
atkurtul <atilkurtulmus@gmail.com>
2023-09-07 19:56:35 +0300

    fix friend usage

--- include/nbl/builtin/hlsl/cpp_compat/matrix.h
+++ include/nbl/builtin/hlsl/cpp_compat/matrix.h
@@ -38,15 +38,25 @@ struct matrix final : private glm::mat<N,M,T>
     friend matrix operator+(matrix const& lhs, matrix const& rhs){ return matrix(lhs.asBase() + rhs.asBase()); }
     friend matrix operator-(matrix const& lhs, matrix const& rhs){ return matrix(lhs.asBase() - rhs.asBase()); }
     
-    template<uint16_t Z>
-    inline friend matrix<T, N, Z> mul(matrix const& lhs, matrix<T, M, Z> const& rhs){ return rhs.asBase() * lhs.asBase(); }
 
-    inline friend vector<T, N> mul(matrix const& lhs, vector<T, M> const& rhs) { return rhs * lhs.asBase(); }
-    inline friend vector<T, M> mul(vector<T, N> const& lhs, matrix const& rhs) { return rhs.asBase() * lhs; }
-    
-    private:
+    template<typename T, uint16_t X, uint16_t Y, uint16_t Z>
+    friend matrix<T, X, Z> mul(matrix<T, X, Y> const& lhs, matrix<T, Y, Z> const& rhs);
+    template<typename T, uint16_t X, uint16_t Y>
+    friend vector<T, X> mul(matrix<T, X, Y> const& lhs, vector<T, Y> const& rhs);
+    template<typename T, uint16_t X, uint16_t Y>
+    friend vector<T, Y> mul(vector<T, X> const& lhs, matrix<T, X, Y> const& rhs);
+private:
     Base const& asBase() const { return *reinterpret_cast<const Base*>(this); }
 };
+
+template<typename T, uint16_t X, uint16_t Y, uint16_t Z>
+inline matrix<T, X, Z> mul(matrix<T, X, Y> const& lhs, matrix<T, Y, Z> const& rhs) { return matrix<T, X, Z>(rhs.asBase() * lhs.asBase()); }
+
+template<typename T, uint16_t X, uint16_t Y>
+inline vector<T, X> mul(matrix<T, X, Y> const& lhs, vector<T, Y> const& rhs) { return rhs * lhs.asBase(); }
+
+template<typename T, uint16_t X, uint16_t Y>
+inline vector<T, Y> mul(vector<T, X> const& lhs, matrix<T, X, Y> const& rhs) { return rhs.asBase() * lhs; }
 
 using float4x4 = matrix<float, 4, 4>;
 using float4x3 = matrix<float, 4, 3>;
