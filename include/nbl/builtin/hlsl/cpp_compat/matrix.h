568f847cef2b8b746a57b87848707bfe48f36ce9
atkurtul <atilkurtulmus@gmail.com>
2023-09-13 22:09:13 +0300

    address outstanding pr comments

--- include/nbl/builtin/hlsl/cpp_compat/matrix.h
+++ include/nbl/builtin/hlsl/cpp_compat/matrix.h
@@ -22,41 +22,34 @@ struct matrix final : private glm::mat<N,M,T>
     using Base::operator=;
     using Base::operator[];
 
-    template<uint16_t X, uint16_t Y> matrix(matrix<T, X, Y> const&) = delete;
+    template<uint16_t X, uint16_t Y, std::enable_if<!(X == N && Y == M) && X <= N && Y <= M>>
+    matrix(matrix<T, X, Y> const& m) : Base(m)
+    {
+    }
 
     matrix(matrix const&) = default;
     explicit matrix(Base const& base) : Base(base) {}
 
-    friend matrix operator*(matrix const& lhs, matrix const& rhs)
-    { 
-        matrix re = lhs;
-        for(int i = 0; i < N; ++i)
-            lhs[i] *= rhs[i];
-        return re;
+    friend matrix operator+(matrix const& lhs, matrix const& rhs){ return matrix(reinterpret_cast<Base const&>(lhs) + reinterpret_cast<Base const&>(rhs)); }
+    friend matrix operator-(matrix const& lhs, matrix const& rhs){ return matrix(reinterpret_cast<Base const&>(lhs) - reinterpret_cast<Base const&>(rhs)); }
+    
+    
+    template<uint16_t K>
+    inline friend matrix<T, N, K> mul(matrix const& lhs, matrix<T, M, K> const& rhs)
+    {
+        return matrix<T, N, K>(glm::operator*(reinterpret_cast<Base const&>(rhs), reinterpret_cast<matrix<T, M, K>::Base const&>(lhs)));
+    }
+    
+    inline friend vector<T, N> mul(matrix const& lhs, vector<T, M> const& rhs)
+    {
+        return glm::operator* (rhs, reinterpret_cast<Base const&>(lhs));
     }
 
-    friend matrix operator+(matrix const& lhs, matrix const& rhs){ return matrix(lhs.asBase() + rhs.asBase()); }
-    friend matrix operator-(matrix const& lhs, matrix const& rhs){ return matrix(lhs.asBase() - rhs.asBase()); }
-    
-
-    template<typename T, uint16_t X, uint16_t Y, uint16_t Z>
-    friend matrix<T, X, Z> mul(matrix<T, X, Y> const& lhs, matrix<T, Y, Z> const& rhs);
-    template<typename T, uint16_t X, uint16_t Y>
-    friend vector<T, X> mul(matrix<T, X, Y> const& lhs, vector<T, Y> const& rhs);
-    template<typename T, uint16_t X, uint16_t Y>
-    friend vector<T, Y> mul(vector<T, X> const& lhs, matrix<T, X, Y> const& rhs);
-private:
-    Base const& asBase() const { return *reinterpret_cast<const Base*>(this); }
+    inline friend vector<T, M> mul(vector<T, N> const& lhs, matrix const& rhs)
+    {
+        return glm::operator*(reinterpret_cast<Base const&>(rhs), lhs);
+    }
 };
-
-template<typename T, uint16_t X, uint16_t Y, uint16_t Z>
-inline matrix<T, X, Z> mul(matrix<T, X, Y> const& lhs, matrix<T, Y, Z> const& rhs) { return matrix<T, X, Z>(rhs.asBase() * lhs.asBase()); }
-
-template<typename T, uint16_t X, uint16_t Y>
-inline vector<T, X> mul(matrix<T, X, Y> const& lhs, vector<T, Y> const& rhs) { return rhs * lhs.asBase(); }
-
-template<typename T, uint16_t X, uint16_t Y>
-inline vector<T, Y> mul(vector<T, X> const& lhs, matrix<T, X, Y> const& rhs) { return rhs.asBase() * lhs; }
 
 using float4x4 = matrix<float, 4, 4>;
 using float4x3 = matrix<float, 4, 3>;
