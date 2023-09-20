b41c72821df1c96d76e5b276b97a3c8dfbad6ab9
atkurtul <atilkurtulmus@gmail.com>
2023-09-20 16:21:56 +0300

    type_traits checklist

--- include/nbl/builtin/hlsl/type_traits.hlsl
+++ include/nbl/builtin/hlsl/type_traits.hlsl
@@ -4,6 +4,125 @@
 #ifndef _NBL_BUILTIN_HLSL_TYPE_TRAITS_INCLUDED_
 #define _NBL_BUILTIN_HLSL_TYPE_TRAITS_INCLUDED_
 
+// Since HLSL currently doesnt allow type aliases we declare them as seperate structs thus they are (WORKAROUND)s
+/*
+  // helper class
+  template<class T, T v> struct integral_constant; (DONE)
+ 
+  template<bool B>
+    using bool_constant = integral_constant<bool, B>; (WORKDAROUND)
+  using true_type  = bool_constant<true>; (WORKDAROUND)
+  using false_type = bool_constant<false>; (WORKDAROUND)
+ 
+  // primary type categories
+  template<class T> struct is_void; (DONE)
+  template<class T> struct is_null_pointer; (NOT-APPLICABLE)
+  template<class T> struct is_integral; (DONE)
+  template<class T> struct is_floating_point;  (DONE)
+  template<class T> struct is_array; (DONE)
+  template<class T> struct is_pointer; (TODO)
+  template<class T> struct is_lvalue_reference; (TODO)
+  template<class T> struct is_rvalue_reference; (TODO)
+  template<class T> struct is_member_object_pointer; (TODO)
+  template<class T> struct is_member_function_pointer; (TODO)
+  template<class T> struct is_enum;  (NOT-APPLICABLE)
+  template<class T> struct is_union; (NOT-APPLICABLE)
+  template<class T> struct is_class; (NOT-APPLICABLE)
+  template<class T> struct is_function; (NOT-APPLICABLE)
+ 
+  // composite type categories
+  template<class T> struct is_reference; (TODO)
+  template<class T> struct is_arithmetic; (DONE)
+  template<class T> struct is_fundamental; (TODO)
+  template<class T> struct is_object; (NOT-APPLICABLE)
+  template<class T> struct is_scalar; (DONE)
+  template<class T> struct is_compound; (DONE)
+  template<class T> struct is_member_pointer; (TODO)
+ 
+  // type properties
+  template<class T> struct is_const; (DONE)
+  template<class T> struct is_volatile; (NOT-APPLICABLE)
+  template<class T> struct is_trivial; (EVERYTHING IS)
+  template<class T> struct is_trivially_copyable; (EVERYTHING IS)
+  template<class T> struct is_standard_layout; (NOT-APPLICABLE)
+  template<class T> struct is_empty; (NOT-APPLICABLE)
+  template<class T> struct is_polymorphic; (NOT-APPLICABLE)
+  template<class T> struct is_abstract; (NOT-APPLICABLE)
+  template<class T> struct is_final; (NOT-APPLICABLE)
+  template<class T> struct is_aggregate; (DONE)
+ 
+  template<class T> struct is_signed; (DONE)
+  template<class T> struct is_unsigned; (DONE)
+  template<class T> struct is_bounded_array;
+  template<class T> struct is_unbounded_array;
+  template<class T> struct is_scoped_enum; (NOT-APPLICABLE)
+
+  // type property queries
+  template<class T> struct alignment_of; (TODO)
+  template<class T> struct rank; (TODO)
+  template<class T, unsigned I = 0> struct extent; (TODO)
+ 
+  // type relations
+  template<class T, class U> struct is_same; (DONE)
+  template<class Base, class Derived> struct is_base_of; (TODO)
+  template<class From, class To> struct is_convertible; (TODO)
+  template<class From, class To> struct is_nothrow_convertible; (NOT-APPLICABLE)
+  template<class T, class U> struct is_layout_compatible; (TODO)
+  template<class Base, class Derived> struct is_pointer_interconvertible_base_of; (NOT-APPLICABLE)
+ 
+  NO FOLD EXPRESSIONS IN HLSL!
+    template<class Fn, class... ArgTypes> struct is_invocable;
+    template<class R, class Fn, class... ArgTypes> struct is_invocable_r;
+    template<class Fn, class... ArgTypes> struct is_nothrow_invocable;
+    template<class R, class Fn, class... ArgTypes> struct is_nothrow_invocable_r;
+ 
+  // const-volatile modifications
+  template<class T> struct remove_const; (TODO)
+  template<class T> struct remove_volatile; (TODO)
+  template<class T> struct remove_cv; (TODO)
+  template<class T> struct add_const; (TODO)
+  template<class T> struct add_volatile; (TODO)
+  template<class T> struct add_cv; (TODO)
+
+  // reference modifications
+  template<class T> struct remove_reference; (TODO)
+  template<class T> struct add_lvalue_reference; (TODO)
+  template<class T> struct add_rvalue_reference; (TODO)
+
+  // sign modifications
+  template<class T> struct make_signed; (TODO)
+  template<class T> struct make_unsigned; (TODO)
+ 
+  // array modifications
+  template<class T> struct remove_extent; (TODO)
+  template<class T> struct remove_all_extents; (TODO)
+
+  // pointer modifications
+  template<class T> struct remove_pointer; (TODO)
+  template<class T> struct add_pointer; (TODO)
+
+  // other transformations
+  template<class T> struct type_identity; (TODO)
+  template<class T> struct remove_cvref; (TODO)
+  template<class T> struct decay; (TODO)
+  template<bool, class T = void> struct enable_if; (NOT-APPLICABLE)
+  template<bool, class T, class F> struct conditional; (DONE)
+  template<class... T> struct common_type; (NOT-APPLICABLE)
+  template<class T, class U, template<class> class TQual, template<class> class UQual>
+    struct basic_common_reference { }; (NOT-APPLICABLE)
+  template<class... T> struct common_reference; (NOT-APPLICABLE)
+  template<class T> struct underlying_type;
+  template<class Fn, class... ArgTypes> struct invoke_result;
+  template<class T> struct unwrap_reference; (TODO)
+  template<class T> struct unwrap_ref_decay; (TODO)
+
+  NO FOLD EXPRESSIONS IN HLSL!
+    // logical operator traits
+    template<class... B> struct conjunction;
+    template<class... B> struct disjunction;
+    template<class B> struct negation;
+*/
+
 // C++ headers
 #ifndef __HLSL_VERSION
 #include <type_traits>
@@ -13,7 +132,7 @@
 namespace nbl::hlsl::type_traits
 {
 
-#ifdef __HLSL_VERSION // HLSL
+#if 1 //def __HLSL_VERSION // HLSL
 
 template<class T, T val>
 struct integral_constant {
@@ -23,6 +142,9 @@ struct integral_constant {
 
 template <bool val>
 struct bool_constant : integral_constant<bool, val> {};
+
+struct true_type : bool_constant<true> {};
+struct false_type : bool_constant<true> {};
 
 template <bool C, class T, class F>
 struct conditional 
@@ -56,6 +178,13 @@ namespace impl
 {
 
 template<class T>
+struct is_unsigned : bool_constant<
+    is_same<T, uint16_t>::value ||
+    is_same<T, uint32_t>::value ||
+    is_same<T, uint64_t>::value ||
+> {};
+
+template<class T>
 struct is_integral : bool_constant<
     is_same<T, bool>::value ||
     is_same<T, uint16_t>::value ||
@@ -66,12 +195,21 @@ struct is_integral : bool_constant<
     is_same<T, int64_t>::value
 > {};
 
-
 template<class T>
 struct is_floating_point : bool_constant<
     is_same<T, half>::value ||
     is_same<T, float>::value ||
     is_same<T, double>::value
+> {};
+
+// Is bool signed?
+template<class T>
+struct is_signed : bool_constant<
+    /*is_same<T, bool>::value ||*/
+    is_same<T, int16_t>::value ||
+    is_same<T, int32_t>::value ||
+    is_same<T, int64_t>::value ||
+    is_floating_point<T>::value
 > {};
 
 }
@@ -81,6 +219,25 @@ struct is_scalar : bool_constant<
     impl::is_integral<T>::value || 
     impl::is_floating_point<T>::value
 > {};
+
+
+template<class T>
+struct is_const : bool_constant<false> {};
+
+template<class T>
+struct is_const<const T> : bool_constant<true> {};
+
+template <class T>
+struct is_fundamental : bool_constant<
+    is_scalar<T>::value || 
+    is_void<T>::value
+> {};
+
+template <class T>
+struct is_compound : bool_constant<!is_fundamental<T>::value> {};
+
+template <class T>
+struct is_aggregate : is_aggregate<T> {};
 
 #else // C++
 
@@ -104,6 +261,24 @@ using is_array = std::is_array<T>;
 
 template<class T>
 using is_scalar = std::is_scalar<T>;
+
+template<class T>
+using is_signed = std::is_signed<T>;
+
+template<class T>
+using is_unsigned = std::is_unsigned<T>;
+
+template<class T>
+using is_const = std::is_const<T>;
+
+template<class T>
+using is_fundamental = std::is_fundamental<T>;
+
+template<class T>
+using is_compound = std::is_compound<T>;
+
+template<class T>
+using is_aggregate = std::is_aggregate<T>;
 
 #endif
 
