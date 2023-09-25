4857ba5ca9f16c0e506d2ce78bba61ae31839664
Przemek <minikers21@gmail.com>
2023-09-25 16:51:24 +0200

    Modified hlsl color matrices so they match the Khronos spec

--- include/nbl/builtin/hlsl/colorspace/decodeCIEXYZ.hlsl
+++ include/nbl/builtin/hlsl/colorspace/decodeCIEXYZ.hlsl
@@ -17,9 +17,11 @@ namespace colorspace
 namespace decode
 {
 
-NBL_CONSTEXPR float3x3 XYZtoscRGB = float3x3(   float3( 3.2404542, -1.5371385, -0.4985314),
-										float3(-0.9692660,  1.8760108,  0.0415560),
-										float3( 0.0556434, -0.2040259,  1.0572252));
+NBL_CONSTEXPR float3x3 XYZtoscRGB = float3x3(
+    float3( 3.240970, -1.537383, -0.498611),
+    float3(-0.969244,  1.875968,  0.041555),
+    float3( 0.055630, -0.203977,  1.056972)
+);
 
 
 NBL_CONSTEXPR float3x3 XYZtosRGB = XYZtoscRGB;
@@ -27,15 +29,17 @@ NBL_CONSTEXPR float3x3 XYZtosRGB = XYZtoscRGB;
 NBL_CONSTEXPR float3x3 XYZtoBT709 = XYZtoscRGB;
 
   
-NBL_CONSTEXPR float3x3 XYZtoDisplay_P3 = float3x3(  float3( 2.4934969, -0.9313836, -0.4027108),
-                                            float3(-0.8294890,  1.7626641,  0.0236247),
-                                            float3( 0.0358458, -0.0761724,  0.9568845));
+NBL_CONSTEXPR float3x3 XYZtoDisplay_P3 = float3x3(
+    float3( 2.4934969119, -0.9313836179, -0.4027107845),
+    float3(-0.8294889696,  1.7626640603,  0.0236246858),
+    float3( 0.0358458302, -0.0761723893,  0.9568845240)
+);
 
 
 NBL_CONSTEXPR float3x3 XYZtoDCI_P3 = float3x3(float3(1.0,0.0,0.0),float3(0.0,1.0,0.0),float3(0.0,0.0,1.0));
 
  
-NBL_CONSTEXPR float3x3 XYZtoBT2020 = float3x3(  float3( 1.7166512, -0.3556708, -0.2533663),
+NBL_CONSTEXPR float3x3 XYZtoBT2020 = float3x3(  float3( 1.7166512, -0.3556708, -0.2533663), // TODO
                                         float3(-0.6666844,  1.6164812,  0.0157685),
                                         float3( 0.0176399, -0.0427706,  0.9421031));
  
@@ -46,19 +50,21 @@ NBL_CONSTEXPR float3x3 XYZtoDOLBYIVISION = XYZtoBT2020;
 NBL_CONSTEXPR float3x3 XYZtoHDR10_HLG = XYZtoBT2020;
 
 
-NBL_CONSTEXPR float3x3 XYZtoAdobeRGB = float3x3(    float3( 2.04159, -0.56501, -0.34473),
-                                            float3(-0.96924,  1.87597, -0.04156),
-                                            float3( 0.01344, -0.11836,  1.01517));
+NBL_CONSTEXPR float3x3 XYZtoAdobeRGB = float3x3(    float3( 2.0415879038, -0.5650069743, -0.3447313508),
+                                            float3(-0.9692436363, 1.8759675015, -0.0415550574),
+                                            float3( 0.0134442806, -0.1183623922,  1.0151749944));
 
 
-NBL_CONSTEXPR float3x3 XYZtoACES2065_1 = float3x3(  float3( 1.0498110175, -0.4959030231,  0.0000000000),
-                                            float3( 0.0000000000,  1.3733130458,  0.0000000000),
-                                            float3(-0.0000974845,  0.0982400361,  0.9912520182));
+NBL_CONSTEXPR float3x3 XYZtoACES2065_1 = float3x3(  
+    float3( 1.0498110175, 0.0000000000,  -0.0000974845),
+    float3( -0.4959030231,  1.3733130458,  0.0982400361),
+    float3(0.0000000000,  0.0000000000,  0.9912520182));
 
 
-NBL_CONSTEXPR float3x3 XYZtoACEScc = float3x3(  float3( 1.6410234, -0.3248033, -0.2364247),
-                                        float3(-0.6636629,  1.6153316,  0.0167563),
-                                        float3( 0.0117219, -0.0082844,  0.9883949));
+NBL_CONSTEXPR float3x3 XYZtoACEScc = float3x3(  
+    float3( 1.6410233797, -0.3248032942, -0.2364246952),
+    float3(-0.6636628587,  1.6153315917,  0.0167563477),
+    float3( 0.0117218943, -0.0082844420,  0.9883948585));
 
 NBL_CONSTEXPR float3x3 XYZtoACEScct = XYZtoACEScc;
 
