2ea518dc2693d5a226af451b06794e5424722bfd
Przemek <minikers21@gmail.com>
2023-09-29 16:59:15 +0200

    Added .hlsl and .glsl files

---
+++ include/nbl/builtin/hlsl/colorspace/EOTF.hlsl
@@ -0,0 +1,136 @@
+
+// Copyright (C) 2018-2022 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+
+#ifndef _NBL_BUILTIN_HLSL_COLOR_SPACE_EOTF_INCLUDED_
+#define _NBL_BUILTIN_HLSL_COLOR_SPACE_EOTF_INCLUDED_
+
+//#include <nbl/builtin/hlsl/common.hlsl>
+#include <nbl/builtin/hlsl/cpp_compat/cpp_compat.hlsl>
+
+namespace nbl
+{
+namespace hlsl
+{
+namespace colorspace
+{
+namespace eotf
+{
+
+template<typename T>
+T identity(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    return nonlinear;
+}
+
+template<typename T>
+T impl_shared_2_4(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear, typename ValueType<T>::type vertex)
+{
+    typedef typename ValueType<T>::type Val_t;
+    bool3 right = (nonlinear > promote<T, Val_t>(vertex));
+    return lerp(nonlinear / Val_t(12.92), pow((nonlinear + promote<T, Val_t>(0.055)) / Val_t(1.055), promote<T, Val_t>(2.4)), right);
+}
+
+// compatible with scRGB as well
+template<typename T>
+T sRGB(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    bool3 negatif = (nonlinear < promote<T, Val_t>(0.0));
+    T absVal = impl_shared_2_4<T>(abs(nonlinear), 0.04045);
+    return lerp(absVal, -absVal, negatif);
+}
+
+// also known as P3-D65
+template<typename T>
+T Display_P3(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    return impl_shared_2_4<T>(nonlinear, 0.039000312);
+}
+
+template<typename T>
+T DCI_P3_XYZ(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    return pow(nonlinear * Val_t(52.37), promote<T, Val_t>(2.6));
+}
+
+template<typename T>
+T SMPTE_170M(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    // ITU specs (and the outlier BT.2020) give different constants for these, but they introduce discontinuities in the mapping
+    // because HDR swapchains often employ the RGBA16_SFLOAT format, this would become apparent because its higher precision than 8,10,12 bits
+    Val_t alpha = 1.099296826809443; // 1.099 for all ITU but the BT.2020 12 bit encoding, 1.0993 otherwise
+    T delta = promote<T, Val_t>(0.081242858298635); // 0.0812 for all ITU but the BT.2020 12 bit encoding
+    return lerp(nonlinear / Val_t(4.5), pow((nonlinear + promote<T, Val_t>(alpha - 1.0)) / alpha, promote<T, Val_t>(1.0 / 0.45)), (nonlinear >= delta));
+}
+
+template<typename T>
+T SMPTE_ST2084(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    const T invm2 = promote<T, Val_t>(1.0 / 78.84375);
+    T _common = pow(invm2, invm2);
+
+    const T c2 = promote<T, Val_t>(18.8515625);
+    const Val_t c3 = 18.68875;
+    const T c1 = promote<T, Val_t>(c3 + 1.0) - c2;
+
+    const T invm1 = promote<T, Val_t>(1.0 / 0.1593017578125);
+    return pow(max(_common - c1, promote<T, Val_t>(0.0)) / (c2 - _common * c3), invm1);
+}
+
+// did I do this right by applying the function for every color?
+template<typename T>
+T HDR10_HLG(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    // done with log2 so constants are different
+    const Val_t a = 0.1239574303172;
+    const T b = promote<T, Val_t>(0.02372241);
+    const T c = promote<T, Val_t>(1.0042934693729);
+    bool3 right = (nonlinear > promote<T, Val_t>(0.5));
+    return lerp(nonlinear * nonlinear / Val_t(3.0), exp2((nonlinear - c) / a) + b, right);
+}
+
+template<typename T>
+T AdobeRGB(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    return pow(nonlinear, promote<T, Val_t>(2.19921875));
+}
+
+template<typename T>
+T Gamma_2_2(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    return pow(nonlinear, promote<T, Val_t>(2.2));
+}
+
+template<typename T>
+T ACEScc(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    bool3 right = (nonlinear >= promote<T, Val_t>(-0.301369863));
+    T _common = exp2(nonlinear * Val_t(17.52) - promote<T, Val_t>(9.72));
+    return max(lerp(_common * Val_t(2.0) - promote<T, Val_t>(0.000030517578125), _common, right), promote<T, Val_t>(65504.0));
+}
+
+template<typename T>
+T ACEScct(TYPENAME_CPP_ONLY NBL_CONST_REF_ARG(T) nonlinear)
+{
+    typedef typename ValueType<T>::type Val_t;
+    bool3 right = (nonlinear >= promote<T, Val_t>(0.155251141552511));
+    return max(lerp((nonlinear - promote<T, Val_t>(0.0729055341958355)) / Val_t(10.5402377416545), exp2(nonlinear * Val_t(17.52) - promote<T, Val_t>(9.72)), right), promote<T, Val_t>(65504.0));
+}
+
+}
+}
+}
+}
+
+#endif
+
---
+++ include/nbl/builtin/hlsl/colorspace/encodeCIEXYZ.hlsl
@@ -0,0 +1,83 @@
+
+
+// Copyright (C) 2018-2022 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+
+#ifndef _NBL_BUILTIN_HLSL_COLOR_SPACE_ENCODE_CIE_XYZ_INCLUDED_
+#define _NBL_BUILTIN_HLSL_COLOR_SPACE_ENCODE_CIE_XYZ_INCLUDED_
+
+#include <nbl/builtin/hlsl/cpp_compat/cpp_compat.hlsl>
+
+namespace nbl
+{
+namespace hlsl
+{
+namespace colorspace
+{
+
+NBL_CONSTEXPR float3x3 scRGBtoXYZ = float3x3(
+    float3(0.412391f, 0.357584f, 0.180481f),
+    float3(0.212639f, 0.715169f, 0.072192f),
+    float3(0.019331f, 0.119195f, 0.950532f)
+);
+
+NBL_CONSTEXPR float3x3 sRGBtoXYZ = scRGBtoXYZ;
+
+NBL_CONSTEXPR float3x3 BT709toXYZ = scRGBtoXYZ;
+
+
+NBL_CONSTEXPR float3x3 Display_P3toXYZ = float3x3(
+    float3(0.4865709486f, 0.2656676932f, 0.1982172852f),
+    float3(0.2289745641f, 0.6917385218f, 0.0792869141f),
+    float3(0.0000000000f, 0.0451133819f, 1.0439443689f)
+);
+
+
+NBL_CONSTEXPR float3x3 DCI_P3toXYZ = float3x3(
+    float3(1.0f, 0.0f, 0.0f),
+    float3(0.0f, 1.0f, 0.0f),
+    float3(0.0f, 0.0f, 1.0f)
+);
+
+
+NBL_CONSTEXPR float3x3 BT2020toXYZ = float3x3(
+    float3(0.636958f, 0.144617f, 0.168881f),
+    float3(0.262700f, 0.677998f, 0.059302f),
+    float3(0.000000f, 0.028073f, 1.060985f)
+);
+
+NBL_CONSTEXPR float3x3 HDR10_ST2084toXYZ = BT2020toXYZ;
+
+NBL_CONSTEXPR float3x3 DOLBYIVISIONtoXYZ = BT2020toXYZ;
+
+NBL_CONSTEXPR float3x3 HDR10_HLGtoXYZ = BT2020toXYZ;
+
+
+NBL_CONSTEXPR float3x3 AdobeRGBtoXYZ = float3x3(
+    float3(0.5766690429f, 0.1855582379f, 0.1882286462f),
+    float3(0.2973449753f, 0.6273635663f, 0.0752914585f),
+    float3(0.0270313614f, 0.0706888525f, 0.9913375368f)
+);
+
+
+NBL_CONSTEXPR float3x3 ACES2065_1toXYZ = float3x3(
+    float3(0.9525523959f, 0.0000000000f,  0.0000936786f),
+    float3(0.3439664498f, 0.7281660966f, -0.0721325464f),
+    float3(0.0000000000f, 0.0000000000f,  1.0088251844f)
+);
+
+
+NBL_CONSTEXPR float3x3 ACEScctoXYZ = float3x3(
+    float3( 0.6624541811f, 0.1340042065f, 0.1561876870f),
+    float3( 0.2722287168f, 0.6740817658f, 0.0536895174f),
+    float3(-0.0055746495f, 0.0040607335f, 1.0103391003f)
+);
+
+NBL_CONSTEXPR float3x3 ACESccttoXYZ = ACEScctoXYZ;
+
+}
+}
+}
+
+#endif
