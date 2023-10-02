16a8d66b6dd6ab8e8ef512b527fe2fdcb916506e
Erfan Ahmadi <ahmadierfan99@gmail.com>
2023-10-02 18:30:55 +0330

    create integral hlsl builtin file

---
+++ include/nbl/builtin/hlsl/math/integral/gauss_legendre.hlsl
@@ -0,0 +1,21 @@
+// Copyright (C) 2018-2023 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+#ifndef _NBL_BUILTIN_HLSL_MATH_INTEGRAL_GAUSS_LEGENDRE_INCLUDED_
+#define _NBL_BUILTIN_HLSL_MATH_INTEGRAL_GAUSS_LEGENDRE_INCLUDED_
+
+namespace nbl
+{
+namespace hlsl
+{
+namespace math
+{
+    namespace integral
+    {
+        
+    }
+} // math
+} // hlsl
+} // nbl
+
+#endif
