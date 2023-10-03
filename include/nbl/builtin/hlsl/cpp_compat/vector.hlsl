87b95a5569fc006cef46bc1209b02c7ecd270cb4
Mateusz Kielan <devsh.graphicsprogramming@gmail.com>
2023-10-03 11:49:24 +0200

    Make the vector typedefs sized to encourage their usage in HLSL
    also fix the nested namespace issue

--- include/nbl/builtin/hlsl/cpp_compat/vector.hlsl
+++ include/nbl/builtin/hlsl/cpp_compat/vector.hlsl
@@ -2,40 +2,50 @@
 #ifndef _NBL_BUILTIN_HLSL_VECTOR_INCLUDED_
 #define _NBL_BUILTIN_HLSL_VECTOR_INCLUDED_
 
-#include <nbl/builtin/hlsl/cpp_compat/matrix.hlsl>
+
+#ifndef __HLSL_VERSION 
+
+#define GLM_FORCE_SWIZZLE
+#include <glm/glm.hpp>
+#include "glm/detail/_swizzle.hpp"
+#include <stdint.h>
 
 namespace nbl::hlsl
 {
 
-#ifndef __HLSL_VERSION 
-
 template<typename T, uint16_t N>
 using vector = glm::vec<N, T>;
 
+// ideally we should have sized bools, but no idea what they'd be
 using bool4 = vector<bool, 4>;
 using bool3 = vector<bool, 3>;
 using bool2 = vector<bool, 2>;
+using bool1 = vector<bool, 1>;
 
-using int4 = vector<int32_t, 4>;
-using int3 = vector<int32_t, 3>;
-using int2 = vector<int32_t, 2>;
+using int32_t4 = vector<int32_t, 4>;
+using int32_t3 = vector<int32_t, 3>;
+using int32_t2 = vector<int32_t, 2>;
+using int32_t1 = vector<int32_t, 1>;
 
-using uint4 = vector<uint32_t, 4>;
-using uint3 = vector<uint32_t, 3>;
-using uint2 = vector<uint32_t, 2>;
+using uint32_t4 = vector<uint32_t, 4>;
+using uint32_t3 = vector<uint32_t, 3>;
+using uint32_t2 = vector<uint32_t, 2>;
+using uint32_t1 = vector<uint32_t, 1>;
 
-// TODO: halfN -> needs class implementation
+// TODO: halfN -> needs class implementation or C++23 std:float16_t
 
-using float4 = vector<float, 4>;
-using float3 = vector<float, 3>;
-using float2 = vector<float, 2>;
+using float32_t = float;
+using float32_t4 = vector<float32_t, 4>;
+using float32_t3 = vector<float32_t, 3>;
+using float32_t2 = vector<float32_t, 2>;
+using float32_t1 = vector<float32_t, 1>;
 
-using double4 = vector<double, 4>;
-using double3 = vector<double, 3>;
-using double2 = vector<double, 2>;
-
-#endif
-
+using float64_t = double;
+using float64_t4 = vector<float64_t, 4>;
+using float64_t3 = vector<float64_t, 3>;
+using float64_t2 = vector<float64_t, 2>;
+using float64_t1 = vector<float64_t, 1>;
 }
+#endif
 
 #endif
