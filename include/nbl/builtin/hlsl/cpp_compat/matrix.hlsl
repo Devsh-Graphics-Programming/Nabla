8c28efaa39267318fb139ef5d40658a25156889f
Mateusz Kielan <devsh.graphicsprogramming@gmail.com>
2023-10-03 11:41:53 +0200

    Provide proper HLSL2021 explicitly sized named typedefs for matrices

--- include/nbl/builtin/hlsl/cpp_compat/matrix.hlsl
+++ include/nbl/builtin/hlsl/cpp_compat/matrix.hlsl
@@ -65,47 +65,49 @@ using bool2x4 = matrix<bool, 2, 4>;
 using bool2x3 = matrix<bool, 2, 3>;
 using bool2x2 = matrix<bool, 2, 2>;
 
-using int4x4 = matrix<int32_t, 4, 4>;
-using int4x3 = matrix<int32_t, 4, 3>;
-using int4x2 = matrix<int32_t, 4, 2>;
-using int3x4 = matrix<int32_t, 3, 4>;
-using int3x3 = matrix<int32_t, 3, 3>;
-using int3x2 = matrix<int32_t, 3, 2>;
-using int2x4 = matrix<int32_t, 2, 4>;
-using int2x3 = matrix<int32_t, 2, 3>;
-using int2x2 = matrix<int32_t, 2, 2>;
+using int32_t4x4 = matrix<int32_t, 4, 4>;
+using int32_t4x3 = matrix<int32_t, 4, 3>;
+using int32_t4x2 = matrix<int32_t, 4, 2>;
+using int32_t3x4 = matrix<int32_t, 3, 4>;
+using int32_t3x3 = matrix<int32_t, 3, 3>;
+using int32_t3x2 = matrix<int32_t, 3, 2>;
+using int32_t2x4 = matrix<int32_t, 2, 4>;
+using int32_t2x3 = matrix<int32_t, 2, 3>;
+using int32_t2x2 = matrix<int32_t, 2, 2>;
 
-using uint4x4 = matrix<uint32_t, 4, 4>;
-using uint4x3 = matrix<uint32_t, 4, 3>;
-using uint4x2 = matrix<uint32_t, 4, 2>;
-using uint3x4 = matrix<uint32_t, 3, 4>;
-using uint3x3 = matrix<uint32_t, 3, 3>;
-using uint3x2 = matrix<uint32_t, 3, 2>;
-using uint2x4 = matrix<uint32_t, 2, 4>;
-using uint2x3 = matrix<uint32_t, 2, 3>;
-using uint2x2 = matrix<uint32_t, 2, 2>;
+using uint32_t4x4 = matrix<uint32_t, 4, 4>;
+using uint32_t4x3 = matrix<uint32_t, 4, 3>;
+using uint32_t4x2 = matrix<uint32_t, 4, 2>;
+using uint32_t3x4 = matrix<uint32_t, 3, 4>;
+using uint32_t3x3 = matrix<uint32_t, 3, 3>;
+using uint32_t3x2 = matrix<uint32_t, 3, 2>;
+using uint32_t2x4 = matrix<uint32_t, 2, 4>;
+using uint32_t2x3 = matrix<uint32_t, 2, 3>;
+using uint32_t2x2 = matrix<uint32_t, 2, 2>;
 
 // TODO: halfMxN
 
-using float4x4 = matrix<float, 4, 4>;
-using float4x3 = matrix<float, 4, 3>;
-using float4x2 = matrix<float, 4, 2>;
-using float3x4 = matrix<float, 3, 4>;
-using float3x3 = matrix<float, 3, 3>;
-using float3x2 = matrix<float, 3, 2>;
-using float2x4 = matrix<float, 2, 4>;
-using float2x3 = matrix<float, 2, 3>;
-using float2x2 = matrix<float, 2, 2>;
+using float32_t = float;
+using float32_t4x4 = matrix<float, 4, 4>;
+using float32_t4x3 = matrix<float, 4, 3>;
+using float32_t4x2 = matrix<float, 4, 2>;
+using float32_t3x4 = matrix<float, 3, 4>;
+using float32_t3x3 = matrix<float, 3, 3>;
+using float32_t3x2 = matrix<float, 3, 2>;
+using float32_t2x4 = matrix<float, 2, 4>;
+using float32_t2x3 = matrix<float, 2, 3>;
+using float32_t2x2 = matrix<float, 2, 2>;
 
-using double4x4 = matrix<double, 4, 4>;
-using double4x3 = matrix<double, 4, 3>;
-using double4x2 = matrix<double, 4, 2>;
-using double3x4 = matrix<double, 3, 4>;
-using double3x3 = matrix<double, 3, 3>;
-using double3x2 = matrix<double, 3, 2>;
-using double2x4 = matrix<double, 2, 4>;
-using double2x3 = matrix<double, 2, 3>;
-using double2x2 = matrix<double, 2, 2>;
+using float64_t = double;
+using float64_t4x4 = matrix<float64_t, 4, 4>;
+using float64_t4x3 = matrix<float64_t, 4, 3>;
+using float64_t4x2 = matrix<float64_t, 4, 2>;
+using float64_t3x4 = matrix<float64_t, 3, 4>;
+using float64_t3x3 = matrix<float64_t, 3, 3>;
+using float64_t3x2 = matrix<float64_t, 3, 2>;
+using float64_t2x4 = matrix<float64_t, 2, 4>;
+using float64_t2x3 = matrix<float64_t, 2, 3>;
+using float64_t2x2 = matrix<float64_t, 2, 2>;
 #endif
 
 }
