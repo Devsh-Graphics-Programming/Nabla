be7d796ba1c1643652a0ae2720668e274cbafe88
atkurtul <atilkurtulmus@gmail.com>
2023-10-04 17:50:49 +0300

    compiling workaround for clz

--- include/nbl/builtin/hlsl/mpl.hlsl
+++ include/nbl/builtin/hlsl/mpl.hlsl
@@ -4,6 +4,9 @@
 #ifndef _NBL_BUILTIN_HLSL_META_MATH_INCLUDED_
 #define _NBL_BUILTIN_HLSL_META_MATH_INCLUDED_
 
+#include <nbl/builtin/hlsl/type_traits.hlsl>
+
+
 namespace nbl
 {
 namespace hlsl
@@ -14,42 +17,46 @@ namespace mpl
 #ifdef __HLSL_VERSION
 namespace impl
 {
-template<uint64_t N, uint8_t bits_log2>
-struct clz;
 
-template<uint64_t N>
-struct clz<N, 0>
+template<uint16_t bits_log2>
+struct clz_masks
 {
-    static const uint64_t value = 1 - (N & 1);
+    static const uint16_t SHIFT = uint16_t(1)<<(bits_log2-1);
+    static const uint64_t LO_MASK = (1ull<<SHIFT)-1;
 };
 
-template<uint64_t N, uint8_t bits_log2>
+template<>
+struct clz_masks<0>
+{
+    static const uint16_t SHIFT = 0;
+    static const uint64_t LO_MASK = 0;
+};
+
+template<uint64_t N, uint16_t bits_log2>
 struct clz
 {
-    static const uint64_t SHIFT = 1ull<<(bits_log2-1);
-    static const uint64_t LO_MASK = (1ull<<SHIFT)-1;
-    static const bool CHOOSE_HIGH = N&(LO_MASK<<SHIFT);
-    static const uint64_t value   = clz<(CHOOSE_HIGH ? (N>>SHIFT):N)&LO_MASK,bits_log2-1>::value + (CHOOSE_HIGH ? 0ull:SHIFT);
+    static const bool CHOOSE_HIGH = N&(clz_masks<bits_log2>::LO_MASK<<clz_masks<bits_log2>::SHIFT);
+    static const uint64_t NEXT_N = (CHOOSE_HIGH ? (N>>clz_masks<bits_log2>::SHIFT):N)&clz_masks<bits_log2>::LO_MASK;
+    static const uint16_t value   = type_traits::conditional<bits_log2,clz<NEXT_N,bits_log2-1>,type_traits::integral_constant<uint16_t,0> >::type::value + (CHOOSE_HIGH ? 0ull:clz_masks<bits_log2>::SHIFT);
 };
 
 }
 
-template<uint64_t X>
+template<uint64_t N>
 struct clz
 {
-    static const uint64_t value = impl::clz<X, 6>::value;
+    static const uint16_t value = impl::clz<N, 6>::value;
 };
 
 template<uint64_t X>
 struct consteval_log2
 {
-    static const uint64_t value = X ? (1ull<<6)-impl::clz<X,6>::value-1 : -1ull;
+    static const uint16_t value = X ? (1ull<<6)-clz<X>::value-1 : -1ull;
 };
 
-}
 #endif
 
-
+}
 }
 }
 
