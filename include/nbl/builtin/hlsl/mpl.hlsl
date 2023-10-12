0b905c488bd1d828aa50ca8fd82acd1edd372f73
Przemek <minikers21@gmail.com>
2023-10-12 22:29:51 +0200

    Saving work

--- include/nbl/builtin/hlsl/mpl.hlsl
+++ include/nbl/builtin/hlsl/mpl.hlsl
@@ -24,21 +24,24 @@ struct clz_masks
 {
     static const uint16_t SHIFT = uint16_t(1)<<(bits_log2-1);
     static const uint64_t LO_MASK = (1ull<<SHIFT)-1;
+
+    // static const uint16_t SHIFT = type_traits::conditional<bool(bits_log2),type_traits::integral_constant<uint16_t,uint16_t(1)<<(bits_log2-1)>,type_traits::integral_constant<uint16_t,0> >::type::value;
+    // static const uint64_t LO_MASK = type_traits::conditional<bool(bits_log2),type_traits::integral_constant<uint64_t,(1ull<<SHIFT)-1>,type_traits::integral_constant<uint64_t,0> >::type::value;
 };
 
-template<>
-struct clz_masks<0>
-{
-    static const uint16_t SHIFT = 0;
-    static const uint64_t LO_MASK = 0;
-};
+// template<>
+// struct clz_masks<0>
+// {
+//     static const uint16_t SHIFT = 0;
+//     static const uint64_t LO_MASK = 0;
+// };
 
 template<uint64_t N, uint16_t bits_log2>
 struct clz
 {
     static const bool CHOOSE_HIGH = N&(clz_masks<bits_log2>::LO_MASK<<clz_masks<bits_log2>::SHIFT);
     static const uint64_t NEXT_N = (CHOOSE_HIGH ? (N>>clz_masks<bits_log2>::SHIFT):N)&clz_masks<bits_log2>::LO_MASK;
-    static const uint16_t value   = type_traits::conditional<bits_log2,clz<NEXT_N,bits_log2-1>,type_traits::integral_constant<uint16_t,0> >::type::value + (CHOOSE_HIGH ? 0ull:clz_masks<bits_log2>::SHIFT);
+    static const uint16_t value = type_traits::conditional<bits_log2,clz<NEXT_N,bits_log2-1>,type_traits::integral_constant<uint16_t,0> >::type::value + (CHOOSE_HIGH ? 0ull:clz_masks<bits_log2>::SHIFT);
 };
 
 }
@@ -53,6 +56,27 @@ template<uint64_t X>
 struct log2
 {
     static const uint16_t value = X ? (1ull<<6)-clz<X>::value-1 : -1ull;
+};
+
+template<typename T, T X, int32_t S>
+struct rotl;
+
+template<typename T, T X, int32_t S>
+struct rotr
+{
+    // TODO: all uint typese
+    static const uint32_t N = 32u;
+    static const int32_t r = S % N;
+    static const T value = (S >= 0) ? ((X >> r) | (X << (N - r))) : rotl<T, -X, S>::value;
+};
+
+template<typename T, T X, int32_t S>
+struct rotl
+{
+    // TODO: all uint typese
+    static const uint32_t N = 32u;
+    static const int32_t r = S % N;
+    static const T value = (S >= 0) ? ((X << r) | (X >> (N - r))) : rotr<T, -X, S>::value;
 };
 
 #endif
