3efaa62566460651c47c6ffd03849f034a52254e
Nahim Yaya <107263862+nahiim@users.noreply.github.com>
2023-10-23 13:25:56 +0100

    Update subgroup_shuffle.hlsl

--- include/nbl/builtin/hlsl/spirv_intrinsics/subgroup_shuffle.hlsl
+++ include/nbl/builtin/hlsl/spirv_intrinsics/subgroup_shuffle.hlsl
@@ -15,18 +15,18 @@ namespace hlsl
 namespace spirv
 {
 template<typename T>
-[[vk::ext_instruction(/* OpGroupNonUniformShuffle */ 345)]]
+[[vk::ext_instruction( spv::OpGroupNonUniformShuffle  /*345*/)]]
 T groupShuffle(uint executionScope, T value, uint invocationId);
 
 #ifdef NBL_GL_KHR_shader_subgroup_shuffle_relative
 template<typename T>
-[[vk::ext_capability(/* GroupNonUniformShuffleRelative */ 66)]]
-[[vk::ext_instruction(/* OpGroupNonUniformShuffleUp */ 347)]]
+[[vk::ext_capability( spv::CapabilityGroupNonUniformShuffleRelative  /*66*/)]]
+[[vk::ext_instruction( spv::OpGroupNonUniformShuffleUp  /*347*/)]]
 T groupShuffleUp(uint executionScope, T value, uint delta);
 
 template<typename T>
-[[vk::ext_capability(/* GroupNonUniformShuffleRelative */ 66)]]
-[[vk::ext_instruction(/* OpGroupNonUniformShuffleDown */ 348)]]
+[[vk::ext_capability( spv::CapabilityGroupNonUniformShuffleRelative  /*66*/)]]
+[[vk::ext_instruction( spv::OpGroupNonUniformShuffleDown  /*348*/)]]
 T groupShuffleDown(uint executionScope, T value, uint delta);
 #endif
 }
