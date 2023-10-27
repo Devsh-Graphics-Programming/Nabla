6e7e4467501d41a87b1638db55cf16336b842763
devsh <devsh.graphicsprogramming@gmail.com>
2023-10-27 16:43:20 +0200

    move the subgroup invocation masks to `ballot` and add missing ballot functions, also make sure everyone is using uint32_t

--- include/nbl/builtin/hlsl/glsl_compat/subgroup_basic.hlsl
+++ include/nbl/builtin/hlsl/glsl_compat/subgroup_basic.hlsl
@@ -13,43 +13,24 @@ namespace hlsl
 namespace glsl
 {
 
-// TODO (Future): Accessing gl_SubgroupSize and other gl_* values is not yet possible due to https://github.com/microsoft/DirectXShaderCompiler/issues/4217
+// TODO (Future): Accessing gl_NumSubgroups and other gl_* values is not yet possible due to https://github.com/microsoft/DirectXShaderCompiler/issues/4217
 
-uint gl_SubgroupSize() {
+uint32_t gl_SubgroupSize() {
     return WaveGetLaneCount();
 }
 
-uint gl_SubgroupSizeLog2() {
+uint32_t gl_SubgroupSizeLog2() {
     return firstbithigh(gl_SubgroupSize());
 }
 
-uint gl_SubgroupInvocationID() {
+uint32_t gl_SubgroupInvocationID() {
     return WaveGetLaneIndex();
 }
 
-uint gl_SubgroupID() {
+// only available in compute
+uint32_t gl_SubgroupID() {
     // TODO (PentaKon): This is not always correct (subgroup IDs aren't always aligned with invocation index per the spec)
     return gl_LocalInvocationIndex >> gl_SubgroupSizeLog2();
-}
-
-uint4 gl_SubgroupEqMask() {
-    return uint4(0,0,0,1) << gl_SubgroupInvocationID();
-}
-
-uint4 gl_SubgroupGeMask() {
-    return uint4(0xffffffffu, 0xffffffffu, 0xffffffffu, 0xffffffffu) << gl_SubgroupInvocationID();
-}
-
-uint4 gl_SubgroupGtMask() {
-    return gl_SubgroupGeMask() << 1;
-}
-
-uint4 gl_SubgroupLeMask() {
-    return ~gl_SubgroupGtMask();
-}
-
-uint4 gl_SubgroupLtMask() {
-    return ~gl_SubgroupGeMask();
 }
 
 bool subgroupElect() {
