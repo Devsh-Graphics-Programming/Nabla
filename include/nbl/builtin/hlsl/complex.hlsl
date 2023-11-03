5b575e65d226887d3150cffc161a8a043dfd1f8f
en_why <yynahim@gmail.com>
2023-11-03 16:33:52 +0100

    rewrite

--- include/nbl/builtin/hlsl/complex.hlsl
+++ include/nbl/builtin/hlsl/complex.hlsl
@@ -16,65 +16,35 @@ namespace hlsl
 {
 
 
-template <typename T, uint16_t N>
+template<typename T>
 struct complex_t
 {
-    T real[N];
-    T imag[N];
+    T real;
+    T imag;
 };
 
-
-template <typename T, uint16_t N>
-complex_t<T, N> add(const complex_t<T, N> a, const complex_t<T, N> b)
+template<typename T>
+complex_t<T> add(complex_t<T> a, complex_t<T> b)
 {
-    complex_t<T, N> result;
-    for (uint16_t i = 0; i < N; ++i)
-    {
-        result.real[i] = a.real[i] + b.real[i];
-        result.imag[i] = a.imag[i] + b.imag[i];
-    }
-
-    return result;
+    return a + b;
 }
 
-template <typename T, uint16_t N>
-complex_t<T, N> subtract(const complex_t<T, N> a, const complex_t<T, N> b)
+template<typename T>
+complex_t<T> subract(complex_t<T> a, complex_t<T> b)
 {
-    complex_t<T, N> result;
-    for (uint16_t i = 0; i < N; ++i)
-    {
-        result.real[i] = a.real[i] - b.real[i];
-        result.imag[i] = a.imag[i] - b.imag[i];
-    }
-
-    return result;
+    return a - b;
 }
 
-template <typename T, uint16_t N>
-complex_t<T, N> multiply(const complex_t<T, N> a, const complex_t<T, N> b)
+template<typename T>
+complex_t<T> multiply(complex_t<T> a, complex_t<T> b)
 {
-    complex_t<T, N> result;
-    for (uint16_t i = 0; i < N; ++i)
-    {
-        result.real[i] = a.real[i] * b.real[i] - a.imag[i] * b.imag[i];
-        result.imag[i] = a.real[i] * b.imag[i] + a.imag[i] * b.real[i];
-    }
-
-    return result;
+    return a * b;
 }
 
-template <typename T, uint16_t N>
-complex_t<T, N> divide(const complex_t<T, N> a, const complex_t<T, N> b)
+template<typename T>
+complex_t<T> divide(complex_t<T> a, complex_t<T> b)
 {
-    complex_t<T, N> result;
-    for (uint16_t i = 0; i < N; ++i)
-    {
-        T denominator = b.real[i] * b.real[i] + b.imag[i] * b.imag[i];
-        result.real[i] = (a.real[i] * b.real[i] + a.imag[i] * b.imag[i]) / denominator;
-        result.imag[i] = (a.imag[i] * b.real[i] - a.real[i] * b.imag[i]) / denominator;
-    }
-
-    return result;
+    return a / b;
 }
 
 
