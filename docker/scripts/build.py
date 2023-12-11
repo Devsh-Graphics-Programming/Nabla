0f5bff57d0b36758c9aee7e8b619da3c62e75c90
AnastaZIuk <areklachowicz@gmail.com>
2023-12-11 21:02:11 +0100

    Add build step of examples to build.py, add CPack package installs, update CMakeLists.txt

--- docker/scripts/build.py
+++ docker/scripts/build.py
@@ -1,4 +1,4 @@
-import os, subprocess, sys, argparse
+import os, subprocess, sys, argparse, glob
 
 def parseInputArguments():
     parser = argparse.ArgumentParser(description="Nabla CI Framework cross platform build pipeline script")
@@ -20,19 +20,36 @@ def clone(targetRevision):
     subprocess.run(f"git checkout {targetRevision}", check=True)
     
 def configure(libType, updateSubmodulesOnly = False):
-    subprocess.run(f"cmake . --preset ci-configure-{libType}-msvc", check=True)
+    return subprocess.run(f"cmake . --preset ci-configure-{libType}-msvc", check=True)
 
 def updateSubmodules():
-    subprocess.run(f"cmake -S . -B ./build_submodules_update -DNBL_EXIT_ON_UPDATE_GIT_SUBMODULE=ON -DNBL_CI_GIT_SUBMODULES_SHALLOW=ON", check=True)
+    return subprocess.run(f"cmake -S . -B ./build_submodules_update -DNBL_EXIT_ON_UPDATE_GIT_SUBMODULE=ON -DNBL_CI_GIT_SUBMODULES_SHALLOW=ON", check=True)
 
-def build(libType, config):
-    subprocess.run(f"cmake --build --preset ci-build-{libType}-msvc --config {config}", check=True)
+def buildNabla(libType, config):
+    return subprocess.run(f"cmake --build --preset ci-build-{libType}-msvc --config {config}", check=False)
+    
+def buildProject(libType, config, buildDirectory):
+    return subprocess.run(f"cmake --build \"{buildDirectory}\" --config {config}", check=False)
+  
+def cpack(libType, config, CPACK_INSTALL_CMAKE_PROJECTS, packageDirectory):
+    if not packageDirectory:
+        packageDirectory = f"./package/{config}"
+        
+    return subprocess.run(f"cpack --preset ci-package-{libType}-msvc -C {config} -B \"{packageDirectory}\" -D CPACK_INSTALL_CMAKE_PROJECTS=\"{CPACK_INSTALL_CMAKE_PROJECTS}\"", check=True)
+  
+def getCPackBundleHash(buildDirectory, target, component = "ALL", relativeDirectory = "/"):
+    return f"{buildDirectory};{target};{component};{relativeDirectory};"
 
 try:
     THIS_PROJECT_NABLA_DIRECTORY = os.environ.get('THIS_PROJECT_NABLA_DIRECTORY', '')
 
     if not THIS_PROJECT_NABLA_DIRECTORY:
         raise ValueError("THIS_PROJECT_NABLA_DIRECTORY enviroment variables doesn't exist!")
+        
+    THIS_PROJECT_ARTIFACTORY_NABLA_DIRECTORY = os.environ.get('THIS_PROJECT_ARTIFACTORY_NABLA_DIRECTORY', '')
+    
+    if not THIS_PROJECT_ARTIFACTORY_NABLA_DIRECTORY:
+        print("THIS_PROJECT_ARTIFACTORY_NABLA_DIRECTORY not defined, using default <buildPath>/package/<configuration> path for artifacts") 
         
     os.chdir(THIS_PROJECT_NABLA_DIRECTORY)
     
@@ -52,8 +69,26 @@ try:
         configure("dynamic") # update submodule mode and then configure async
     else:
         if args.build:
-            build(libType, config)
-    
+            topBuildDirectory = os.path.normpath(os.path.join(THIS_PROJECT_NABLA_DIRECTORY, f"build/{libType}"))
+            cpackBundleHash = getCPackBundleHash(topBuildDirectory, "Headers")
+            
+            result = buildNabla(libType, config)
+            
+            if result.returncode == 0:
+                cpackBundleHash += getCPackBundleHash(topBuildDirectory, "Libraries") + getCPackBundleHash(topBuildDirectory, "Runtime")
+                
+                matchingFiles = glob.glob('examples_tests/*/config.json.template', recursive=True)
+                exBuildDirectories = list(set(os.path.normpath(os.path.join(topBuildDirectory, os.path.dirname(file))) for file in matchingFiles))
+
+                for exBuildDirectory in exBuildDirectories:
+                    result = buildProject(libType, config, exBuildDirectory)
+                    
+                    if result.returncode == 0:
+                        cpackBundleHash += getCPackBundleHash(exBuildDirectory, "Executables") + getCPackBundleHash(exBuildDirectory, "Media")
+                        
+            print(cpackBundleHash)            
+            cpack(libType, config, cpackBundleHash, f"{THIS_PROJECT_ARTIFACTORY_NABLA_DIRECTORY}/Nabla/artifacts/{config}")
+                
 except subprocess.CalledProcessError as e:
     print(f"Subprocess failed with exit code {e.returncode}")
     sys.exit(e.returncode)
