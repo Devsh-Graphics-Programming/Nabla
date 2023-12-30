01a2fec2491bc3f6a72f99fadbf401a448d11a37
AnastaZIuk <areklachowicz@gmail.com>
2023-12-30 13:55:48 +0100

    Remove ssh persistent volume from compose, we don't need it since we work with host's codebase, submodules update operation which requires authentication is performed by dev.py script on host

--- docker/dev.py
+++ docker/dev.py
@@ -4,7 +4,6 @@ import os, subprocess, sys, argparse
 def parseInputArguments():
     parser = argparse.ArgumentParser(description="Nabla CI Pipeline compose Framework script")
     
-    parser.add_argument("--ssh", help="SSH key file used for github authentication, required to clone Nabla", type=str, required=True)
     parser.add_argument("--platform", help="Target platform", type=str, default="windows")
     parser.add_argument("--arch", help="Target arch", type=str, default="x86_64")
    
@@ -24,20 +23,16 @@ def main():
         
         root = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
                
-        #updateSubmodules(root)
+        updateSubmodules(root)
          
         os.chdir(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "compose/ci/stages/dev")))
         
-        key = args.ssh
         platform = args.platform
         arch = args.arch
         
         if subprocess.call(["docker", "network", "inspect", "nabla.network"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
            subprocess.run(["docker", "network", "create", "--driver", "nat", "--subnet", "172.28.0.0/16", "--gateway", "172.28.5.1", "nabla.network"], check=True) # create nabla.network network if not present
         
-        if subprocess.call(["docker", "volume", "inspect", "ssh"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
-           subprocess.run(["docker", "volume", "create", "ssh"], check=True) # create ssh volume if not present
-
         envFile = os.path.abspath(f"../.env/platform/{platform}/.env")
 
         compose = [
@@ -48,7 +43,6 @@ def main():
         
         subprocess.run(compose + ["build"], check=True)
         subprocess.run(compose + ["create", "--force-recreate"], check=True)
-        subprocess.run(compose + ["cp", key, "nabla.init:key"], check=True)
         subprocess.run(compose + ["up"], check=True)
         subprocess.run(compose + ["down"], check=True)
         
