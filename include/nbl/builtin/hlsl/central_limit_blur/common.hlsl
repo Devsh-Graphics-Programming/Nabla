22f32c712d87ae04654f877c33615ccb0d92aa57
Eichenherz <transferusuniversalis@gmail.com>
2024-03-07 15:39:14 +0200

    Use bit fields

--- include/nbl/builtin/hlsl/central_limit_blur/common.hlsl
+++ include/nbl/builtin/hlsl/central_limit_blur/common.hlsl
@@ -2,8 +2,6 @@
 #define _NBL_BUILTIN_HLSL_CENTRAL_LIMIT_BLUR_COMMON_INCLUDED_
 
 #include <nbl/builtin/hlsl/cpp_compat.hlsl>
-#include <nbl/builtin/hlsl/cpp_compat/intrinsics.h>
-#include <nbl/builtin/hlsl/glsl_compat/core.hlsl>
 
 namespace nbl 
 {
@@ -32,32 +30,16 @@ enum BorderColor : uint32_t
 
 struct BoxBlurParams
 {
-	nbl::hlsl::uint32_t4 inputDimensions;
-	nbl::hlsl::uint32_t2 chosenAxis;
-	float radius;
-
-	uint32_t getDirection()
-	{
-		return nbl::hlsl::glsl::bitfieldExtract( inputDimensions.w, 30, 2 );
-	}
-	uint32_t getChannelCount()
-	{
-		return nbl::hlsl::glsl::bitfieldExtract( inputDimensions.w, 27, 3 );
-	}
-
-	uint32_t getWrapMode()
-	{
-		return nbl::hlsl::glsl::bitfieldExtract( inputDimensions.w, 25, 2 );
-	}
-
-	uint32_t getBorderColorType()
-	{
-		return nbl::hlsl::glsl::bitfieldExtract( inputDimensions.w, 22, 3 );
-	}
+	float32_t radius;
+	uint32_t direction : 2;
+	uint32_t channelCount : 3;
+	uint32_t wrapMode : 2;
+	uint32_t borderColorType : 3;
+	
 	nbl::hlsl::float32_t4 getBorderColor()
 	{
 		nbl::hlsl::float32_t4 borderColor = nbl::hlsl::float32_t4( 1.f, 0.f, 1.f, 1.f );
-		switch( getBorderColorType() )
+		switch( borderColorType )
 		{
 		case BORDER_COLOR_FLOAT_TRANSPARENT_BLACK:
 		case BORDER_COLOR_INT_TRANSPARENT_BLACK:
@@ -82,12 +64,3 @@ struct BoxBlurParams
 }
 }
 #endif
-
-
-//struct BufferAccessor;
-/*
-{
-	nbl::hlsl::float32_t getPaddedData( const nbl::hlsl::uint32_t3 coordinate, const uint32_t channel );
-	void setData( const nbl::hlsl::uint32_t3 coordinate, const uint32_t channel, const nbl::hlsl::float32_t val );
-};
-*/
