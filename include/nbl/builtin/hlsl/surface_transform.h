4a2c18c18fcdb5314cd7c6ea27e0f8e44466e5e2
devsh <devsh.graphicsprogramming@gmail.com>
2024-03-14 17:48:45 +0100

    removed tons of duplicate code!

--- include/nbl/builtin/hlsl/surface_transform.h
+++ include/nbl/builtin/hlsl/surface_transform.h
@@ -3,6 +3,7 @@
 // For conditions of distribution and use, see copyright notice in nabla.h
 #ifndef _NBL_BUILTIN_HLSL_SURFACE_TRANSFORM_INCLUDED_
 #define _NBL_BUILTIN_HLSL_SURFACE_TRANSFORM_INCLUDED_
+#include <nbl/builtin/hlsl/limits.hlsl>
 
 namespace nbl
 {
@@ -24,6 +25,70 @@ enum class FLAG_BITS : uint16_t
     HORIZONTAL_MIRROR_ROTATE_270_BIT = 0x0080,
     INHERIT_BIT = 0x0100,
     ALL_BITS = 0x01FF
+};
+
+// define everything else in terms of this
+float32_t2x2 transformMatrix(const FLAG_BITS transform)
+{
+    switch (transform)
+    {
+        case FLAG_BITS::IDENTITY_BIT:
+            return float32_t2x2( 1.f, 0.f,
+                                 0.f, 1.f);
+        case FLAG_BITS::ROTATE_90_BIT:
+            return float32_t2x2( 0.f, 1.f,
+                                -1.f, 0.f);
+        case FLAG_BITS::ROTATE_180_BIT:
+            return float32_t2x2(-1.f, 0.f,
+                                 0.f,-1.f);
+        case FLAG_BITS::ROTATE_270_BIT:
+            return float32_t2x2( 0.f,-1.f,
+                                 1.f, 0.f);
+        case FLAG_BITS::HORIZONTAL_MIRROR_BIT:
+            return float32_t2x2(-1.f, 0.f,
+                                 0.f, 1.f);
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_90_BIT:
+            return float32_t2x2( 0.f, 1.f,
+                                 1.f, 0.f);
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_180_BIT:
+            return float32_t2x2( 1.f, 0.f,
+                                 0.f,-1.f);
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_270_BIT:
+            return float32_t2x2( 0.f,-1.f,
+                                -1.f, 0.f);
+        default:
+            break;
+    }
+    const float _nan = numeric_limits<float>::signaling_NaN;
+    return float32_t2x2(_nan,_nan,_nan,_nan);
+}
+
+//! [width,height] might switch to [height, width] in orientations such as 90°CW
+//! Usecase: Find out how big the viewport has to be after or before a tranform is applied
+uint16_t2 transformedExtents(const FLAG_BITS transform, const uint16_t2 screenSize)
+{
+    switch (transform)
+    {
+        case FLAG_BITS::IDENTITY_BIT:
+        case FLAG_BITS::HORIZONTAL_MIRROR_BIT:
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_180_BIT:
+        case FLAG_BITS::ROTATE_180_BIT:
+            return screenSize;
+        case FLAG_BITS::ROTATE_90_BIT:
+        case FLAG_BITS::ROTATE_270_BIT:
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_90_BIT:
+        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_270_BIT:
+            return screenSize.yx;
+        default:
+            break;
+    }
+    return uint16_t2(0);
+}
+
+float transformedAspectRatio(const FLAG_BITS transform, const uint16_t2 screenSize)
+{
+    const uint16_t2 newExtents = transformedExtents(transform,screenSize);
+    return float(newExtents[1])/float(newExtents[0]);
 }
 
 //! Use this function to apply the INVERSE of swapchain tranformation to the screenspace coordinate `coord` 
@@ -35,6 +100,7 @@ enum class FLAG_BITS : uint16_t
 //! - Be aware that almost always you'd want to do a single transform in your rendering pipeline.
 uint16_t2 applyInverseToScreenSpaceCoordinate(const FLAG_BITS transform, const uint16_t2 coord, const uint16_t2 screenSize)
 {
+    // TODO: use inverse(transformMatrix(transform)) somehow
     const uint16_t2 lastTexel = screenSize - uint16_t2(1,1);
     switch (transform)
     {
@@ -66,8 +132,9 @@ uint16_t2 applyInverseToScreenSpaceCoordinate(const FLAG_BITS transform, const u
 //! Warning: Be aware that almost always you'd want to do a single transform in your rendering pipeline.
 uint16_t2 applyToScreenSpaceCoordinate(const FLAG_BITS transform, const uint16_t2 coord, const uint16_t2 screenSize)
 {
-    const uint16_t2 lastTexel = screenSize - uint16_t2(1);
-    switch (swapchainTransform)
+    // TODO: use transformMatrix(transform) somehow
+    const uint16_t2 lastTexel = screenSize - uint16_t2(1, 1);
+    switch (transform)
     {
         case FLAG_BITS::IDENTITY_BIT:
             return coord;
@@ -91,56 +158,13 @@ uint16_t2 applyToScreenSpaceCoordinate(const FLAG_BITS transform, const uint16_t
     return uint16_t2(0);
 }
 
-//! [width,height] might switch to [height, width] in orientations such as 90°CW
-//! Usecase: Find out how big the viewport has to be after or before a tranform is applied
-uint16_t2 transformedExtents(const FLAG_BITS transform, const uint16_t2 screenSize)
-{
-    switch (swapchainTransform) 
-    {
-        case FLAG_BITS::IDENTITY_BIT:
-        case FLAG_BITS::HORIZONTAL_MIRROR_BIT:
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_180_BIT:
-        case FLAG_BITS::ROTATE_180_BIT:
-            return screenSize;
-        case FLAG_BITS::ROTATE_90_BIT:
-        case FLAG_BITS::ROTATE_270_BIT:
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_90_BIT:
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_270_BIT:
-            return screenSize.yx;
-        default:
-            break;
-    }
-    return uint16_t2(0);
-}
-
 //! Same as `applyToScreenSpaceCoordinate` but const NDC space
 //! If rendering to the swapchain, you may use this function to transform the NDC coordinates directly
 //! to be fed into gl_Position in vertex shading
 //! Warning: Be aware that almost always you'd want to do a single transform in your rendering pipeline.
 float32_t2 applyToNDC(const FLAG_BITS transform, const float32_t2 ndc)
 {
-    switch (swapchainTransform) 
-    {
-        case FLAG_BITS::IDENTITY_BIT:
-            return ndc;
-        case FLAG_BITS::ROTATE_90_BIT:
-            return float32_t2(-ndc.y,ndc.x);
-        case FLAG_BITS::ROTATE_180_BIT:
-            return -ndc;
-        case FLAG_BITS::ROTATE_270_BIT:
-            return float32_t2(ndc.y,-ndc.x);
-        case FLAG_BITS::HORIZONTAL_MIRROR_BIT:
-            return float32_t2(-ndc.x,ndc.y);
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_90_BIT:
-            return ndc.yx;
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_180_BIT:
-            return float32_t2(ndc.x,-ndc.y);
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_270_BIT:
-            return -ndc.yx;
-        default:
-            break;
-    }
-    return float32_t2(0,0)/0.f;
+    return mul(transformMatrix(transform),ndc);
 }
 
 // TODO: This is untested
@@ -149,37 +173,8 @@ float32_t2 applyToNDC(const FLAG_BITS transform, const float32_t2 ndc)
 template<typename TwoColumns>
 TwoColumns applyToDerivatives(const FLAG_BITS transform, TwoColumns dDx_dDy)
 {
-    switch (swapchainTransform)
-    {
-        case FLAG_BITS::IDENTITY_BIT:
-            return dDx_dDy;
-        case FLAG_BITS::ROTATE_90_BIT:
-            return mul(dDx_dDy,float32_t2x2( 0.f,-1.f
-                                             1.f, 0.f));
-        case FLAG_BITS::ROTATE_180_BIT:
-            return mul(dDx_dDy,float32_t2x2(-1.f, 0.f
-                                             0.f,-1.f));
-        case FLAG_BITS::ROTATE_270_BIT:
-            return mul(dDx_dDy,float32_t2x2( 0.f, 1.f
-                                            -1.f, 0.f));
-        case FLAG_BITS::HORIZONTAL_MIRROR_BIT:
-            return mul(dDx_dDy,float32_t2x2(-1.f, 0.f
-                                             0.f, 1.f));
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_90_BIT:
-            return mul(dDx_dDy,float32_t2x2( 0.f, 1.f
-                                             1.f, 0.f));
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_180_BIT:
-            return mul(dDx_dDy,float32_t2x2( 1.f, 0.f
-                                             0.f,-1.f));
-        case FLAG_BITS::HORIZONTAL_MIRROR_ROTATE_270_BIT:
-            return mul(dDx_dDy,float32_t2x2( 0.f,-1.f
-                                            -1.f, 0.f));
-        default:
-            break;
-    }
-    return dDx_dDy*(0.f/0.f);
+    return mul(inverse(transformMatrix(transform)),dDx_dDy);
 }
-// TODO: could define the NDC in terms of inverse Derivative XForm
 
 }
 }
