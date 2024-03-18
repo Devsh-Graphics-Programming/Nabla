15381bac3a7365231d75b6a000d4064a3ef820cd
AnastaZIuk <areklachowicz@gmail.com>
2024-03-18 09:12:25 +0100

    begin implementing FindPackageHandleStandardArgs module, save work

--- cmake/cpack/find/compoment/template.cmake
+++ cmake/cpack/find/compoment/template.cmake
@@ -1,1 +1,4 @@
 list(TRANSFORM @_NBL_PROXY_@ PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(@_NBL_PACKAGE_@ DEFAULT_MSG @_NBL_PROXY_@)
