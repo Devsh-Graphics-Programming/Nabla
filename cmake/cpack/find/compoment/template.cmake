7b17a40e1aa6a9e27f077308283d560d2022b1cc
AnastaZIuk <areklachowicz@gmail.com>
2024-03-18 20:05:41 +0100

    Implement FindNabla.cmake, add NABLA_INSTALL_<COMPOMENT>_DIRECTORY_<LIBRARY_TYPE>_<CONFIG> install package variable, perform tests

--- cmake/cpack/find/compoment/template.cmake
+++ cmake/cpack/find/compoment/template.cmake
@@ -1,4 +1,6 @@
 list(TRANSFORM @_NBL_PROXY_@ PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
 
+set(@_NBL_COMPOMENT_D_@ "${CMAKE_CURRENT_LIST_DIR}/@_NBL_COMPOMENT_D_V_@")
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(@_NBL_PACKAGE_@ DEFAULT_MSG @_NBL_PROXY_@)
+find_package_handle_standard_args(@_NBL_PACKAGE_@ DEFAULT_MSG @_NBL_PROXY_@ @_NBL_COMPOMENT_D_@)
