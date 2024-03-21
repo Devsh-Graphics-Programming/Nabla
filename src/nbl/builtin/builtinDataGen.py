950db850b160be3ab63e14a526e7d4c91cee505a
AnastaZIuk <areklachowicz@gmail.com>
2024-03-21 12:07:51 +0100

    Update python builtin resource build system generation scripts - limit file writes, use strings with variable substitution, exchange std::pair<DATA, SIZE> with new SBuiltinFile struct which also now stores special file xx256sha hash and timestamp as std::tm. Have data stored with constexpr, have constexpr SBuiltinFile for which const reference is returned by getters

--- src/nbl/builtin/builtinDataGen.py
+++ src/nbl/builtin/builtinDataGen.py
@@ -1,18 +1,16 @@
 # Creates a c++ file for builtin resources that contains binary data of all resources
 
 # parameters are
-#0 - path to the .py file
-#1 - output file path
-#2 - cmake source dir
-#3 - list of paths to resource files
+# 0 - path to the .py file
+# 1 - output file path
+# 2 - cmake source dir
+# 3 - list of paths to resource files
 
-import sys
-import os
+import sys, os
 
 if  len(sys.argv) < 4 :
     print(sys.argv[0] + " - Incorrect argument count")
 else:
-    #arguments
     outputFilename = sys.argv[1]
     cmakeSourceDir = sys.argv[2]
     resourcesFile  = sys.argv[3]
@@ -22,23 +20,30 @@ else:
     file = open(resourcesFile, 'r')
     resourcePaths = file.readlines()
 
-    #opening a file
-    outp = open(outputFilename,"w+")
+    outp = open(outputFilename, "w+")
     
-    outp.write("#include \"" + correspondingHeaderFile + "\"\n")
-    outp.write("\tnamespace " + resourcesNamespace + " {\n")
-
-    outp.write("template<nbl::core::StringLiteral Path>")
-    outp.write("const std::pair<const uint8_t*, size_t> get_resource();")
-  
-    # writing binary  data of all files in a loop
+    outp.write(f"""
+    #include "{correspondingHeaderFile}"
+    namespace {resourcesNamespace}
+    {{
+    
+    static constexpr SBuiltinFile DUMMY_BUILTIN_FILE = {{ .contents = nullptr, .size = 0, .xx256Hash = 69, .modified = {{}} }};
+    
+    template<nbl::core::StringLiteral Path>
+    const SBuiltinFile& get_resource();
+    """)
+     
+    # writing binary data of all files
     for z in resourcePaths:
         itemData = z.split(',')
         x = itemData[0].rstrip()
-    
-        outp.write('\n\ttemplate<> const std::pair<const uint8_t*, size_t> get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("%s")>()' % x)
-        outp.write('\n\t{')
-        outp.write('\n\t\tstatic const uint8_t data[] = {\n\t\t\t')
+        
+        outp.write(f"""
+        template<> const SBuiltinFile& get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("{x}")>()
+        {{
+        static constexpr uint8_t data[] = {{
+        """)
+            
         try:
             with open(cmakeSourceDir+'/'+x, "rb") as f:
                 index = 0
@@ -47,28 +52,41 @@ else:
                     outp.write("0x%s, " % byte.hex())
                     index += 1  
                     if index % 20 == 0 :
-                        outp.write("\n\t\t\t")
+                        outp.write("""
+                        """)
                     byte = f.read(1)
             # don't write null terminator, it messes up non-text files
 
         except IOError: 
             # file not found
-            print('Error: BuiltinResources - file with the following path not found: ' + x)
-            outp.write('\n\n Error: BuiltinResources - file with the following path not found: %s' % x )
-
-        outp.write('\n\t\t};')
-        outp.write('\n\t\treturn { data, sizeof(data) };')
-        outp.write('\n\t}\n')
+            print(f"Error: BuiltinResources - file with the following path not found: {x}")
+            outp.write(f"""
+            Error: BuiltinResources - file with the following path not found: {x}
+            """)
+            
+        outp.write(f"""
+        }};
+        
+        static constexpr SBuiltinFile builtinFile = {{ .contents = data, .size = sizeof(data), .xx256Hash = 69, .modified = {{}} }};    
+        
+        return builtinFile;
+        }}
+        """)
         
         if len(itemData) > 1:
             for i in range(1, len(itemData)):
-                outp.write('\n\ttemplate<> const std::pair<const uint8_t*, size_t> get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("%s")>()' % itemData[i].rstrip())
-                outp.write('\n\t{')
-                outp.write('\n\t\treturn get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("%s")>();' % x)
-                outp.write('\n\t}\n')
-            
-    outp.write("\tstd::pair<const uint8_t*, size_t> get_resource_runtime(const std::string& filename) {\n")
-    outp.write("\t\tstatic std::unordered_map<std::string, int> resourcesByFilename( {\n")
+                outp.write(f"""
+                template<> const SBuiltinFile& get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("{itemData[i].rstrip()}")>()
+                {{
+                    return get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("{x}")>();
+                }}
+                """)
+    
+    outp.write(f"""
+    const SBuiltinFile& get_resource_runtime(const std::string& filename) {{
+    static std::unordered_map<std::string, int> resourcesByFilename( {{
+    """)
+
     counter = 1
     
     for z in resourcePaths:
@@ -83,21 +101,36 @@ else:
                 
         counter+= 1
         
-    outp.write("\t\t});\n\n")
-    outp.write("\t\tauto resource = resourcesByFilename.find(filename);\n")
-    outp.write("\t\tif(resource == resourcesByFilename.end()) return { nullptr,0ull };\n")
-    outp.write("\t\tswitch (resource->second) \n\t\t\t{\n")
+    outp.write(f"""
+    }});
+    
+    auto resource = resourcesByFilename.find(filename);
+    if(resource == resourcesByFilename.end())
+        return DUMMY_BUILTIN_FILE;
+        
+    switch (resource->second)
+    {{
+    """)
+
     counter = 1
     
     for z in resourcePaths:
         itemData = z.split(',')
         x = itemData[0].rstrip()
         
-        outp.write("\t\t\tcase %d:\n\t\t\t\t\treturn get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE(\"%s\")>();\n" % (counter,x))
+        outp.write(f"""
+        case {counter}:
+            return get_resource<NBL_CORE_UNIQUE_STRING_LITERAL_TYPE("{x}")>();
+        """)
+        
         counter+= 1
   
-    outp.write("\t\t\tdefault:\n")
-    outp.write("\t\t\t\treturn { nullptr,0ull };\n")
-    outp.write("\t\t}\n\t}")
-    outp.write("\n}")
+    outp.write(f"""
+    default:
+        return DUMMY_BUILTIN_FILE;
+    }}
+    }}
+    }}
+    """)
+    
     outp.close()
