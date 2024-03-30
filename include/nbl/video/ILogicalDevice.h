2b863c353a64d2471ff50dfc239de35d4dce8ee0
Erfan Ahmadi <ahmadierfan99@gmail.com>
2024-03-30 20:57:33 +0330

    Fixed a bug with using more than one layouts in createDescriptorPoolForDSLayouts

--- include/nbl/video/ILogicalDevice.h
+++ include/nbl/video/ILogicalDevice.h
@@ -620,7 +620,7 @@ class NBL_API2 ILogicalDevice : public core::IReferenceCounted, public IDeviceMe
             {
                 if (layout)
                 {
-                    const auto setCount = setCounts ? *(setCountsIt++):1u;
+                    const auto setCount = setCounts ? *(setCountsIt):1u;
                     createInfo.maxSets += setCount;
                     for (uint32_t t=0; t<static_cast<uint32_t>(asset::IDescriptor::E_TYPE::ET_COUNT); ++t)
                     {
