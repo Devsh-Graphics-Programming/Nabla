18f549f20050197c66754823c3f1ec373d77baa4
devsh <devsh@devsh.eu>
2024-04-07 03:22:51 +0200

    Correct a lot of stuff that didn't make sense with the SPIR-V intrinsics, also left a lot of nice comments for @Hazardu

--- include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
+++ include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
@@ -48,82 +48,46 @@ static const uint32_t3 GlobalInvocationId;
 static const uint32_t LocalInvocationIndex;
 
 //! General Operations
-template<typename T>
-T atomicAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
+
+// Here's the thing with atomics, it's not only the data type that dictates whether you can do an atomic or not.
+// It's the storage class that has the most effect (shared vs storage vs image) and we can't check that easily
+template<typename T> // integers operate on 2s complement so same op for signed and unsigned
 [[vk::ext_instruction(spv::OpAtomicIAdd)]]
-int32_t atomicAdd([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicIAdd )]]
-uint32_t atomicAdd([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
+T atomicIAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
 
 template<typename T>
+[[vk::ext_instruction(spv::OpAtomicAnd)]]
 T atomicAnd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicAnd )]]
-int32_t atomicAnd([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicAnd )]]
-uint32_t atomicAnd([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
 
 template<typename T>
+[[vk::ext_instruction(spv::OpAtomicOr)]]
 T atomicOr([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicOr )]]
-int32_t atomicOr([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicOr )]]
-uint32_t atomicOr([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
 
 template<typename T>
+[[vk::ext_instruction(spv::OpAtomicXor)]]
 T atomicXor([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicXor )]]
-int32_t atomicXor([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicXor )]]
-uint32_t atomicXor([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
 
-template<typename T>
-T atomicMin([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
+template<typename Signed>
 [[vk::ext_instruction( spv::OpAtomicSMin )]]
-int32_t atomicMin([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicSMin )]]
-uint32_t atomicMin([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
+Signed atomicSMin([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, Signed value);
+template<typename Unsigned>
+[[vk::ext_instruction( spv::OpAtomicUMin )]]
+Unsigned atomicUMin([[vk::ext_reference]] Unsigned ptr, uint32_t memoryScope, uint32_t memorySemantics, Unsigned value);
+
+template<typename Signed>
+[[vk::ext_instruction( spv::OpAtomicSMax )]]
+Signed atomicSMax([[vk::ext_reference]] Signed ptr, uint32_t memoryScope, uint32_t memorySemantics, Signed value);
+template<typename Unsigned>
+[[vk::ext_instruction( spv::OpAtomicUMax )]]
+Unsigned atomicUMax([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, Unsigned value);
 
 template<typename T>
-T atomicMax([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicSMax )]]
-int32_t atomicMax([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicSMax )]]
-uint32_t atomicMax([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
-
-template<typename T>
+[[vk::ext_instruction(spv::OpAtomicExchange)]]
 T atomicExchange([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicExchange )]]
-int32_t atomicExchange([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, int32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicExchange )]]
-uint32_t atomicExchange([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, uint32_t value);
-template<>
-[[vk::ext_instruction( spv::OpAtomicExchange )]]
-float32_t atomicExchange([[vk::ext_reference]] float32_t ptr, uint32_t memoryScope, uint32_t memorySemantics, float32_t value);
-
 
 template<typename T>
-T atomicCompSwap([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memSemanticsEqual, uint32_t memSemanticsUnequal, T value, T comparator);
-template<>
-[[vk::ext_instruction( spv::OpAtomicCompareExchange )]]
-int32_t atomicCompSwap([[vk::ext_reference]] int32_t ptr, uint32_t memoryScope, uint32_t memSemanticsEqual, uint32_t memSemanticsUnequal, int32_t value, int32_t comparator);
-template<>
-[[vk::ext_instruction( spv::OpAtomicCompareExchange )]]
-uint32_t atomicCompSwap([[vk::ext_reference]] uint32_t ptr, uint32_t memoryScope, uint32_t memSemanticsEqual, uint32_t memSemanticsUnequal, uint32_t value, uint32_t comparator);
-
+[[vk::ext_instruction(spv::OpAtomicCompareExchange)]]
+T atomicCompareExchange([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memSemanticsEqual, uint32_t memSemanticsUnequal, T value, T comparator);
 
 //! Std 450 Extended set operations
 template<typename SquareMatrix>
