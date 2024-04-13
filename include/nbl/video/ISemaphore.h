d4bf6d98f85cca02465d807694ff7ac97a194c1d
devsh <devsh.graphicsprogramming@gmail.com>
2024-04-13 14:00:31 +0200

    add GPU `future_t`

--- include/nbl/video/ISemaphore.h
+++ include/nbl/video/ISemaphore.h
@@ -41,6 +41,89 @@ class ISemaphore : public IBackendObject
             _ERROR
         };
 
+        // Utility structs
+        class future_base_t
+        {
+            public:
+                inline void set(const SWaitInfo& wait)
+                {
+                    m_semaphore = core::smart_refctd_ptr<const ISemaphore>(wait.semaphore);
+                    m_waitValue = wait.value;
+                }
+                inline bool ready() const
+                {
+                    return m_semaphore->getCounterValue()>=m_waitValue;
+                }
+                WAIT_RESULT wait() const;
+
+            protected:
+                // the base class is not directly usable
+                inline future_base_t() = default;
+                // derived won't be copyable
+                future_base_t(const future_base_t&) = delete;
+                inline future_base_t(future_base_t&&) = default;
+                future_base_t& operator=(const future_base_t&) = delete;
+                inline future_base_t& operator=(future_base_t&&) = default;
+
+                // smartpointer cause lifetime needs to be maintained
+                core::smart_refctd_ptr<const ISemaphore> m_semaphore;
+                uint64_t m_waitValue;
+        };
+        // Similar to `ISystem::future_t` but NOT thread-safe and the condition we wait on is signalled by the Device
+        template<typename T>
+        class future_t : private core::StorageTrivializer<T>, public future_base_t
+        {
+                using storage_t = core::StorageTrivializer<T>;
+                using this_t = future_t<T>;
+
+            public:
+                template<typename... Args>
+                inline future_t(Args&&... args)
+                {
+                    storage_t::construct(std::forward(args)...);
+                }
+                inline future_t(this_t&& other) : future_base_t(std::move<future_base_t>(other))
+                {
+                    if constexpr (!std::is_void_v<T>)
+                        storage_t::construct(std::move(*other.getStorage()));
+                }
+                inline ~future_t()
+                {
+                    const bool success = wait();
+                    assert(success);
+                    storage_t::destruct();
+                }
+
+                inline this_t& operator=(this_t&& rhs)
+                {
+                    future_base_t::operator=(std::move<future_base_t>(rhs));
+                    if constexpr (!std::is_void_v<T>)
+                        *storage_t::getStorage() = *rhs.getStorage();
+                    return *this;
+                }
+
+                inline const T* get() const
+                {
+                    if (ready())
+                        return storage_t::getStorage();
+                    return nullptr;
+                }
+
+                template<std::copyable U> requires std::is_same_v<T,U>
+                inline T copy() const
+                {
+                    const bool success = wait();
+                    assert(success);
+                    return *get();
+                }
+
+                template<std::movable U> requires std::is_same_v<T,U>
+                inline void move_into(U& dst)
+                {
+                    dst = std::move(*get());
+                }
+        };
+
         // Vulkan: const VkSemaphore*
         virtual const void* getNativeHandle() const = 0;
 
