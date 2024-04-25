dc6bf001d6c57fb1341195c55beff9579c7a5d1a
Przemek <minikers21@gmail.com>
2024-04-25 21:24:28 +0200

    Updated examples, removed debug code

--- include/nbl/asset/IDescriptorSetLayout.h
+++ include/nbl/asset/IDescriptorSetLayout.h
@@ -293,9 +293,6 @@ public:
 
 	bool isSubsetOf(const IDescriptorSetLayout<sampler_type>* other) const
 	{
-		auto a = getTotalBindingCount();
-		auto b = other->getTotalBindingCount();
-
 		if (!other || getTotalBindingCount() > other->getTotalBindingCount())
 			return false;
 
