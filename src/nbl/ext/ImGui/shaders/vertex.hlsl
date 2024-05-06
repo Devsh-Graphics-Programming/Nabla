92da0f1643c268b5a526c529de911c8c4f140f01
AnastaZIuk <areklachowicz@gmail.com>
2024-05-06 07:24:02 +0200

    fix IMGUI includes, use implicit std::span conversions, get push constants from common.hlsl across cpp files as well, use builtins to get deployed SPIRV of IMGUI shaders, use transient command buffer for gpu image creation, update memory range mapping, use CCustomAllocatorCPUBuffer to avoid extra memcpy copies to adapt memory, add logging more output, use IUtilities & allocations, update API usage to latest. Save work - time to compile it.

--- src/nbl/ext/ImGui/shaders/vertex.hlsl
+++ src/nbl/ext/ImGui/shaders/vertex.hlsl
@@ -1,18 +1,5 @@
 #include "common.hlsl"
 
-struct VSInput
-{
-    [[vk::location(0)]] float2 position : POSITION;
-	[[vk::location(1)]] float2 uv : TEXCOORD0;
-    [[vk::location(2)]] float4 color : COLOR0;
-};
-
-struct PushConstants
-{
-    float2 scale;
-    float2 translate;
-};
-
 [[vk::push_constant]] struct PushConstants pc;
 
 PSInput VSMain(VSInput input)
