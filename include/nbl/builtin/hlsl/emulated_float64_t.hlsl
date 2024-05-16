338213c9e9a7b4991b19d5ffea7e7e5d67184309
Przemek <minikers21@gmail.com>
2024-05-16 18:37:17 +0200

    Fixed GPU values test

--- include/nbl/builtin/hlsl/emulated_float64_t.hlsl
+++ include/nbl/builtin/hlsl/emulated_float64_t.hlsl
@@ -55,8 +55,8 @@ namespace emulated
         bool operator!=(const emulated_float64_t rhs) { return uint64_t(data) ^ uint64_t(rhs.data); }
         bool operator<(const emulated_float64_t rhs) { return data < rhs.data; }
         bool operator>(const emulated_float64_t rhs) { return data > rhs.data; }
-        bool operator<=(const emulated_float64_t rhs) { return !operator>(rhs); }
-        bool operator>=(const emulated_float64_t rhs) { return !operator<(rhs); }
+        bool operator<=(const emulated_float64_t rhs) { return data <= rhs.data; }
+        bool operator>=(const emulated_float64_t rhs) { return data >= rhs.data; }
 
         //logical operators
         bool operator&&(const emulated_float64_t rhs) { return bool(data) && bool(rhs.data); }
@@ -73,46 +73,13 @@ namespace emulated
         operator min16int() { return min16int(data);}
         operator float64_t() { return float64_t(data); }
         operator half() { return half(data); }
-#else
-        operator uint16_t() { return uint16_t(data);}
-        operator double() { return double(data); }
 #endif
 
         //explicit operator int() const { return int(data); }
 
-        // HERE OMITED OPERATORS
+        // OMITED OPERATORS
         //  - not implementing bitwise and modulo operators since floating point types doesn't support them
         //  - compound operator overload not supported in HLSL
         //  - access operators (dereference and addressof) not supported in HLSL
-#ifndef __HLSL_VERSION
-        // compound assignment operators
-        emulated_float64_t operator+=(emulated_float64_t rhs)
-        {
-            data = data + rhs.data;
-            return create(data);
-        }
-        
-        emulated_float64_t operator-=(emulated_float64_t rhs)
-        {
-            data = data - rhs.data;
-            return create(data);
-        }
-        
-        emulated_float64_t operator*=(emulated_float64_t rhs)
-        {
-            data = data * rhs.data;
-            return create(data);
-        }
-        
-        emulated_float64_t operator/=(emulated_float64_t rhs)
-        {
-            data = data / rhs.data;
-            return create(data);
-        }
-        
-        // access operators
-        emulated_float64_t operator*() { return *this; }
-        emulated_float64_t* operator&() { return this; }
-#endif
     };
 }
