189246b85d37374951f2815a36b6f09ee673f980
Fletterio <fj.letterio@gmail.com>
2024-05-17 17:07:45 -0300

    Adds all functions up to before exponentials in the cppreference list. Missing compound operators and template specializations for integer types. Arithmetic logic is off, different from the STL

--- include/nbl/builtin/hlsl/complex.hlsl
+++ include/nbl/builtin/hlsl/complex.hlsl
@@ -7,7 +7,7 @@
 
 
 
-#ifdef __HLSL_VERSION
+#ifndef __HLSL_VERSION
 
 
 namespace nbl
@@ -15,87 +15,163 @@ namespace nbl
 namespace hlsl
 {
 
+    template<typename T>
+    struct complex_t
+    {
+        T m_real;
+        T m_imag;
+            
+        // ------------------------- Constructors ---------------------------------------
+        static complex_t create(const T real, const T imag)
+        {
+                complex_t retVal = { real, imag };
+                return retVal;
+        }
+            
+        static complex_t create(const complex_t other)
+        {
+                return other;
+        }
+            
+        // ------------------------- Member functions -------------------------------      
+        T real() {
+            return m_real;
+        }
+            
+        void real(const T value)
+        {
+            m_real = value;
+        }
+            
+        T imag()
+        {
+            return m_imag;
+        }
+            
+        void imag(const T value)
+        {
+            m_imag = value;
+        }
+        // ------------------------- Arithmetic operators -------------------------------   
+        complex_t operator+(const complex_t rhs)
+        {
+            complex_t result;
+            result.m_real = m_real + rhs.m_real;
+            result.m_imag = m_imag + rhs.m_imag;
 
+            return result;
+        }
+            
+        complex_t operator-(const complex_t rhs)
+        {
+            complex_t result;
 
-template<typename T>
-struct complex_t
-{
-    T real;
-    T imag;
-};
+            result.m_real = m_real - rhs.m_real;
+            result.m_imag = m_imag - rhs.m_imag;
 
-template<typename T>
-complex_t<T> plus(const complex_t<T> a, const complex_t<T> b)
-{
-    complex_t<T> result;
+            return result;
+        }
 
-    result.real = a.real + b.real;
-    result.imag = a.imag + b.imag;
+        complex_t operator*(const complex_t rhs)
+        {
+            complex_t result;
 
-    return result;
+            result.m_real = m_real * rhs.m_real - m_imag * rhs.m_imag;
+            result.m_imag = m_real * rhs.m_imag + m_imag * rhs.m_real;
+
+            return result;
+        }
+            
+        // multiply by scalar
+        complex_t operator*(const T scalar)
+        {
+            complex_t result;
+            result.m_real = m_real * scalar;
+            result.m_imag = m_imag * scalar;
+
+            return result;
+        }
+            
+        // Divide by scalar
+        complex_t operator/(const T scalar)
+        {
+            complex_t result;
+            result.m_real = m_real / scalar;
+            result.m_imag = m_imag / scalar;
+
+            return result;
+        }
+            
+        complex_t operator/(const complex_t rhs)
+        {
+            complex_t result;
+
+            T denominator = rhs.m_real * rhs.m_real + rhs.m_imag * rhs.m_imag;
+            result.m_real = (m_real * rhs.m_real + m_imag * rhs.m_imag) / denominator;
+            result.m_imag = (m_imag * rhs.m_real - m_real * rhs.m_imag) / denominator;
+
+            return result;
+        }
+            
+        // ----------------- Relational operators -----------------------------
+        bool operator==(const complex_t rhs)
+        {
+                return !(uint64_t(m_real) ^ uint64_t(rhs.m_real)) && !(uint64_t(m_imag) ^ uint64_t(rhs.m_imag));
+        }
+        bool operator!=(const complex_t rhs)
+        {
+                return uint64_t(m_real) ^ uint64_t(rhs.m_real) || uint64_t(m_imag) ^ uint64_t(rhs.m_imag);
+        }
+            
+    };
+
+    // ---------------------- Non-member functions -----------------------------    
+    template<typename T>
+    T real(const complex_t<T> c) {
+        return c.m_real;
+    }
+    
+    template<typename T>
+    T imag(const complex_t<T> c) {
+        return c.m_imag;
+    }
+    
+    template<typename T>
+    T norm(const complex_t<T> c) {
+        return c.m_real * c.m_real + c.m_imag * c.m_imag;
+    }
+
+    template<typename T>
+    T abs(const complex_t<T> c) {
+        return sqrt(norm(c));
+    }
+
+    template<typename T>
+    T arg(const complex_t<T> c) {
+        return atan2(c.m_imag, c.m_real);
+    }
+
+    template<typename T>
+    complex_t<T> conj(const complex_t<T> c) {
+        complex_t<T> retVal = {c.m_real, - c.m_imag};
+        return retVal;
+    }
+
+    template<typename T>
+    complex_t<T> proj(const complex_t<T> c) {
+        T den = norm(c) + T(1.0);
+        complex_t<T> retVal = { (T(2.0) * c.m_real) / den , (T(2.0) * c.m_imag) / den};
+        return retVal;
+    }
+
+    template<typename T>
+    complex_t<T> polar(const T r, const T theta) {
+        complex_t<T> retVal = {r * cos(theta), r * sin(theta)};
+        return retVal;
+    }
+
 }
-
-template<typename T>
-complex_t<T> minus(const complex_t<T> a, const complex_t<T> b)
-{
-    complex_t<T> result;
-
-    result.real = a.real - b.real;
-    result.imag = a.imag - b.imag;
-
-    return result;
 }
-
-template<typename T>
-complex_t<T> mul(const complex_t<T> a, const complex_t<T> b)
-{
-    complex_t<T> result;
-
-    result.real = a.real * b.real - a.imag * b.imag;
-    result.imag = a.real * b.imag + a.imag * b.real;
-
-    return result;
-}
-
-// multiply by scalar
-template<typename T>
-complex_t<T> mul(const complex_t<T> c, T scalar)
-{
-    complex_t<T> result;
-    result.real = c.real * scalar;
-    result.imag = c.imag * scalar;
-
-    return result;
-}
-
-
-template<typename T>
-complex_t<T> div(const complex_t<T> a, const complex_t<T> b)
-{
-    complex_t<T> result;
-
-    T denominator = b.real * b.real + b.imag * b.imag;
-    result.real = (a.real * b.real + a.imag * b.imag) / denominator;
-    result.imag = (a.imag * b.real - a.real * b.imag) / denominator;
-
-    return result;
-}
-
-// Divide by scalar
-template<typename T>
-complex_t<T> div(const complex_t<T> c, T scalar)
-{
-    complex_t<T> result;
-    result.real = c.real / scalar;
-    result.imag = c.imag / scalar;
-
-    return result;
-}
-
-
-}
-}
-
 
 #endif
 
