d471a8c3ba1f5bf7bb2721b605da4e1ac5f68ff8
Nipun Garg <nipung271@gmail.com>
2024-05-20 21:44:28 +0530

    Use enable_if for int64atomic checks

--- include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
+++ include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
@@ -58,11 +58,21 @@ using pointer_t = vk::SpirvOpaqueType<spv::OpTypePointer,vk::Literal<vk::integra
 // It's the storage class that has the most effect (shared vs storage vs image) and we can't check that easily
 template<typename T> // integers operate on 2s complement so same op for signed and unsigned
 [[vk::ext_instruction(spv::OpAtomicIAdd)]]
-T atomicIAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
+typename nbl::hlsl::enable_if<is_same_v<T,uint32_t> || is_same_v<T,int32_t>, T>::type atomicIAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
 
 template<typename T, typename Ptr_T> // DXC Workaround
 [[vk::ext_instruction(spv::OpAtomicIAdd)]]
-typename nbl::hlsl::enable_if<is_spirv_type_v<Ptr_T>, T>::type atomicIAdd(Ptr_T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
+typename nbl::hlsl::enable_if<is_spirv_type_v<Ptr_T> && (is_same_v<T,uint32_t> || is_same_v<T,int32_t>), T>::type atomicIAdd(Ptr_T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
+
+template<typename T> // integers operate on 2s complement so same op for signed and unsigned
+[[vk::ext_capability(spv::CapabilityInt64Atomics)]]
+[[vk::ext_instruction(spv::OpAtomicIAdd)]]
+typename nbl::hlsl::enable_if<is_same_v<T,uint64_t> || is_same_v<T,int64_t>, T>::type atomicIAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
+
+template<typename T, typename Ptr_T> // DXC Workaround
+[[vk::ext_capability(spv::CapabilityInt64Atomics)]]
+[[vk::ext_instruction(spv::OpAtomicIAdd)]]
+typename nbl::hlsl::enable_if<is_spirv_type_v<Ptr_T> && (is_same_v<T,uint64_t> || is_same_v<T,int64_t>), T>::type atomicIAdd(Ptr_T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
 
 template<typename T>
 [[vk::ext_instruction(spv::OpAtomicAnd)]]
@@ -202,20 +212,8 @@ template<typename T, typename U, uint32_t StorageClass>
 [[vk::ext_instruction(spv::OpBitcast)]]
 pointer_t<StorageClass,T> bitcast(pointer_t<StorageClass,U>);
 }
-
-namespace atomic64 {
-template<typename T> // integers operate on 2s complement so same op for signed and unsigned
-[[vk::ext_capability(spv::CapabilityInt64Atomics)]]
-[[vk::ext_instruction(spv::OpAtomicIAdd)]]
-T atomicIAdd([[vk::ext_reference]] T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-
-template<typename T, typename Ptr_T> // DXC Workaround
-[[vk::ext_capability(spv::CapabilityInt64Atomics)]]
-[[vk::ext_instruction(spv::OpAtomicIAdd)]]
-typename nbl::hlsl::enable_if<is_spirv_type_v<Ptr_T>, T>::type atomicIAdd(Ptr_T ptr, uint32_t memoryScope, uint32_t memorySemantics, T value);
-}
 #endif
-}
+    }
 }
 
 #endif
