b20de536558e4cddacd02b176eca0ced55a78908
Przemek <minikers21@gmail.com>
2024-05-31 18:55:50 +0200

    Implemented add sub i mul operators

--- include/nbl/builtin/hlsl/emulated_float64_t.hlsl
+++ include/nbl/builtin/hlsl/emulated_float64_t.hlsl
@@ -1,7 +1,30 @@
 #include <nbl/builtin/hlsl/cpp_compat.hlsl>
 
-using float32_t = float;
-//using emulated_float64_t = double;
+#ifdef __HLSL_VERSION
+#define LERP lerp
+#else
+#define LERP nbl::hlsl::lerp
+#endif
+
+#ifdef __HLSL_VERSION
+#define ABS abs
+#else
+#define ABS std::abs
+#endif
+
+// TODO: inline function
+#define EXCHANGE(a, b) \
+   do {                \
+       a ^= b;         \
+       b ^= a;         \
+       a ^= b;         \
+   } while (false)
+   
+#define FLOAT_ROUND_NEAREST_EVEN    0
+#define FLOAT_ROUND_TO_ZERO         1
+#define FLOAT_ROUND_DOWN            2
+#define FLOAT_ROUND_UP              3
+#define FLOAT_ROUNDING_MODE         FLOAT_ROUND_NEAREST_EVEN
 
 namespace emulated
 {
@@ -11,8 +34,8 @@ namespace emulated
         {
             uint64_t product = uint64_t(lhs) * uint64_t(rhs);
             nbl::hlsl::uint32_t2 output;
-            output.x = (product & 0xFFFFFFFF00000000) >> 32;
-            output.y = product & 0x00000000FFFFFFFFull;
+            output.x = uint32_t((product & 0xFFFFFFFF00000000) >> 32);
+            output.y = uint32_t(product & 0x00000000FFFFFFFFull);
             return output;
         }
 
@@ -24,15 +47,55 @@ namespace emulated
 
            return output;
         }
+        
+        nbl::hlsl::uint32_t2 sub64(uint32_t a0, uint32_t a1, uint32_t b0, uint32_t b1)
+        {
+            nbl::hlsl::uint32_t2 output;
+            output.y = a1 - b1;
+            output.x = a0 - b0 - uint32_t(a1 < b1);
+            
+            return output;
+        }
+
+        // TODO: test
+        int countLeadingZeros32(uint32_t val)
+        {
+#ifndef __HLSL_VERSION
+            return 31 - nbl::hlsl::findMSB(val);
+#else
+            return 31 - firstbithigh(val);
+#endif
+        }
+
 
         nbl::hlsl::uint32_t2 shortShift64Left(uint32_t a0, uint32_t a1, int count)
         {
             nbl::hlsl::uint32_t2 output;
             output.y = a1 << count;
-            output.x = nbl::hlsl::lerp((a0 << count | (a1 >> ((-count) & 31))), a0, count == 0);
+            output.x = LERP((a0 << count | (a1 >> ((-count) & 31))), a0, count == 0);
             
             return output;
         };
+        
+        nbl::hlsl::uint32_t2 shift64RightJamming(uint32_t a0, uint32_t a1, int count)
+        {
+            nbl::hlsl::uint32_t2 output;
+            const int negCount = (-count) & 31;
+        
+            output.x = LERP(0u, a0, count == 0);
+            output.x = LERP(output.x, (a0 >> count), count < 32);
+        
+            output.y = uint32_t((a0 | a1) != 0u); /* count >= 64 */
+            uint32_t z1_lt64 = (a0>>(count & 31)) | uint32_t(((a0<<negCount) | a1) != 0u);
+            output.y = LERP(output.y, z1_lt64, count < 64);
+            output.y = LERP(output.y, (a0 | uint32_t(a1 != 0u)), count == 32);
+            uint32_t z1_lt32 = (a0<<negCount) | (a1>>count) | uint32_t ((a1<<negCount) != 0u);
+            output.y = LERP(output.y, z1_lt32, count < 32);
+            output.y = LERP(output.y, a1, count == 0);
+            
+            return output;
+        }
+
         
         nbl::hlsl::uint32_t4 mul64to128(uint32_t a0, uint32_t a1, uint32_t b0, uint32_t b1)
         {
@@ -84,23 +147,23 @@ namespace emulated
            
            int negCount = (-count) & 31;
         
-           output.z = nbl::hlsl::lerp(uint32_t(a0 != 0u), a0, count == 64);
-           output.z = nbl::hlsl::lerp(output.z, a0 << negCount, count < 64);
-           output.z = nbl::hlsl::lerp(output.z, a1 << negCount, count < 32);
+           output.z = LERP(uint32_t(a0 != 0u), a0, count == 64);
+           output.z = LERP(output.z, a0 << negCount, count < 64);
+           output.z = LERP(output.z, a1 << negCount, count < 32);
         
-           output.y = nbl::hlsl::lerp(0u, (a0 >> (count & 31)), count < 64);
-           output.y = nbl::hlsl::lerp(output.y, (a0<<negCount) | (a1>>count), count < 32);
+           output.y = LERP(0u, (a0 >> (count & 31)), count < 64);
+           output.y = LERP(output.y, (a0<<negCount) | (a1>>count), count < 32);
         
-           a2 = nbl::hlsl::lerp(a2 | a1, a2, count < 32);
-           output.x = nbl::hlsl::lerp(output.x, a0 >> count, count < 32);
+           a2 = LERP(a2 | a1, a2, count < 32);
+           output.x = LERP(output.x, a0 >> count, count < 32);
            output.z |= uint32_t(a2 != 0u);
         
-           output.x = nbl::hlsl::lerp(output.x, 0u, (count == 32));
-           output.y = nbl::hlsl::lerp(output.y, a0, (count == 32));
-           output.z = nbl::hlsl::lerp(output.z, a1, (count == 32));
-           output.x = nbl::hlsl::lerp(output.x, a0, (count == 0));
-           output.y = nbl::hlsl::lerp(output.y, a1, (count == 0));
-           output.z = nbl::hlsl::lerp(output.z, a2, (count == 0));
+           output.x = LERP(output.x, 0u, (count == 32));
+           output.y = LERP(output.y, a0, (count == 32));
+           output.z = LERP(output.z, a1, (count == 32));
+           output.x = LERP(output.x, a0, (count == 0));
+           output.y = LERP(output.y, a1, (count == 0));
+           output.z = LERP(output.z, a2, (count == 0));
            
            return output;
         }
@@ -178,11 +241,11 @@ namespace emulated
               {
                  if (zSign != 0u)
                  {
-                    //increment = (FLOAT_ROUNDING_MODE == FLOAT_ROUND_DOWN) && (zFrac2 != 0u);
+                    increment = (FLOAT_ROUNDING_MODE == FLOAT_ROUND_DOWN) && (zFrac2 != 0u);
                  }
                  else
                  {
-                    //increment = (FLOAT_ROUNDING_MODE == FLOAT_ROUND_UP) && (zFrac2 != 0u);
+                    increment = (FLOAT_ROUNDING_MODE == FLOAT_ROUND_UP) && (zFrac2 != 0u);
                  }
               }
            }
@@ -196,12 +259,36 @@ namespace emulated
            }
            else
            {
-              zExp = nbl::hlsl::lerp(zExp, 0, (zFrac0 | zFrac1) == 0u);
+              zExp = LERP(zExp, 0, (zFrac0 | zFrac1) == 0u);
            }
            
            return packFloat64(zSign, zExp, zFrac0, zFrac1);
         }
-
+        
+        uint64_t normalizeRoundAndPackFloat64(uint32_t sign, int exp, uint32_t frac0, uint32_t frac1)
+        {
+           int shiftCount;
+           nbl::hlsl::uint32_t3 frac = nbl::hlsl::uint32_t3(frac0, frac1, 0u);
+        
+           if (frac.x == 0u)
+           {
+              exp -= 32;
+              frac.x = frac.y;
+              frac.y = 0u;
+           }
+        
+           shiftCount = countLeadingZeros32(frac.x) - 11;
+           if (0 <= shiftCount)
+           {
+              frac.xy = shortShift64Left(frac.x, frac.y, shiftCount);
+           }
+           else
+           {
+              frac.xyz = shift64ExtraRightJamming(frac.x, frac.y, 0u, -shiftCount);
+           }
+           exp -= shiftCount;
+           return roundAndPackFloat64(sign, exp, frac.x, frac.y, frac.z);
+        }
 
     }
 
@@ -215,12 +302,37 @@ namespace emulated
         // TODO: specializations
         template <typename T>
         static emulated_float64_t create(T val)
-        { 
+        {
             emulated_float64_t output;
             output.data = val;
             return output;
         }
         
+        static emulated_float64_t createEmulatedFloat64PreserveBitPattern(uint64_t val)
+        {
+            emulated_float64_t output;
+            output.data = val;
+            return output;
+        }
+        
+        // TODO: won't not work for uints with msb of index > 52
+        template<>
+        static emulated_float64_t create(uint64_t val)
+        {
+#ifndef __HLSL_VERSION
+            const uint64_t msbIndex = nbl::hlsl::findMSB(val);
+#else
+            const uint64_t msbIndex = firstbithigh(val);
+#endif
+            uint64_t exp = ((msbIndex + 1023) << 52) & 0x7FF0000000000000;
+            uint64_t mantissa = (val << (52 - msbIndex)) & 0x000FFFFFFFFFFFFFull;
+            emulated_float64_t output;
+            output.data = exp | mantissa;
+            return output;
+        }
+        
+        // TODO: temporary, remove
+#ifndef __HLSL_VERSION
         template<>
         static emulated_float64_t create(double val)
         {
@@ -228,36 +340,184 @@ namespace emulated
             output.data = reinterpret_cast<uint64_t&>(val);
             return output;
         }
+#endif
 
         // arithmetic operators
         emulated_float64_t operator+(const emulated_float64_t rhs)
         {
-            emulated_float64_t retval;
-            retval.data = data + rhs.data;
-            return retval;
+            emulated_float64_t retval = createEmulatedFloat64PreserveBitPattern(0u);
+            
+            uint32_t lhsSign = uint32_t((data & 0x8000000000000000ull) >> 32);
+            uint32_t rhsSign = uint32_t((rhs.data & 0x8000000000000000ull) >> 32);
+
+            uint32_t lhsLow = uint32_t(data & 0x00000000FFFFFFFFull);
+            uint32_t rhsLow = uint32_t(rhs.data & 0x00000000FFFFFFFFull);
+            uint32_t lhsHigh = uint32_t((data & 0x000FFFFF00000000ull) >> 32);
+            uint32_t rhsHigh = uint32_t((rhs.data & 0x000FFFFF00000000ull) >> 32);
+
+            int lhsExp = int((data >> 52) & 0x7FFull);
+            int rhsExp = int((rhs.data >> 52) & 0x7FFull);
+            
+            int expDiff = lhsExp - rhsExp;
+
+            if (lhsSign == rhsSign)
+            {
+                nbl::hlsl::uint32_t3 frac;
+                int exp;
+
+                if (expDiff == 0)
+                {
+                   //if (lhsExp == 0x7FF)
+                   //{
+                   //   bool propagate = ((lhsHigh | rhsHigh) | (lhsLow| rhsLow)) != 0u;
+                   //   return nbl::hlsl::lerp(a, propagateFloat64NaN(a, b), propagate);
+                   //}
+                   frac.xy = impl::add64(lhsHigh, lhsLow, rhsHigh, rhsLow);
+                   if (lhsExp == 0)
+                      return createEmulatedFloat64PreserveBitPattern(impl::packFloat64(lhsSign, 0, frac.x, frac.y));
+                   frac.z = 0u;
+                   frac.x |= 0x00200000u;
+                   exp = lhsExp;
+                   frac = impl::shift64ExtraRightJamming(frac.x, frac.y, frac.z, 1);
+                }
+                else
+                {
+                     if (expDiff < 0)
+                     {
+                        EXCHANGE(lhsHigh, rhsHigh);
+                        EXCHANGE(lhsLow, rhsLow);
+                        EXCHANGE(lhsExp, rhsExp);
+                     }
+
+                     //if (lhsExp == 0x7FF)
+                     //{
+                     //   bool propagate = (lhsHigh | lhsLow) != 0u;
+                     //   return nbl::hlsl::lerp(__packFloat64(lhsSign, 0x7ff, 0u, 0u), __propagateFloat64NaN(a, b), propagate);
+                     //}
+
+                     expDiff = LERP(ABS(expDiff), ABS(expDiff) - 1, rhsExp == 0);
+                     rhsHigh = LERP(rhsHigh | 0x00100000u, rhsHigh, rhsExp == 0);
+                     nbl::hlsl::float32_t3 shifted = impl::shift64ExtraRightJamming(rhsHigh, rhsLow, 0u, expDiff);
+                     rhsHigh = shifted.x;
+                     rhsLow = shifted.y;
+                     frac.z = shifted.z;
+                     exp = lhsExp;
+
+                     lhsHigh |= 0x00100000u;
+                     frac.xy = impl::add64(lhsHigh, lhsLow, rhsHigh, rhsLow);
+                     --exp;
+                     if (!(frac.x < 0x00200000u))
+                     {
+                         frac = impl::shift64ExtraRightJamming(frac.x, frac.y, frac.z, 1);
+                         ++exp;
+                     }
+                     
+                     return createEmulatedFloat64PreserveBitPattern(impl::roundAndPackFloat64(lhsSign, exp, frac.x, frac.y, frac.z));
+                }
+                
+                // cannot happen but compiler cries about not every path returning value
+                return createEmulatedFloat64PreserveBitPattern(0xdeadbeefbadcaffeull);
+            }
+            else
+            {
+                int exp;
+                
+                nbl::hlsl::uint32_t2 lhsShifted = impl::shortShift64Left(lhsHigh, lhsLow, 10);
+                lhsHigh = lhsShifted.x;
+                lhsLow = lhsShifted.y;
+                nbl::hlsl::uint32_t2 rhsShifted = impl::shortShift64Left(rhsHigh, rhsLow, 10);
+                rhsHigh = rhsShifted.x;
+                rhsLow = rhsShifted.y;
+                
+                if (expDiff != 0)
+                {
+                    nbl::hlsl::uint32_t2 frac;
+                
+                    if (expDiff < 0)
+                    {
+                       EXCHANGE(lhsHigh, rhsHigh);
+                       EXCHANGE(lhsLow, rhsLow);
+                       EXCHANGE(lhsExp, rhsExp);
+                       lhsSign ^= 0x80000000u; // smth not right about that
+                    }
+                    
+                    //if (lhsExp == 0x7FF)
+                    //{
+                    //   bool propagate = (lhsHigh | lhsLow) != 0u;
+                    //   return nbl::hlsl::lerp(__packFloat64(lhsSign, 0x7ff, 0u, 0u), __propagateFloat64NaN(a, b), propagate);
+                    //}
+                    
+                    expDiff = LERP(ABS(expDiff), ABS(expDiff) - 1, rhsExp == 0);
+                    rhsHigh = LERP(rhsHigh | 0x40000000u, rhsHigh, rhsExp == 0);
+                    nbl::hlsl::uint32_t2 shifted = impl::shift64RightJamming(rhsHigh, rhsLow, expDiff);
+                    rhsHigh = shifted.x;
+                    rhsLow = shifted.y;
+                    lhsHigh |= 0x40000000u;
+                    frac.xy = impl::sub64(lhsHigh, lhsLow, rhsHigh, rhsLow);
+                    exp = lhsExp;
+                    --exp;
+                    return createEmulatedFloat64PreserveBitPattern(impl::normalizeRoundAndPackFloat64(lhsSign, exp - 10, frac.x, frac.y));
+                }
+                //if (lhsExp == 0x7FF)
+                //{
+                //   bool propagate = ((lhsHigh | rhsHigh) | (lhsLow | rhsLow)) != 0u;
+                //   return nbl::hlsl::lerp(0xFFFFFFFFFFFFFFFFUL, __propagateFloat64NaN(a, b), propagate);
+                //}
+                rhsExp = LERP(rhsExp, 1, lhsExp == 0);
+                lhsExp = LERP(lhsExp, 1, lhsExp == 0);
+                
+                nbl::hlsl::uint32_t2 frac;
+                uint32_t signOfDifference = 0;
+                if (rhsHigh < lhsHigh)
+                {
+                   frac.xy = impl::sub64(lhsHigh, lhsLow, rhsHigh, rhsLow);
+                }
+                else if (lhsHigh < rhsHigh)
+                {
+                   frac.xy = impl::sub64(rhsHigh, rhsLow, lhsHigh, lhsLow);
+                   signOfDifference = 0x80000000;
+                }
+                else if (rhsLow <= lhsLow)
+                {
+                   /* It is possible that frac.x and frac.y may be zero after this. */
+                   frac.xy = impl::sub64(lhsHigh, lhsLow, rhsHigh, rhsLow);
+                }
+                else
+                {
+                   frac.xy = impl::sub64(rhsHigh, rhsLow, lhsHigh, lhsLow);
+                   signOfDifference = 0x80000000;
+                }
+                
+                exp = LERP(rhsExp, lhsExp, signOfDifference == 0u);
+                lhsSign ^= signOfDifference;
+                uint64_t retval_0 = impl::packFloat64(uint32_t(FLOAT_ROUNDING_MODE == FLOAT_ROUND_DOWN) << 31, 0, 0u, 0u);
+                uint64_t retval_1 = impl::normalizeRoundAndPackFloat64(lhsSign, exp - 11, frac.x, frac.y);
+                return createEmulatedFloat64PreserveBitPattern(LERP(retval_0, retval_1, frac.x != 0u || frac.y != 0u));
+            }
         }
 
-        emulated_float64_t operator-(const emulated_float64_t rhs)
+        emulated_float64_t operator-(emulated_float64_t rhs)
         {
-            emulated_float64_t retval;
-            retval.data = data - rhs.data;
-            return retval;
+            emulated_float64_t lhs = createEmulatedFloat64PreserveBitPattern(data);
+            emulated_float64_t rhsFlipped = rhs.flipSign();
+            
+            return lhs + rhsFlipped;
         }
 
         emulated_float64_t operator*(const emulated_float64_t rhs)
         {
             emulated_float64_t retval = emulated_float64_t::create(0u);
             
-            
             uint32_t lhsLow = uint32_t(data & 0x00000000FFFFFFFFull);
             uint32_t rhsLow = uint32_t(rhs.data & 0x00000000FFFFFFFFull);
             uint32_t lhsHigh = uint32_t((data & 0x000FFFFF00000000ull) >> 32);
             uint32_t rhsHigh = uint32_t((rhs.data & 0x000FFFFF00000000ull) >> 32);
+
             uint32_t lhsExp = uint32_t((data >> 52) & 0x7FFull);
             uint32_t rhsExp = uint32_t((rhs.data >> 52) & 0x7FFull);
 
             int32_t exp = int32_t(lhsExp + rhsExp) - 0x400u;
-            uint64_t sign = (data ^ rhs.data) & 0x8000000000000000ull;
+            uint64_t sign = uint32_t(((data ^ rhs.data) & 0x8000000000000000ull) >> 32);
             
 
             lhsHigh |= 0x00100000u;
@@ -274,9 +534,10 @@ namespace emulated
                ++exp;
             }
             
-            return emulated_float64_t::create(impl::roundAndPackFloat64(sign, exp, fracUnpacked.x, fracUnpacked.y, fracUnpacked.z));
+            return createEmulatedFloat64PreserveBitPattern(impl::roundAndPackFloat64(sign, exp, fracUnpacked.x, fracUnpacked.y, fracUnpacked.z));
         }
 
+        // TODO
         emulated_float64_t operator/(const emulated_float64_t rhs)
         {
             emulated_float64_t retval;
@@ -313,5 +574,14 @@ namespace emulated
         //  - not implementing bitwise and modulo operators since floating point types doesn't support them
         //  - compound operator overload not supported in HLSL
         //  - access operators (dereference and addressof) not supported in HLSL
+        
+        // TODO: should modify self?
+        emulated_float64_t flipSign()
+        {
+            const uint64_t flippedSign = ((~data) & 0x8000000000000000ull);
+            return createEmulatedFloat64PreserveBitPattern(flippedSign | (data & 0x7FFFFFFFFFFFFFFFull));
+        }
     };
+    
+    //_NBL_STATIC_INLINE_CONSTEXPR emulated_float64_t EMULATED_FLOAT64_NAN = emulated_float64_t::create(0.0 / 0.0);
 }
