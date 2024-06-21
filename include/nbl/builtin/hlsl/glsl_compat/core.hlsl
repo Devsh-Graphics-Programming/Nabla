89913626c70c23b690ddec21d09730a62edf251e
Fletterio <fj.letterio@gmail.com>
2024-06-20 23:20:30 -0300

    Changing workgroup FFT, implementing it as single workgroup-sized FFT for now

--- include/nbl/builtin/hlsl/glsl_compat/core.hlsl
+++ include/nbl/builtin/hlsl/glsl_compat/core.hlsl
@@ -184,7 +184,7 @@ struct bitfieldExtract<T, false, true>
     } 
 };
 
-}
+} //namespace impl
 
 template<typename T>
 T bitfieldExtract( T val, uint32_t offsetBits, uint32_t numBits )
@@ -192,37 +192,26 @@ T bitfieldExtract( T val, uint32_t offsetBits, uint32_t numBits )
     return impl::bitfieldExtract<T, is_signed<T>::value, is_integral<T>::value>::template  __call(val,offsetBits,numBits);
 }
 
-template<Unsigned>
-enable_if_t<is_unsigned_v<Unsigned>,Unsigned> bitfieldInsert(Unsigned base, Unsigned value, uint16_t offset, uint16_t count){
-    const Unsigned allOnes = ~ Unsigned(0);
-    // Produce a mask with `count` ones starting at the `offset` bit (counting from LSB), everything else is 0
-    Unsigned mask = ~(allOnes << count) << offset;
-    // Offset `value` by `offset`, then `&` with the mask. This puts the value's first `count` LSB starting at the offset, everything else is 0
-    value = (value << offset) & mask 
-    // Next invert the mask so all those positions are 0, and `&` the base with the new mask to set those bits in the base to 0
-    base &= ~mask;
-    // Finally, insert the bits
-    return base | value;
-}
 
-// ---------------- Marked for deletion -----------------------------
-/*
-namespace impl {
-uint32_t bitfieldInsert(uint32_t base, uint32_t shifted_masked_value, uint32_t lo, uint32_t count)
+namespace impl 
 {
-    const uint32_t hi = base^lo;
-    return (hi<<count)|shifted_masked_value|lo;
-}
+
+template<typename T>
+struct bitfieldInsert
+{
+    enable_if_t<is_integral_v<T>, T> __call( T base, T insert, uint32_t offset, uint32_t count )
+    {
+        return spirv::bitFieldInsert<T>( base, insert, offset, count );
+    }
+};
 
 } //namespace impl
 
-uint32_t bitfieldInsert(uint32_t base, uint32_t value, uint32_t offset, uint32_t count)
+template<typename T>
+T bitfieldInsert( T base, T insert, uint32_t offset, uint32_t count )
 {
-    const uint32_t shifted_masked_value = (value&((1u<<count)-1))<<offset;
-    return impl::bitfieldInsert(base,shifted_masked_value,base&((1u<<offset)-1),count);
+    return impl::bitfieldExtract<T>::template  __call(base, insert, offset, count);
 }
-*/
-// ---------------- End mark -----------------------------
 
 #endif
 
--- include/nbl/builtin/hlsl/workgroup/fft.hlsl
+++ include/nbl/builtin/hlsl/workgroup/fft.hlsl
@@ -1,8 +1,10 @@
 #ifndef _NBL_BUILTIN_HLSL_WORKGROUP_FFT_INCLUDED_
 #define _NBL_BUILTIN_HLSL_WORKGROUP_FFT_INCLUDED_
 
-#include <nbl/builtin/hlsl/subgroup/fft.hlsl>
-#include <nbl/builtin/hlsl/bit.hlsl>
+#include "nbl/builtin/hlsl/subgroup/fft.hlsl"
+#include "nbl/builtin/hlsl/workgroup/basic.hlsl"
+#include "nbl/builtin/hlsl/glsl_compat/core.hlsl"
+#include "nbl/builtin/hlsl/memory_accessor.hlsl"
 
 namespace nbl 
 {
@@ -10,6 +12,8 @@ namespace hlsl
 {
 namespace workgroup
 {
+
+/*
 
 template<typename Accessor, typename Scalar, uint32_t WorkgroupSize>
 void store(NBL_REF_ARG(Accessor) sharedmemAccessor, uint32_t threadID, NBL_CONST_REF_ARG(complex_t<Scalar>) lo, NBL_CONST_REF_ARG(complex_t<Scalar>) hi) {
@@ -103,6 +107,197 @@ void FFT(NBL_REF_ARG(Accessor) sharedmemAccessor, NBL_REF_ARG(complex_t<Scalar>)
     }
 }
 
+*/
+
+// ----------------------------------------- ABOVE MARKED FOR DELETION -----------------------------------------------------
+
+template<uint16_t K, bool Inverse, typneame Scalar, class device_capabilities=void>
+struct FFT;
+
+// For the FFT methods below, we assume:
+//      - Accessor is a global memory accessor to an array fitting 2 * _NBL_HLSL_WORKGROUP_SIZE_ elements of type complex_t<Scalar>, used to get inputs / set outputs of the FFT
+//      - SharedMemoryAccessor accesses a shared memory array that can fit _NBL_HLSL_WORKGROUP_SIZE_ elements of type complex_t<Scalar> 
+
+// 2 items per invocation forward specialization
+template<typename Scalar, class device_capabilities>
+struct FFT<2,false,device_capabilities>
+{
+    template<typename SharedMemoryAccessor>
+    static void FFT_loop(uint32_t stride, NBL_REF_ARG(complex_t<Scalar>) lo, NBL_REF_ARG(complex_t<Scalar>) hi, uint32_t threadID, NBL_REF_ARG(MemoryAdaptor<SharedMemoryAccessor>) sharedmemAdaptor)
+    {
+        const bool topHalf = (threadID & stride) != 0;
+        vector <Scalar, 2> toTrade = topHalf ? vector <Scalar, 2>(lo.real(), lo.imag()) : vector <Scalar, 2>(hi.real(), hi.imag());
+        
+        // Block memory writes until all threads are done with previous work
+        sharedmemAdaptor.workgroupExecutionAndMemoryBarrier();
+        sharedmemAdaptor.set(threadID, toTrade);
+        
+        // Wait until all writes are done before reading
+        sharedmemAdaptor.workgroupExecutionAndMemoryBarrier();
+        sharedmemAdaptor.get(threadID ^ stride, toTrade);
+        
+        if (topHalf)
+        {
+            lo.real(toTrade.x);
+            lo.imag(toTrade.y);
+        }
+        else
+        {
+            hi.real(toTrade.x);
+            hi.imag(toTrade.y);
+        }
+        // Get twiddle with k = threadID mod stride, N = 2 * stride
+        fft::DIF<Scalar>::radix2(fft::twiddle<false, Scalar>(threadID & (stride - 1), stride << 1), lo, hi);    
+    }
+
+
+    template<typename Accessor, typename SharedMemoryAccessor>
+    static void __call(NBL_REF_ARG(Accessor) accessor, NBL_REF_ARG(SharedMemoryAccessor) sharedmemAccessor)
+    {
+        // Set up the MemAdaptors
+        MemoryAdaptor<Accessor> memAdaptor;
+        memAdaptor.accessor = accessor;
+        MemoryAdaptor<SharedMemoryAccessor> sharedmemAdaptor;
+        sharedmemAdaptor.accessor = sharedmemAccessor;
+
+        // Compute the SubgroupContiguousIndex only once
+        const uint32_t threadID = uint32_t(SubgroupContiguousIndex());
+
+        // Read lo, hi values from global memory
+        vector <Scalar, 4> loHiPacked;
+        memAdaptor.get(threadID, loHiPacked);
+        complex_t<Scalar> lo = {loHiPacked.x, loHiPacked.y};  
+        complex_t<Scalar> hi = {loHiPacked.z, loHiPacked.w};
+
+        // special first iteration
+        fft::DIF<Scalar>::radix2(fft::twiddle<false, Scalar>(threadID, _NBL_HLSL_WORKGROUP_SIZE_ << 1), lo, hi); 
+
+        // Run bigger steps until Subgroup-sized
+        for (uint32_t stride = _NBL_HLSL_WORKGROUP_SIZE_ >> 1; stride > glsl::gl_SubgroupSize(); stride >>= 1)
+            FFT_loop<SharedMemoryAccessor>(stride, lo, hi, threadID, sharedmemAdaptor);
+  
+        // Subgroup-sized FFT
+        subgroup::FFT<2, false, Scalar, device_capabilities>::__call(lo, hi);
+
+        // Put values back in global mem
+        loHiPacked = vector <Scalar, 4>(lo.real(), lo.imag(), hi.real(), hi.imag());
+        memAdaptor.set(threadID, loHiPacked);
+    }
+};
+
+
+
+// 2 items per invocation inverse specialization
+template<class device_capabilities>
+struct FFT<2,true,device_capabilities>
+{
+    template<typename SharedMemoryAccessor>
+    static void FFT_loop(uint32_t stride, NBL_REF_ARG(complex_t<Scalar>) lo, NBL_REF_ARG(complex_t<Scalar>) hi, uint32_t threadID, NBL_REF_ARG(MemoryAdaptor<SharedMemoryAccessor>) sharedmemAdaptor)
+    {
+        // Get twiddle with k = threadID mod stride, N = 2 * stride
+        fft::DIF<Scalar>::radix2(fft::twiddle<false, Scalar>(threadID & (stride - 1), stride << 1), lo, hi);     
+    
+        const bool topHalf = (threadID & stride) != 0;
+        vector <Scalar, 2> toTrade = topHalf ? vector <Scalar, 2>(lo.real(), lo.imag()) : vector <Scalar, 2>(hi.real(), hi.imag());
+        
+        // Block memory writes until all threads are sync'd
+        sharedmemAdaptor.workgroupExecutionAndMemoryBarrier();
+        sharedmemAdaptor.set(threadID, toTrade);
+        
+        // Wait until all writes are done before reading
+        sharedmemAdaptor.workgroupExecutionAndMemoryBarrier();
+        sharedmemAdaptor.get(threadID ^ stride, toTrade);
+        
+        if (topHalf)
+        {
+            lo.real(toTrade.x);
+            lo.imag(toTrade.y);
+        }
+        else
+        {
+            hi.real(toTrade.x);
+            hi.imag(toTrade.y);
+        }  
+    }
+
+
+    template<typename Accessor, typename SharedMemoryAccessor>
+    static void __call(NBL_REF_ARG(Accessor) accessor, NBL_REF_ARG(SharedMemoryAccessor) sharedmemAccessor)
+    {
+        // Set up the MemAdaptors
+        MemoryAdaptor<Accessor> memAdaptor;
+        memAdaptor.accessor = accessor;
+        MemoryAdaptor<SharedMemoryAccessor> sharedmemAdaptor;
+        sharedmemAdaptor.accessor = sharedmemAccessor;
+
+        // Compute the SubgroupContiguousIndex only once
+        const uint32_t threadID = uint32_t(SubgroupContiguousIndex());
+
+        // Read lo, hi values from global memory
+        vector <Scalar, 4> loHiPacked;
+        memAdaptor.get(threadID, loHiPacked);
+        complex_t<Scalar> lo = {loHiPacked.x, loHiPacked.y};  
+        complex_t<Scalar> hi = {loHiPacked.z, loHiPacked.w}; 
+
+        // Run a subgroup-sized FFT, then continue with bigger steps
+        subgroup::FFT<2, true, Scalar, device_capabilities>::__call(lo, hi);
+        
+        // The bigger steps
+        for (uint32_t stride = glsl::gl_SubgroupSize() << 1; stride < _NBL_HLSL_WORKGROUP_SIZE_; stride <<= 1)
+            FFT_loop<SharedMemoryAccessor>(stride, lo, hi, threadID, sharedmemAdaptor);
+
+        // special last iteration
+        fft::DIT<Scalar>::radix2(fft::twiddle<Scalar, true>(threadID, _NBL_HLSL_WORKGROUP_SIZE_ << 1), lo, hi); 
+        divides_assign< complex_t<Scalar> > divAss;
+        divAss(lo, _NBL_HLSL_WORKGROUP_SIZE_ / glsl::gl_SubgroupSize());
+        divAss(hi, _NBL_HLSL_WORKGROUP_SIZE_ / glsl::gl_SubgroupSize());
+        
+        // Put values back in global mem
+        loHiPacked = vector <Scalar, 4>(lo.real(), lo.imag(), hi.real(), hi.imag());
+        memAdaptor.set(threadID, loHiPacked);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------- Below pending --------------------------------------------------
+
+/*
+
+// then define 4,8,16 in terms of calling the FFT<2> and doing the special radix steps before/after
+template<uint16_t K, bool Inverse, class device_capabilities>
+struct FFT
+{
+    template<typename Accessor, typename ShaderMemoryAccessor>
+    static void __call(NBL_REF_ARG(Accessor) accessor, NBL_REF_ARG(ShaderMemoryAccessor) sharedmemAccessor)
+    {
+        if (!Inverse)
+        {
+           ... special steps ...
+        }
+        FFT<2,Inverse,device_capabilities>::template __call<Accessor,SharedMemoryAccessor>(access,sharedMemAccessor);
+        if (Inverse)
+        {
+           ... special steps ...
+        }
+    }
+};
+
+*/
 
 }
 }
