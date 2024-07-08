cbac7c7e01c2bee47c0196e05bfea1806c09b442
devsh <devsh@devsh.eu>
2024-07-08 18:51:42 +0200

    remove all the dummy restoration logic from Asset Manager and Loaders, huge mental and maintenance burden gone

--- include/nbl/asset/ICPURenderpass.h
+++ include/nbl/asset/ICPURenderpass.h
@@ -19,26 +19,12 @@ class ICPURenderpass : public IRenderpass, public IAsset
             // TODO
             return nullptr;
         }
-        bool canBeRestoredFrom(const IAsset* _other) const override
-        {
-            return false; // TODO
-        }
         E_TYPE getAssetType() const override
         {
             return ET_RENDERPASS;
         }
 
         ~ICPURenderpass() = default;
-
-    private:
-        void restoreFromDummy_impl(IAsset* _other, uint32_t _levelsBelow) override
-        {
-            // TODO
-        }
-        void convertToDummyObject(uint32_t referenceLevelsBelowToConvert = 0u) override
-        {
-            // TODO
-        }
 };
 
 }
