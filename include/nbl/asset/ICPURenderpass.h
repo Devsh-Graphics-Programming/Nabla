699303367836debb58e49f9114a182454902f5ae
devsh <devsh@devsh.eu>
2024-07-15 12:50:50 +0200

    implement ICPURenderpass

--- include/nbl/asset/ICPURenderpass.h
+++ include/nbl/asset/ICPURenderpass.h
@@ -12,19 +12,40 @@ namespace nbl::asset
 class ICPURenderpass : public IRenderpass, public IAsset
 {
     public:
-        using IRenderpass::IRenderpass;
-
-        core::smart_refctd_ptr<IAsset> clone(uint32_t _depth = ~0u) const override
+        static inline core::smart_refctd_ptr<ICPURenderpass> create(const SCreationParams& _params)
         {
-            // TODO
-            return nullptr;
+            const SCreationParamValidationResult validation = validateCreationParams(_params);
+            if (!validation)
+                return nullptr;
+            return core::smart_refctd_ptr<ICPURenderpass>(new ICPURenderpass(_params,validation), core::dont_grab);
         }
+
+        inline core::smart_refctd_ptr<IAsset> clone(uint32_t _depth = ~0u) const override
+        {
+            return core::smart_refctd_ptr<ICPURenderpass>(new ICPURenderpass(m_params,SCreationParamValidationResult{
+                .depthStencilAttachmentCount = m_depthStencilAttachments ? static_cast<uint32_t>(m_depthStencilAttachments->size()):0u,
+                .colorAttachmentCount = m_colorAttachments ? static_cast<uint32_t>(m_colorAttachments->size()):0u,
+                .subpassCount = m_subpasses ? static_cast<uint32_t>(m_subpasses->size()):0u,
+                .totalInputAttachmentCount = m_inputAttachments ? static_cast<uint32_t>(m_inputAttachments->size()):0u,
+                .totalPreserveAttachmentCount = m_preserveAttachments ? static_cast<uint32_t>(m_preserveAttachments->size()):0u,
+                .dependencyCount = m_subpassDependencies ? static_cast<uint32_t>(m_subpassDependencies->size()):0u,
+                .viewMaskMSB = m_viewMaskMSB,
+            }),core::dont_grab);
+        }
+
+        constexpr static inline auto AssetType = ET_RENDERPASS;
         E_TYPE getAssetType() const override
         {
             return ET_RENDERPASS;
         }
 
-        ~ICPURenderpass() = default;
+        inline size_t getDependantCount() const override {return 0ull;}
+
+    protected:
+        using IRenderpass::IRenderpass;
+        inline ~ICPURenderpass() = default;
+
+        inline IAsset* getDependant_impl(const size_t ix) override {return nullptr;}
 };
 
 }
