096e09d83f113767ec3f85bb03a1f7d24c612e64
Nipun Garg <24457793+nipunG314@users.noreply.github.com>
2024-07-20 00:33:56 +0530

    Add luma_meter and tonemapper

---
+++ include/nbl/builtin/hlsl/luma_meter/luma_meter.hlsl
@@ -0,0 +1,16 @@
+// Copyright (C) 2018-2024 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+
+#ifndef _NBL_BUILTIN_HLSL_LUMA_METER_INCLUDED_
+#define _NBL_BUILTIN_HLSL_LUMA_METER_INCLUDED_
+
+namespace nbl
+{
+namespace hls
+{
+
+}
+}
+
+#endif
---
+++ include/nbl/builtin/hlsl/tonemapper/operators.hlsl
@@ -0,0 +1,16 @@
+// Copyright (C) 2018-2024 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+
+#ifndef _NBL_BUILTIN_HLSL_TONE_MAPPER_OPERATORS_INCLUDED_
+#define _NBL_BUILTIN_HLSL_TONE_MAPPER_OPERATORS_INCLUDED_
+
+namespace nbl
+{
+namespace hls
+{
+
+}
+}
+
+#endif
