ac390393cca2c89237532b57f12d95cc5584f0be
Nipun Garg <24457793+nipunG314@users.noreply.github.com>
2024-08-27 00:41:14 +0530

    Add uploadFloat, downloadFloat and gatherLuma

--- include/nbl/builtin/hlsl/luma_meter/luma_meter.hlsl
+++ include/nbl/builtin/hlsl/luma_meter/luma_meter.hlsl
@@ -7,6 +7,7 @@
 
 #include "nbl/builtin/hlsl/glsl_compat/core.hlsl"
 #include "nbl/builtin/hlsl/glsl_compat/subgroup_basic.hlsl"
+#include "nbl/builtin/hlsl/glsl_compat/subgroup_arithmetic.hlsl"
 #include "nbl/builtin/hlsl/workgroup/basic.hlsl"
 #include "nbl/builtin/hlsl/workgroup/arithmetic.hlsl"
 #include "nbl/builtin/hlsl/type_traits.hlsl"
@@ -27,10 +28,11 @@ struct geom_meter {
     using float_t3 = typename conditional<is_same_v<float_t, float32_t>, float32_t3, float16_t3>::type;
     using this_t = geom_meter<GroupSize, ValueAccessor, SharedAccessor, TexAccessor>;
 
-    static this_t create(float_t2 lumaMinMax)
+    static this_t create(float_t2 lumaMinMax, float_t sampleCount)
     {
         this_t retval;
         retval.lumaMinMax = lumaMinMax;
+        retval.sampleCount = sampleCount;
         return retval;
     }
 
@@ -55,6 +57,34 @@ struct geom_meter {
         return max(log2(luma), log2(lumaMinMax.x));
     }
 
+    void uploadFloat(
+        NBL_REF_ARG(ValueAccessor) val_accessor,
+        uint32_t index,
+        float_t val,
+        float_t minLog2,
+        float_t rangeLog2
+    )
+    {
+        uint32_t3 workGroupCount = glsl::gl_NumWorkGroups();
+        uint32_t fixedPointBitsLeft = 32 - uint32_t(ceil(log2(workGroupCount.x * workGroupCount.y * workGroupCount.z))) + glsl::gl_SubgroupSizeLog2();
+
+        uint32_t lumaSumBitPattern = uint32_t(clamp((val - minLog2) * rangeLog2, 0.f, float32_t((1 << fixedPointBitsLeft) - 1)));
+
+        val_accessor.atomicAdd(index & ((1 << glsl::gl_SubgroupSizeLog2()) - 1), lumaSumBitPattern);
+    }
+
+    float_t downloadFloat(
+        NBL_REF_ARG(ValueAccessor) val_accessor,
+        uint32_t index,
+        float_t minLog2,
+        float_t rangeLog2
+    )
+    {
+        float_t luma = (float_t)val.get(index & ((1 << glsl::gl_SubgroupSizeLog2()) - 1));
+        luma = luma / rangeLog2 + minLog2;
+        return luma;
+    }
+
     void sampleLuma(
         NBL_CONST_REF_ARG(MeteringWindow) window,
         NBL_REF_ARG(ValueAccessor) val,
@@ -74,18 +104,37 @@ struct geom_meter {
         float_t lumaSum = reduction(luma, sdata);
 
         if (tid == GroupSize - 1) {
-            uint32_t3 workGroupCount = glsl::gl_NumWorkGroups();
-            uint32_t fixedPointBitsLeft = 32 - uint32_t(ceil(log2(workGroupCount.x * workGroupCount.y * workGroupCount.z))) + glsl::gl_SubgroupSizeLog2();
-
-            uint32_t lumaSumBitPattern = uint32_t(clamp((lumaSum - log2(lumaMinMax.x)) * (log2(lumaMinMax.y) - log2(lumaMinMax.x)), 0.f, float32_t((1 << fixedPointBitsLeft) - 1)));
-
             uint32_t3 workgroupSize = glsl::gl_WorkGroupSize();
             uint32_t workgroupIndex = dot(uint32_t3(workgroupSize.y * workgroupSize.z, workgroupSize.z, 1), glsl::gl_WorkGroupID());
 
-            val.atomicAdd(workgroupIndex & ((1 << glsl::gl_SubgroupSizeLog2()) - 1), lumaSumBitPattern);
+            uploadFloat(
+                val,
+                workgroupIndex,
+                lumaSum,
+                log2(lumaMinMax.x),
+                log2(lumaMinMax.y / lumaMinMax.x)
+            );
         }
     }
 
+    void gatherLuma(
+        NBL_REF_ARG(ValueAccessor) val
+    )
+    {
+        uint32_t tid = workgroup::SubgroupContiguousIndex();
+        float_t lumaSum = glsl::subgroupAdd(
+            downloadFloat(
+                val,
+                tid,
+                log2(lumaMinMax.x),
+                log2(lumaMinMax.y / lumaMinMax.x)
+            )
+        );
+
+        uploadFloat(val, 0, lumaSum, log2(lumaMinMax.x), log2(lumaMinMax.y / lumaMinMax.x));
+    }
+
+    float_t sampleCount;
     float_t2 lumaMinMax;
 };
 }
