0c3d88059d52e57d454da6b2ac3796f4624df4d7
tad1 <tadziobrzeski@gmail.com>
2024-09-12 18:14:52 +0200

    fool proof logging macros

--- include/nbl/logging_macros.h
+++ include/nbl/logging_macros.h
@@ -1,8 +1,8 @@
-#ifdef _GIT_INFO_H_INCLUDED_
-// when "git_info.h" is included
-#	define NBL_LOG(SEVERITY, FORMAT, ...) NBL_LOG_FUNCTION(FORMAT" [%s][%s - %s:%d]", SEVERITY, __VA_ARGS__, nbl::gtml::nabla_git_info.commitShortHash, __FUNCTION__, __FILE__, __LINE__);
-#	define NBL_LOG_ERROR(FORMAT, ...) NBL_LOG(nbl::system::ILogger::ELL_ERROR, FORMAT, __VA_ARGS__)
+#if defined(NBL_LOG) || defined(NBL_LOG_ERROR)
+	#error redefinition of NBL_LOG/NBL_LOG_ERROR. did you forgot to undefine logging macros somewhere? #include "nbl/undefine_logging_macros.h"
+#elif !defined(_GIT_INFO_H_INCLUDED_)
+	#error logging macros require git meta info, include "git_info.h"
 #else
-#	define NBL_LOG(SEVERITY, FORMAT, ...) NBL_LOG_FUNCTION(FORMAT" [%s - %s:%d]", SEVERITY, __VA_ARGS__, __FUNCTION__, __FILE__, __LINE__);
-#	define NBL_LOG_ERROR(FORMAT, ...) NBL_LOG(nbl::system::ILogger::ELL_ERROR, FORMAT, __VA_ARGS__)
+	#define NBL_LOG(SEVERITY, FORMAT, ...) NBL_LOG_FUNCTION(FORMAT" [%s][%s - %s:%d]", SEVERITY, __VA_ARGS__, nbl::gtml::nabla_git_info.commitShortHash, __FUNCTION__, __FILE__, __LINE__);
+	#define NBL_LOG_ERROR(FORMAT, ...) NBL_LOG(nbl::system::ILogger::ELL_ERROR, FORMAT, __VA_ARGS__)
 #endif
