b24d4914cf6730467a4b8969dfb8e3d4663118f2
devsh <devsh@devsh.eu>
2024-10-17 22:06:59 +0200

    remove and ignore more files

--- vars.bat
+++
@@ -1,3 +0,0 @@
-call "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat"
-call wsl -e fish
-@REM cmake.exe -DNBL_DYNAMIC_MSVC_RUNTIME=OFF -DNBL_STATIC_BUILD=ON -DNBL_CI_MODE=OFF -DNBL_UPDATE_GIT_SUBMODULE=ON -DNBL_RUN_TESTS=OFF -DNBL_CPACK_CI=OFF -SD:/Nabla -BD:/Nabla/build/static -G "Ninja Multi-Config"
