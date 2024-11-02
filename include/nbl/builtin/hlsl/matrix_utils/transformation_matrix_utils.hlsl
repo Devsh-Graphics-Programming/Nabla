a0890f0df957e0d989d956ec41c41fd3be72791a
AnastaZIuk <areklachowicz@gmail.com>
2024-11-02 08:52:21 +0100

    forgot to commit projection build methods, update examples_tests submodule

--- include/nbl/builtin/hlsl/matrix_utils/transformation_matrix_utils.hlsl
+++ include/nbl/builtin/hlsl/matrix_utils/transformation_matrix_utils.hlsl
@@ -109,6 +109,74 @@ inline void setTranslation(matrix<T, N, 4>& outMat, NBL_CONST_REF_ARG(vector<T, 
 	outMat[2].w = translation.z;
 }
 
+
+template<typename T = float>
+inline matrix<T, 4, 4> buildProjectionMatrixPerspectiveFovRH(float fieldOfViewRadians, float aspectRatio, float zNear, float zFar)
+{
+	const float h = core::reciprocal<float>(tanf(fieldOfViewRadians * 0.5f));
+	_NBL_DEBUG_BREAK_IF(aspectRatio == 0.f); //division by zero
+	const float w = h / aspectRatio;
+
+	_NBL_DEBUG_BREAK_IF(zNear == zFar); //division by zero
+
+	matrix<T, 4, 4> m;
+	m[0] = vector<T, 4>(w, 0.f, 0.f, 0.f);
+	m[1] = vector<T, 4>(0.f, -h, 0.f, 0.f);
+	m[2] = vector<T, 4>(0.f, 0.f, -zFar / (zFar - zNear), -zNear * zFar / (zFar - zNear));
+	m[3] = vector<T, 4>(0.f, 0.f, -1.f, 0.f);
+
+	return m;
+}
+template<typename T = float>
+inline matrix<T, 4, 4> buildProjectionMatrixPerspectiveFovLH(float fieldOfViewRadians, float aspectRatio, float zNear, float zFar)
+{
+	const float h = core::reciprocal<float>(tanf(fieldOfViewRadians * 0.5f));
+	_NBL_DEBUG_BREAK_IF(aspectRatio == 0.f); //division by zero
+	const float w = h / aspectRatio;
+
+	_NBL_DEBUG_BREAK_IF(zNear == zFar); //division by zero
+
+	matrix<T, 4, 4> m;
+	m[0] = vector<T, 4>(w, 0.f, 0.f, 0.f);
+	m[1] = vector<T, 4>(0.f, -h, 0.f, 0.f);
+	m[2] = vector<T, 4>(0.f, 0.f, zFar / (zFar - zNear), -zNear * zFar / (zFar - zNear));
+	m[3] = vector<T, 4>(0.f, 0.f, 1.f, 0.f);
+
+	return m;
+}
+
+template<typename T = float>
+inline matrix<T, 4, 4> buildProjectionMatrixOrthoRH(float widthOfViewVolume, float heightOfViewVolume, float zNear, float zFar)
+{
+	_NBL_DEBUG_BREAK_IF(widthOfViewVolume == 0.f); //division by zero
+	_NBL_DEBUG_BREAK_IF(heightOfViewVolume == 0.f); //division by zero
+	_NBL_DEBUG_BREAK_IF(zNear == zFar); //division by zero
+
+	matrix<T, 4, 4> m;
+	m[0] = vector<T, 4>(2.f / widthOfViewVolume, 0.f, 0.f, 0.f);
+	m[1] = vector<T, 4>(0.f, -2.f / heightOfViewVolume, 0.f, 0.f);
+	m[2] = vector<T, 4>(0.f, 0.f, -1.f / (zFar - zNear), -zNear / (zFar - zNear));
+	m[3] = vector<T, 4>(0.f, 0.f, 0.f, 1.f);
+
+	return m;
+}
+
+template<typename T = float>
+inline matrix<T, 4, 4> buildProjectionMatrixOrthoLH(float widthOfViewVolume, float heightOfViewVolume, float zNear, float zFar)
+{
+	_NBL_DEBUG_BREAK_IF(widthOfViewVolume == 0.f); //division by zero
+	_NBL_DEBUG_BREAK_IF(heightOfViewVolume == 0.f); //division by zero
+	_NBL_DEBUG_BREAK_IF(zNear == zFar); //division by zero
+
+	matrix<T, 4, 4> m;
+	m[0] = vector<T, 4>(2.f / widthOfViewVolume, 0.f, 0.f, 0.f);
+	m[1] = vector<T, 4>(0.f, -2.f / heightOfViewVolume, 0.f, 0.f);
+	m[2] = vector<T, 4>(0.f, 0.f, 1.f / (zFar - zNear), -zNear / (zFar - zNear));
+	m[3] = vector<T, 4>(0.f, 0.f, 0.f, 1.f);
+
+	return m;
+}
+
 }
 }
 
