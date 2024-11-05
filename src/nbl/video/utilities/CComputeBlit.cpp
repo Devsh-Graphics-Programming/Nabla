395ac581fd5c28efaf8bb4c3a7123ea6b194d6e0
devsh <devsh@devsh.eu>
2024-11-05 16:46:58 +0100

    start using the asset converter to make Blit shaders

--- src/nbl/video/utilities/CComputeBlit.cpp
+++ src/nbl/video/utilities/CComputeBlit.cpp
@@ -23,36 +23,25 @@ CComputeBlit::CComputeBlit(smart_refctd_ptr<ILogicalDevice>&& logicalDevice, sma
 auto CComputeBlit::createAndCachePipelines(const SPipelinesCreateInfo& info) -> SPipelines
 {
 	SPipelines retval;
-	core::vector<smart_refctd_ptr<ICPUComputePipeline>> cpuPplns;
-	cpuPplns.reserve(tasks.size());
+
+	std::array<smart_refctd_ptr<ICPUComputePipeline>,2> cpuPplns;
 
 	const auto& limits = m_device->getPhysicalDevice()->getLimits();
-	for (auto task : tasks)
+	retval.workgroupSize = 0x1u<<info.workgroupSizeLog2;
+	if (retval.workgroupSize <limits.maxSubgroupSize)
+		retval.workgroupSize = core::roundDownToPoT(limits.maxComputeWorkGroupInvocations);
+	// the absolute minimum needed to store a single pixel of a worst case format (precise, all 4 channels)
+	constexpr auto singlePixelStorage = 4*sizeof(hlsl::float32_t);
+	// also slightly more memory is needed to even have a skirt of any size
+	const auto sharedMemoryPerInvocation = core::max(singlePixelStorage*2,info.sharedMemoryPerInvocation);
+
+	const auto* layout = info.layout;
+
+	// 
+	const auto common = [&]()->std::string
 	{
-		// adjust task default values
-		{
-			if (task.workgroupSizeLog2<limits.maxSubgroupSize)
-				task.workgroupSizeLog2 = core::roundDownToPoT(limits.maxComputeWorkGroupInvocations);
-			bool useFloat16 = false;
-			uint16_t channels = 4;
-			using namespace hlsl::format;
-			if (task.outputFormat!=TexelBlockFormat::TBF_UNKNOWN)
-			{
-				channels = getTraits(task.outputFormat).Channels;
-				const auto precisionAt1 = getFormatPrecision(static_cast<E_FORMAT>(task.outputFormat),3,1.f);
-				const auto precisionAt0 = getFormatPrecision(static_cast<E_FORMAT>(task.outputFormat),3,0.f);
-				if (limits.workgroupMemoryExplicitLayout16BitAccess && limits.shaderFloat16 && precisionAt1>=std::exp2f(-11.f) && precisionAt0>=std::numeric_limits<hlsl::float16_t>::min())
-					useFloat16 = true;
-			}
-			// the absolute minimum needed to store a single pixel
-			const auto singlePixelStorage = channels*(useFloat16 ? sizeof(hlsl::float16_t):sizeof(hlsl::float32_t));
-			// also slightly more memory is needed
-			task.sharedMemoryPerInvocation = core::max(singlePixelStorage*2,task.sharedMemoryPerInvocation);
-		}
-		const auto common = [&]()->std::string
-		{
-			std::ostringstream tmp;
-			tmp << R"===(
+		std::ostringstream tmp;
+		tmp << R"===(
 #include "nbl/builtin/hlsl/binding_info.hlsl"
 
 
@@ -61,54 +50,58 @@ using namespace nbl::hlsl;
 
 struct ConstevalParameters
 {
-	NBL_CONSTEXPR_STATIC_INLINE uint32_t WorkGroupSize = )===" << (0x1u<<task.workgroupSizeLog2) << R"===(;
-    using kernel_weight_binding_t = )===" << layout->getBindingInfoForHLSL() << R"===(;
-    using input_sampler_binding_t = )===" << serializeBindingInfo() << R"===(;
-    using input_image_binding_t = )===" << serializeBindingInfo() << R"===(;
-    using output_binding_t = )===" << serializeBindingInfo() << R"===(;
-    NBL_CONSTEXPR_STATIC_INLINE uint32_t uint32_t SharedMemoryDWORDs = )===" << task.sharedMemoryPerInvocation/sizeof(uint32_t) << R"===(;
+NBL_CONSTEXPR_STATIC_INLINE uint32_t WorkGroupSize = )===" << retval.workgroupSize << R"===(;
+using kernel_weight_binding_t = )===" << layout->getBindingInfoForHLSL({.binding=info.kernelWeights,.requiredStages=IShader::E_SHADER_STAGE::ESS_COMPUTE}) << R"===(;
+using input_sampler_binding_t = )===" << layout->getBindingInfoForHLSL({.binding=info.samplers,.requiredStages=IShader::E_SHADER_STAGE::ESS_COMPUTE}) << R"===(;
+using input_image_binding_t = )===" << layout->getBindingInfoForHLSL({.binding=info.inputs,.requiredStages=IShader::E_SHADER_STAGE::ESS_COMPUTE}) << R"===(;
+using output_binding_t = )===" << layout->getBindingInfoForHLSL({.binding=info.outputs,.requiredStages=IShader::E_SHADER_STAGE::ESS_COMPUTE}) << R"===(;
+NBL_CONSTEXPR_STATIC_INLINE uint32_t uint32_t SharedMemoryDWORDs = )===" << (sharedMemoryPerInvocation* retval.workgroupSize)/sizeof(uint32_t) << R"===(;
 };
 )===";
-			return tmp.str();
-		}();
-		auto createPipeline = [&limits,&common](const char* mainPath)->smart_refctd_ptr<ICPUComputePipeline>
+		return tmp.str();
+	}();
+	auto createPipeline = [&limits,layout,&common](const char* mainPath)->smart_refctd_ptr<ICPUComputePipeline>
+	{
+		auto shader = make_smart_refctd_ptr<ICPUShader>(
+			(common+"\n#include \""+mainPath+"\"\n").c_str(),
+			IShader::E_SHADER_STAGE::ESS_COMPUTE,
+			IShader::E_CONTENT_TYPE::ECT_HLSL,
+			mainPath
+		);
+		// make sure there's a hash so asset converter doesn't fail
 		{
-			auto shader = make_smart_refctd_ptr<ICPUShader>(
-				(common+"\n#include \""+mainPath+"\"\n").c_str(),
-				IShader::E_SHADER_STAGE::ESS_COMPUTE,
-				IShader::E_CONTENT_TYPE::ECT_HLSL,
-				mainPath
-			);
+			auto source = const_cast<ICPUBuffer*>(shader->getContent());
+			source->setContentHash(source->computeContentHash());
+		}
 
-			ICPUComputePipeline::SCreationParams params = {};
-			params.layout = nullptr; // TODO
-			params.shader.entryPoint = "main";
-			params.shader.shader = shader.get();
-			params.shader.requiredSubgroupSize = static_cast<IShader::SSpecInfoBase::SUBGROUP_SIZE>(hlsl::findMSB(limits.maxSubgroupSize));
-			// needed for the prefix and reductions to work
-			params.shader.requireFullSubgroups = true;
-			return ICPUComputePipeline::create(params);
-		};
-		// create blit pipeline
-		cpuPplns.emplace_back(createPipeline("default_blit.comp.hlsl"));
-		cpuPplns.emplace_back(createPipeline("default_normalize.comp.hlsl"));
-	}
+		ICPUComputePipeline::SCreationParams params = {};
+		params.layout = layout;
+		params.shader.entryPoint = "main";
+		params.shader.shader = shader.get();
+		params.shader.requiredSubgroupSize = static_cast<IShader::SSpecInfoBase::SUBGROUP_SIZE>(hlsl::findMSB(limits.maxSubgroupSize));
+		// needed for the prefix and reductions to work
+		params.shader.requireFullSubgroups = true;
+		return ICPUComputePipeline::create(params);
+	};
+	// create blit pipeline
+	cpuPplns[0] = createPipeline("nbl/builtin/hlsl/blit/default_blit.comp.hlsl");
+	cpuPplns[1] = createPipeline("nbl/builtin/hlsl/blit/default_normalize.comp.hlsl");
 
 	CAssetConverter::SInputs inputs = {};
-	inputs.readCache = converter;
+	inputs.readCache = info.converter;
 	inputs.logger = m_logger.getRaw();
 	std::get<CAssetConverter::SInputs::asset_span_t<ICPUComputePipeline>>(inputs.assets) = {&cpuPplns.data()->get(),cpuPplns.size()};
 	inputs.readShaderCache = m_shaderCache.get();
 	inputs.writeShaderCache = m_shaderCache.get();
 	// no pipeline cache, because we only make the same pipeline once, ever
-	auto reserveResults = converter->reserve(inputs);
+	auto reserveResults = info.converter->reserve(inputs);
 	assert(reserveResults.getRequiredQueueFlags().value==IQueue::FAMILY_FLAGS::NONE);
+
 	// copy over the results
 	{
 		auto rIt = reserveResults.getGPUObjects<ICPUComputePipeline>().data();
-		// TODO: redo
-		for (size_t i=0; i<tasks.size(); i++)
-			*(pipelines++) =  (rIt++)->value;
+		retval.blit = (rIt++)->value;
+		retval.coverage = (rIt++)->value;
 	}
 
 	// this just inserts the pipelines into the cache
@@ -198,6 +191,53 @@ core::smart_refctd_ptr<video::IGPUShader> createBlitSpecializedShader(
 	return gpuShader;
 }
 
+template <typename BlitUtilities>
+core::smart_refctd_ptr<video::IGPUComputePipeline> getBlitPipeline(
+	const asset::E_FORMAT									outFormat,
+	const asset::IImage::E_TYPE								imageType,
+	const core::vectorSIMDu32& inExtent,
+	const core::vectorSIMDu32& outExtent,
+	const asset::IBlitUtilities::E_ALPHA_SEMANTIC			alphaSemantic,
+	const typename BlitUtilities::convolution_kernels_t& kernels,
+	const uint32_t											workgroupSize = 256,
+	const uint32_t											alphaBinCount = asset::IBlitUtilities::DefaultAlphaBinCount)
+{
+	const auto paddedAlphaBinCount = getPaddedAlphaBinCount(core::vectorSIMDu32(workgroupSize, 1, 1, 1), alphaBinCount);
+
+	const SBlitCacheKey key =
+	{
+		.wgSize = workgroupSize,
+		.imageType = imageType,
+		.alphaBinCount = paddedAlphaBinCount,
+		.outFormat = outFormat,
+		.smemSize = m_availableSharedMemory,
+		.coverageAdjustment = (alphaSemantic == asset::IBlitUtilities::EAS_REFERENCE_OR_COVERAGE)
+	};
+
+	if (m_blitPipelines.find(key) == m_blitPipelines.end())
+	{
+		const auto blitType = (alphaSemantic == asset::IBlitUtilities::EAS_REFERENCE_OR_COVERAGE) ? EBT_COVERAGE_ADJUSTMENT : EBT_REGULAR;
+
+		auto specShader = createBlitSpecializedShader<BlitUtilities>(
+			outFormat,
+			imageType,
+			inExtent,
+			outExtent,
+			alphaSemantic,
+			kernels,
+			workgroupSize,
+			paddedAlphaBinCount);
+
+		IGPUComputePipeline::SCreationParams creationParams;
+		creationParams.shader.shader = specShader.get();
+		creationParams.shader.entryPoint = "main";
+		creationParams.layout = m_blitPipelineLayout[blitType].get();
+		m_device->createComputePipelines(nullptr, { &creationParams, &creationParams + 1 }, &m_blitPipelines[key]);
+	}
+
+	return m_blitPipelines[key];
+}
+
 core::smart_refctd_ptr<video::IGPUShader> CComputeBlit::createAlphaTestSpecializedShader(const asset::IImage::E_TYPE imageType, const uint32_t alphaBinCount)
 {
 	const auto workgroupDims = getDefaultWorkgroupDims(imageType);
@@ -233,6 +273,28 @@ core::smart_refctd_ptr<video::IGPUShader> CComputeBlit::createAlphaTestSpecializ
 	auto cpuShader = core::make_smart_refctd_ptr<asset::ICPUShader>(shaderSourceStream.str().c_str(), IGPUShader::E_SHADER_STAGE::ESS_COMPUTE, IGPUShader::E_CONTENT_TYPE::ECT_HLSL, "CComputeBlitGLSLGLSL::createAlphaTestSpecializedShader");
 }
 
+core::smart_refctd_ptr<video::IGPUComputePipeline> getAlphaTestPipeline(const uint32_t alphaBinCount, const asset::IImage::E_TYPE imageType)
+{
+	const auto workgroupDims = getDefaultWorkgroupDims(imageType);
+	const auto paddedAlphaBinCount = getPaddedAlphaBinCount(workgroupDims, alphaBinCount);
+
+	assert(paddedAlphaBinCount >= asset::IBlitUtilities::MinAlphaBinCount);
+	const auto pipelineIndex = (paddedAlphaBinCount / asset::IBlitUtilities::MinAlphaBinCount) - 1;
+
+	if (m_alphaTestPipelines[pipelineIndex][imageType])
+		return m_alphaTestPipelines[pipelineIndex][imageType];
+
+	auto specShader = createAlphaTestSpecializedShader(imageType, paddedAlphaBinCount);
+	IGPUComputePipeline::SCreationParams creationParams;
+	creationParams.shader.shader = specShader.get();
+	creationParams.shader.entryPoint = "main";
+	creationParams.layout = m_blitPipelineLayout[EBT_COVERAGE_ADJUSTMENT].get();
+	assert(m_device->createComputePipelines(nullptr, { &creationParams, &creationParams + 1 }, &m_alphaTestPipelines[pipelineIndex][imageType]));
+
+	return m_alphaTestPipelines[pipelineIndex][imageType];
+}
+
+// @param `outFormat` dictates encoding.
 core::smart_refctd_ptr<video::IGPUShader> CComputeBlit::createNormalizationSpecializedShader(const asset::IImage::E_TYPE imageType, const uint32_t alphaBinCount)
 {
 	const auto workgroupDims = getDefaultWorkgroupDims(imageType);
@@ -275,5 +337,25 @@ core::smart_refctd_ptr<video::IGPUShader> CComputeBlit::createNormalizationSpeci
 		   "}\n";
 
 	auto cpuShader = core::make_smart_refctd_ptr<asset::ICPUShader>(shaderSourceStream.str().c_str(), IGPUShader::E_SHADER_STAGE::ESS_COMPUTE, IGPUShader::E_CONTENT_TYPE::ECT_HLSL, "CComputeBlitGLSL::createNormalizationSpecializedShader");
+}
+
+core::smart_refctd_ptr<video::IGPUComputePipeline> getNormalizationPipeline(const asset::IImage::E_TYPE imageType, const asset::E_FORMAT outFormat,
+	const uint32_t alphaBinCount = asset::IBlitUtilities::DefaultAlphaBinCount)
+{
+	const auto workgroupDims = getDefaultWorkgroupDims(imageType);
+	const uint32_t paddedAlphaBinCount = getPaddedAlphaBinCount(workgroupDims, alphaBinCount);
+	const SNormalizationCacheKey key = { imageType, paddedAlphaBinCount, outFormat };
+
+	if (m_normalizationPipelines.find(key) == m_normalizationPipelines.end())
+	{
+		auto specShader = createNormalizationSpecializedShader(imageType, outFormat, paddedAlphaBinCount);
+		IGPUComputePipeline::SCreationParams creationParams;
+		creationParams.shader.shader = specShader.get();
+		creationParams.shader.entryPoint = "main";
+		creationParams.layout = m_blitPipelineLayout[EBT_COVERAGE_ADJUSTMENT].get();
+		assert(m_device->createComputePipelines(nullptr, { &creationParams, &creationParams + 1 }, &m_normalizationPipelines[key]));
+	}
+
+	return m_normalizationPipelines[key];
 }
 #endif
