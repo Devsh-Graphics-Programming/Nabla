c2459bb31724022613c411a96dca5b075bd3dfa5
devsh <devsh@devsh.eu>
2024-11-07 17:00:10 +0100

    actually test using concepts

--- include/nbl/builtin/hlsl/blit/default_blit.comp.hlsl
+++ include/nbl/builtin/hlsl/blit/default_blit.comp.hlsl
@@ -21,21 +21,38 @@ struct KernelWeightsAccessor
 		return kernelWeights[idx];
 	}
 };
-struct InCSAccessor
-{
-	float32_t4 get(float32_t3 c, uint32_t l)
-	{
-		return inCS.SampleLevel(inSamp, blit::impl::dim_to_image_properties<ConstevalParameters::BlitDimCount>::getIndexCoord<float32_t>(c, l), 0);
-	}
-};
-struct OutImgAccessor
-{
-	void set(int32_t3 c, uint32_t l, float32_t4 v)
-	{
-		outImg[blit::impl::dim_to_image_properties<ConstevalParameters::BlitDimCount>::getIndexCoord<int32_t>(c, l)] = v;
-	}
-};
+
+inCS.SampleLevel(inSamp, blit::impl::dim_to_image_properties<ConstevalParameters::BlitDimCount>::getIndexCoord<float32_t>(c, l), 0);
 */
+struct InImgAccessor
+{
+	template<typename T, int32_t Dims NBL_FUNC_REQUIRES(is_same_v<T,float>)
+	vector<T,4> data get(const vector<uint16_t,Dims> uv, uint16_t layer, uint16_t level)
+	{
+		return __get_impl<Dims>(uv,_static_cast<float>(layer),_static_cast<float>(level),data);
+	}
+
+	template<int32_t Dims>
+	float32_t4 __get_impl(const vector<float32_t,Dims> uv, float layer, float level);
+
+	uint32_t descIx : 20;
+	uint32_t samplerIx : 12;
+};
+template<>
+float32_t4 InImgAccessor::__get_impl<1>(const float32_t1 uv, float layer, float level)
+{
+	return inAs1DArray[descIx].SampleLevel(inSamp[samplerIx],float32_t2(uv,layer),level);
+}
+template<>
+float32_t4 InImgAccessor::__get_impl<2>(const float32_t2 uv, float layer, float level)
+{
+	return inAs2DArray[descIx].SampleLevel(inSamp[samplerIx],float32_t3(uv,layer),level);
+}
+template<>
+float32_t4 InImgAccessor::__get_impl<3>(const float32_t3 uv, float layer, float level)
+{
+	return inAs3D[descIx].SampleLevel(inSamp[samplerIx],uv,level);
+}
 
 using namespace nbl::hlsl::blit;
 
@@ -44,6 +61,8 @@ using namespace nbl::hlsl::blit;
 [numthreads(ConstevalParameters::WorkGroupSize,1,1)]
 void main()
 {
+	InImgAccessor inImgA;
+	OutImgAccessor outImgA;
 /*
 	blit::compute_blit_t<ConstevalParameters> blit = blit::compute_blit_t<ConstevalParameters>::create(params);
     InCSAccessor inCSA;
---
+++ include/nbl/builtin/hlsl/concepts/loadable_image.hlsl
@@ -0,0 +1,66 @@
+// Copyright (C) 2024-2025 - DevSH Graphics Programming Sp. z O.O.
+// This file is part of the "Nabla Engine".
+// For conditions of distribution and use, see copyright notice in nabla.h
+#ifndef _NBL_BUILTIN_HLSL_CONCEPTS_LOADABLE_IMAGE_INCLUDED_
+#define _NBL_BUILTIN_HLSL_CONCEPTS_LOADABLE_IMAGE_INCLUDED_
+
+
+#include <nbl/builtin/hlsl/concepts.hlsl>
+
+
+namespace nbl
+{
+namespace hlsl
+{
+namespace concepts
+{
+// declare concept
+#define NBL_CONCEPT_NAME StorableImage
+#define NBL_CONCEPT_TPLT_PRM_KINDS (typename)(typename)(int32_t)
+#define NBL_CONCEPT_TPLT_PRM_NAMES (U)(T)(Dims)
+// not the greatest syntax but works
+#define NBL_CONCEPT_PARAM_0 (a,U)
+#define NBL_CONCEPT_PARAM_1 (uv,vector<uint16_t,Dims>)
+#define NBL_CONCEPT_PARAM_2 (layer,uint16_t)
+// start concept
+NBL_CONCEPT_BEGIN(3)
+// need to be defined AFTER the cocnept begins
+#define a NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_0
+#define uv NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_1
+#define layer NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_2
+NBL_CONCEPT_END(
+    ((NBL_CONCEPT_REQ_EXPR_RET_TYPE)((a.template get<T,Dims>(uv,layer)) , ::nbl::hlsl::is_same_v, vector<T,4>))
+);
+#undef layer
+#undef uv
+#undef a
+#include <nbl/builtin/hlsl/concepts/__end.hlsl>
+
+// declare concept
+#define NBL_CONCEPT_NAME MipmappedStorableImage
+#define NBL_CONCEPT_TPLT_PRM_KINDS (typename)(typename)(int32_t)
+#define NBL_CONCEPT_TPLT_PRM_NAMES (U)(T)(Dims)
+// not the greatest syntax but works
+#define NBL_CONCEPT_PARAM_0 (a,U)
+#define NBL_CONCEPT_PARAM_1 (uv,vector<uint16_t,Dims>)
+#define NBL_CONCEPT_PARAM_2 (layer,uint16_t)
+#define NBL_CONCEPT_PARAM_3 (level,uint16_t)
+// start concept
+NBL_CONCEPT_BEGIN(4)
+// need to be defined AFTER the cocnept begins
+#define a NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_0
+#define uv NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_1
+#define layer NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_2
+#define level NBL_CONCEPT_PARAM_T NBL_CONCEPT_PARAM_3
+NBL_CONCEPT_END(
+    ((NBL_CONCEPT_REQ_EXPR_RET_TYPE)((a.template get<T,Dims>(uv,layer,level)) , ::nbl::hlsl::is_same_v, vector<T,4>))
+);
+#undef level
+#undef layer
+#undef uv
+#undef a
+#include <nbl/builtin/hlsl/concepts/__end.hlsl>
+}
+}
+}
+#endif
