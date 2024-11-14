89b3af8ba2a0285cb95afc0590c8b8bd5f36657b
devsh <devsh@devsh.eu>
2024-11-14 17:49:49 +0100

    redo the snake curves

--- include/nbl/builtin/hlsl/ndarray_addressing.hlsl
+++ include/nbl/builtin/hlsl/ndarray_addressing.hlsl
@@ -13,27 +13,67 @@ namespace hlsl
 namespace ndarray_addressing
 {
 
-uint32_t snakeCurve(NBL_CONST_REF_ARG(uint32_t3) coordinate, NBL_CONST_REF_ARG(uint32_t3) extents)
+template<int32_t Dims, typename U=uint16_t, typename T=unsigned_integer_of_size<sizeof(U)*2>::type>
+T snakeCurve(const vector<U,Dims> coordinate, const vector<U,Dims-1> extents)
 {
-	return (coordinate.z * extents.y + coordinate.y) * extents.x + coordinate.x;
+	T retval = _static_cast<T>(coordinate[Dims-1]);
+	for (int32_t i=Dims-2; i>=0; i--)
+	{
+		retval *= _static_cast<T>(extents[i]);
+		retval += _static_cast<T>(coordinate[i]);
+	}
+	return retval;
 }
 
-uint32_t3 snakeCurveInverse(uint32_t linearIndex, NBL_CONST_REF_ARG(uint32_t2) gridDimPrefixProduct)
+// highly specialized function, requires you know the prefices already and that dimension is higher than 1
+// TODO: make an even better one that takes precomputed reciprocals and stuff for fast integer division and modulo
+template<int32_t Dims, typename U=uint32_t, typename T=conditional<sizeof(U)==2,unsigned_integer_of_size_t<sizeof(U)/2>,uint16_t>::type> // TODO: NBL_REQUIRE Dims>=2
+vector<T,Dims> snakeCurveInverse(const U linearIndex, const vector<U,Dims-1> gridDimPrefixProduct)
 {
-	uint32_t3 index3D;
+	vector<T,Dims> coord;
+	coord[Dims-1] = linearIndex/gridDimPrefixProduct[Dims-2];
+	{
+		U prevRemainder = linearIndex;
+		for (int32_t i=Dims-2; i>0; i--)
+		{
+			prevRemainder -= gridDimPrefixProduct[i]*coord[i+1];
+			coord[i] = prevRemainder/gridDimPrefixProduct[i-1];
+		}
+		coord[0] = prevRemainder-gridDimPrefixProduct[0]*coord[1];
+	}
+	coord[Dims-2] = linearIndex-coord[Dims-1]*gridDimPrefixProduct[Dims-2];
 
-	index3D.z = linearIndex / gridDimPrefixProduct.y;
-
-	const uint32_t tmp = linearIndex - (index3D.z * gridDimPrefixProduct.y);
-	index3D.y = tmp / gridDimPrefixProduct.x;
-	index3D.x = tmp - (index3D.y * gridDimPrefixProduct.x);
-
-	return index3D;
+	return coord;
 }
 
-uint32_t3 snakeCurveInverse(uint32_t linearIndex, NBL_CONST_REF_ARG(uint32_t3) gridDim)
+namespace impl
 {
-	return snakeCurveInverse(linearIndex, uint32_t2(gridDim.x, gridDim.x*gridDim.y));
+template<int32_t Dims, typename U, typename T>
+struct snakeCurveInverse
+{
+	static vector<T,Dims> __call(const U linearIndex, const vector<T,Dims> gridDim)
+	{
+		vector<U,Dims-1> gridDimPrefixProduct;
+		gridDimPrefixProduct[0] = gridDim[0];
+		for (int32_t i=1; i<Dims-1; i++)
+			gridDimPrefixProduct[i] = gridDimPrefixProduct[i-1]*gridDim[i];
+		return snakeCurveInverse<Dims,U,T>(linearIndex,gridDimPrefixProduct);
+	}
+};
+template<typename U, typename T>
+struct snakeCurveInverse<1,U,T>
+{
+	static vector<T,Dims> __call(const U linearIndex, const vector<T,Dims> gridDim)
+	{
+		return vector<T,Dims>(linearIndex);
+	}
+};
+}
+
+template<int32_t Dims, typename U=uint32_t, typename T=conditional<sizeof(U)==2,unsigned_integer_of_size_t<sizeof(U)/2>,uint16_t>::type>
+vector<T,Dims> snakeCurveInverse(const U linearIndex, const vector<T,Dims> gridDim)
+{
+	return impl::snakeCurveInverse<Dims,U,T>::__call(linearIndex,gridDim);
 }
 
 }
