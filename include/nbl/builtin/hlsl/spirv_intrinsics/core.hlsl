6789fa74e4029e9e030fc9ed9af189b369db9bad
keptsecret <sorchon@gmail.com>
2024-11-18 15:45:34 +0700

    use snorm unpack instead

--- include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
+++ include/nbl/builtin/hlsl/spirv_intrinsics/core.hlsl
@@ -197,8 +197,8 @@ SquareMatrix matrixInverse(NBL_CONST_REF_ARG(SquareMatrix) mat);
 [[vk::ext_instruction(GLSLstd450UnpackSnorm2x16, "GLSL.std.450")]]
 float32_t2 unpackSnorm2x16(uint32_t p);
 
-[[vk::ext_instruction(GLSLstd450UnpackUnorm4x8, "GLSL.std.450")]]
-float32_t4 unpackUnorm4x8(uint32_t p);
+[[vk::ext_instruction(GLSLstd450UnpackSnorm4x8, "GLSL.std.450")]]
+float32_t4 unpackSnorm4x8(uint32_t p);
 
 // Memory Semantics link here: https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#Memory_Semantics_-id-
 
