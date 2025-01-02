76bf038f16439bb1f15aa96145eaeaf22b628070
AnastaZIuk <areklachowicz@gmail.com>
2025-01-02 17:31:13 +0100

    typo

--- cmake/cpack/find/licence/template.cmake
+++ cmake/cpack/find/licence/template.cmake
@@ -1,3 +1,3 @@
-# Copyright (C) 2018-2040 - DevSH Graphics Programming Sp. z O.O.
+# Copyright (C) 2018-2025 - DevSH Graphics Programming Sp. z O.O.
 # This file is part of the "Nabla Engine".
 # For conditions of distribution and use, see copyright notice in nabla.h
