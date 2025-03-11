e9ea6b987630dfa16f6e9e50361acbdacbf90780
Fletterio <fj.letterio@gmail.com>
2025-03-10 23:34:46 -0300

    Refactor to use a single generic loop

--- include/nbl/system/demote_promote_writer_readers_lock.h
+++ include/nbl/system/demote_promote_writer_readers_lock.h
@@ -4,6 +4,7 @@
 #include <thread>
 
 // TODO: Bring back proper memory semantics on fetch/store
+// TODO: CRTP/F-Bound on `perform_under_locked_state`?
 
 namespace nbl::system
 {
@@ -12,23 +13,21 @@ namespace nbl::system
 namespace impl
 {
 	template<typename U>
-	struct DPWRLstate_lock
+	struct DPWRLStateLock
 	{
 		using type = std::atomic<U>;
 	};
 
 	// will fail if `atomic_unsiged_lock_free` doesn't exist or its value type has wrong size
 	template<typename U> requires (sizeof(std::atomic_unsigned_lock_free::value_type) >= sizeof(U))
-	struct DPWRLstate_lock<U>
+	struct DPWRLStateLock<U>
 	{
 		using type = std::atomic_unsigned_lock_free;
 	};
 
-	constexpr inline DPWRLstate_lock<uint32_t>::type::value_type DPWRLMaxActors = 1023;
-
 	struct DPWRLStateSemantics
 	{
-		using state_lock_value_t = typename DPWRLstate_lock<uint32_t>::type::value_type;
+		using state_lock_value_t = typename DPWRLStateLock<uint32_t>::type::value_type;
 		uint32_t currentReaders : 10 = 0;
 		uint32_t pendingWriters : 10 = 0;
 		uint32_t pendingUpgrades : 10 = 0;
@@ -37,7 +36,6 @@ namespace impl
 
 		consteval inline operator state_lock_value_t() const
 		{ 
-			assert(currentReaders <= DPWRLMaxActors && pendingWriters <= DPWRLMaxActors && pendingUpgrades <= DPWRLMaxActors);
 			return static_cast<state_lock_value_t>((currentReaders << 22) | (pendingWriters << 12) | (pendingUpgrades << 2) | (writing << 1) | stateLocked);
 		}
 	};
@@ -46,45 +44,34 @@ namespace impl
 class demote_promote_writer_readers_lock
 {
 public:
-	using state_lock_t = impl::DPWRLstate_lock<uint32_t>::type;
+	using state_lock_t = impl::DPWRLStateLock<uint32_t>::type;
 	using state_lock_value_t = impl::DPWRLStateSemantics::state_lock_value_t;
-	// Limit on how many threads can be launched concurrently that try to read/write from/to the same file
-	constexpr static inline state_lock_value_t MaxActors = impl::DPWRLMaxActors;
+	// Limit on how many threads can be launched concurrently that try to read/write from/to the resource behind this lock
+	constexpr static inline state_lock_value_t MaxActors = 1023;
 	
 	friend class dp_read_lock_guard;
 	friend class dp_write_lock_guard;
 
 	/**
-	* @brief Acquires lock for reading. This thread will be blocked until there are no writers (writing or pending) and no readers waiting for a writer upgrade
+	* @brief Acquires lock for reading. This thread will be blocked until there are no writers (writing or pending) and no readers waiting for a writer upgrade.
+	*		 It will preempt any further readers from acquiring lock between first state lock acquire and releasing the lock it will acquire when this method returns
 	*/
 	void read_lock()
 	{
-		constexpr state_lock_value_t preemptedMask = pendingWritersMask | pendingUpgradesMask | writingMask;
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
-		{
-			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
-			if (oldState & flipLock)
+		const auto success = [](const state_lock_value_t oldState) -> state_lock_value_t
 			{
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
-			// If there are any pending writers, upgrades or a thread is currently writing, cannot acquire lock (`preemptedMask`)
-			// Must release flipLock and yield for other threads to progress
-			state_lock_value_t newState = oldState;
-			if (newState & preemptedMask)
+				// increase currentReaders 
+				return oldState + impl::DPWRLStateSemantics{ .currentReaders = 1,.pendingWriters = 0,.pendingUpgrades = 0,.writing = false,.stateLocked = false };
+			};
+
+		const auto preemptionCheck = [](const state_lock_value_t oldState) -> bool 
 			{
-				state.store(newState);
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
-			// release the flipLock, increment currentReaders 
-			newState += impl::DPWRLStateSemantics{ .currentReaders = 1,.pendingWriters = 0,.pendingUpgrades = 0,.writing = false,.stateLocked = false };
-			state.store(newState);
-			break;
-		}
+				// Can't acquire lock if there's threads writing or otherwise waiting to write
+				constexpr state_lock_value_t preemptedMask = pendingWritersMask | pendingUpgradesMask | writingMask;
+				return oldState & preemptedMask;
+			};
+
+		perform_under_locked_state(std::move(success), preemptionCheck);
 	}
 
 	/**
@@ -92,27 +79,20 @@ public:
 	*/
 	void read_unlock()
 	{
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
-		{
-			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
-			if (oldState & flipLock)
+		const auto success = [](const state_lock_value_t oldState) -> state_lock_value_t
 			{
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
-			
-			// Sanity check: ensure no one's writing if we had a reader's lock
-			assert(!(oldState & writingMask));
-			// Sanity check: if this thread is reading, then `currentReaders` can't be 0
-			assert(oldState & currentReadersMask);
-			
-			// release the flipLock, decrement currentReaders
-			state_lock_value_t newState = oldState - impl::DPWRLStateSemantics{ .currentReaders = 1, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
-			state.store(newState);
-			break;
-		}
+				// decrease currentReaders 
+				return oldState - impl::DPWRLStateSemantics{ .currentReaders = 1,.pendingWriters = 0,.pendingUpgrades = 0,.writing = false,.stateLocked = false };
+			};
+
+		const auto sanityChecks = [](const state_lock_value_t oldState) -> bool
+			{
+				// If this thread is reading, then `currentReaders` can't be 0
+				// If we had a reader's lock, then no thread can be writing
+				return (oldState & currentReadersMask) && !(oldState & writingMask);
+			};
+
+		perform_under_locked_state(std::move(success), defaultPreemptionCheck, sanityChecks);
 	}
 
 	/**
@@ -122,40 +102,38 @@ public:
 	*/
 	void write_lock()
 	{
-		constexpr state_lock_value_t preemptedMask = currentReadersMask | pendingUpgradesMask | writingMask;
 		bool registeredPending = false;
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
-		{
-			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
-			if (oldState & flipLock)
+
+		const auto success = [&](const state_lock_value_t oldState) -> state_lock_value_t
 			{
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
-			// If there are any pending upgrades or a thread is currently reading or writing, cannot acquire lock (`preemptedMask`), so release flipLock and yield for other threads to progress
-			// Also registers this thread as a pending writer if not done so already
-			state_lock_value_t newState = oldState;
-			if (newState & preemptedMask)
+				// Declare this thread as writer
+				state_lock_value_t newState = oldState | writingMask;
+				// If this thread had been registered as a pending writer, unregister
+				if (registeredPending)
+					newState -= impl::DPWRLStateSemantics{ .currentReaders = 0,.pendingWriters = 1, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
+				return newState;
+			};
+
+		const auto preemptionCheck = [](const state_lock_value_t oldState) -> bool
 			{
+				// Can't acquire lock if there's threads still reading, writing, or waiting to upgrade
+				constexpr state_lock_value_t preemptedMask = currentReadersMask | pendingUpgradesMask | writingMask;
+				return oldState & preemptedMask;
+			};
+
+		const auto preempted = [&](const state_lock_value_t oldState) -> state_lock_value_t
+			{
+				state_lock_value_t preemptedState = oldState;
+				// Register this thread as a pending writer if it hadn't done so already
 				if (!registeredPending)
 				{
 					registeredPending = true;
-					newState += impl::DPWRLStateSemantics{ .currentReaders = 0,.pendingWriters = 1,.pendingUpgrades = 0,.writing = false,.stateLocked = false };
+					preemptedState += impl::DPWRLStateSemantics{ .currentReaders = 0,.pendingWriters = 1,.pendingUpgrades = 0,.writing = false,.stateLocked = false };
 				}
-				state.store(newState);
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
-			// release the flipLock, declare that this thread will be writing, mark that this thread is no longer pending IF had been registered as such
-			if (registeredPending)
-				newState -= impl::DPWRLStateSemantics{ .currentReaders = 0,.pendingWriters = 1, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
-			newState |= writingMask;
-			state.store(newState);
-			break;
-		}
+				return preemptedState;
+			};
+
+		perform_under_locked_state(std::move(success), preemptionCheck, defaultSanityChecks, preempted);
 	}
 
 	/**
@@ -163,27 +141,20 @@ public:
 	*/
 	void write_unlock()
 	{
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
-		{
-			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
-			if (oldState & flipLock)
+		const auto success = [](const state_lock_value_t oldState) -> state_lock_value_t
 			{
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
+				// Declare that this thread is no longer writing
+				return oldState & ~writingMask;
+			};
 
-			// Sanity check: writing flag is actually on
-			assert(oldState & writingMask);
-			//Sanity check: no one should be reading if this thread was just writing
-			assert(!(oldState & currentReadersMask));
+		const auto sanityChecks = [](const state_lock_value_t oldState) -> bool
+			{
+				// If this thread was writing, then the writing mask must have been set to 1 when we acquired the state lock
+				// If this thread was writing, then there can't be any threads currently reading
+				return (oldState & writingMask) && !(oldState & currentReadersMask);
+			};
 
-			// release the flipLock, decrement currentReaders
-			state_lock_value_t newState = oldState & ~writingMask;
-			state.store(newState);
-			break;
-		}
+		perform_under_locked_state(std::move(success), defaultPreemptionCheck, sanityChecks);
 	}
 
 	/**
@@ -193,46 +164,49 @@ public:
 	*/
 	void upgrade()
 	{
-		constexpr state_lock_value_t preemptedMask = currentReadersMask;
 		bool registeredPending = false;
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
-		{
-			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
-			if (oldState & flipLock)
-			{
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-				continue;
-			}
 
-			// Sanity check: ensure no one's writing if we had a reader's lock
-			assert(!(oldState & writingMask));
-			// Sanity check: if this thread is reading, then `currentReaders` can't be 0
-			assert(oldState & currentReadersMask);
-
-			state_lock_value_t nextState = oldState;
-			// If there are threads currently reading or writing, must release flipLock for other threads to progress
-			if (nextState & preemptedMask)
+		const auto success = [&](const state_lock_value_t oldState) -> state_lock_value_t
 			{
-				// Register pending if not done so earlier
+				// Declare this thread as writer
+				state_lock_value_t newState = oldState | writingMask;
+				// If this thread had been registered as a pending upgrade, unregister
+				if (registeredPending)
+					newState -= impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = 1, .writing = false, .stateLocked = false };
+				// If not, then we didn't unregister as a reader yet, so do that as well
+				else
+					newState -= impl::DPWRLStateSemantics{ .currentReaders = 1, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
+				return newState;
+			};
+
+		const auto preemptionCheck = [&](const state_lock_value_t oldState) -> bool
+			{
+				// If there's another reader, can't upgrade
+				return (oldState & currentReadersMask) > (state_lock_value_t(registeredPending ? 0 : 1) << 22);
+			};
+
+		const auto sanityChecks = [&](const state_lock_value_t oldState) -> bool
+			{
+				// If we had a reader's lock, no thread should be writing
+				// Also if this thread has not registered as a pending upgrade, then the count of current readers should be at least 1
+				return !(oldState & writingMask) && (registeredPending || (oldState & currentReadersMask)) ;
+			};
+
+		const auto preempted = [&](const state_lock_value_t oldState) -> state_lock_value_t
+			{
+				constexpr state_lock_value_t preemptedMask = currentReadersMask;
+				state_lock_value_t preemptedState = oldState;
+				// Register as pending upgrade if not done so earlier, and unregister this thread as a reader
 				if (!registeredPending)
 				{
 					registeredPending = true;
-					nextState += impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = 1, .writing = false, .stateLocked = false };
+					preemptedState += impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = 1, .writing = false, .stateLocked = false };
+					preemptedState -= impl::DPWRLStateSemantics{ .currentReaders = 1, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
 				}
-				// Release flipLock
-				state.store(nextState);
-				if (spinCount > SpinsBeforeYield)
-					std::this_thread::yield();
-			}
-			// Release flipLock, declare that this thread is writing, mark that this thread is no longer pending IF had been registered as such
-			if (registeredPending)
-				nextState -= impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = 1, .writing = false, .stateLocked = false };
-			nextState |= writingMask;
-			state.store(nextState);
-			break;
-		}
+				return preemptedState;
+			};
+
+		perform_under_locked_state(std::move(success), preemptionCheck, sanityChecks, preempted);
 	}
 
 	/**
@@ -240,31 +214,64 @@ public:
 	*/
 	void downgrade()
 	{
-		for (state_lock_value_t spinCount = 0; true; spinCount++)
+		const auto success = [](const state_lock_value_t oldState) -> state_lock_value_t
+			{
+				// Declare that thread is no longer writing and that it will be reading
+				state_lock_value_t newState = oldState & ~writingMask;
+				newState += impl::DPWRLStateSemantics{ .currentReaders = 1, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
+				return newState;
+			};
+
+		const auto sanityChecks = [&](const state_lock_value_t oldState) -> bool
+			{
+				// If we had a writer's lock, the writing flag should be on
+				// If we had a writer's lock, no thread should be currently reading
+				return (oldState & writingMask) && !(oldState & currentReadersMask);
+			};
+
+		perform_under_locked_state(std::move(success), defaultPreemptionCheck, sanityChecks);
+	}
+
+private:
+
+	constexpr static auto defaultPreemptionCheck = [](const state_lock_value_t oldState) -> bool {return false; };
+	constexpr static auto defaultPreempted = [](const state_lock_value_t oldState)->state_lock_value_t {return oldState; };
+	constexpr static auto defaultSanityChecks = [](const state_lock_value_t oldState)->bool {return true; };
+
+	template<typename Success, typename PreemptionCheck = decltype(defaultPreemptionCheck), typename SanityChecks = decltype(defaultSanityChecks), typename Preempted = decltype(defaultPreempted)>
+	inline void perform_under_locked_state(
+		Success&& success,
+		PreemptionCheck& preemptionCheck = defaultPreemptionCheck,
+		SanityChecks& sanityChecks = defaultSanityChecks,
+		Preempted& preempted = defaultPreempted
+		)
+	{
+		for (uint32_t spinCount = 0; true; spinCount++)
 		{
 			const state_lock_value_t oldState = state.fetch_or(flipLock);
-			// stateLocked: some other thread is doing work and expects to update state
 			if (oldState & flipLock)
 			{
 				if (spinCount > SpinsBeforeYield)
 					std::this_thread::yield();
 				continue;
 			}
-
-			// Sanity check: writing flag is actually on
-			assert(oldState & writingMask);
-			//Sanity check: no one should be reading if this thread was just writing
-			assert(!(oldState & currentReadersMask));
-
-			// Release flipLock, declare that this thread is no longer writing and that it's reading
-			state_lock_value_t nextState = oldState & ~writingMask;
-			nextState += impl::DPWRLStateSemantics{ .currentReaders = 1, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
-			state.store(nextState);
+			assert(sanityChecks(oldState));
+			const bool wasPreempted = preemptionCheck(oldState);
+			// thankfully `?` operator actually short circuits
+			const state_lock_value_t newState = wasPreempted ? preempted(oldState) : success(oldState);
+			// new state must unlock the state lock
+			assert(!(newState & flipLock));
+			state.store(newState);
+			if (wasPreempted)
+			{
+				if (spinCount > SpinsBeforeYield)
+					std::this_thread::yield();
+				continue;
+			}
 			break;
 		}
 	}
 
-private:
 	state_lock_t state;
 
 	constexpr static inline state_lock_value_t flipLock = impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = true };
@@ -272,7 +279,7 @@ private:
 	constexpr static inline state_lock_value_t pendingUpgradesMask = impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = 0, .pendingUpgrades = MaxActors, .writing = false, .stateLocked = false };
 	constexpr static inline state_lock_value_t pendingWritersMask = impl::DPWRLStateSemantics{ .currentReaders = 0, .pendingWriters = MaxActors, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
 	constexpr static inline state_lock_value_t currentReadersMask = impl::DPWRLStateSemantics{ .currentReaders = MaxActors, .pendingWriters = 0, .pendingUpgrades = 0, .writing = false, .stateLocked = false };
-	constexpr static inline state_lock_value_t SpinsBeforeYield = 5000;
+	constexpr static inline uint32_t SpinsBeforeYield = 5000;
 };
 
 namespace impl
@@ -337,12 +344,12 @@ public:
 	}
 };
 
-inline explicit dp_read_lock_guard::dp_read_lock_guard(dp_write_lock_guard&& wl) : impl::dpwr_lock_guard_base(std::move(wl))
+inline dp_read_lock_guard::dp_read_lock_guard(dp_write_lock_guard&& wl) : impl::dpwr_lock_guard_base(std::move(wl))
 {
 	m_lock->downgrade();
 }
 
-inline explicit dp_write_lock_guard::dp_write_lock_guard(dp_read_lock_guard&& rl) : impl::dpwr_lock_guard_base(std::move(rl))
+inline dp_write_lock_guard::dp_write_lock_guard(dp_read_lock_guard&& rl) : impl::dpwr_lock_guard_base(std::move(rl))
 {
 	m_lock->upgrade();
 }
