2b62f2a1f5144ccc6efd9a5e6863b6574a557984
YasInvolved <szrtxm.op@gmail.com>
2025-03-27 15:21:55 +0100

    use correct paths, stop deleting some of dependencies

--- cmake/buildinfo/buildinfo.cmake
+++ cmake/buildinfo/buildinfo.cmake
@@ -4,27 +4,26 @@ cmake_host_system_information(RESULT CPU_DESC QUERY PROCESSOR_DESCRIPTION)
 add_custom_command(
 	OUTPUT cmake_info.json
 	COMMAND ${CMAKE_COMMAND} -E capabilities >> cmake_info.json
-	COMMENT "Generating ${CMAKE_BINARY_DIR}/cmake_info.json"
+	COMMENT "Generating ${NBL_ROOT_PATH_BINARY}/cmake_info.json"
 )
 
 set(VKSDK_INFO_COMMAND "$ENV{VULKAN_SDK}/bin/vulkaninfoSDK")
 add_custom_command(
 	OUTPUT vulkan_info.json
-	COMMAND ${VKSDK_INFO_COMMAND} -j -o ${CMAKE_BINARY_DIR}/vulkan_info.json
-	COMMENT "Generating ${CMAKE_BINARY_DIR}/vulkan_info.json"
+	COMMAND ${VKSDK_INFO_COMMAND} -j -o ${NBL_ROOT_PATH_BINARY}/vulkan_info.json
+	COMMENT "Generating ${NBL_ROOT_PATH_BINARY}/vulkan_info.json"
 )
 
-set(TEMP_SYSTEM_INFO_DEPENDENCIES cmake_info.json vulkan_info.json)
 set(SYSTEM_INFO_DEPENDENCIES 
-	${TEMP_SYSTEM_INFO_DEPENDENCIES}
+	cmake_info.json 
+	vulkan_info.json
 	CMakeCache.txt
-	"${CMAKE_BINARY_DIR}/3rdparty/git-version-tracking/nabla_git_info.cpp" 
-	"${CMAKE_BINARY_DIR}/3rdparty/git-version-tracking/dxc_git_info.cpp"
+	"${NBL_ROOT_PATH_BINARY}/3rdparty/git-version-tracking/nabla_git_info.cpp" 
+	"${NBL_ROOT_PATH_BINARY}/3rdparty/git-version-tracking/dxc_git_info.cpp"
 )
 
 add_custom_target(system_info 
 	DEPENDS ${SYSTEM_INFO_DEPENDENCIES}
 	COMMAND ${CMAKE_COMMAND} -E tar c build_info.zip --format=zip ${SYSTEM_INFO_DEPENDENCIES}
-	COMMAND ${CMAKE_COMMAND} -E rm -- ${TEMP_SYSTEM_INFO_DEPENDENCIES}
-	COMMENT "Generating ${CMAKE_BINARY_DIR}/build_info.zip"
+	COMMENT "Generating ${NBL_ROOT_PATH_BINARY}/build_info.zip"
 )
