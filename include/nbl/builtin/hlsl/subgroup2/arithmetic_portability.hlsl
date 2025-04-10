a8e02a327abb2097571812efa2d8ea07143e7f28
keptsecret <sorchon@gmail.com>
2025-04-10 16:01:24 +0700

    changes to Params, Config handling types

--- include/nbl/builtin/hlsl/subgroup2/arithmetic_portability.hlsl
+++ include/nbl/builtin/hlsl/subgroup2/arithmetic_portability.hlsl
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 - DevSH Graphics Programming Sp. z O.O.
+// Copyright (C) 2025 - DevSH Graphics Programming Sp. z O.O.
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
 #ifndef _NBL_BUILTIN_HLSL_SUBGROUP2_ARITHMETIC_PORTABILITY_INCLUDED_
@@ -7,7 +7,7 @@
 
 #include "nbl/builtin/hlsl/device_capabilities_traits.hlsl"
 
-#include "nbl/builtin/hlsl/subgroup/basic.hlsl"
+#include "nbl/builtin/hlsl/subgroup2/ballot.hlsl"
 #include "nbl/builtin/hlsl/subgroup2/arithmetic_portability_impl.hlsl"
 #include "nbl/builtin/hlsl/concepts.hlsl"
 
@@ -19,24 +19,26 @@ namespace hlsl
 namespace subgroup2
 {
 
-template<typename Config, class BinOp, int32_t _ItemsPerInvocation=1, class device_capabilities=void NBL_PRIMARY_REQUIRES(subgroup::is_configuration_v<Config>)
+template<typename Config, class BinOp, int32_t _ItemsPerInvocation=1, class device_capabilities=void, bool OverrideUseNativeInstrinsics=true NBL_PRIMARY_REQUIRES(is_configuration_v<Config>)
 struct ArithmeticParams
 {
     using config_t = Config;
     using binop_t = BinOp;
     using scalar_t = typename BinOp::type_t;    // BinOp should be with scalar type
-    using type_t = conditional_t<_ItemsPerInvocation<2, scalar_t, vector<scalar_t, _ItemsPerInvocation> >;
+    using type_t = vector<scalar_t, _ItemsPerInvocation>;// conditional_t<_ItemsPerInvocation<2, scalar_t, vector<scalar_t, _ItemsPerInvocation> >;
 
     NBL_CONSTEXPR_STATIC_INLINE int32_t ItemsPerInvocation = _ItemsPerInvocation;
-    NBL_CONSTEXPR_STATIC_INLINE bool UseNativeIntrinsics = device_capabilities_traits<device_capabilities>::shaderSubgroupArithmetic /*&& /*some heuristic for when its faster*/;
+    // if OverrideUseNativeInstrinsics is true, tries to use native spirv intrinsics
+    // if OverrideUseNativeInstrinsics is false, will always use emulated versions
+    NBL_CONSTEXPR_STATIC_INLINE bool UseNativeIntrinsics = device_capabilities_traits<device_capabilities>::shaderSubgroupArithmetic && OverrideUseNativeInstrinsics /*&& /*some heuristic for when its faster*/;
 };
 
 template<typename Params>
-struct reduction : impl::reduction<typename Params::binop_t,typename Params::type_t,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
+struct reduction : impl::reduction<Params,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
 template<typename Params>
-struct inclusive_scan : impl::inclusive_scan<typename Params::binop_t,typename Params::type_t,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
+struct inclusive_scan : impl::inclusive_scan<Params,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
 template<typename Params>
-struct exclusive_scan : impl::exclusive_scan<typename Params::binop_t,typename Params::type_t,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
+struct exclusive_scan : impl::exclusive_scan<Params,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
 
 }
 }
--- include/nbl/builtin/hlsl/subgroup2/arithmetic_portability_impl.hlsl
+++ include/nbl/builtin/hlsl/subgroup2/arithmetic_portability_impl.hlsl
@@ -1,8 +1,15 @@
-// Copyright (C) 2023 - DevSH Graphics Programming Sp. z O.O.
+// Copyright (C) 2025 - DevSH Graphics Programming Sp. z O.O.
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
 #ifndef _NBL_BUILTIN_HLSL_SUBGROUP2_ARITHMETIC_PORTABILITY_IMPL_INCLUDED_
 #define _NBL_BUILTIN_HLSL_SUBGROUP2_ARITHMETIC_PORTABILITY_IMPL_INCLUDED_
+
+// #include "nbl/builtin/hlsl/glsl_compat/subgroup_shuffle.hlsl"
+// #include "nbl/builtin/hlsl/glsl_compat/subgroup_arithmetic.hlsl"
+
+// #include "nbl/builtin/hlsl/subgroup/ballot.hlsl"
+
+// #include "nbl/builtin/hlsl/functional.hlsl"
 
 #include "nbl/builtin/hlsl/subgroup/arithmetic_portability_impl.hlsl"
 
@@ -16,12 +23,12 @@ namespace subgroup2
 namespace impl
 {
 
-template<class Binop, typename T, uint32_t ItemsPerInvocation, bool native>
+template<class Params, uint32_t ItemsPerInvocation, bool native>
 struct inclusive_scan
 {
-    using type_t = T;
-    using scalar_t = typename Binop::type_t;
-    using binop_t = Binop;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
     using exclusive_scan_op_t = subgroup::impl::exclusive_scan<binop_t, native>;
 
     // NBL_CONSTEXPR_STATIC_INLINE uint32_t ItemsPerInvocation = vector_traits<T>::Dimension;
@@ -31,27 +38,27 @@ struct inclusive_scan
         binop_t binop;
         type_t retval;
         retval[0] = value[0];
-        //[unroll(ItemsPerInvocation-1)]
+        [unroll]
         for (uint32_t i = 1; i < ItemsPerInvocation; i++)
             retval[i] = binop(retval[i-1], value[i]);
         
         exclusive_scan_op_t op;
         scalar_t exclusive = op(retval[ItemsPerInvocation-1]);
 
-        //[unroll(ItemsPerInvocation)]
+        [unroll]
         for (uint32_t i = 0; i < ItemsPerInvocation; i++)
             retval[i] = binop(retval[i], exclusive);
         return retval;
     }
 };
 
-template<class Binop, typename T, uint32_t ItemsPerInvocation, bool native>
+template<class Params, uint32_t ItemsPerInvocation, bool native>
 struct exclusive_scan
 {
-    using type_t = T;
-    using scalar_t = typename Binop::type_t;
-    using binop_t = Binop;
-    using inclusive_scan_op_t = subgroup2::impl::inclusive_scan<binop_t, T, ItemsPerInvocation, native>;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
+    using inclusive_scan_op_t = subgroup2::impl::inclusive_scan<Params, ItemsPerInvocation, native>;
 
     // NBL_CONSTEXPR_STATIC_INLINE uint32_t ItemsPerInvocation = vector_traits<T>::Dimension;
 
@@ -64,19 +71,19 @@ struct exclusive_scan
 
         type_t retval;
         retval[0] = bool(glsl::gl_SubgroupInvocationID()) ? left[ItemsPerInvocation-1] : binop_t::identity;
-        //[unroll(ItemsPerInvocation-1)]
+        [unroll]
         for (uint32_t i = 1; i < ItemsPerInvocation; i++)
             retval[i] = value[i-1];
         return retval;
     }
 };
 
-template<class Binop, typename T, uint32_t ItemsPerInvocation, bool native>
+template<class Params, uint32_t ItemsPerInvocation, bool native>
 struct reduction
 {
-    using type_t = T;   // TODO? assert scalar_type<T> == scalar_t
-    using scalar_t = typename Binop::type_t;
-    using binop_t = Binop;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
     using op_t = subgroup::impl::reduction<binop_t, native>;
 
     // NBL_CONSTEXPR_STATIC_INLINE uint32_t ItemsPerInvocation = vector_traits<T>::Dimension;
@@ -86,7 +93,7 @@ struct reduction
         binop_t binop;
         op_t op;
         scalar_t retval = value[0];
-        //[unroll(ItemsPerInvocation-1)]
+        [unroll]
         for (uint32_t i = 1; i < ItemsPerInvocation; i++)
             retval = binop(retval, value[i]);
         return op(retval);
@@ -94,41 +101,73 @@ struct reduction
 };
 
 
-// spec for N=1 uses subgroup funcs
-template<class Binop, typename T, bool native>
-struct inclusive_scan<Binop, T, 1, native>
+// specs for N=1 uses subgroup funcs
+// specialize native
+// #define SPECIALIZE(NAME,BINOP,SUBGROUP_OP) template<typename T> struct NAME<BINOP<T>,true> \
+// { \
+//     using type_t = T; \
+//  \
+//     type_t operator()(NBL_CONST_REF_ARG(type_t) v) {return glsl::subgroup##SUBGROUP_OP<type_t>(v);} \
+// }
+
+// #define SPECIALIZE_ALL(BINOP,SUBGROUP_OP) SPECIALIZE(reduction,BINOP,SUBGROUP_OP); \
+//     SPECIALIZE(inclusive_scan,BINOP,Inclusive##SUBGROUP_OP); \
+//     SPECIALIZE(exclusive_scan,BINOP,Exclusive##SUBGROUP_OP);
+
+// SPECIALIZE_ALL(bit_and,And);
+// SPECIALIZE_ALL(bit_or,Or);
+// SPECIALIZE_ALL(bit_xor,Xor);
+
+// SPECIALIZE_ALL(plus,Add);
+// SPECIALIZE_ALL(multiplies,Mul);
+
+// SPECIALIZE_ALL(minimum,Min);
+// SPECIALIZE_ALL(maximum,Max);
+
+// #undef SPECIALIZE_ALL
+// #undef SPECIALIZE
+
+// specialize portability
+template<class Params, bool native>
+struct inclusive_scan<Params, 1, native>
 {
-    using binop_t = Binop;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
     using op_t = subgroup::impl::inclusive_scan<binop_t, native>;
     // assert T == scalar type, binop::type == T
 
-    T operator()(NBL_CONST_REF_ARG(T) value)
+    type_t operator()(NBL_CONST_REF_ARG(type_t) value)
     {
         op_t op;
         return op(value);
     }
 };
 
-template<class Binop, typename T, bool native>
-struct exclusive_scan<Binop, T, 1, native>
+template<class Params, bool native>
+struct exclusive_scan<Params, 1, native>
 {
-    using binop_t = Binop;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
     using op_t = subgroup::impl::exclusive_scan<binop_t, native>;
 
-    T operator()(NBL_CONST_REF_ARG(T) value)
+    type_t operator()(NBL_CONST_REF_ARG(type_t) value)
     {
         op_t op;
         return op(value);
     }
 };
 
-template<class Binop, typename T, bool native>
-struct reduction<Binop, T, 1, native>
+template<class Params, bool native>
+struct reduction<Params, 1, native>
 {
-    using binop_t = Binop;
+    using type_t = typename Params::type_t;
+    using scalar_t = typename Params::scalar_t;
+    using binop_t = typename Params::binop_t;
     using op_t = subgroup::impl::reduction<binop_t, native>;
 
-    T operator()(NBL_CONST_REF_ARG(T) value)
+    scalar_t operator()(NBL_CONST_REF_ARG(type_t) value)
     {
         op_t op;
         return op(value);
