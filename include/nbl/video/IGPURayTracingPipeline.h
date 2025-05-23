d042f42597fb6e12f9be04bb045145934de09d08
kevyuu <kevin.kayu@gmail.com>
2025-05-23 20:26:30 +0700

    Add some utility function to IGPURayTracingPipeline SShaderGroup

--- include/nbl/video/IGPURayTracingPipeline.h
+++ include/nbl/video/IGPURayTracingPipeline.h
@@ -38,6 +38,34 @@ class IGPURayTracingPipeline :  public IGPUPipeline<asset::IRayTracingPipeline<c
                     return 1 + hits.size() + misses.size() + callables.size();
                 }
 
+                inline uint32_t getMissShaderCount() const
+                {
+                    auto count = 0; 
+                    for (const auto& miss : misses)
+                        count += (miss.shader != nullptr);
+                    return count;
+                }
+
+                inline uint32_t getHitShaderCount() const
+                {
+                    auto count = 0; 
+                    for (const auto& hit : hits)
+                    {
+                        count += (hit.closestHit.shader != nullptr);
+                        count += (hit.anyHit.shader != nullptr);
+                        count += (hit.intersection.shader != nullptr);
+                    }
+                    return count;
+                }
+
+                inline uint32_t getCallableShaderCount() const
+                {
+                    auto count = 0; 
+                    for (const auto& callable : callables)
+                        count += (callable.shader != nullptr ? 1 : 0);
+                    return count;
+                }
+
             };
 
             IGPUPipelineLayout* layout = nullptr;
