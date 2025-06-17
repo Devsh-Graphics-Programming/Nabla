11df7a6b89ae6d79c2b6b42e059daa4d069c96ce
kevyuu <kevin.kayu@gmail.com>
2025-06-17 14:00:48 +0700

    Initial refinement for IDescriptorSet::valid()

--- include/nbl/asset/ICPUDescriptorSet.h
+++ include/nbl/asset/ICPUDescriptorSet.h
@@ -79,6 +79,34 @@ class NBL_API2 ICPUDescriptorSet final : public IDescriptorSet<ICPUDescriptorSet
 
 		inline bool valid() const override {
 			if (!m_layout->valid()) return false;
+			for (auto type_i = 0u; type_i < static_cast<uint32_t>(IDescriptor::E_TYPE::ET_COUNT); type_i++)
+			{
+				const auto descriptorType = static_cast<IDescriptor::E_TYPE>(type_i);
+				const auto descriptorCategory = IDescriptor::GetTypeCategory(descriptorType);
+				const auto& descriptorRedirect = m_layout->getDescriptorRedirect(descriptorType);
+				const auto& descriptorInfoArr = m_descriptorInfos[type_i];
+
+				if (descriptorInfoArr->size() != descriptorRedirect.getTotalCount()) return false;
+
+				auto offset = 0;
+				for (auto binding_i = 0; binding_i < descriptorRedirect.getBindingCount(); binding_i++)
+				{
+					const auto storageIndex = IDescriptorSetLayoutBase::CBindingRedirect::storage_range_index_t(binding_i);
+					const auto descriptorCount = descriptorRedirect.getCount(storageIndex);
+					const auto createFlags = descriptorRedirect.getCreateFlags(storageIndex);
+					const auto isPartiallyBound = !createFlags.hasFlags(IDescriptorSetLayoutBase::SBindingBase::E_CREATE_FLAGS::ECF_PARTIALLY_BOUND_BIT);
+					for (auto descriptor_i = 0; descriptor_i < descriptorCount; descriptor_i++)
+					{
+						const auto storageOffset = IDescriptorSetLayoutBase::CBindingRedirect::storage_offset_t(offset);
+						const auto& descriptorInfo = descriptorInfoArr->operator[](offset);
+
+						// partiallyBound layout can have null descriptor, otherwise not
+						if (!isPartiallyBound && !descriptorInfo.desc) return false;
+						if (descriptorInfo.desc && descriptorInfo.desc->getTypeCategory() != descriptorCategory) return false;
+					}
+				}
+			}
+
 			return true;
 		}
 
