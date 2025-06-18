62353af6e25e9f49ef36a9a120b65a9aefe4843d
keptsecret <sorchon@gmail.com>
2025-06-18 15:17:56 +0700

    generate params struct string for subgroup

--- include/nbl/builtin/hlsl/subgroup2/arithmetic_portability.hlsl
+++ include/nbl/builtin/hlsl/subgroup2/arithmetic_portability.hlsl
@@ -9,8 +9,6 @@
 
 #include "nbl/builtin/hlsl/subgroup2/ballot.hlsl"
 #include "nbl/builtin/hlsl/subgroup2/arithmetic_portability_impl.hlsl"
-#include "nbl/builtin/hlsl/concepts.hlsl"
-
 
 namespace nbl
 {
@@ -18,20 +16,6 @@ namespace hlsl
 {
 namespace subgroup2
 {
-
-template<typename Config, class BinOp, int32_t _ItemsPerInvocation=1, class device_capabilities=void NBL_PRIMARY_REQUIRES(is_configuration_v<Config> && is_scalar_v<typename BinOp::type_t>)
-struct ArithmeticParams
-{
-    using config_t = Config;
-    using binop_t = BinOp;
-    using scalar_t = typename BinOp::type_t;
-    using type_t = vector<scalar_t, _ItemsPerInvocation>;
-    using device_traits = device_capabilities_traits<device_capabilities>;
-
-    NBL_CONSTEXPR_STATIC_INLINE int32_t ItemsPerInvocation = _ItemsPerInvocation;
-    NBL_CONSTEXPR_STATIC_INLINE bool UseNativeIntrinsics = device_capabilities_traits<device_capabilities>::shaderSubgroupArithmetic /*&& /*some heuristic for when its faster*/;
-    // TODO add a IHV enum to device_capabilities_traits to check !is_nvidia
-};
 
 template<typename Params>
 struct reduction : impl::reduction<Params,typename Params::binop_t,Params::ItemsPerInvocation,Params::UseNativeIntrinsics> {};
