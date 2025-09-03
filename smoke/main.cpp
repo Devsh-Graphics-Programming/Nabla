e2b2cb89cf4e60b8c567722aa6f8ac3b4ada4fdf
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2025-09-03 17:56:24 +0200

    CDB and WER integration, more logs, PCH and some minor updates

--- smoke/main.cpp
+++ smoke/main.cpp
@@ -1,43 +1,49 @@
-#include <nabla.h>
-#include "nbl/system/IApplicationFramework.h"
-
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <algorithm>
+#define ENABLE_SMOKE
 
 using namespace nbl;
 using namespace nbl::system;
 using namespace nbl::core;
 using namespace nbl::asset;
 
+#ifdef ENABLE_SMOKE
+
 class Smoke final : public system::IApplicationFramework
 {
-	using base_t = system::IApplicationFramework;
+    using base_t = system::IApplicationFramework;
 
 public:
-	using base_t::base_t;
+    using base_t::base_t;
 
-	bool onAppInitialized(smart_refctd_ptr<ISystem>&& system) override
-	{
-		const auto argc = argv.size();
+    bool onAppInitialized(smart_refctd_ptr<ISystem>&& system) override
+    {
+        const char* sdk = std::getenv("NBL_INSTALL_DIRECTORY");
 
-		if (isAPILoaded())
-		{
-			std::cout << "Loaded Nabla API";
-		}
-		else
-		{
-			std::cerr << "Could not load Nabla API, terminating!";
-			return false;
-		}
+        if (sdk)
+        {
+            auto dir = std::filesystem::absolute(std::filesystem::path(sdk).make_preferred()).string();
+            std::cout << "[INFO]: NBL_INSTALL_DIRECTORY = \"" << dir.c_str() << "\"\n";
+        }
+        else
+            std::cerr << "[INFO]: NBL_INSTALL_DIRECTORY env was not defined!\n";
 
-		return true;
-	}
+        if (isAPILoaded())
+        {
+            std::cout << "[INFO]: Loaded Nabla API\n";
+        }
+        else
+        {
+            std::cerr << "[ERROR]: Could not load Nabla API, terminating!\n";
+            return false;
+        }
 
-	void workLoopBody() override {}
+        return true;
+    }
 
-	bool keepRunning() override { return false; }
+    void workLoopBody() override {}
+    bool keepRunning() override { return false; }
 };
 
 NBL_MAIN_FUNC(Smoke)
+#else
+int main() { return 0; }
+#endif
