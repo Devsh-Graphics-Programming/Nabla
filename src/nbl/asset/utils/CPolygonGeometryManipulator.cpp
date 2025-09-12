ede1794490bde99296d8ac0fe9e8886e93644b7d
devsh <devsh@devsh.eu>
2025-09-12 17:16:53 +0200

    generalize `CPolygonGeometryManipulator::createUnweldedList` for all Ngons

--- src/nbl/asset/utils/CPolygonGeometryManipulator.cpp
+++ src/nbl/asset/utils/CPolygonGeometryManipulator.cpp
@@ -18,40 +18,45 @@ namespace nbl::asset
 core::smart_refctd_ptr<ICPUPolygonGeometry> CPolygonGeometryManipulator::createUnweldedList(const ICPUPolygonGeometry* inGeo)
 {
     const auto* indexing = inGeo->getIndexingCallback();
-    if (!indexing) return nullptr;
-    if (indexing->degree() != 3) return nullptr;
+    if (!indexing)
+        return nullptr;
 
     const auto indexView = inGeo->getIndexView();
     const auto primCount = inGeo->getPrimitiveCount();
+    const uint8_t degree = indexing->degree();
+    const auto outIndexCount = primCount*degree;
+    if (outIndexCount<primCount)
+        return nullptr;
 
     const auto outGeometry = core::move_and_static_cast<ICPUPolygonGeometry>(inGeo->clone(0u));
 
     auto* outGeo = outGeometry.get();
-    outGeo->setIndexing(IPolygonGeometryBase::TriangleList());
+    outGeo->setIndexing(IPolygonGeometryBase::NGonList(degree));
 
     auto createOutView = [&](const ICPUPolygonGeometry::SDataView& inView) -> ICPUPolygonGeometry::SDataView
-      {
-        if (!inView) return {};
-        auto buffer = ICPUBuffer::create({ inGeo->getPrimitiveCount() * indexing->degree() * inView.composed.stride , inView.src.buffer->getUsageFlags() });
+    {
+        if (!inView)
+            return {};
+        auto buffer = ICPUBuffer::create({ outIndexCount*inView.composed.stride , inView.src.buffer->getUsageFlags() });
         return {
-          .composed = inView.composed,
-          .src = {.offset = 0, .size = buffer->getSize(), .buffer = std::move(buffer)}
+            .composed = inView.composed,
+            .src = {.offset = 0, .size = buffer->getSize(), .buffer = std::move(buffer)}
         };
-      };
+    };
 
     const auto inIndexView = inGeo->getIndexView();
     auto outIndexView = createOutView(inIndexView);
     auto indexBuffer = outIndexView.src.buffer;
     const auto indexSize = inIndexView.composed.stride;
-    std::byte* outIndexes = reinterpret_cast<std::byte*>(outIndexView.getPointer());
+    std::byte* outIndices = reinterpret_cast<std::byte*>(outIndexView.getPointer());
     outGeo->setIndexView({});
 
     const auto inVertexView = inGeo->getPositionView();
     auto outVertexView = createOutView(inVertexView);
     auto vertexBuffer = outVertexView.src.buffer;
     const auto vertexSize = inVertexView.composed.stride;
-    const std::byte* inVertexes = reinterpret_cast<const std::byte*>(inVertexView.getPointer());
-    std::byte* const outVertexes = reinterpret_cast<std::byte*>(vertexBuffer->getPointer());
+    const std::byte* inVertices = reinterpret_cast<const std::byte*>(inVertexView.getPointer());
+    std::byte* const outVertices = reinterpret_cast<std::byte*>(vertexBuffer->getPointer());
     outGeo->setPositionView(std::move(outVertexView));
 
     const auto inNormalView = inGeo->getNormalView();
@@ -63,68 +68,67 @@ core::smart_refctd_ptr<ICPUPolygonGeometry> CPolygonGeometryManipulator::createU
     outGeometry->getJointWeightViews()->resize(inGeo->getJointWeightViews().size());
     for (uint64_t jointView_i = 0u; jointView_i < inGeo->getJointWeightViews().size(); jointView_i++)
     {
-      auto& inJointWeightView = inGeo->getJointWeightViews()[jointView_i];
-      auto& outJointWeightView = outGeometry->getJointWeightViews()->operator[](jointView_i);
-      outJointWeightView.indices = createOutView(inJointWeightView.indices);
-      outJointWeightView.weights = createOutView(inJointWeightView.weights);
+        auto& inJointWeightView = inGeo->getJointWeightViews()[jointView_i];
+        auto& outJointWeightView = outGeometry->getJointWeightViews()->operator[](jointView_i);
+        outJointWeightView.indices = createOutView(inJointWeightView.indices);
+        outJointWeightView.weights = createOutView(inJointWeightView.weights);
     }
 
     outGeometry->getAuxAttributeViews()->resize(inGeo->getAuxAttributeViews().size());
     for (uint64_t auxView_i = 0u; auxView_i < inGeo->getAuxAttributeViews().size(); auxView_i++)
-    {
-      outGeo->getAuxAttributeViews()->operator[](auxView_i) = createOutView(inGeo->getAuxAttributeViews()[auxView_i]);
-    }
+        outGeo->getAuxAttributeViews()->operator[](auxView_i) = createOutView(inGeo->getAuxAttributeViews()[auxView_i]);
 
+    std::array<uint32_t,255> indices;
     for (uint64_t prim_i = 0u; prim_i < primCount; prim_i++)
     {
-      hlsl::uint32_t3 indexes;
-      IPolygonGeometryBase::IIndexingCallback::SContext<uint32_t> context{
-        .indexBuffer = indexView.getPointer(),
-        .indexSize = indexView.composed.stride,
-        .beginPrimitive = prim_i,
-        .endPrimitive = prim_i + 1,
-        .out = &indexes
-      };
-      indexing->operator()(context);
-      for (uint64_t primIndex_i = 0u; primIndex_i < indexing->degree(); primIndex_i++)
-      {
-        const auto outIndex = prim_i * indexing->degree() + primIndex_i;
-        const auto inIndex = indexes[primIndex_i];
-        memcpy(outIndexes + outIndex * indexSize, &outIndex, indexSize);
-        memcpy(outVertexes + outIndex * vertexSize, inVertexes + inIndex * vertexSize, vertexSize);
-        if (inNormalView)
+        IPolygonGeometryBase::IIndexingCallback::SContext<uint32_t> context{
+            .indexBuffer = indexView.getPointer(),
+            .indexSize = indexView.composed.stride,
+            .beginPrimitive = prim_i,
+            .endPrimitive = prim_i + 1,
+            .out = indices.data()
+        };
+        indexing->operator()(context);
+        for (uint8_t primIndex_i=0; primIndex_i<degree; primIndex_i++)
         {
-          std::byte* const outNormals = reinterpret_cast<std::byte*>(outNormalBuffer->getPointer());
-          const auto normalSize = inNormalView.composed.stride;
-          memcpy(outNormals + outIndex * normalSize, inNormals + inIndex * normalSize, normalSize);
+            const auto outIndex = prim_i * degree + primIndex_i;
+            const auto inIndex = indices[primIndex_i];
+            // TODO: these memcpys from view to view could really be DRY-ed and lambdified
+            memcpy(outIndices + outIndex * indexSize, &outIndex, indexSize);
+            memcpy(outVertices + outIndex * vertexSize, inVertices + inIndex * vertexSize, vertexSize);
+            if (inNormalView)
+            {
+                std::byte* const outNormals = reinterpret_cast<std::byte*>(outNormalBuffer->getPointer());
+                const auto normalSize = inNormalView.composed.stride;
+                memcpy(outNormals + outIndex * normalSize, inNormals + inIndex * normalSize, normalSize);
+            }
+
+            for (uint64_t jointView_i = 0u; jointView_i < inGeo->getJointWeightViews().size(); jointView_i++)
+            {
+                auto& inView = inGeo->getJointWeightViews()[jointView_i];
+                auto& outView = outGeometry->getJointWeightViews()->operator[](jointView_i);
+
+                const std::byte* const inJointIndices = reinterpret_cast<const std::byte*>(inView.indices.getPointer());
+                const auto jointIndexSize = inView.indices.composed.stride;
+                std::byte* const outJointIndices = reinterpret_cast<std::byte*>(outView.indices.getPointer());
+                memcpy(outJointIndices + outIndex * jointIndexSize, inJointIndices + inIndex * jointIndexSize, jointIndexSize);
+
+                const std::byte* const inWeights = reinterpret_cast<const std::byte*>(inView.weights.getPointer());
+                const auto jointWeightSize = inView.weights.composed.stride;
+                std::byte* const outWeights = reinterpret_cast<std::byte*>(outView.weights.getPointer());
+                memcpy(outWeights + outIndex * jointWeightSize, outWeights + inIndex * jointWeightSize, jointWeightSize);
+            }
+
+            for (uint64_t auxView_i = 0u; auxView_i < inGeo->getAuxAttributeViews().size(); auxView_i++)
+            {
+                auto& inView = inGeo->getAuxAttributeViews()[auxView_i];
+                auto& outView = outGeometry->getAuxAttributeViews()->operator[](auxView_i);
+                const auto attrSize = inView.composed.stride;
+                const std::byte* const inAuxs = reinterpret_cast<const std::byte*>(inView.getPointer());
+                std::byte* const outAuxs = reinterpret_cast<std::byte*>(outView.getPointer());
+                memcpy(outAuxs + outIndex * attrSize, inAuxs + inIndex * attrSize, attrSize);
+            }
         }
-
-        for (uint64_t jointView_i = 0u; jointView_i < inGeo->getJointWeightViews().size(); jointView_i++)
-        {
-          auto& inView = inGeo->getJointWeightViews()[jointView_i];
-          auto& outView = outGeometry->getJointWeightViews()->operator[](jointView_i);
-
-          const std::byte* const inJointIndices = reinterpret_cast<const std::byte*>(inView.indices.getPointer());
-          const auto jointIndexSize = inView.indices.composed.stride;
-          std::byte* const outJointIndices = reinterpret_cast<std::byte*>(outView.indices.getPointer());
-          memcpy(outJointIndices + outIndex * jointIndexSize, inJointIndices + inIndex * jointIndexSize, jointIndexSize);
-
-          const std::byte* const inWeights = reinterpret_cast<const std::byte*>(inView.weights.getPointer());
-          const auto jointWeightSize = inView.weights.composed.stride;
-          std::byte* const outWeights = reinterpret_cast<std::byte*>(outView.weights.getPointer());
-          memcpy(outWeights + outIndex * jointWeightSize, outWeights + inIndex * jointWeightSize, jointWeightSize);
-        }
-
-        for (uint64_t auxView_i = 0u; auxView_i < inGeo->getAuxAttributeViews().size(); auxView_i++)
-        {
-          auto& inView = inGeo->getAuxAttributeViews()[auxView_i];
-          auto& outView = outGeometry->getAuxAttributeViews()->operator[](auxView_i);
-          const auto attrSize = inView.composed.stride;
-          const std::byte* const inAuxs = reinterpret_cast<const std::byte*>(inView.getPointer());
-          std::byte* const outAuxs = reinterpret_cast<std::byte*>(outView.getPointer());
-          memcpy(outAuxs + outIndex * attrSize, inAuxs + inIndex * attrSize, attrSize);
-        }
-      }
     }
 
     recomputeContentHashes(outGeo);
