3a161ab03bc7adba045c275145fe90f04089f63b
CrabeExtra <abbasgaroosi7@gmail.com>
2025-10-02 12:00:31 +0330

    created a structured like fft

---
+++ include/nbl/builtin/hlsl/workgroup/bitonic_sort.hlsl
@@ -0,0 +1,23 @@
+#include <nbl/builtin/hlsl/cpp_compat.hlsl>
+#include <nbl/builtin/hlsl/concepts.hlsl>
+#include <nbl/builtin/hlsl/bitonic_sort/common.hlsl>
+
+#ifndef _NBL_BUILTIN_HLSL_WORKGROUP_BITONIC_SORT_INCLUDED_
+#define _NBL_BUILTIN_HLSL_WORKGROUP_BITONIC_SORT_INCLUDED_
+
+// ------------------------------- COMMON -----------------------------------------
+
+namespace nbl
+{
+    namespace hlsl
+    {
+        namespace workgroup
+        {
+			namespace bitonic_sort
+            {
+            }
+        }
+    }
+}
+
+#endif
