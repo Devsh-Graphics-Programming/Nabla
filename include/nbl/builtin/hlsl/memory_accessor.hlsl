ef7edd51e4b529dcf11b98ff090ae36e153d0e07
Abbas Garousi <69919151+CrabExtra@users.noreply.github.com>
2025-11-05 19:23:16 +0330

    Remove unused pair struct from memory_accessor.hlsl
    Removed commented-out template struct for pair.

--- include/nbl/builtin/hlsl/memory_accessor.hlsl
+++ include/nbl/builtin/hlsl/memory_accessor.hlsl
@@ -22,18 +22,6 @@ namespace nbl
 {
 namespace hlsl
 {
-
-// TODO: flesh out and move to `nbl/builtin/hlsl/utility.hlsl`
-//template<typename T1, typename T2>
-//struct pair
-//{
-//    using first_type = T1;
-//    using second_type = T2;
-//
-//    first_type first;
-//    second_type second;
-//};
-
 namespace accessor_adaptors
 {
 namespace impl
