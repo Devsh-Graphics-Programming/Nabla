5db92d301cd284069ca00b4b81462ed8767b82a8
Przemog1 <minikers21@gmail.com>
2025-11-27 14:08:26 +0100

    Disabled extensions not supported by llvmpipe

--- src/nbl/video/CVulkanPhysicalDevice.cpp
+++ src/nbl/video/CVulkanPhysicalDevice.cpp
@@ -1892,6 +1892,10 @@ core::smart_refctd_ptr<ILogicalDevice> CVulkanPhysicalDevice::createLogicalDevic
                 extensionStrings[i++] = feature.c_str();
         }
 
+        // remove inheritedQueries since llvmpipe doesn't support it
+        vk_deviceFeatures2.features.inheritedQueries = VK_FALSE;
+        //vk_deviceFeatures2.features = VkPhysicalDeviceFeatures{};
+
         // Create Device
         VkDeviceCreateInfo vk_createInfo = { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
         vk_createInfo.pNext = &vk_deviceFeatures2;
@@ -1913,7 +1917,32 @@ core::smart_refctd_ptr<ILogicalDevice> CVulkanPhysicalDevice::createLogicalDevic
         }
         vk_createInfo.queueCreateInfoCount = queueCreateInfos.size();
         vk_createInfo.pQueueCreateInfos = queueCreateInfos.data();
-    
+        
+        // erase extensions not supported by llvm
+        {
+            core::vector<const char*> llvmUnsupportedExtensions = {
+                "VK_NV_shader_image_footprint",
+                "VK_KHR_fragment_shader_barycentric",
+                "VK_EXT_conservative_rasterization",
+                "VK_KHR_external_memory_win32",
+                "VK_KHR_shader_subgroup_uniform_control_flow",
+                "VK_EXT_pci_bus_info",
+                "VK_KHR_external_fence_win32",
+                "VK_KHR_external_semaphore_win32",
+                "VK_KHR_win32_keyed_mutex",
+                "VK_EXT_discard_rectangles"
+                "VK_NV_compute_shader_derivatives",
+                "VK_NV_shader_sm_builtins"
+            };
+
+            for (const char* extension : llvmUnsupportedExtensions)
+            {
+                auto it = std::find_if(extensionStrings.begin(), extensionStrings.end(), [extension](const char* s) { return std::strcmp(s, extension) == 0; });
+                if(it != extensionStrings.end())
+                    extensionStrings.erase(it);
+            }
+        }
+
         vk_createInfo.enabledExtensionCount = static_cast<uint32_t>(extensionStrings.size());
         vk_createInfo.ppEnabledExtensionNames = extensionStrings.data();
         
