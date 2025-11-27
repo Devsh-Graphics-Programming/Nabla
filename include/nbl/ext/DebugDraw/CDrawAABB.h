a40f5402a70c38c1e509d61ca0186e039b1ce5cf
keptsecret <sorchon@gmail.com>
2025-11-27 16:58:28 +0700

    some fixes to draw aabb

--- include/nbl/ext/DebugDraw/CDrawAABB.h
+++ include/nbl/ext/DebugDraw/CDrawAABB.h
@@ -2,21 +2,21 @@
 // This file is part of the "Nabla Engine".
 // For conditions of distribution and use, see copyright notice in nabla.h
 
-#ifndef _NBL_EXT_DRAW_AABB_H_
-#define _NBL_EXT_DRAW_AABB_H_
+#ifndef _NBL_EXT_DEBUG_DRAW_DRAW_AABB_H_
+#define _NBL_EXT_DEBUG_DRAW_DRAW_AABB_H_
 
 #include "nbl/video/declarations.h"
 #include "nbl/builtin/hlsl/cpp_compat.hlsl"
 #include "nbl/builtin/hlsl/shapes/aabb.hlsl"
+#include "nbl/builtin/hlsl/math/linalg/fast_affine.hlsl"
 #include "nbl/ext/DebugDraw/builtin/hlsl/common.hlsl"
 
 namespace nbl::ext::debug_draw
 {
-class DrawAABB final : public core::IReferenceCounted
-{
+    class DrawAABB final : public core::IReferenceCounted
+    {
     public:
         static constexpr inline uint32_t IndicesCount = 24u;
-        static constexpr inline uint32_t VerticesCount = 8u;
 
         enum DrawMode : uint16_t
         {
@@ -39,7 +39,8 @@ class DrawAABB final : public core::IReferenceCounted
             //! optional, default MDI buffer allocated if not provided
             core::smart_refctd_ptr<streaming_buffer_t> streamingBuffer = nullptr;
         };
-        
+
+        // only used to make the 24 element index buffer and instanced pipeline on create
         struct SCreationParameters : SCachedCreationParameters
         {
             video::IQueue* transfer = nullptr;
@@ -48,6 +49,29 @@ class DrawAABB final : public core::IReferenceCounted
             core::smart_refctd_ptr<video::IGPUPipelineLayout> singlePipelineLayout;
             core::smart_refctd_ptr<video::IGPUPipelineLayout> batchPipelineLayout;
             core::smart_refctd_ptr<video::IGPURenderpass> renderpass = nullptr;
+
+            inline bool validate() const
+            {
+                const auto validation = std::to_array
+                ({
+                    std::make_pair(bool(assetManager), "Invalid `creationParams.assetManager` is nullptr!"),
+                    std::make_pair(bool(assetManager->getSystem()), "Invalid `creationParams.assetManager->getSystem()` is nullptr!"),
+                    std::make_pair(bool(utilities), "Invalid `creationParams.utilities` is nullptr!"),
+                    std::make_pair(bool(transfer), "Invalid `creationParams.transfer` is nullptr!"),
+                    std::make_pair(bool(renderpass), "Invalid `creationParams.renderpass` is nullptr!"),
+                    (assetManager && utilities && transfer && renderpass) ? std::make_pair(bool(utilities->getLogicalDevice()->getPhysicalDevice()->getQueueFamilyProperties()[transfer->getFamilyIndex()].queueFlags.hasFlags(video::IQueue::FAMILY_FLAGS::TRANSFER_BIT)), "Invalid `creationParams.transfer` is not capable of transfer operations!") : std::make_pair(false, "Pass valid required DrawAABB::S_CREATION_PARAMETERS!")
+                    });
+
+                system::logger_opt_ptr logger = utilities->getLogger();
+                for (const auto& [ok, error] : validation)
+                    if (!ok)
+                    {
+                        logger.log(error, system::ILogger::ELL_ERROR);
+                        return false;
+                    }
+
+                return true;
+            }
         };
 
         // creates an instance that can draw one AABB via push constant or multiple using streaming buffer
@@ -67,7 +91,73 @@ class DrawAABB final : public core::IReferenceCounted
         // records draw command for single AABB, user has to set pipeline outside
         bool renderSingle(video::IGPUCommandBuffer* commandBuffer, const hlsl::shapes::AABB<3, float>& aabb, const hlsl::float32_t4& color, const hlsl::float32_t4x4& cameraMat);
 
-        bool render(video::IGPUCommandBuffer* commandBuffer, video::ISemaphore::SWaitInfo waitInfo, std::span<const InstanceData> aabbInstances, const hlsl::float32_t4x4& cameraMat);
+        // records draw command for rendering batch of AABB instances as InstanceData
+        // user has to set span of filled-in InstanceData; camera matrix used in push constant
+        inline bool render(video::IGPUCommandBuffer* commandBuffer, video::ISemaphore::SWaitInfo waitInfo, std::span<const InstanceData> aabbInstances, const hlsl::float32_t4x4& cameraMat)
+        {
+            if (!(m_cachedCreationParams.drawMode & ADM_DRAW_BATCH))
+            {
+                m_cachedCreationParams.utilities->getLogger()->log("DrawAABB has not been enabled for draw batches!", system::ILogger::ELL_ERROR);
+                return false;
+            }
+
+            using offset_t = SCachedCreationParameters::streaming_buffer_t::size_type;
+            constexpr auto MdiSizes = std::to_array<offset_t>({ sizeof(hlsl::float32_t3), sizeof(InstanceData) });
+            // shared nPoT alignment needs to be divisible by all smaller ones to satisfy an allocation from all
+            constexpr offset_t MaxAlignment = std::reduce(MdiSizes.begin(), MdiSizes.end(), 1, [](const offset_t a, const offset_t b)->offset_t {return std::lcm(a, b); });
+            // allocator initialization needs us to round up to PoT
+            const auto MaxPOTAlignment = hlsl::roundUpToPoT(MaxAlignment);
+
+            auto* streaming = m_cachedCreationParams.streamingBuffer.get();
+
+            auto* const streamingPtr = reinterpret_cast<uint8_t*>(streaming->getBufferPointer());
+            assert(streamingPtr);
+
+            commandBuffer->bindGraphicsPipeline(m_batchPipeline.get());
+            commandBuffer->setLineWidth(1.f);
+            asset::SBufferBinding<video::IGPUBuffer> indexBinding = { .offset = 0, .buffer = m_indicesBuffer };
+            commandBuffer->bindIndexBuffer(indexBinding, asset::EIT_32BIT);
+
+            std::vector<InstanceData> instances(aabbInstances.size());
+            for (uint32_t i = 0; i < aabbInstances.size(); i++)
+            {
+                auto& inst = instances[i];
+                inst = aabbInstances[i];
+                inst.transform = hlsl::mul(cameraMat, inst.transform);
+            }
+
+            auto instancesIt = instances.begin();
+            const uint32_t instancesPerIter = streaming->getBuffer()->getSize() / sizeof(InstanceData);
+            using suballocator_t = core::LinearAddressAllocatorST<offset_t>;
+            while (instancesIt != instances.end())
+            {
+                const uint32_t instanceCount = hlsl::min<uint32_t>(instancesPerIter, instances.size());
+                offset_t inputOffset = 0u;
+                offset_t ImaginarySizeUpperBound = 0x1 << 30;
+                suballocator_t imaginaryChunk(nullptr, inputOffset, 0, hlsl::roundUpToPoT(MaxAlignment), ImaginarySizeUpperBound);
+                uint32_t instancesByteOffset = imaginaryChunk.alloc_addr(sizeof(InstanceData) * instanceCount, sizeof(InstanceData));
+                const uint32_t totalSize = imaginaryChunk.get_allocated_size();
+
+                inputOffset = SCachedCreationParameters::streaming_buffer_t::invalid_value;
+                std::chrono::steady_clock::time_point waitTill = std::chrono::steady_clock::now() + std::chrono::milliseconds(1u);
+                streaming->multi_allocate(waitTill, 1, &inputOffset, &totalSize, &MaxAlignment);
+
+                memcpy(streamingPtr + instancesByteOffset, std::addressof(*instancesIt), sizeof(InstanceData) * instanceCount);
+                instancesIt += instanceCount;
+
+                assert(!streaming->needsManualFlushOrInvalidate());
+
+                SPushConstants pc;
+                pc.pInstanceBuffer = m_cachedCreationParams.streamingBuffer->getBuffer()->getDeviceAddress() + instancesByteOffset;
+
+                commandBuffer->pushConstants(m_batchPipeline->getLayout(), asset::IShader::E_SHADER_STAGE::ESS_VERTEX, 0, sizeof(SPushConstants), &pc);
+                commandBuffer->drawIndexed(IndicesCount, instanceCount, 0, 0, 0);
+
+                streaming->multi_deallocate(1, &inputOffset, &totalSize, waitInfo);
+            }
+
+            return true;
+        }
 
         static hlsl::float32_t4x4 getTransformFromAABB(const hlsl::shapes::AABB<3, float>& aabb);
 
@@ -77,7 +167,7 @@ class DrawAABB final : public core::IReferenceCounted
 	    ~DrawAABB() override;
 
     private:
-        static bool validateCreationParameters(SCreationParameters& params);
+        //static bool validateCreationParameters(SCreationParameters& params);
         static core::smart_refctd_ptr<video::IGPUGraphicsPipeline> createPipeline(SCreationParameters& params, const video::IGPUPipelineLayout* pipelineLayout, const std::string& vsPath, const std::string& fsPath);
         static bool createStreamingBuffer(SCreationParameters& params);
         static core::smart_refctd_ptr<video::IGPUBuffer> createIndicesBuffer(SCreationParameters& params);
