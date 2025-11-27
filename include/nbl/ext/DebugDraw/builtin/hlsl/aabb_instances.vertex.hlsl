0879ce7b6fc300efe18e88269100e382de0c2749
keptsecret <sorchon@gmail.com>
2025-11-27 15:47:52 +0700

    fix + optimize aabb vertex calc, includes

--- include/nbl/ext/DebugDraw/builtin/hlsl/aabb_instances.vertex.hlsl
+++ include/nbl/ext/DebugDraw/builtin/hlsl/aabb_instances.vertex.hlsl
@@ -1,8 +1,5 @@
 #pragma shader_stage(vertex)
 
-#include "nbl/builtin/hlsl/math/linalg/fast_affine.hlsl"
-#include "nbl/builtin/hlsl/glsl_compat/core.hlsl"
-#include "nbl/builtin/hlsl/bda/__ptr.hlsl"
 #include "nbl/ext/DebugDraw/builtin/hlsl/common.hlsl"
 
 using namespace nbl::hlsl;
@@ -13,19 +10,8 @@ using namespace nbl::ext::debug_draw;
 [shader("vertex")]
 PSInput main()
 {
-    const float32_t3 unitAABBVertices[8] = {
-        float32_t3(0.0, 0.0, 0.0),
-        float32_t3(1.0, 0.0, 0.0),
-        float32_t3(0.0, 0.0, 1.0),
-        float32_t3(1.0, 0.0, 1.0),
-        float32_t3(0.0, 1.0, 0.0),
-        float32_t3(1.0, 1.0, 0.0),
-        float32_t3(0.0, 1.0, 1.0),
-        float32_t3(1.0, 1.0, 1.0)
-    };
-
     PSInput output;
-    float32_t3 vertex = unitAABBVertices[glsl::gl_VertexIndex()];
+    const float32_t3 vertex = getUnitAABBVertex();
     InstanceData instance = vk::BufferPointer<InstanceData>(pc.pInstanceBuffer + sizeof(InstanceData) * glsl::gl_InstanceIndex()).Get();
 
     output.position = math::linalg::promoted_mul(instance.transform, vertex);
