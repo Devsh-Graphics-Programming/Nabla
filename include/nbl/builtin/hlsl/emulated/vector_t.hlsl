6683cd5a0f7965caa8484ea40b3847bab23b54a0
kevyuu <kevin.kayu@gmail.com>
2025-12-01 18:42:29 +0700

    Remove commented code on emulated/vector_t.hlsl

--- include/nbl/builtin/hlsl/emulated/vector_t.hlsl
+++ include/nbl/builtin/hlsl/emulated/vector_t.hlsl
@@ -40,9 +40,6 @@ struct _2_component_vec
             return y;
 
         // TODO: avoid code duplication, make it constexpr
-        //using TAsUint = typename unsigned_integer_of_size<sizeof(T)>::type;
-        //TAsUint invalidComponentValue = nbl::hlsl::_static_cast<TAsUint>(0xdeadbeefbadcaffeull);
-        //return nbl::hlsl::bit_cast<T>(invalidComponentValue);
         return nbl::hlsl::undef<T>();
     }
 
@@ -77,9 +74,6 @@ struct _3_component_vec
             return z;
 
         // TODO: avoid code duplication, make it constexpr
-        //using TAsUint = typename unsigned_integer_of_size<sizeof(T)>::type;
-        //TAsUint invalidComponentValue = nbl::hlsl::_static_cast<TAsUint>(0xdeadbeefbadcaffeull >> (64 - sizeof(T) * 8));
-        //return nbl::hlsl::bit_cast<T>(invalidComponentValue);
         return nbl::hlsl::undef<T>();
     }
 
@@ -118,9 +112,6 @@ struct _4_component_vec
             return w;
 
         // TODO: avoid code duplication, make it constexpr
-        //using TAsUint = typename unsigned_integer_of_size<sizeof(T)>::type;
-        //uint64_t invalidComponentValue = nbl::hlsl::_static_cast<TAsUint>(0xdeadbeefbadcaffeull >> (64 - sizeof(T) * 8));
-        //return nbl::hlsl::bit_cast<T>(invalidComponentValue);
         return nbl::hlsl::undef<T>();
     }
 
