62f2c99d01166ea2e077bc73856727d5af016d43
keptsecret <sorchon@gmail.com>
2025-12-16 17:27:55 +0700

    changes to splatting params precompute for the last time

--- include/nbl/builtin/hlsl/rwmc/SplattingParameters.hlsl
+++ include/nbl/builtin/hlsl/rwmc/SplattingParameters.hlsl
@@ -15,18 +15,18 @@ struct SplattingParameters
 {
     using scalar_t = float;
 
-    static SplattingParameters create(const scalar_t base, const scalar_t start)
+    static SplattingParameters create(const scalar_t base, const scalar_t start, const uint32_t cascadeCount)
     {
         SplattingParameters retval;
-        retval.log2Base = hlsl::log2(base);
-        retval.log2Start = hlsl::log2(start);
-        retval.rcpLog2Base = scalar_t(1.0) / retval.log2Base;
-        retval.baseRootOfStart = retval.log2Start * retval.rcpLog2Base;
+        const scalar_t log2Base = hlsl::log2(base);
+        const scalar_t log2Start = hlsl::log2(start);
+        retval.lastCascadeLuma = hlsl::exp2(log2Start + log2Base * (cascadeCount - 1));
+        retval.rcpLog2Base = scalar_t(1.0) / log2Base;
+        retval.baseRootOfStart = log2Start * retval.rcpLog2Base;
         return retval;
     }
 
-    scalar_t log2Base;
-    scalar_t log2Start;
+    scalar_t lastCascadeLuma;
     scalar_t baseRootOfStart;
     scalar_t rcpLog2Base;
 };
