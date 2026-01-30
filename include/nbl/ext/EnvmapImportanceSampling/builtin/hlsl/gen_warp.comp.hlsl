695e667f2db3ada3baf67709fa98b1fbe25c04ca
kevyuu <kevin.kayu@gmail.com>
2026-01-30 15:23:37 +0700

    gen_warpmap to gen_warp

--- include/nbl/ext/EnvmapImportanceSampling/builtin/hlsl/gen_warp.comp.hlsl
+++ include/nbl/ext/EnvmapImportanceSampling/builtin/hlsl/gen_warp.comp.hlsl
@@ -1,13 +1,8 @@
-#include "nbl/ext/EnvmapImportanceSampling/builtin/hlsl/common.hlsl"
 #include "nbl/builtin/hlsl/sampling/hierarchical_image.hlsl"
-
 
 [[vk::binding(0, 0)]] Texture2D<float32_t> lumaMap;
 
-[[vk::binding(1, 0)]] RWTexture2D<float32_t> outImage;
-
-// TODO(kevinyu): Temporary to make nsc compiles
-#define WARPMAP_GEN_WORKGROUP_DIM 16
+[[vk::binding(1, 0)]] RWTexture2D<float32_t2> outImage;
 
 using namespace nbl;
 using namespace nbl::hlsl;
@@ -32,17 +27,19 @@ struct LuminanceAccessor
     }
 };
 
-[numthreads(WARPMAP_GEN_WORKGROUP_DIM, WARPMAP_GEN_WORKGROUP_DIM, 1)]
+[numthreads(WORKGROUP_DIM, WORKGROUP_DIM, 1)]
 [shader("compute")]
 void main(uint32_t3 threadID : SV_DispatchThreadID)
 {
     LuminanceAccessor luminanceAccessor;
     uint32_t lumaMapWidth, lumaMapHeight;
 
+    lumaMap.GetDimensions(lumaMapWidth, lumaMapHeight);
+
     using LuminanceSampler = LuminanceMapSampler<float32_t, LuminanceAccessor>;
 
     LuminanceSampler luminanceSampler = 
-      LuminanceSampler::create(luminanceAccessor, lumaMapWidth, lumaMapHeight, lumaMapWidth != lumaMapHeight);
+      LuminanceSampler::create(luminanceAccessor, uint32_t2(lumaMapWidth, lumaMapHeight), lumaMapWidth != lumaMapHeight, uint32_t2(lumaMapWidth, lumaMapHeight));
 
     uint32_t2 pixelCoord = threadID.xy;
 
