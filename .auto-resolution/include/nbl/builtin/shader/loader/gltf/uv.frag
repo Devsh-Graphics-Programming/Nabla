5c159d07bc001794d4f281d2be5ddb2450a1537a
kevyuu <kevin.kayu@gmail.com>
2026-02-21 13:53:10 +0700

    Fix binarySearch implementation. when last is 2x1 we should check for 0,0 and 1,0 not 0,1

--- .auto-resolution/include/nbl/builtin/shader/loader/gltf/uv.frag
+++
@@ -1,7 +0,0 @@
-#version 430 core
-
-#define _DISABLE_COLOR_ATTRIBUTES
-
-#include "nbl/builtin/shader/loader/gltf/fragment_impl.glsl"
-
-#undef _DISABLE_COLOR_ATTRIBUTES
--- .conflict-side-1/3rdparty/dxc/test/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#if __cplusplus >= 201703L
-#pragma message("'__cplusplus' is " _CRT_STRINGIZE(__cplusplus))
-#error C++ compiler with too high standard for DXC compilation. Your compiler must be capable of using C++11 or C++14 standard!
-#endif 
-
-int main() { return 0; }
