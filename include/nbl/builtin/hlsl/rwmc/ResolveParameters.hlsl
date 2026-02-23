0a85285c3d3618029c662f47dd1a1ec2e05ddd80
keptsecret <sorchon@gmail.com>
2026-02-23 15:55:30 +0700

    added a create param struct for resolve params

--- include/nbl/builtin/hlsl/rwmc/ResolveParameters.hlsl
+++ include/nbl/builtin/hlsl/rwmc/ResolveParameters.hlsl
@@ -15,17 +15,26 @@ struct SResolveParameters
 {
 	using scalar_t = float32_t;
 
-	static SResolveParameters create(scalar_t base, uint32_t sampleCount, scalar_t minReliableLuma, scalar_t kappa)
+	struct SCreateParams
+	{
+		scalar_t minReliableLuma;
+		scalar_t kappa;
+		scalar_t start;
+		scalar_t base;
+		uint32_t sampleCount;
+	};
+
+	static SResolveParameters create(SCreateParams params)
 	{
 		SResolveParameters retval;
-		retval.initialEmin = minReliableLuma;
-		retval.reciprocalBase = 1.f / base;
-		const scalar_t N = scalar_t(sampleCount);
+		retval.initialEmin = params.minReliableLuma;
+		retval.reciprocalBase = 1.f / params.base;
+		const scalar_t N = scalar_t(params.sampleCount);
 		retval.reciprocalN = 1.f / N;
-		retval.reciprocalKappa = 1.f / kappa;
+		retval.reciprocalKappa = 1.f / params.kappa;
 		// if not interested in exact expected value estimation (kappa!=1.f), can usually accept a bit more variance relative to the image brightness we already have
 		// allow up to ~<cascadeBase> more energy in one sample to lessen bias in some cases
-		retval.colorReliabilityFactor = base + (1.f - base) * retval.reciprocalKappa;
+		retval.colorReliabilityFactor = params.base + (1.f - params.base) * retval.reciprocalKappa;
 		retval.NOverKappa = N * retval.reciprocalKappa;
 
 		return retval;
