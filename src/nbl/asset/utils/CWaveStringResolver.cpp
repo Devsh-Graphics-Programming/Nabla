fdea055895dfdd99b8a961074836d5ff791036fa
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2026-03-17 13:39:17 +0100

    Fix NSC preprocess output

--- src/nbl/asset/utils/CWaveStringResolver.cpp
+++ src/nbl/asset/utils/CWaveStringResolver.cpp
@@ -39,6 +39,8 @@
 
 #include "nabla.h"
 #include <boost/exception/diagnostic_information.hpp>
+#include <boost/wave/util/insert_whitespace_detection.hpp>
+#include <optional>
 
 using namespace nbl;
 using namespace nbl::asset;
@@ -115,75 +117,144 @@ std::string makeWaveFailureContext(
 
     return stream.str();
 }
+
+template<typename TokenT>
+bool isWhitespaceLikeToken(const TokenT& token)
+{
+    using namespace boost::wave;
+
+    const auto id = token_id(token);
+    return id == T_NEWLINE || id == T_GENERATEDNEWLINE || id == T_CONTLINE || IS_CATEGORY(token, WhiteSpaceTokenType);
+}
+
+template<typename TokenT>
+std::string tokenValueToString(const TokenT& token)
+{
+    const auto& value = token.get_value();
+    return std::string(value.data(), value.size());
+}
+
+core::string renderPreprocessedOutput(nbl::wave::context& context)
+{
+    using namespace boost::wave;
+
+    core::string output;
+    util::insert_whitespace_detection whitespace(true);
+    std::optional<nbl::wave::context::position_type> previousPosition = std::nullopt;
+    bool previousWasExplicitWhitespace = false;
+
+    for (auto it = context.begin(); it != context.end(); ++it)
+    {
+        const auto& token = *it;
+        const auto id = token_id(token);
+        if (id == T_EOF || id == T_EOI)
+            continue;
+
+        const auto explicitWhitespace = isWhitespaceLikeToken(token);
+        const auto& position = token.get_position();
+        const auto value = tokenValueToString(token);
+
+        if (previousPosition.has_value() && !explicitWhitespace)
+        {
+            const auto movedToNewLogicalLine =
+                position.get_file() != previousPosition->get_file() ||
+                position.get_line() > previousPosition->get_line();
+
+            if (movedToNewLogicalLine)
+            {
+                if (output.empty() || output.back() != '\n')
+                {
+                    output.push_back('\n');
+                    whitespace.shift_tokens(T_NEWLINE);
+                }
+            }
+            else if (!previousWasExplicitWhitespace && whitespace.must_insert(id, value))
+            {
+                if (output.empty() || (output.back() != ' ' && output.back() != '\n' && output.back() != '\r' && output.back() != '\t'))
+                {
+                    output.push_back(' ');
+                    whitespace.shift_tokens(T_SPACE);
+                }
+            }
+        }
+
+        output += value;
+        whitespace.shift_tokens(id);
+        previousPosition = position;
+        previousWasExplicitWhitespace = explicitWhitespace;
+    }
+
+    return output;
+}
+
+std::string preprocessImpl(
+    std::string& code,
+    const nbl::asset::IShaderCompiler::SPreprocessorOptions& preprocessOptions,
+    const bool withCaching,
+    std::function<void(nbl::wave::context&)> post)
+{
+    nbl::wave::context context(code.begin(), code.end(), preprocessOptions.sourceIdentifier.data(), { preprocessOptions });
+    context.set_caching(withCaching);
+    context.add_macro_definition("__HLSL_VERSION");
+    context.add_macro_definition("__SPIRV_MAJOR_VERSION__=" + std::to_string(IShaderCompiler::getSpirvMajor(preprocessOptions.targetSpirvVersion)));
+    context.add_macro_definition("__SPIRV_MINOR_VERSION__=" + std::to_string(IShaderCompiler::getSpirvMinor(preprocessOptions.targetSpirvVersion)));
+
+    core::string resolvedString;
+    const char* phase = "registering built-in macros";
+    std::string activeMacroDefinition;
+    try
+    {
+        phase = "registering extra macro definitions";
+        for (const auto& define : preprocessOptions.extraDefines)
+        {
+            activeMacroDefinition = define.identifier;
+            activeMacroDefinition.push_back('=');
+            activeMacroDefinition.append(define.definition);
+            context.add_macro_definition(activeMacroDefinition);
+        }
+        activeMacroDefinition.clear();
+
+        phase = "expanding translation unit";
+        resolvedString = renderPreprocessedOutput(context);
+    }
+    catch (boost::wave::preprocess_exception& e)
+    {
+        const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, e.file_name(), e.line_no(), e.column_no());
+        preprocessOptions.logger.log("%s exception caught. %s [%s:%d:%d]\n%s", system::ILogger::ELL_ERROR, e.what(), e.description(), e.file_name(), e.line_no(), e.column_no(), failureContext.c_str());
+        preprocessOptions.logger.log("Boost diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::diagnostic_information(e).c_str());
+        return {};
+    }
+    catch (const boost::exception& e)
+    {
+        const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
+        preprocessOptions.logger.log("Boost exception caught during Wave preprocessing.\n%s", system::ILogger::ELL_ERROR, failureContext.c_str());
+        preprocessOptions.logger.log("Boost diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::diagnostic_information(e).c_str());
+        return {};
+    }
+    catch (const std::exception& e)
+    {
+        const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
+        preprocessOptions.logger.log("std::exception caught during Wave preprocessing. %s\n%s", system::ILogger::ELL_ERROR, e.what(), failureContext.c_str());
+        return {};
+    }
+    catch (...)
+    {
+        const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
+        preprocessOptions.logger.log("Unknown exception caught during Wave preprocessing.\n%s", system::ILogger::ELL_ERROR, failureContext.c_str());
+        preprocessOptions.logger.log("Current exception diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::current_exception_diagnostic_information().c_str());
+        return {};
+    }
+
+    post(context);
+
+    return resolvedString;
+}
 }
 
 namespace nbl::wave
 {
     std::string preprocess(std::string& code, const nbl::asset::IShaderCompiler::SPreprocessorOptions& preprocessOptions, bool withCaching, std::function<void(nbl::wave::context&)> post)
     {
-        nbl::wave::context context(code.begin(), code.end(), preprocessOptions.sourceIdentifier.data(), { preprocessOptions });
-        context.set_caching(withCaching);
-        context.add_macro_definition("__HLSL_VERSION");
-        context.add_macro_definition("__SPIRV_MAJOR_VERSION__=" + std::to_string(IShaderCompiler::getSpirvMajor(preprocessOptions.targetSpirvVersion)));
-        context.add_macro_definition("__SPIRV_MINOR_VERSION__=" + std::to_string(IShaderCompiler::getSpirvMinor(preprocessOptions.targetSpirvVersion)));
-
-        // instead of defining extraDefines as "NBL_GLSL_LIMIT_MAX_IMAGE_DIMENSION_1D 32768", 
-        // now define them as "NBL_GLSL_LIMIT_MAX_IMAGE_DIMENSION_1D=32768" 
-        // to match boost wave syntax
-        // https://www.boost.org/doc/libs/1_82_0/libs/wave/doc/class_reference_context.html#:~:text=Maintain%20defined%20macros-,add_macro_definition,-bool%20add_macro_definition
-
-        // preprocess
-        core::string resolvedString;
-        const char* phase = "registering built-in macros";
-        std::string activeMacroDefinition;
-        try
-        {
-            phase = "registering extra macro definitions";
-            for (const auto& define : preprocessOptions.extraDefines)
-            {
-                activeMacroDefinition = define.identifier;
-                activeMacroDefinition.push_back('=');
-                activeMacroDefinition.append(define.definition);
-                context.add_macro_definition(activeMacroDefinition);
-            }
-            activeMacroDefinition.clear();
-
-            phase = "expanding translation unit";
-            auto stream = std::stringstream();
-            for (auto i = context.begin(); i != context.end(); i++)
-                stream << i->get_value();
-            resolvedString = stream.str();
-        }
-        catch (boost::wave::preprocess_exception& e)
-        {
-            const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, e.file_name(), e.line_no(), e.column_no());
-            preprocessOptions.logger.log("%s exception caught. %s [%s:%d:%d]\n%s", system::ILogger::ELL_ERROR, e.what(), e.description(), e.file_name(), e.line_no(), e.column_no(), failureContext.c_str());
-            preprocessOptions.logger.log("Boost diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::diagnostic_information(e).c_str());
-            return {};
-        }
-        catch (const boost::exception& e)
-        {
-            const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
-            preprocessOptions.logger.log("Boost exception caught during Wave preprocessing.\n%s", system::ILogger::ELL_ERROR, failureContext.c_str());
-            preprocessOptions.logger.log("Boost diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::diagnostic_information(e).c_str());
-            return {};
-        }
-        catch (const std::exception& e)
-        {
-            const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
-            preprocessOptions.logger.log("std::exception caught during Wave preprocessing. %s\n%s", system::ILogger::ELL_ERROR, e.what(), failureContext.c_str());
-            return {};
-        }
-        catch (...)
-        {
-            const auto failureContext = makeWaveFailureContext(preprocessOptions, code, phase, activeMacroDefinition, preprocessOptions.sourceIdentifier.data(), 0, 0);
-            preprocessOptions.logger.log("Unknown exception caught during Wave preprocessing.\n%s", system::ILogger::ELL_ERROR, failureContext.c_str());
-            preprocessOptions.logger.log("Current exception diagnostic information:\n%s", system::ILogger::ELL_ERROR, boost::current_exception_diagnostic_information().c_str());
-            return {};
-        }
-
-        post(context);
-
-        return resolvedString;
+        return preprocessImpl(code, preprocessOptions, withCaching, std::move(post));
     }
 }
