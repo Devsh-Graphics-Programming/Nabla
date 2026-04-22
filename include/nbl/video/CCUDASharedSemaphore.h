f04dcdb03b1d74b1354340d3d499050ea30de2d1
kevyuu <kyu.kevinyu@gmail.com>
2026-04-22 17:38:13 +0700

    Remove unused constructor parameter in CCUDASharedSemaphore

--- include/nbl/video/CCUDASharedSemaphore.h
+++ include/nbl/video/CCUDASharedSemaphore.h
@@ -30,8 +30,7 @@ protected:
    
     CCUDASharedSemaphore(core::smart_refctd_ptr<CCUDADevice> device, 
       core::smart_refctd_ptr<ISemaphore> src, 
-      CUexternalSemaphore semaphore, 
-      external_handle_t osHandle)
+      CUexternalSemaphore semaphore)
         : m_device(std::move(device))
         , m_src(std::move(m_src))
         , m_handle(semaphore)
