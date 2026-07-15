9604ca6005438f9d45b5a22db24474814051d069
keptsecret <sorchon@gmail.com>
2026-07-15 16:21:13 +0700

    implement proper read/write with separate data accessor for workgroup

--- include/nbl/builtin/hlsl/scan/chained_scan.hlsl
+++ include/nbl/builtin/hlsl/scan/chained_scan.hlsl
@@ -12,8 +12,60 @@ namespace scan
 
 namespace impl
 {
-template<class Config, class BinOp, bool Exclusive, class device_capabilities>
-struct scan
+template<uint16_t WorkgroupSizeLog2, uint16_t VirtualWorkgroupSize, uint16_t ItemsPerInvocation>
+struct WorkgroupDataProxy
+{
+    using dtype_t = vector<uint32_t, ItemsPerInvocation>;
+
+    NBL_CONSTEXPR_STATIC_INLINE uint16_t WorkgroupSize = uint16_t(1u) << WorkgroupSizeLog2;
+    NBL_CONSTEXPR_STATIC_INLINE uint16_t PreloadedDataCount = VirtualWorkgroupSize / WorkgroupSize;
+
+    static WorkgroupDataProxy<WorkgroupSizeLog2, VirtualWorkgroupSize, ItemsPerInvocation> create(const uint64_t inputBuf, const uint64_t outputBuf)
+    {
+        WorkgroupDataProxy<WorkgroupSizeLog2, VirtualWorkgroupSize, ItemsPerInvocation> retval;
+        const uint32_t workgroupOffset = glsl::gl_WorkGroupID().x * VirtualWorkgroupSize * sizeof(dtype_t);
+        retval.accessor = DoubleLegacyBdaAccessor<dtype_t>::create(inputBuf + workgroupOffset, outputBuf + workgroupOffset);
+        return retval;
+    }
+
+    template<typename AccessType, typename IndexType>
+    void get(const IndexType ix, NBL_REF_ARG(AccessType) value)
+    {
+        value = preloaded[ix>>WorkgroupSizeLog2];
+    }
+    template<typename AccessType, typename IndexType>
+    void set(const IndexType ix, const AccessType value)
+    {
+        preloaded[ix>>WorkgroupSizeLog2] = value;
+    }
+
+    void preload()
+    {
+        const uint16_t invocIx = workgroup::SubgroupContiguousIndex();
+        NBL_UNROLL
+        for (uint16_t idx = 0; idx < PreloadedDataCount; idx++)
+            accessor.get(idx * WorkgroupSize + invocIx, preloaded[idx]);
+    }
+    void unload()
+    {
+        const uint16_t invocIx = workgroup::SubgroupContiguousIndex();
+        NBL_UNROLL
+        for (uint16_t idx = 0; idx < PreloadedDataCount; idx++)
+            accessor.set(idx * WorkgroupSize + invocIx, preloaded[idx]);
+    }
+
+    void workgroupExecutionAndMemoryBarrier()
+    {
+        glsl::barrier();
+        //glsl::memoryBarrierShared(); implied by the above
+    }
+
+    DoubleLegacyBdaAccessor<dtype_t> accessor;
+    dtype_t preloaded[PreloadedDataCount];
+};
+
+template<class Config, class BinOp, bool Exclusive, class device_capabilities>  // TODO: Config is same as workgroup2 stuff?
+struct Scan
 {
     using scalar_t = typename BinOp::type_t;
     using vector_t = vector<scalar_t, Config::ItemsPerInvocation_0>;   // data accessor needs to be this type
@@ -25,22 +77,38 @@ struct scan
     
     NBL_CONSTEXPR_STATIC_INLINE uint16_t Flag_Shift = 2;
 
-    // TODO: I think we need separate DataAccessor classes between device-wide and workgroup scan
+    NBL_CONSTEXPR_STATIC_INLINE uint16_t WorkgroupSize = uint16_t(1u) << Config::WorkgroupSizeLog2;
+    NBL_CONSTEXPR_STATIC_INLINE uint16_t ItemsPerInvoc = Config::VirtualWorkgroupSize / WorkgroupSize;
     
     // TODO: double check op, it's all plus right now
 
     template<class DataAccessor, class ScratchAccessor>
     void __call(NBL_REF_ARG(DataAccessor) dataAccessor, NBL_REF_ARG(ScratchAccessor) scratchAccessor, NBL_REF_ARG(DataAccessor) workgroupReduction)   // TODO: need different type for workgroupReduction
-    {
-        // TODO: what to do with config?
-        
+    {        
         const uint16_t invocIx = workgroup::SubgroupContiguousIndex();
         const uint16_t workgroupId = glsl::gl_WorkGroupID();
 
-        // TODO: workgroup scan here, don't want to deal with that now
+        scalar_t currGroupReduction;
+        {
+            using data_proxy_t = WorkgroupDataProxy<Config::WorkgroupSizeLog2,Config::VirtualWorkgroupSize,Config::ItemsPerInvocation_0>;
+            data_proxy_t wgDataAccessor = data_proxy_t::create(pc.pInputBuf, pc.pOutputBuf[Binop::BindingIndex]);
+            wgDataAccessor.preload();
+
+            if (Exclusive)
+                workgroup2::exclusive_scan<Config,BinOp,device_capabilities>::template __call<data_proxy_t, ScratchAccessor>(wgDataAccessor, scratchAccessor);
+            else
+                workgroup2::inclusive_scan<Config,BinOp,device_capabilities>::template __call<data_proxy_t, ScratchAccessor>(wgDataAccessor, scratchAccessor);
+            scratchAccessor.workgroupExecutionAndMemoryBarrier();
+
+            wgDataAccessor.unload();    // TODO: maybe we don't have to unload, just write once after everything is done
+
+            // TODO: double check this but it should be the last element of the last workgroup thread
+            // don't know what it's like if virtual workgroup size doesn't divide by workgroup size exactly
+            if (invocIx == glsl::gl_SubgroupSize() * glsl::gl_NumSubgroups() - 1u)
+                currGroupReduction = wgDataAccessor.preloaded[data_proxy_t::PreloadedDataCount-1u][config_t::ItemsPerInvocation_0-1u];
+        }
         scratchAccessor.workgroupExecutionAndMemoryBarrier();
 
-        const scalar_t currGroupReduction = // TODO get last elem of curr workgroup;
         if (!invocIx)
         {
             const scalar_t storeVal = hlsl::mix(Flag_Inclusive, Flag_Reduction, workgroupId > 0u) | currGroupReduction << Flag_Shift;
@@ -82,8 +150,14 @@ struct scan
         scratchAccessor.get(0u, prevReduction);
         prevReduction += currGroupReduction;
 
-        // TODO: propagate correctly when data accessor done
-        dataAccessor[invocIx] += prevReduction;
+        NBL_UNROLL
+        for (uint16_t idx = 0; idx < ItemsPerInvoc; idx++)
+        {
+            dtype_t data;
+            dataAccessor.template get<dtype_t, uint16_t>(idx * WorkgroupSize + invocIx, data);
+            data += prevReduction;
+            dataAccessor.template set<dtype_t, uint16_t>(idx * WorkgroupSize + invocIx, data);
+        }
     }
 };
 }
@@ -94,10 +168,10 @@ struct inclusive_scan
     using scalar_t = typename BinOp::type_t;
 
     template<class DataAccessor, class ScratchAccessor NBL_FUNC_REQUIRES(ArithmeticDataAccessor<DataAccessor,scalar_t> && ArithmeticSharedMemoryAccessor<ScratchAccessor,scalar_t>)
-    static void __call(NBL_REF_ARG(DataAccessor) dataAccessor, NBL_REF_ARG(ScratchAccessor) scratchAccessor)
+    static void __call(NBL_REF_ARG(DataAccessor) dataAccessor, NBL_REF_ARG(ScratchAccessor) scratchAccessor, NBL_REF_ARG(DataAccessor) workgroupReduction)
     {
-        impl::scan<Config,BinOp,false,Config::LevelCount,device_capabilities> fn;
-        fn.template __call<DataAccessor,ScratchAccessor>(dataAccessor, scratchAccessor);
+        impl::Scan<Config,BinOp,false,device_capabilities> fn;
+        fn.template __call<DataAccessor,ScratchAccessor>(dataAccessor, scratchAccessor, workgroupReduction);
     }
 };
 
@@ -107,8 +181,10 @@ struct exclusive_scan
     using scalar_t = typename BinOp::type_t;
 
     template<class DataAccessor, class ScratchAccessor NBL_FUNC_REQUIRES(ArithmeticDataAccessor<DataAccessor,scalar_t> && ArithmeticSharedMemoryAccessor<ScratchAccessor,scalar_t>)
-    static void __call(NBL_REF_ARG(DataAccessor) dataAccessor, NBL_REF_ARG(ScratchAccessor) scratchAccessor)
+    static void __call(NBL_REF_ARG(DataAccessor) dataAccessor, NBL_REF_ARG(ScratchAccessor) scratchAccessor, NBL_REF_ARG(DataAccessor) workgroupReduction)
     {
+        impl::Scan<Config,BinOp,true,device_capabilities> fn;
+        fn.template __call<DataAccessor,ScratchAccessor>(dataAccessor, scratchAccessor, workgroupReduction);
     }
 };
 
